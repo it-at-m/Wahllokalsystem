3bcd89637fc29781fec68fb5ff00bd274abc852b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-03-21 18:11:08 +0100

    builder wird nicht benötigt

--- wls-common/exception/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/exception/model/WlsExceptionData.java
+++ wls-common/exception/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/exception/model/WlsExceptionData.java
@@ -1,14 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.wls.common.exception.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class WlsExceptionData {
 
     private String message;

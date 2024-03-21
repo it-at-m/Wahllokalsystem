20d41db9e92efc804beadcdf45f02d9312c31789
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-03-21 18:29:01 +0100

    Add ExceptionHandler for in and out messages

---
+++ wls-common/exception/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/exception/rest/model/DTOMapper.java
@@ -0,0 +1,24 @@
+package de.muenchen.oss.wahllokalsystem.wls.common.exception.rest.model;
+
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.WlsException;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface DTOMapper {
+
+    @Mapping(target = "service", source = "serviceName")
+    WlsExceptionDTO toDTO(WlsException wlsException);
+
+    default WlsExceptionCategory toDTOCategory(de.muenchen.oss.wahllokalsystem.wls.common.exception.model.WlsExceptionCategory exceptionCategory) {
+        if (exceptionCategory == null) {
+            return null;
+        }
+        return switch (exceptionCategory) {
+        case FACHLICH -> WlsExceptionCategory.F;
+        case INFRASTRUKTUR -> WlsExceptionCategory.I;
+        case SECURITY -> WlsExceptionCategory.S;
+        case TECHNISCH -> WlsExceptionCategory.T;
+        };
+    }
+}
---
+++ wls-common/exception/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/exception/rest/model/WlsExceptionDTO.java
@@ -0,0 +1,32 @@
+package de.muenchen.oss.wahllokalsystem.wls.common.exception.rest.model;
+
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionKonstanten;
+import jakarta.validation.constraints.NotNull;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public record WlsExceptionDTO(@NotNull WlsExceptionCategory category, @NotNull String code, @NotNull String service, @NotNull String message) {
+
+    public WlsExceptionDTO(final WlsExceptionCategory category, final String code, final String service, final String message) {
+        this.category = Optional.ofNullable(category).orElseGet(() -> {
+            log.warn("Category ist nicht enthalten");
+            return WlsExceptionCategory.T;
+        });
+
+        this.code = Optional.ofNullable(code).orElseGet(() -> {
+            log.warn("Code ist nicht enthalten. Setze allgemeinen Code");
+            return ExceptionKonstanten.CODE_ALLGEMEIN_UNBEKANNT;
+        });
+
+        this.service = Optional.ofNullable(service).orElseGet(() -> {
+            log.warn("Service war nicht enthalten. Setzte allgemeinen Service");
+            return ExceptionKonstanten.SERVICE_UNBEKANNT;
+        });
+
+        this.message = Optional.ofNullable(message).orElseGet(() -> {
+            log.warn("Nachricht war nicht enthalten. Setze allgemeine Nachricht");
+            return ExceptionKonstanten.MESSAGE_UNBEKANNTER_FEHLER;
+        });
+    }
+}

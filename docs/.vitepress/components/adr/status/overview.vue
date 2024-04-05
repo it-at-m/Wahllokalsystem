fbf5f6912287eca8bcdcda01e48f98b61c68e999
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-04-05 15:14:44 +0200

    components and composable for adr-status

---
+++ docs/.vitepress/components/adr/status/overview.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-for="status in Status" :key="status">
+    <adr-status :status="status"></adr-status>
+  </div>
+</template>
+
+<script setup>
+import {Status, useStatus} from "../../../composables/status";
+import {useData} from "vitepress";
+
+const statusComposable = useStatus();
+const {lang} = useData();
+
+</script>

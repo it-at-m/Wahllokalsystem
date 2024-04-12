e319413bca917008b8a5938ea34d1c0ec26d2fe9
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-04-12 11:08:10 +0200

    debugging authorizationcredentialsnotfoundexception errors

--- wls-broadcast-service/src/main/java/de/muenchen/oss/wahllokalsystem/domain/MessageRepository.java
+++ wls-broadcast-service/src/main/java/de/muenchen/oss/wahllokalsystem/domain/MessageRepository.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Repository;
 
 @PreAuthorize("hasAuthority('Broadcast_READ_Message')")
+@Repository
 public interface MessageRepository extends CrudRepository<Message, UUID> {
 
     Optional<Message> findFirstByWahlbezirkIDOrderByEmpfangsZeit(String wahlbezirkID);
---
+++ wls-broadcast-service/src/test/java/de/muenchen/oss/wahllokalsystem/utils/BroadcastSecurityUtils.java
@@ -0,0 +1,45 @@
+package de.muenchen.oss.wahllokalsystem.utils;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class BroadcastSecurityUtils {
+
+    public static String BROADCAST_BUSINESSACTION_BROADCAST = "Broadcast_BUSINESSACTION_Broadcast";
+    public static String BROADCAST_BUSINESSACTION_NACHRICHTABRUFEN = "Broadcast_BUSINESSACTION_GetMessage";
+    public static String BROADCAST_BUSINESSACTION_NACHRICHTGELESEN = "Broadcast_BUSINESSACTION_MessageRead";
+
+    public static String BROADCAST_READ_MESSAGE = "Broadcast_READ_Message";
+    public static String BROADCAST_WRITE_MESSAGE = "Broadcast_WRITE_Message";
+    public static String BROADCAST_DELETE_MESSAGE = "Broadcast_DELETE_Message";
+
+    private static String TESTUSER = "redv-101";
+    private static String TESTPASSWORD = "password";
+
+
+    /**
+     * Gewährung eines Vollzugriffs durch Zuteilung von allen möglichen Rollen.
+     */
+    public static void grantFullAccess() {
+        runAs(TESTUSER, TESTPASSWORD, getAllAuthorities());
+    }
+
+    public static void runAs(String username, String password, String... roles) {
+        assertNotNull(username, "Username must not be null!");
+        assertNotNull(password, "Password must not be null!");
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(username, password, AuthorityUtils.createAuthorityList(roles)));
+    }
+
+    public static String[] getAllAuthorities() {
+        return new String[]{
+                BROADCAST_BUSINESSACTION_BROADCAST,
+                BROADCAST_BUSINESSACTION_NACHRICHTABRUFEN,
+                BROADCAST_BUSINESSACTION_NACHRICHTGELESEN,
+
+                BROADCAST_READ_MESSAGE,
+                BROADCAST_WRITE_MESSAGE,
+                BROADCAST_DELETE_MESSAGE
+        };
+    }
+}

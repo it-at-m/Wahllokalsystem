f78e49f4c134a6d16e9067600ab749520efdffea
GerhardPx <gerhard.zollner@muenchen.de>
2024-04-15 13:25:32 +0200

    Tests EncryptionBuilder

--- wls-common/security/src/test/java/de/muenchen/wls/common/wls/security/EncryptionBuilderTest.java
+++ wls-common/security/src/test/java/de/muenchen/wls/common/wls/security/EncryptionBuilderTest.java
@@ -2,29 +2,33 @@ package de.muenchen.wls.common.wls.security;
 
 import de.muenchen.wls.common.security.EncryptionBuilder;
 import lombok.val;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-import java.util.HexFormat;
-@ExtendWith(MockitoExtension.class)
 class EncryptionBuilderTest {
-
-    byte[] aByte = HexFormat.of().parseHex("d1299fe9384daf3d82dd3f");
-    @Mock
-    private EncryptionBuilder mockedCreator;
+    byte[] toBeDecrypted = "Mzc2NTI2NzIzQUZEQUIzRD==".getBytes();
+    byte [] toBeEncrypted = "376526723AFDAB3D".getBytes();
 
     @Test
-    void decrypt() {
-        val unitUnderTest = new EncryptionBuilder(mockedCreator);
-        unitUnderTest.decryptValue("ZDEyOTlmZTkzODRkYWYzZDgyZGQzZg==").contains("d1299fe9384daf3d82dd3f");
+    void decrypt() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        val aByte = new byte[16];
+        val secureRandom = new SecureRandom();
+        secureRandom.nextBytes(aByte);
+        val unitUnderTest = new EncryptionBuilder(aByte);
+        Assertions.assertThat(unitUnderTest.decryptValue("Mzc2NTI2NzIzQUZEQUIzRD==").contains((Base64.getUrlDecoder().decode(toBeDecrypted).toString())));
     }
 
     @Test
-    void encrypt() {
-        val unitUnderTest = new EncryptionBuilder(mockedCreator);
-        unitUnderTest.encryptValue("d1299fe9384daf3d82dd3f").contains("ZDEyOTlmZTkzODRkYWYzZDgyZGQzZg==");
+    void encrypt() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        val aByte = new byte[16];
+        val secureRandom = new SecureRandom();
+        secureRandom.nextBytes(aByte);
+        val unitUnderTest = new EncryptionBuilder(aByte);
+        Assertions.assertThat(unitUnderTest.encryptValue("376526723AFDAB3D").contains(Base64.getUrlEncoder().encode(toBeEncrypted).toString()));
     }
 }

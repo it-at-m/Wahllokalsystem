1f47db1205ac4c785a77d1db95cbcdd893552d82
GerhardPx <gerhard.zollner@muenchen.de>
2024-04-17 13:58:37 +0200

    Text raus

--- wls-common/security/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/security/BezirkIDPermissionEvaluator.java
+++ wls-common/security/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/security/BezirkIDPermissionEvaluator.java
@@ -2,18 +2,7 @@ package de.muenchen.oss.wahllokalsystem.wls.common.security;
 
 import org.springframework.security.core.Authentication;
 
-/**
- * Interface um eine BezirkId gegen die BezirkId aus dem wls-infomanagement-service zu prüfen.
- *
- */
 public interface BezirkIDPermissionEvaluator {
-    /**
-     * Checkt die BezirkId.
-     *
-     * @param bezirkId BezirkId aus dem Request
-     * @param authentication Current authentication object
-     * @return true wenn die BezirkId aus dem Request mit der BezirkId des users übereinstimmt, sonst
-     *         false
-     */
-    public boolean tokenUserBezirkIdMatches(String bezirkId, Authentication authentication);
+
+    boolean tokenUserBezirkIdMatches(String bezirkId, Authentication authentication);
 }

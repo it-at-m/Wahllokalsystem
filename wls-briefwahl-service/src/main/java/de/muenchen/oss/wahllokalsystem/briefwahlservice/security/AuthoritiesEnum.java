6de736ab20a716c2f45db8b7d79628be6c1a7ad5
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-04-26 09:00:38 +0200

    wls-briefwahl-service generated and reduced files to oss

---
+++ wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/security/AuthoritiesEnum.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2024
+ */
+package de.muenchen.oss.wahllokalsystem.briefwahlservice.security;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * Each possible authority in this project is represented by an enum.
+ * The enums are used within the {@link PagingAndSortingRepository}
+ * in the annotation e.g. {@link PreAuthorize}.
+ */
+public enum AuthoritiesEnum {
+    WLS_BRIEFWAHL_SERVICE_READ_THEENTITY, WLS_BRIEFWAHL_SERVICE_WRITE_THEENTITY, WLS_BRIEFWAHL_SERVICE_DELETE_THEENTITY,
+    // add your authorities here and also add these new authorities to sso-authorisation.json.
+
+}

98250f70baa5cfe6e04b6cff7f7480d9d267e672
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-04-30 11:22:39 +0200

    init maven project for eai service

---
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/configuration/SwaggerConfiguration.java
@@ -0,0 +1,43 @@
+package de.muenchen.oss.wahllokalsystem.eaiservice.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Value("${info.application.version:unknown}")
+    String version;
+
+    @Bean
+    GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("EAI Service")
+                                .version(version)
+                                .contact(new Contact().name("Your Name").email("Your E-Mail-Address")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
---
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/security/AuthoritiesEnum.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2024
+ */
+package de.muenchen.oss.wahllokalsystem.eaiservice.security;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * Each possible authority in this project is represented by an enum. The enums are used within the
+ * {@link PagingAndSortingRepository} in the annotation e.g.
+ * {@link PreAuthorize}.
+ */
+public enum AuthoritiesEnum {
+    WLS_EAI_SERVICE_READ_THEENTITY, WLS_EAI_SERVICE_WRITE_THEENTITY, WLS_EAI_SERVICE_DELETE_THEENTITY,
+    // add your authorities here and also add these new authorities to sso-authorisation.json.
+
+}

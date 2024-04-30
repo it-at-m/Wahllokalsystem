674e82e3ba3cd7834fe1fe4ba6b643302cb23fe2
Robert Jasny <robert.jasny@muenchen.de>
2024-04-30 16:47:03 +0200

    infomanagement service generated, stripped down and improved similar to briefwahl-service

---
+++ wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/MicroServiceApplication.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2024
+ */
+package de.muenchen.oss.wahllokalsystem.infomanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Application class for starting the micro-service.
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {
+                "org.springframework.data.jpa.convert.threeten",
+                "de.muenchen.oss.wahllokalsystem.infomanagementservice"
+        }
+)
+@EntityScan(
+        basePackages = {
+                "org.springframework.data.jpa.convert.threeten",
+                "de.muenchen.oss.wahllokalsystem.infomanagementservice"
+        }
+)
+@EnableJpaRepositories(
+        basePackages = {
+                "de.muenchen.oss.wahllokalsystem.infomanagementservice"
+        }
+)
+@EnableAutoConfiguration
+public class MicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceApplication.class, args);
+    }
+
+}

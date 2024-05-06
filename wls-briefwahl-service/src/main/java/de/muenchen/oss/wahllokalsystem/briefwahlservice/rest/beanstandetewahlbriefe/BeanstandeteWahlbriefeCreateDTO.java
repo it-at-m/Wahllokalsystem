e42ee933f42e0aa63830713d32d5132c4bec538b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-05-06 19:59:19 +0200

    update open api doc

--- wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/rest/beanstandetewahlbriefe/BeanstandeteWahlbriefeCreateDTO.java
+++ wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/rest/beanstandetewahlbriefe/BeanstandeteWahlbriefeCreateDTO.java
@@ -1,10 +1,13 @@
 package de.muenchen.oss.wahllokalsystem.briefwahlservice.rest.beanstandetewahlbriefe;
 
 import de.muenchen.oss.wahllokalsystem.briefwahlservice.common.beanstandetewahlbriefe.Zurueckweisungsgrund;
+import de.muenchen.oss.wahllokalsystem.briefwahlservice.configuration.SwaggerConfiguration;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.util.Map;
 import lombok.Builder;
 
 @Builder
-public record BeanstandeteWahlbriefeCreateDTO(@NotNull Map<String, Zurueckweisungsgrund[]> beanstandeteWahlbriefe) {
+public record BeanstandeteWahlbriefeCreateDTO(
+        @Schema(ref = SwaggerConfiguration.SCHEMA_BEANSTANDETEWAHLBRIEFE_PROPERTY) @NotNull Map<String, Zurueckweisungsgrund[]> beanstandeteWahlbriefe) {
 }

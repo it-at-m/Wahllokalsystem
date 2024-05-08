1ae7e6996d271d341afccf9a1fc134b3dc15e146
MrSebastian <MrSebastian@users.noreply.github.com>
2024-05-08 09:23:38 +0000

    deploy: 9f3374904d1f3c6aecae4305cd20b7a021ae49f2

---
+++ assets/about_index.md.sHlDGZ8f.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as n,o as a,a1 as t}from"./chunks/framework.DKMJckKw.js";const p=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"about/index.md","filePath":"about/index.md"}'),r={name:"about/index.md"},i=t("",9),s=[i];function d(l,o,h,u,c,m){return a(),n("div",null,s)}const g=e(r,[["render",d]]);export{p as __pageData,g as default};

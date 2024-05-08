a51e962b68b8e1bdba71fbd6a307367bd16fc355
Robert Jasny <robert.jasny@muenchen.de>
2024-05-08 11:40:20 +0200

    Erster Durchstich

--- wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/service/wahltag/KonfigurierterWahltagModel.java
+++ wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/service/wahltag/KonfigurierterWahltagModel.java
@@ -1,7 +1,11 @@
 package de.muenchen.oss.wahllokalsystem.infomanagementservice.service.wahltag;
 
+import de.muenchen.oss.wahllokalsystem.infomanagementservice.rest.wahltag.WahltagStatus;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import lombok.Builder;
 
 @Builder
-public class KonfigurierterWahltagModel {
+public record KonfigurierterWahltagModel(@NotNull LocalDate wahltag, @NotNull String wahltagID,
+                                         @NotNull WahltagStatus wahltagStatus, @NotNull String nummer) {
 }

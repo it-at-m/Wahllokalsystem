10463c136ed2ae6d799102441e46c7b5295a4db9
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-05-10 12:26:25 +0200

    flyway h2 und oracle Skripte sinnvoll angelegt

--- wls-broadcast-service/src/main/resources/db/migrations/oracle/V1_1__createMessage.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE message (
-    oid varchar(36),
-    wahlbezirkid varchar(1024) not null,
-    nachricht varchar(1024) not null,
-    empfangszeit TIMESTAMP not null,
-    primary key (oid)
-);

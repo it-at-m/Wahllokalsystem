8e244230096398a68d05d441d257b84aa457ea6e
Robert Jasny <robert.jasny@muenchen.de>
2024-05-13 11:47:58 +0200

    @Size hinzugefügt

--- wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/rest/wahltag/KonfigurierterWahltagDTO.java
+++ wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/rest/wahltag/KonfigurierterWahltagDTO.java
@@ -1,10 +1,11 @@
 package de.muenchen.oss.wahllokalsystem.infomanagementservice.rest.wahltag;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Builder;
 
 @Builder
-public record KonfigurierterWahltagDTO(@NotNull LocalDate wahltag, @NotNull String wahltagID,
-                                       @NotNull WahltagStatus wahltagStatus, @NotNull String nummer) {
+public record KonfigurierterWahltagDTO(@NotNull LocalDate wahltag, @NotNull @Size(max = 255) String wahltagID,
+                                       @NotNull WahltagStatus wahltagStatus, @NotNull @Size(max = 255) String nummer) {
 }
--- wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/service/wahltag/KonfigurierterWahltagModel.java
+++ wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/service/wahltag/KonfigurierterWahltagModel.java
@@ -2,10 +2,11 @@ package de.muenchen.oss.wahllokalsystem.infomanagementservice.service.wahltag;
 
 import de.muenchen.oss.wahllokalsystem.infomanagementservice.rest.wahltag.WahltagStatus;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 import lombok.Builder;
 
 @Builder
-public record KonfigurierterWahltagModel(@NotNull LocalDate wahltag, @NotNull String wahltagID,
-                                         @NotNull WahltagStatus wahltagStatus, @NotNull String nummer) {
+public record KonfigurierterWahltagModel(@NotNull LocalDate wahltag, @NotNull @Size(max = 255) String wahltagID,
+                                         @NotNull WahltagStatus wahltagStatus, @NotNull @Size(max = 255) String nummer) {
 }

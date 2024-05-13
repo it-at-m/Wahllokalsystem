c3110996befccbfdf905e38ae1746bad3f8d61b1
Robert Jasny <robert.jasny@muenchen.de>
2024-05-13 11:33:19 +0200

    Kleinere Änderungen - Abgleich mit vorhandener Implementierung

--- wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/domain/wahltag/KonfigurierterWahltag.java
+++ wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/domain/wahltag/KonfigurierterWahltag.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import de.muenchen.oss.wahllokalsystem.infomanagementservice.rest.wahltag.WahltagStatus;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -15,31 +14,29 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Indexed;
 
 @Entity
+@Indexed
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class KonfigurierterWahltag {
 
-    @Column(name = "wahltag")
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     @NotNull
     private java.time.LocalDate wahltag;
 
     @Id
-    @Column(name = "wahltagID")
     @NotNull
     @Size(max = 255)
     private String wahltagID;
 
-    @Column(name = "wahltagStatus")
     @Enumerated(EnumType.STRING)
     @NotNull
     private WahltagStatus wahltagStatus;
 
-    @Column(name = "nummer")
     @NotNull
     @Size(max = 255)
     private String nummer;
--- wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/domain/wahltag/KonfigurierterWahltagRepository.java
+++ wls-infomanagement-service/src/main/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/domain/wahltag/KonfigurierterWahltagRepository.java
@@ -1,20 +1,24 @@
 package de.muenchen.oss.wahllokalsystem.infomanagementservice.domain.wahltag;
 
 import de.muenchen.oss.wahllokalsystem.infomanagementservice.rest.wahltag.WahltagStatus;
+import java.util.Optional;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface KonfigurierterWahltagRepository extends CrudRepository<KonfigurierterWahltag, String> {
 
-    String CACHE = "KONFIGURIERTERWAHLTAG_CACHE";
+    String CACHE = "KonfigurierterWahltagCACHE";
 
     @Override
-    @NonNull
     Iterable<KonfigurierterWahltag> findAll();
+
+    @Override
+    @Cacheable(value = CACHE, key = "#p0")
+    Optional<KonfigurierterWahltag> findById(String key);
 
     @Override
     @CachePut(value = CACHE, key = "#p0.wahltagID")
@@ -24,16 +28,17 @@ public interface KonfigurierterWahltagRepository extends CrudRepository<Konfigur
     @Override
     @CacheEvict(value = CACHE, key = "#p0")
     @PreAuthorize("hasAuthority('Infomanagement_DELETE_KonfigurierterWahltag')")
-    void deleteById(@NonNull String wahltagID);
+    void deleteById(String wahltagID);
 
     @Override
     @CacheEvict(value = CACHE, key = "#p0.wahltagID")
     @PreAuthorize("hasAuthority('Infomanagement_DELETE_KonfigurierterWahltag')")
-    void delete(@NonNull KonfigurierterWahltag entity);
+    void delete(KonfigurierterWahltag entity);
 
+    @Override
     @CacheEvict(value = CACHE, allEntries = true)
     @PreAuthorize("hasAuthority('Infomanagement_DELETE_KonfigurierterWahltag')")
-    void deleteAll(@NonNull Iterable<? extends KonfigurierterWahltag> entities);
+    void deleteAll(Iterable<? extends KonfigurierterWahltag> entities);
 
     @Override
     @CacheEvict(value = CACHE, allEntries = true)

0883108028e4d6ddfd1ba0a193e3ba47a90f054b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-05-15 09:44:12 +0200

    add tests to verify existence of bean in specific states

---
+++ wls-infomanagement-service/src/test/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/MicroServiceApplication_DefaultProfile_Test.java
@@ -0,0 +1,20 @@
+package de.muenchen.oss.wahllokalsystem.infomanagementservice;
+
+import de.muenchen.oss.wahllokalsystem.infomanagementservice.common.security.AnonymousHandler;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = MicroServiceApplication.class)
+class MicroServiceApplication_DefaultProfile_Test {
+
+    @Autowired(required = false)
+    AnonymousHandler anonymousHandler;
+
+    @Test
+    void anonymouseHandlerIsNotPartOfContext() {
+        Assertions.assertThat(anonymousHandler).isNull();
+    }
+
+}

adaa7c11056784e270c5848edee69b2bc4f3be2c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-05-22 10:22:55 +0200

    override constructor to ensure that list is not null

--- wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/urnenwahlvorbereitung/UrnenwahlvorbereitungDTO.java
+++ wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/urnenwahlvorbereitung/UrnenwahlvorbereitungDTO.java
@@ -1,8 +1,19 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.urnenwahlvorbereitung;
 
 import de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.common.WahlurneDTO;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public record UrnenwahlvorbereitungDTO(String wahlbezirkID, long anzahlWahlkabinen, long anzahlWahltische, long anzahlNebenraeume,
                                        List<WahlurneDTO> urnenAnzahl) {
+
+    public UrnenwahlvorbereitungDTO(final String wahlbezirkID, final long anzahlWahlkabinen, final long anzahlWahltische, final long anzahlNebenraeume,
+            final List<WahlurneDTO> urnenAnzahl) {
+        this.wahlbezirkID = wahlbezirkID;
+        this.anzahlWahlkabinen = anzahlWahlkabinen;
+        this.anzahlWahltische = anzahlWahltische;
+        this.anzahlNebenraeume = anzahlNebenraeume;
+        this.urnenAnzahl = Objects.requireNonNullElseGet(urnenAnzahl, ArrayList::new);
+    }
 }

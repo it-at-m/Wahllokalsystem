9589d603af1738aed4639f6fd0ec31805c617900
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-05-22 15:27:43 +0200

    update swagger spec

--- wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/common/WahlurneDTO.java
+++ wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/common/WahlurneDTO.java
@@ -1,7 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
-public record WahlurneDTO(String wahlID, long anzahl, Boolean urneVersiegelt) {
+public record WahlurneDTO(@NotNull @Size(max = 255) String wahlID,
+                          @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahl,
+                          Boolean urneVersiegelt) {
 }
--- wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/urnenwahlvorbereitung/UrnenwahlvorbereitungDTO.java
+++ wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/urnenwahlvorbereitung/UrnenwahlvorbereitungDTO.java
@@ -1,12 +1,18 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.urnenwahlvorbereitung;
 
 import de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.common.WahlurneDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public record UrnenwahlvorbereitungDTO(String wahlbezirkID, long anzahlWahlkabinen, long anzahlWahltische, long anzahlNebenraeume,
-                                       List<WahlurneDTO> urnenAnzahl) {
+public record UrnenwahlvorbereitungDTO(@NotNull String wahlbezirkID,
+                                       @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahlWahlkabinen,
+                                       @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahlWahltische,
+                                       @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahlNebenraeume,
+                                       @NotNull @Size(min = 1) List<WahlurneDTO> urnenAnzahl) {
 
     public UrnenwahlvorbereitungDTO(final String wahlbezirkID, final long anzahlWahlkabinen, final long anzahlWahltische, final long anzahlNebenraeume,
             final List<WahlurneDTO> urnenAnzahl) {

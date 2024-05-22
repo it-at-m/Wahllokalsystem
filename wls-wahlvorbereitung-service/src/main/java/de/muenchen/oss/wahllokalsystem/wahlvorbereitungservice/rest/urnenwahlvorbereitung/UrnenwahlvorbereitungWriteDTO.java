7d32db6c82fd8405b5a2efdbabc68f0f234ef07c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-05-22 15:29:44 +0200

    update swagger spec

--- wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/urnenwahlvorbereitung/UrnenwahlvorbereitungWriteDTO.java
+++ wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/urnenwahlvorbereitung/UrnenwahlvorbereitungWriteDTO.java
@@ -1,15 +1,19 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.urnenwahlvorbereitung;
 
 import de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.common.WahlurneDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import lombok.Builder;
 
 @Builder
-public record UrnenwahlvorbereitungWriteDTO(long anzahlWahlkabinen, long anzahlWahltische, long anzahlNebenraeume,
-                                            @NotNull List<WahlurneDTO> urnenAnzahl) {
+public record UrnenwahlvorbereitungWriteDTO(@Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahlWahlkabinen,
+                                            @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahlWahltische,
+                                            @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long anzahlNebenraeume,
+                                            @NotNull @Size(min = 1) List<WahlurneDTO> urnenAnzahl) {
 
     public UrnenwahlvorbereitungWriteDTO(final long anzahlWahlkabinen, final long anzahlWahltische, final long anzahlNebenraeume,
             final List<WahlurneDTO> urnenAnzahl) {

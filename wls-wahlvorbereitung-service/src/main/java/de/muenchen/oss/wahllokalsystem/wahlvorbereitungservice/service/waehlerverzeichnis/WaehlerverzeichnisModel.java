3b1302e13c4641ad82f3f2881737d5143e630ee1
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-05-24 11:13:12 +0200

    add builder annotation

--- wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/service/waehlerverzeichnis/WaehlerverzeichnisModel.java
+++ wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/service/waehlerverzeichnis/WaehlerverzeichnisModel.java
@@ -2,7 +2,9 @@ package de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.service.waehlerv
 
 import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkIDUndWaehlerverzeichnisNummer;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record WaehlerverzeichnisModel(@NotNull BezirkIDUndWaehlerverzeichnisNummer bezirkIDUndWaehlerverzeichnisNummer,
                                       Boolean verzeichnisLagVor,
                                       Boolean berichtigungVorBeginnDerAbstimmung,

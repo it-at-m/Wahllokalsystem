17e6f3d4aeff374af6e2c900dc5f9981de237554
Robert Jasny <robert.jasny@muenchen.de>
2024-06-11 09:37:55 +0200

    make unterbrechungsuhrzeit a required field

--- wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/unterbrechungsuhrzeit/UnterbrechungsUhrzeitWriteDTO.java
+++ wls-wahlvorbereitung-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorbereitungservice/rest/unterbrechungsuhrzeit/UnterbrechungsUhrzeitWriteDTO.java
@@ -1,9 +1,10 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorbereitungservice.rest.unterbrechungsuhrzeit;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
-public record UnterbrechungsUhrzeitWriteDTO(LocalDateTime unterbrechungsUhrzeit) {
+public record UnterbrechungsUhrzeitWriteDTO(@NotNull LocalDateTime unterbrechungsUhrzeit) {
 
 }

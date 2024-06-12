b1f31f77cb18136af8517e237f69efc25b59328d
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-12 09:46:32 +0200

    create directory for wahlvorstand service

---
+++ wls-wahlvorstand-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorstandservice/security/AuthoritiesEnum.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c): it@M - Dienstleister für Informations- und Telekommunikationstechnik
+ * der Landeshauptstadt München, 2024
+ */
+package de.muenchen.oss.wahllokalsystem.wahlvorstandservice.security;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * Each possible authority in this project is represented by an enum. The enums are used within the {@link PagingAndSortingRepository} in the annotation e.g.
+ * {@link PreAuthorize}.
+ */
+public enum AuthoritiesEnum {
+    WLS_WAHLVORSTAND_SERVICE_READ_THEENTITY, WLS_WAHLVORSTAND_SERVICE_WRITE_THEENTITY, WLS_WAHLVORSTAND_SERVICE_DELETE_THEENTITY,
+    // add your authorities here and also add these new authorities to sso-authorisation.json.
+
+}

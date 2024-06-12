399a70951d0bca1445e76e84996f3f82ca17a483
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-12 13:05:47 +0200

    add missing wahlID

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorschlag/dto/WahlvorschlaegeListeDTO.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorschlag/dto/WahlvorschlaegeListeDTO.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.Set;
 
-public record WahlvorschlaegeListeDTO(@NotNull @Size(min = 1) Set<WahlvorschlaegeDTO> wahlvorschlaege) {
+public record WahlvorschlaegeListeDTO(@NotNull String wahlID,
+                                      @NotNull @Size(min = 1) Set<WahlvorschlaegeDTO> wahlvorschlaege) {
 }

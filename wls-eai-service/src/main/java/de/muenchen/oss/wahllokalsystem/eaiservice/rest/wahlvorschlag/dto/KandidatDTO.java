5d0fdade249a651e9e3405c4881bf8e2358d1ff5
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-12 13:01:34 +0200

    add missing required contraints

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorschlag/dto/KandidatDTO.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorschlag/dto/KandidatDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record KandidatDTO(@NotNull String identifikator,
                           @NotNull String name,
                           @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long listenposition,
-                          boolean direktkandidat,
+                          @Schema(requiredMode = Schema.RequiredMode.REQUIRED) boolean direktkandidat,
                           @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long tabellenSpalteInNiederschrift,
-                          boolean einzelbewerber) {
+                          @Schema(requiredMode = Schema.RequiredMode.REQUIRED) boolean einzelbewerber) {
 }

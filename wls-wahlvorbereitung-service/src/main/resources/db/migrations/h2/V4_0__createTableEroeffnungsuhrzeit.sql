ee2ae2b9bd06d8d359db8e09b071023020956902
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-06-13 15:18:09 +0200

    Update wls-wahlvorbereitung-service/src/main/resources/db/migrations/h2/V4_0__createTableEroeffnungsuhrzeit.sql
    Formattieren sql Files
    
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-wahlvorbereitung-service/src/main/resources/db/migrations/h2/V4_0__createTableEroeffnungsuhrzeit.sql
+++ wls-wahlvorbereitung-service/src/main/resources/db/migrations/h2/V4_0__createTableEroeffnungsuhrzeit.sql
@@ -1,6 +1,6 @@
 create table Eroeffnungsuhrzeit
 (
-    wahlbezirkid          varchar(1000) not null,
+    wahlbezirkid       varchar(1000) not null,
     eroeffnungsuhrzeit TIMESTAMP     not null,
     primary key (wahlbezirkid)
 );

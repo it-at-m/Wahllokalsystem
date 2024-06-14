c81c47d56de18a76643a8c17f8dbcf23fd1d849e
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-14 13:52:00 +0200

    remove ExceptionDataWrapper.java

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/common/exception/ExceptionDataWrapper.java
+++
@@ -1,6 +0,0 @@
-package de.muenchen.oss.wahllokalsystem.eaiservice.rest.common.exception;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ExceptionDataWrapper(@NotNull String code, @NotNull String message) {
-}

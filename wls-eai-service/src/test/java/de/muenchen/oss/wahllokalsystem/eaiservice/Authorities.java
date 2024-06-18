d30355fceafb9bf6178064bba1bb43bdddf6b3ba
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-18 09:47:27 +0200

    all constants with all authorities required to access service

--- wls-eai-service/src/test/java/de/muenchen/oss/wahllokalsystem/eaiservice/Authorities.java
+++ wls-eai-service/src/test/java/de/muenchen/oss/wahllokalsystem/eaiservice/Authorities.java
@@ -8,4 +8,11 @@ public class Authorities {
 
     public static final String SERVICE_LOAD_WAHLVORSTAND = "aoueai_BUSINESSACTION_LoadWahlvorstand";
     public static final String SERVICE_SAVE_ANWESENHEIT = "aoueai_BUSINESSACTION_SaveAnwesenheit";
+
+    public static final String[] ALL_AUTHORITIES_GETWAHLVORSTANDFORWAHLBEZIRK = {
+            SERVICE_LOAD_WAHLVORSTAND
+    };
+    public static final String[] ALL_AUTHORIRITES_SETANWESENHEIT = {
+            SERVICE_SAVE_ANWESENHEIT
+    };
 }

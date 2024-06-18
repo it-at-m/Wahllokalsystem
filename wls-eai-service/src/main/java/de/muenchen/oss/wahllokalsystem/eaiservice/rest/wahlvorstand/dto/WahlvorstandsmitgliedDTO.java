a948e675e6f935e5468b7fb9ce059f8b1bea6f8e
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-18 11:03:15 +0200

    add lombok builder to dtos

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorstand/dto/WahlvorstandsmitgliedDTO.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorstand/dto/WahlvorstandsmitgliedDTO.java
@@ -2,7 +2,9 @@ package de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorstand.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record WahlvorstandsmitgliedDTO(@NotNull String identifikator,
                                        @NotNull String vorname,
                                        @NotNull String nachname,

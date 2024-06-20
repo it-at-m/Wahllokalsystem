5d4f8864161d80fac80fe023bb31fa07b6e91830
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-20 09:47:48 +0200

    generate client as beans

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/configuration/ClientConfiguration.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/configuration/ClientConfiguration.java
@@ -1,11 +1,8 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.clients.eai.ApiClient;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.clients.eai.api.WahlvorschlagControllerApi;
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.errorhandler.WlsResponseErrorHandler;
 import lombok.val;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
@@ -16,24 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ClientConfiguration {
 
-    @Value("${app.clients.eai.basePath}")
-    String eaiBasePath;
-
     @Bean
     public WlsResponseErrorHandler wlsResponseErrorHandler(final ObjectMapper objectMapper) {
         return new WlsResponseErrorHandler(objectMapper);
-    }
-
-    @Bean
-    public WahlvorschlagControllerApi wahlvorschlagControllerApi(final ApiClient apiClient) {
-        return new WahlvorschlagControllerApi(apiClient);
-    }
-
-    @Bean
-    public ApiClient eaiApiClient(final RestTemplate webClient) {
-        val eaiApiClient = new ApiClient(webClient);
-        eaiApiClient.setBasePath(eaiBasePath);
-        return eaiApiClient;
     }
 
     @Bean

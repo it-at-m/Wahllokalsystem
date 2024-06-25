d82c8018638868254991d128eb4c87637afff8a8
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-25 17:19:44 +0200

    provide experimental response in eai service

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorschlag/WahlvorschlagController.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlvorschlag/WahlvorschlagController.java
@@ -1,9 +1,13 @@
 package de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorschlag;
 
+import de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorschlag.dto.KandidatDTO;
 import de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorschlag.dto.ReferendumvorlagenDTO;
 import de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorschlag.dto.WahlvorschlaegeDTO;
 import de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorschlag.dto.WahlvorschlaegeListeDTO;
+import de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlvorschlag.dto.WahlvorschlagDTO;
 import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +29,15 @@ public class WahlvorschlagController {
     @GetMapping("wahl/{wahlID}/{wahlbezirkID}")
     @ResponseStatus(HttpStatus.OK)
     public WahlvorschlaegeDTO loadWahlvorschlaege(@PathVariable("wahlID") String wahlID, @PathVariable("wahlbezirkID") String wahlbezirkID) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //        throw new UnsupportedOperationException("Not supported yet.");
+        //TODO add dummy impl in Service instead
+        return new WahlvorschlaegeDTO(wahlbezirkID, wahlID, wahlbezirkID + wahlID,
+                Set.of(new WahlvorschlagDTO(UUID.randomUUID().toString(), 1L, "kurzname1", true,
+                                Set.of(new KandidatDTO(UUID.randomUUID().toString(), "name1", 1L, true, 1L, true),
+                                        new KandidatDTO(UUID.randomUUID().toString(), "name2", 2L, false, 2L, false))),
+                        new WahlvorschlagDTO(UUID.randomUUID().toString(), 2L, "kurzname2", true,
+                                Set.of(new KandidatDTO(UUID.randomUUID().toString(), "name3", 1L, true, 1L, true),
+                                        new KandidatDTO(UUID.randomUUID().toString(), "name4", 2L, false, 2L, false)))));
     }
 
     @GetMapping("wahl/{wahlID}/liste")

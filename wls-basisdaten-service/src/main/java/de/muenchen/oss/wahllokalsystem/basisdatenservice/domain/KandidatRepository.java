f34177c1f6bb7393c3769452284ce75a23388999
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-25 17:25:01 +0200

    add repos for wahlvorschlag and kandidat

---
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/domain/KandidatRepository.java
@@ -0,0 +1,6 @@
+package de.muenchen.oss.wahllokalsystem.basisdatenservice.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KandidatRepository extends CrudRepository<Kandidat, String> {
+}
---
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/domain/WahlvorschlagRepository.java
@@ -0,0 +1,6 @@
+package de.muenchen.oss.wahllokalsystem.basisdatenservice.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WahlvorschlagRepository extends CrudRepository<Wahlvorschlag, String> {
+}

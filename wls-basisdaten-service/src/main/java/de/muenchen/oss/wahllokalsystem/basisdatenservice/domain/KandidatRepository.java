35351e988bd70d15e71c69836169bce5f088534d
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-26 09:45:13 +0200

    add authorities and fix id class

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/domain/KandidatRepository.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/domain/KandidatRepository.java
@@ -1,6 +1,37 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.domain;
 
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
-public interface KandidatRepository extends CrudRepository<Kandidat, String> {
+@PreAuthorize("hasAuthority('Basisdaten_READ_Kandidat')")
+public interface KandidatRepository extends CrudRepository<Kandidat, UUID> {
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_SAVE_Kandidat')")
+    <S extends Kandidat> S save(S entity);
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_SAVE_Kandidat')")
+    <S extends Kandidat> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_DELETE_Kandidat')")
+    void deleteById(UUID id);
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_DELETE_Kandidat')")
+    void delete(Kandidat entity);
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_DELETE_Kandidat')")
+    void deleteAllById(Iterable<? extends UUID> strings);
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_DELETE_Kandidat')")
+    void deleteAll(Iterable<? extends Kandidat> entities);
+
+    @Override
+    @PreAuthorize("hasAuthority('Basisdaten_DELETE_Kandidat')")
+    void deleteAll();
 }

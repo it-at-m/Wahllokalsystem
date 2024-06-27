d48f47b0f5a24d3ccecfcb0791ac71852c204cbc
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-27 18:02:40 +0200

    refactor service implementation

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahlvorschlaege/WahlvorschlaegeService.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahlvorschlaege/WahlvorschlaegeService.java
@@ -1,6 +1,5 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlvorschlaege;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.KandidatRepository;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Wahlvorschlaege;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.WahlvorschlaegeRepository;
@@ -8,12 +7,10 @@ import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.WahlvorschlagRep
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.exception.ExceptionConstants;
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionFactory;
 import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlID;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,34 +30,28 @@ public class WahlvorschlaegeService {
     @PreAuthorize(
         "hasAuthority('Basisdaten_BUSINESSACTION_GetWahlvorschlaege')"
     )
-    public WahlvorschlaegeModel getWahlvorschlaege(@P("wahlID") final String wahlID, @P("wahlbezirkID") final String wahlbezirkID) {
-        log.debug("#getWahlvorschlaege wahlID {} wahlbezirkID {}", wahlID, wahlbezirkID);
+    public WahlvorschlaegeModel getWahlvorschlaege(final BezirkUndWahlID bezirkUndWahlID) {
+        log.debug("#getWahlvorschlaege bezirkUndWahlID > {}", bezirkUndWahlID);
 
-        wahlvorschlaegeValidator.validWahlIdUndWahlbezirkIDOrThrow(wahlID, wahlbezirkID);
-        Wahlvorschlaege wahlvorschlaege;
-        BezirkUndWahlID bezirkUndWahlID = new BezirkUndWahlID(wahlID, wahlbezirkID);
+        wahlvorschlaegeValidator.validWahlIdUndWahlbezirkIDOrThrow(bezirkUndWahlID);
         val wahlvorschlaegeFromRepo = wahlvorschlaegeRepository.findByBezirkUndWahlID(bezirkUndWahlID);
         if (wahlvorschlaegeFromRepo.isEmpty()) {
-            log.debug("#getWahlvorschlaege: Für Wahlbezirk {} mit WahlID {} waren keine Wahlvorschlaege in der Datenbank", wahlbezirkID, wahlID);
-
-            val clientWahlvorschlaegeDTO = wahlvorschlaegeClient.getWahlvorschlaege(wahlID, wahlbezirkID);
-
-            try {
-                if (clientWahlvorschlaegeDTO == null) throw new IOException();
-                wahlvorschlaege = wahlvorschlaegeModelMapper
-                        .fromClientWahlvorschlaegeDTOtoEntity(clientWahlvorschlaegeDTO);
-                val savedEntity = persistWahlvorschlagEntity(wahlvorschlaege);
-                return wahlvorschlaegeModelMapper.fromEntityToWahlvorschlaegeModel(savedEntity);
-            } catch (JsonProcessingException e) {
-                throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.PARSE_ERROR);
-            } catch (IOException e) {
-                throw exceptionFactory.createInfrastrukturelleWlsException(ExceptionConstants.PARSE_ERROR);
-            } catch (RuntimeException e) {
-                log.error("#getWahlvorschlaege: Fehler beim Cachen", e);
-                throw exceptionFactory.createTechnischeWlsException(ExceptionConstants.UNSAVEABLE);
-            }
+            val savedWahlvorschlaege = importAndSaveWahlvorschlaege(bezirkUndWahlID);
+            return wahlvorschlaegeModelMapper.toModel(savedWahlvorschlaege);
         } else {
-            return wahlvorschlaegeModelMapper.fromEntityToWahlvorschlaegeModel(wahlvorschlaegeFromRepo.get());
+            return wahlvorschlaegeModelMapper.toModel(wahlvorschlaegeFromRepo.get());
+        }
+    }
+
+    private Wahlvorschlaege importAndSaveWahlvorschlaege(BezirkUndWahlID bezirkUndWahlID) {
+        log.debug("#getWahlvorschlaege: Für BezirkUndWahlID {} waren keine Wahlvorschlaege in der Datenbank", bezirkUndWahlID);
+        val clientWahlvorschlaegeDTO = wahlvorschlaegeClient.getWahlvorschlaege(bezirkUndWahlID);
+        try {
+            val entityToCreate = wahlvorschlaegeModelMapper.toEntity(clientWahlvorschlaegeDTO);
+            return persistWahlvorschlagEntity(entityToCreate);
+        } catch (RuntimeException e) {
+            log.error("#getWahlvorschlaege: Fehler beim Cachen", e);
+            throw exceptionFactory.createTechnischeWlsException(ExceptionConstants.UNSAVEABLE);
         }
     }
 

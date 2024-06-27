28558baa3e6311b5aeda89bdef55d30594b63996
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-27 18:05:44 +0200

    remove unnecessary mapping definitions

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/rest/wahlvorschlaege/WahlvorschlaegeDTOMapper.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/rest/wahlvorschlaege/WahlvorschlaegeDTOMapper.java
@@ -1,24 +1,14 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.rest.wahlvorschlaege;
 
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlvorschlaege.WahlvorschlaegeModel;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlvorschlaege.WahlvorschlagModel;
-import java.util.Set;
-import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 
 @Mapper
 public interface WahlvorschlaegeDTOMapper {
 
-    @Mapping(source = "wahlvorschlaege", target = "wahlvorschlaege", qualifiedByName = "fromModelToWLSWahlvorschlagDTOSet")
-    WahlvorschlaegeDTO fromWahlvorschlaegeModelToWLSDTO(WahlvorschlaegeModel wahlvorschlaegeModel);
-
-    @IterableMapping(qualifiedByName = "fromWahlvorschlagModelToWLSDTO")
-    @Named("fromModelToWLSWahlvorschlagDTOSet")
-    Set<WahlvorschlagDTO> fromSetOfModelsToWLSWahlvorschlagDTOSet(Set<WahlvorschlagModel> wahlvorschlagModels);
-
-    @Named("fromWahlvorschlagModelToWLSDTO")
-    WahlvorschlagDTO fromWahlvorschlagModelToWLSDTO(WahlvorschlagModel wahlvorschlagModel);
+    @Mapping(source = "bezirkUndWahlID.wahlID", target = "wahlID")
+    @Mapping(source = "bezirkUndWahlID.wahlbezirkID", target = "wahlbezirkID")
+    WahlvorschlaegeDTO toDTO(WahlvorschlaegeModel wahlvorschlaegeModel);
 
 }

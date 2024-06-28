f01db176142359b5ab1d766325016f3992307ece
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-28 13:00:43 +0200

    add wahl table to h2 flyway

--- wls-eai-service/src/main/resources/db/migrations/h2/V3_0__createWahldaten.sql
+++ wls-eai-service/src/main/resources/db/migrations/h2/V3_0__createWahldaten.sql
@@ -6,4 +6,19 @@ CREATE TABLE wahltag
     nummer       VARCHAR2(10)   NOT NULL,
 
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE wahl
+(
+    id        VARCHAR2(36)  NOT NULL,
+    name      VARCHAR2(100) NOT NULL,
+    wahlart   VARCHAR2(5)   NOT NULL,
+    nummer    VARCHAR2(10)  NOT NULL,
+
+    wahltagID VARCHAR2(36)  NOT NULL,
+
+    FOREIGN KEY (wahltagID) REFERENCES wahltag (id),
+
+    PRIMARY KEY (id)
+
+);

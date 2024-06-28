c03b5b21afbfae107a2519c4067c4ccb8326abd2
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-28 10:17:36 +0200

    add column names to insert queries

--- wls-eai-service/src/main/resources/db/dummydata/h2/V1_1__wahlvorstandTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V1_1__wahlvorstandTestdaten.sql
@@ -1,74 +1,74 @@
-INSERT INTO wahlvorstand
+INSERT INTO wahlvorstand (id, wahlbezirkID)
 VALUES ('00000000-0000-0000-0001-000000000001', '00000000-0000-0000-0000-000000000001');
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn) (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0001-000000000001', 'Homer', 'Simpson', 'W', false, '00000000-0000-0000-0001-000000000001',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0001-000000000002', 'Marge', 'Simpson', 'SB', false, '00000000-0000-0000-0001-000000000001',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0001-000000000003', 'Bart', 'Simpson', 'SWB', false,
         '00000000-0000-0000-0001-000000000001', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0001-000000000004', 'Lisa', 'Simpson', 'SSB', false, '00000000-0000-0000-0001-000000000001',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0001-000000000005', 'Maggie', 'Simpson', 'B', false, '00000000-0000-0000-0001-000000000001',
         null);
 
-INSERT INTO wahlvorstand
+INSERT INTO wahlvorstand (id, wahlbezirkID)
 VALUES ('00000000-0000-0000-0001-000000000002', '00000000-0000-0000-0000-000000000002');
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000001', 'Peter', 'Griffin', 'W', false, '00000000-0000-0000-0001-000000000002',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000002', 'Lois', 'Griffin', 'SB', false, '00000000-0000-0000-0001-000000000002',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000003', 'Megan', 'Griffin', 'SWB', false,
         '00000000-0000-0000-0001-000000000002', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000004', 'Christopher', 'Griffin', 'SSB', false,
         '00000000-0000-0000-0001-000000000002', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000005', 'Stewie', 'Griffin', 'B', false, '00000000-0000-0000-0001-000000000002',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000006', 'Brian', 'Griffin', 'B', false, '00000000-0000-0000-0001-000000000002',
         null);
 
-INSERT INTO wahlvorstand
+INSERT INTO wahlvorstand (id, wahlbezirkID)
 VALUES ('00000000-0000-0000-0001-000000000003', '00000000-0000-0000-0000-000000000003');
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0003-000000000001', 'Grace', 'Hopper', 'W', false, '00000000-0000-0000-0001-000000000003',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0003-000000000002', 'Ada', 'Lovelace', 'SB', false,
         '00000000-0000-0000-0001-000000000003', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0003-000000000003', 'Konrad', 'Zuse', 'SWB', false,
         '00000000-0000-0000-0001-000000000003', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0003-000000000004', 'Alan', 'Turing', 'SSB', false,
         '00000000-0000-0000-0001-000000000003', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0003-000000000005', 'Hedy', 'Lamarr', 'B', false,
         '00000000-0000-0000-0001-000000000003', null);
 
-INSERT INTO wahlvorstand
+INSERT INTO wahlvorstand (id, wahlbezirkID)
 VALUES ('00000000-0000-0000-0001-000000000004', '00000000-0000-0000-0000-000000000004');
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0004-000000000001', 'Diana', 'Prince', 'W', false, '00000000-0000-0000-0001-000000000004',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0004-000000000002', 'Clark', 'Kent', 'SB', false, '00000000-0000-0000-0001-000000000004',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0004-000000000003', 'Bruce', 'Wayne', 'SWB', false,
         '00000000-0000-0000-0001-000000000004', null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0004-000000000004', 'Barry', 'Allen', 'SSB', false, '00000000-0000-0000-0001-000000000004',
         null);
-INSERT INTO wahlvorstandsmitglied
+INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0004-000000000005', 'Arthur', 'Curry', 'B', false, '00000000-0000-0000-0001-000000000004',
         null);

6e5c1bbbe25ea63ed3d634bee11150f7fd2aebb6
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-06-28 10:48:09 +0200

    init flyway files for wahltag

---
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V3_1__wahldatenTestdaten.sql
@@ -0,0 +1,5 @@
+INSERT INTO wahltag (id, tag, beschreibung, nummer)
+values ('00000000-0000-0000-0000-000000000001', '2024-06-09', 'zehnte Direktwahl zum Europäischen Parlament', '0');
+
+INSERT INTO wahltag (id, tag, beschreibung, nummer)
+values ('00000000-0000-0000-0000-000000000002', '2021-09-26', 'Wahl zum 20. Deutschen Bundestag', '0');
---
+++ wls-eai-service/src/main/resources/db/migrations/h2/V3_0__createWahldaten.sql
@@ -0,0 +1,9 @@
+CREATE TABLE wahltag
+(
+    id           VARCHAR2(36)   NOT NULL,
+    tag          TIMESTAMP      NOT NULL,
+    beschreibung VARCHAR2(1024) NOT NULL,
+    nummer       VARCHAR2(10)   NOT NULL,
+
+    PRIMARY KEY (id)
+)

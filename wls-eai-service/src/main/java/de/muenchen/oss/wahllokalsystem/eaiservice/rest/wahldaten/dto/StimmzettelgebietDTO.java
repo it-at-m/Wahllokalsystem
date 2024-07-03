a2cefc6c2e2308d76065f9fa577edabfa0b486ab
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-03 20:25:06 +0200

    add lombok builder for test cases

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahldaten/dto/StimmzettelgebietDTO.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahldaten/dto/StimmzettelgebietDTO.java
@@ -2,7 +2,9 @@ package de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahldaten.dto;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import lombok.Builder;
 
+@Builder
 public record StimmzettelgebietDTO(String identifikator,
                                    String nummer,
                                    String name,

755a87eb531c696a1ab33bd5b4b3192d137001bf
Robert Jasny <robert.jasny@muenchen.de>
2024-07-05 15:24:03 +0200

    rollback letzte Änderungen

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/service/wahlvorschlag/WahlvorschlagMapper.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/service/wahlvorschlag/WahlvorschlagMapper.java
@@ -32,4 +32,7 @@ public interface WahlvorschlagMapper {
 
     ReferendumvorlagenDTO toDTO(Referendumvorlagen referendumvorlagen);
 
+    ReferendumvorlageDTO toDTO(Referendumvorlage referendumvorlage);
+
+    ReferendumoptionDTO toDTO(Referendumoption referendumoption);
 }

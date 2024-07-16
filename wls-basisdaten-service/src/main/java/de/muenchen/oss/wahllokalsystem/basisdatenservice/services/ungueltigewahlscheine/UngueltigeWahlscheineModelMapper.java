5472af6df2e55137ff45a77475a60b38d75fb6a9
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-16 15:29:54 +0200

    add missing mapping information

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/ungueltigewahlscheine/UngueltigeWahlscheineModelMapper.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/ungueltigewahlscheine/UngueltigeWahlscheineModelMapper.java
@@ -3,11 +3,15 @@ package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.ungueltigewah
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.UngueltigeWahlscheine;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.WahltagIdUndWahlbezirksart;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface UngueltigeWahlscheineModelMapper {
 
     WahltagIdUndWahlbezirksart toID(UngueltigeWahlscheineReference ungueltigeWahlscheineReference);
 
+    @Mapping(target = "wahltagIdUndWahlbezirksart.wahltagID", source = "ungueltigeWahlscheineReference.wahltagID")
+    @Mapping(target = "wahltagIdUndWahlbezirksart.wahlbezirksart", source = "ungueltigeWahlscheineReference.wahlbezirksart")
+    @Mapping(target = "ungueltigeWahlscheine", source = "ungueltigeWahlscheineData")
     UngueltigeWahlscheine toEntity(UngueltigeWahlscheineWriteModel ungueltigeWahlscheineWriteModel);
 }

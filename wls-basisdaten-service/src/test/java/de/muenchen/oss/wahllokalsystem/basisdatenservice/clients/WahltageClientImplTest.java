2ea2448cd36d3ecc9e3f79e7fd2e04843f4ce6b3
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-07-17 12:37:31 +0200

    Tests ergänzen der zeigt dass die Exception die von controllerAPI kommt gemappt wird

--- wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/clients/WahltageClientImplTest.java
+++ wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/clients/WahltageClientImplTest.java
@@ -6,6 +6,7 @@ import de.muenchen.oss.wahllokalsystem.basisdatenservice.eai.aou.model.WahltageD
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.exception.ExceptionConstants;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahltag.WahltagModel;
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.FachlicheWlsException;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.TechnischeWlsException;
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionFactory;
 import java.time.LocalDate;
 import java.util.List;
@@ -18,6 +19,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestClientException;
 
 @ExtendWith(MockitoExtension.class)
 class WahltageClientImplTest {
@@ -62,6 +64,16 @@ class WahltageClientImplTest {
             Mockito.when(exceptionFactory.createFachlicheWlsException(ExceptionConstants.NULL_FROM_CLIENT)).thenReturn(mockedWlsException);
 
             Assertions.assertThatException().isThrownBy(() -> unitUnderTest.getWahltage(LocalDate.now())).isSameAs(mockedWlsException);
+        }
+
+        @Test
+        void controllerApiExceptionIsMapped(){
+            val testDate = LocalDate.now().minusMonths(3);
+            val mockedException = TechnischeWlsException.withCode("100").buildWithMessage("Bei der Kommunikation mit dem Aoueai-Service ist ein Fehler aufgetreten. Es konnten daher keine Daten geladen werden.");
+
+            Mockito.when(wahldatenControllerApi.loadWahltageSinceIncluding(any())).thenThrow(new RestClientException("error occurs while attempting to invoke the API"));
+            Mockito.when(exceptionFactory.createTechnischeWlsException(ExceptionConstants.FAILED_COMMUNICATION_WITH_EAI)).thenThrow(mockedException);
+            Assertions.assertThatException().isThrownBy(() -> unitUnderTest.getWahltage(testDate)).isSameAs(mockedException);
         }
     }
 }

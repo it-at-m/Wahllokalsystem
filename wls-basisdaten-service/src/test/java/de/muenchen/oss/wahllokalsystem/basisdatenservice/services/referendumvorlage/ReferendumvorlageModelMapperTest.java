64b7c25be0089d99a0c35824aeb9e545a5f2f645
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-22 13:59:50 +0200

    create tests for ReferendumvorlageModelMapper

---
+++ wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/referendumvorlage/ReferendumvorlageModelMapperTest.java
@@ -0,0 +1,111 @@
+package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.referendumvorlage;
+
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Referendumoption;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Referendumvorlage;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Referendumvorlagen;
+import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlID;
+import java.util.Set;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+class ReferendumvorlageModelMapperTest {
+
+    private final ReferendumvorlageModelMapper unitUnderTest = Mappers.getMapper(ReferendumvorlageModelMapper.class);
+
+    @Nested
+    class ToBezirkUndWahlID {
+
+        @Test
+        void isMapped() {
+            val wahlID = "wahlID";
+            val wahlbezirkID = "wahlbezirkID";
+            val modelToMap = new ReferendumvorlagenReferenceModel(wahlID, wahlbezirkID);
+
+            val result = unitUnderTest.toBezirkUndWahlID(modelToMap);
+
+            val expectedResult = new BezirkUndWahlID(wahlID, wahlbezirkID);
+            Assertions.assertThat(result).isEqualTo(expectedResult);
+        }
+    }
+
+    @Nested
+    class ToModel {
+
+        @Test
+        void isMapped() {
+            val wahlID = "wahlID";
+            val wahlbezirkID = "wahlbezirkID";
+            val stimmzettelgebietID = "stimmzettelgebietID";
+
+            val entityToMap = new Referendumvorlagen(new BezirkUndWahlID(wahlID, wahlbezirkID), stimmzettelgebietID,
+                    Set.of(new Referendumvorlage(null, null, "wahlvorschlagID1", 1L, "kurzname1", "frage1",
+                                    Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L))),
+                            new Referendumvorlage(null, null, "wahlvorschlagID2", 2L, "kurzname2", "frage2",
+                                    Set.of(new Referendumoption("option21", "optionsName21", 3L), new Referendumoption("option22", "optionsName22", 4L)))));
+
+            val result = unitUnderTest.toModel(entityToMap);
+
+            val expectedResult = new ReferendumvorlagenModel(stimmzettelgebietID,
+                    Set.of(new ReferendumvorlageModel("wahlvorschlagID1", 1L, "kurzname1", "frage1",
+                                    Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
+                                            new ReferendumoptionModel("option12", "optionsName12", 2L))),
+                            new ReferendumvorlageModel("wahlvorschlagID2", 2L, "kurzname2", "frage2",
+                                    Set.of(new ReferendumoptionModel("option21", "optionsName21", 3L),
+                                            new ReferendumoptionModel("option22", "optionsName22", 4L)))));
+            Assertions.assertThat(result).isEqualTo(expectedResult);
+            Assertions.assertThat(result).hasNoNullFieldsOrProperties();
+        }
+    }
+
+    @Nested
+    class ToEntity {
+
+        @Nested
+        class OfReferendumvorlagenModel {
+
+            @Test
+            void isMapped() {
+                val stimmzettelgebietID = "stimmzettelgebietID";
+
+                val modelToMap = new ReferendumvorlagenModel(stimmzettelgebietID,
+                        Set.of(new ReferendumvorlageModel("wahlvorschlagID1", 1L, "kurzname1", "frage1",
+                                        Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
+                                                new ReferendumoptionModel("option12", "optionsName12", 2L))),
+                                new ReferendumvorlageModel("wahlvorschlagID2", 2L, "kurzname2", "frage2",
+                                        Set.of(new ReferendumoptionModel("option21", "optionsName21", 3L),
+                                                new ReferendumoptionModel("option22", "optionsName22", 4L)))));
+                val bezirkUndWahlID = new BezirkUndWahlID("wahlID", "wahlbezirkID");
+
+                val result = unitUnderTest.toEntity(modelToMap, bezirkUndWahlID);
+
+                val expectedResult = new Referendumvorlagen(bezirkUndWahlID, stimmzettelgebietID,
+                        Set.of(new Referendumvorlage(null, null, "wahlvorschlagID1", 1L, "kurzname1", "frage1",
+                                        Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L))),
+                                new Referendumvorlage(null, null, "wahlvorschlagID2", 2L, "kurzname2", "frage2",
+                                        Set.of(new Referendumoption("option21", "optionsName21", 3L), new Referendumoption("option22", "optionsName22", 4L)))));
+                Assertions.assertThat(result).isEqualTo(expectedResult);
+            }
+        }
+
+        @Nested
+        class OfReferendumvorlageModel {
+
+            @Test
+            void isMapped() {
+                val modelToMap = new ReferendumvorlageModel("wahlvorschlagID1", 1L, "kurzname1", "frage1",
+                        Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
+                                new ReferendumoptionModel("option12", "optionsName12", 2L)));
+
+                val result = unitUnderTest.toEntity(modelToMap);
+
+                val expectedResult = new Referendumvorlage(null, null, "wahlvorschlagID1", 1L, "kurzname1", "frage1",
+                        Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L)));
+                Assertions.assertThat(result).isEqualTo(expectedResult);
+            }
+        }
+    }
+
+}

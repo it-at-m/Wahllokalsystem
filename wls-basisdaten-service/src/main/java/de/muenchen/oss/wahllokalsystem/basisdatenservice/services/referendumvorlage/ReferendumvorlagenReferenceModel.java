ac66fb58a575d17a19b282b703fee7230b2e40af
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-22 13:08:23 +0200

    creat tests for ReferendumvorlageValidator

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/referendumvorlage/ReferendumvorlagenReferenceModel.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/referendumvorlage/ReferendumvorlagenReferenceModel.java
@@ -1,7 +1,9 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.referendumvorlage;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record ReferendumvorlagenReferenceModel(@NotNull String wahlID,
                                                @NotNull String wahlbezirkID) {
 }

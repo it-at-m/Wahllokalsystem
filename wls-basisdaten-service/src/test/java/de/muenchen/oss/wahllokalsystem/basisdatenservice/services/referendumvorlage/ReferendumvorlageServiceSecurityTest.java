2f6d96c7834bd6d085784a279ddf0e021c45b6ff
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-22 15:07:16 +0200

    created ReferendumvorlageServiceSecurityTest

---
+++ wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/referendumvorlage/ReferendumvorlageServiceSecurityTest.java
@@ -0,0 +1,146 @@
+package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.referendumvorlage;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.MicroServiceApplication;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.TestConstants;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.ReferendumvorlageRepository;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.ReferendumvorlagenRepository;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.eai.aou.model.ReferendumoptionDTO;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.eai.aou.model.ReferendumvorlageDTO;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.eai.aou.model.ReferendumvorlagenDTO;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.utils.Authorities;
+import de.muenchen.oss.wahllokalsystem.wls.common.testing.SecurityUtils;
+import java.util.Set;
+import java.util.stream.Stream;
+import lombok.val;
+import org.apache.commons.lang3.ArrayUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = MicroServiceApplication.class)
+@ActiveProfiles(TestConstants.SPRING_TEST_PROFILE)
+@AutoConfigureWireMock
+public class ReferendumvorlageServiceSecurityTest {
+
+    @Autowired
+    ReferendumvorlageService referendumvorlageService;
+
+    @Autowired
+    ReferendumvorlagenRepository referendumvorlagenRepository;
+
+    @Autowired
+    ReferendumvorlageRepository referendumvorlageRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @AfterEach
+    void tearDown() {
+        SecurityUtils.runWith(Authorities.REPOSITORY_DELETE_REFERENDUMVORLAGEN, Authorities.REPOSITORY_DELETE_REFERENDUMVORLAGE);
+        referendumvorlagenRepository.deleteAll();
+        referendumvorlageRepository.deleteAll();
+    }
+
+    @Nested
+    class LoadReferendumvorlagen {
+
+        @Test
+        void accessGranted() throws Exception {
+            SecurityUtils.runWith(Authorities.ALL_AUTHORITIES_GET_REFERENDUMVORLAGEN);
+
+            val wahlID = "wahlID";
+            val wahlbezirkID = "wahlbezirkID";
+
+            val eaiReferendumvorschlage = createClientReferendumvorlagenDTO();
+            WireMock.stubFor(WireMock.get("/vorschlaege/referendum/" + wahlID + "/" + wahlbezirkID)
+                    .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json").withStatus(
+                            HttpStatus.OK.value()).withBody(objectMapper.writeValueAsBytes(eaiReferendumvorschlage))));
+
+            Assertions.assertThatNoException()
+                    .isThrownBy(() -> referendumvorlageService.loadReferendumvorlagen(new ReferendumvorlagenReferenceModel(wahlID, wahlbezirkID)));
+            //we have to check is data is stores because access denied exceptions are caught too
+            SecurityUtils.runWith(Authorities.REPOSITORY_READ_REFERENDUMVORLAGEN);
+            Assertions.assertThat(referendumvorlagenRepository.count()).isEqualTo(1);
+        }
+
+        @Test
+        void missingServiceAuthorityCausesAccessDenied() throws Exception {
+            SecurityUtils.runWith(Authorities.REPOSITORY_READ_REFERENDUMVORLAGEN, Authorities.REPOSITORY_WRITE_REFERENDUMVORLAGEN,
+                    Authorities.REPOSITORY_WRITE_REFERENDUMVORLAGE);
+
+            val wahlID = "wahlID";
+            val wahlbezirkID = "wahlbezirkID";
+
+            Assertions.assertThatThrownBy(() -> referendumvorlageService.loadReferendumvorlagen(new ReferendumvorlagenReferenceModel(wahlID, wahlbezirkID)))
+                    .isInstanceOf(
+                            AccessDeniedException.class);
+            //we have to check is data is stores because access denied exceptions are caught too
+            SecurityUtils.runWith(Authorities.REPOSITORY_READ_REFERENDUMVORLAGEN);
+            Assertions.assertThat(referendumvorlagenRepository.count()).isEqualTo(0);
+        }
+
+        @ParameterizedTest(name = "{index} - {1} missing")
+        @MethodSource("getMissingRepositoryAuthoritiesVariations")
+        void missingRepositoryAuthoritiesCausesNoFailWithAccessDenied(final ArgumentsAccessor argumentsAccessor) throws Exception {
+            SecurityUtils.runWith(argumentsAccessor.get(0, String[].class));
+
+            val wahlID = "wahlID";
+            val wahlbezirkID = "wahlbezirkID";
+
+            val eaiReferendumvorschlage = createClientReferendumvorlagenDTO();
+            WireMock.stubFor(WireMock.get("/vorschlaege/referendum/" + wahlID + "/" + wahlbezirkID)
+                    .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json").withStatus(
+                            HttpStatus.OK.value()).withBody(objectMapper.writeValueAsBytes(eaiReferendumvorschlage))));
+
+            Assertions.assertThatThrownBy(() -> referendumvorlageService.loadReferendumvorlagen(new ReferendumvorlagenReferenceModel(wahlID, wahlbezirkID)))
+                    .isInstanceOf(
+                            AccessDeniedException.class);
+            //we have to check is data is stores because access denied exceptions are caught too
+            SecurityUtils.runWith(Authorities.REPOSITORY_READ_REFERENDUMVORLAGEN);
+            Assertions.assertThat(referendumvorlagenRepository.count()).isEqualTo(0);
+        }
+
+        private static Stream<Arguments> getMissingRepositoryAuthoritiesVariations() {
+            val serviceAuthoritiesWithoutServiceAuthority = ArrayUtils.removeElements(Authorities.ALL_AUTHORITIES_GET_REFERENDUMVORLAGEN,
+                    Authorities.SERVICE_GET_REFERENDUMVORLAGEN);
+            return SecurityUtils.buildArgumentsForMissingAuthoritiesVariations(serviceAuthoritiesWithoutServiceAuthority);
+        }
+
+        private ReferendumvorlagenDTO createClientReferendumvorlagenDTO() {
+            val dto = new ReferendumvorlagenDTO();
+
+            dto.setStimmzettelgebietID("szgID");
+
+            val referendumOption = new ReferendumoptionDTO();
+            referendumOption.setId("optionID");
+            referendumOption.setName("optionName");
+            referendumOption.setPosition(1L);
+
+            val vorlage = new ReferendumvorlageDTO();
+            vorlage.setFrage("frage");
+            vorlage.setKurzname("kurzname");
+            vorlage.setOrdnungszahl(1L);
+            vorlage.setWahlvorschlagID("wahlvorschlagID");
+            vorlage.setReferendumoption(Set.of(referendumOption));
+
+            dto.setReferendumvorlage(Set.of(vorlage));
+
+            return dto;
+        }
+
+    }
+
+}
--- wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/utils/Authorities.java
+++ wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/utils/Authorities.java
@@ -11,6 +11,8 @@ public class Authorities {
     public static final String SERVICE_GET_HANDBUCH = "Basisdaten_BUSINESSACTION_GetHandbuch";
     public static final String SERVICE_POST_HANDBUCH = "Basisdaten_BUSINESSACTION_PostHandbuch";
 
+    public static final String SERVICE_GET_REFERENDUMVORLAGEN = "Basisdaten_BUSINESSACTION_GetReferendumvorlagen";
+
     public static final String REPOSITORY_READ_WAHLVORSCHLAEGE = "Basisdaten_READ_WLSWahlvorschlaege";
     public static final String REPOSITORY_DELETE_WAHLVORSCHLAEGE = "Basisdaten_DELETE_WLSWahlvorschlaege";
     public static final String REPOSITORY_WRITE_WAHLVORSCHLAEGE = "Basisdaten_WRITE_WLSWahlvorschlaege";
@@ -26,6 +28,14 @@ public class Authorities {
     public static final String REPOSITORY_READ_HANDBUCH = "Basisdaten_READ_Handbuch";
     public static final String REPOSITORY_WRITE_HANDBUCH = "Basisdaten_WRITE_Handbuch";
     public static final String REPOSITORY_DELETE_HANDBUCH = "Basisdaten_DELETE_Handbuch";
+
+    public static final String REPOSITORY_READ_REFERENDUMVORLAGEN = "Basisdaten_READ_Referendumvorlagen";
+    public static final String REPOSITORY_WRITE_REFERENDUMVORLAGEN = "Basisdaten_WRITE_Referendumvorlagen";
+    public static final String REPOSITORY_DELETE_REFERENDUMVORLAGEN = "Basisdaten_DELETE_Referendumvorlagen";
+
+    public static final String REPOSITORY_READ_REFERENDUMVORLAGE = "Basisdaten_READ_Referendumvorlage";
+    public static final String REPOSITORY_WRITE_REFERENDUMVORLAGE = "Basisdaten_WRITE_Referendumvorlage";
+    public static final String REPOSITORY_DELETE_REFERENDUMVORLAGE = "Basisdaten_DELETE_Referendumvorlage";
 
     public static final String[] ALL_AUTHORITIES_GET_WAHLVORSCHLAEGE = new String[] {
             SERVICE_GET_WAHLVORSCHLAEGE,
@@ -50,6 +60,13 @@ public class Authorities {
     public static final String[] ALL_AUTHORITIES_POST_HANDBUCH = {
             SERVICE_POST_HANDBUCH,
             REPOSITORY_WRITE_HANDBUCH
+    };
+
+    public static final String[] ALL_AUTHORITIES_GET_REFERENDUMVORLAGEN = {
+            SERVICE_GET_REFERENDUMVORLAGEN,
+            REPOSITORY_READ_REFERENDUMVORLAGEN,
+            REPOSITORY_WRITE_REFERENDUMVORLAGEN,
+            REPOSITORY_WRITE_REFERENDUMVORLAGE
     };
 
 }

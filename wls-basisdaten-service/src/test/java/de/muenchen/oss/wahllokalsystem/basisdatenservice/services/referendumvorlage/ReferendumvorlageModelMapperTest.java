8784c602035daca8f5aa79f6c7a756f40b79ced4
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-23 13:59:05 +0200

    spotless:apply

--- wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/referendumvorlage/ReferendumvorlageModelMapperTest.java
+++ wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/referendumvorlage/ReferendumvorlageModelMapperTest.java
@@ -43,7 +43,7 @@ class ReferendumvorlageModelMapperTest {
 
             val entityToMap = new Referendumvorlagen(UUID.randomUUID(), new BezirkUndWahlID(wahlID, wahlbezirkID), stimmzettelgebietID,
                     Set.of(new Referendumvorlage(null, null, "wahlvorschlagID1", 1L, "kurzname1", "frage1",
-                                    Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L))),
+                            Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L))),
                             new Referendumvorlage(null, null, "wahlvorschlagID2", 2L, "kurzname2", "frage2",
                                     Set.of(new Referendumoption("option21", "optionsName21", 3L), new Referendumoption("option22", "optionsName22", 4L)))));
 
@@ -51,8 +51,8 @@ class ReferendumvorlageModelMapperTest {
 
             val expectedResult = new ReferendumvorlagenModel(stimmzettelgebietID,
                     Set.of(new ReferendumvorlageModel("wahlvorschlagID1", 1L, "kurzname1", "frage1",
-                                    Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
-                                            new ReferendumoptionModel("option12", "optionsName12", 2L))),
+                            Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
+                                    new ReferendumoptionModel("option12", "optionsName12", 2L))),
                             new ReferendumvorlageModel("wahlvorschlagID2", 2L, "kurzname2", "frage2",
                                     Set.of(new ReferendumoptionModel("option21", "optionsName21", 3L),
                                             new ReferendumoptionModel("option22", "optionsName22", 4L)))));
@@ -73,8 +73,8 @@ class ReferendumvorlageModelMapperTest {
 
                 val modelToMap = new ReferendumvorlagenModel(stimmzettelgebietID,
                         Set.of(new ReferendumvorlageModel("wahlvorschlagID1", 1L, "kurzname1", "frage1",
-                                        Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
-                                                new ReferendumoptionModel("option12", "optionsName12", 2L))),
+                                Set.of(new ReferendumoptionModel("option11", "optionsName11", 1L),
+                                        new ReferendumoptionModel("option12", "optionsName12", 2L))),
                                 new ReferendumvorlageModel("wahlvorschlagID2", 2L, "kurzname2", "frage2",
                                         Set.of(new ReferendumoptionModel("option21", "optionsName21", 3L),
                                                 new ReferendumoptionModel("option22", "optionsName22", 4L)))));
@@ -84,7 +84,7 @@ class ReferendumvorlageModelMapperTest {
 
                 val expectedResult = new Referendumvorlagen(null, bezirkUndWahlID, stimmzettelgebietID,
                         Set.of(new Referendumvorlage(null, null, "wahlvorschlagID1", 1L, "kurzname1", "frage1",
-                                        Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L))),
+                                Set.of(new Referendumoption("option11", "optionsName11", 1L), new Referendumoption("option12", "optionsName12", 2L))),
                                 new Referendumvorlage(null, null, "wahlvorschlagID2", 2L, "kurzname2", "frage2",
                                         Set.of(new Referendumoption("option21", "optionsName21", 3L), new Referendumoption("option22", "optionsName22", 4L)))));
                 Assertions.assertThat(result).isEqualTo(expectedResult);

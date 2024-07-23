cab9742132243d0af40a52a55e81b2d0fe6f4524
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-07-23 13:46:48 +0200

    add open api annotations ReferendumvorlageController

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/rest/referendumvorlage/ReferendumvorlageController.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/rest/referendumvorlage/ReferendumvorlageController.java
@@ -1,6 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.rest.referendumvorlage;
 
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.referendumvorlage.ReferendumvorlageService;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.rest.model.WlsExceptionDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +22,23 @@ public class ReferendumvorlageController {
 
     private final ReferendumvorlagenDTOMapper referendumvorlagenDTOMapper;
 
+    @Operation(description = "Laden der Referendumsvorlagen des Wahllokals {wahlbezirkID} für eine Wahl {wahlID}.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200", description = "OK",
+                            content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ReferendumvorlagenDTO.class)) }
+                    ),
+                    @ApiResponse(
+                            responseCode = "500", description = "Probleme bei der Kommunikation mit dem externen System von dem die Daten importiert werden",
+                            content = { @Content(mediaType = "application/json", schema = @Schema(implementation = WlsExceptionDTO.class)) }
+                    ),
+                    @ApiResponse(
+                            responseCode = "204", description = "Keine Daten vom Fremdsystem geliefert",
+                            content = @Content(schema = @Schema())
+                    )
+            }
+    )
     @GetMapping("{wahlID}/{wahlbezirkID}")
     public ReferendumvorlagenDTO getReferendumvorlagen(@PathVariable("wahlID") final String wahlID, @PathVariable("wahlbezirkID") final String wahlbezirkID) {
         return referendumvorlagenDTOMapper.toDTO(referendumvorlageService.loadReferendumvorlagen(referendumvorlagenDTOMapper.toModel(wahlbezirkID, wahlID)));

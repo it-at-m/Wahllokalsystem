ea40b284695b3736736f14ead996e87b4729a3d7
Robert Jasny <robert.jasny@muenchen.de>
2024-07-24 07:33:29 +0200

    review comment: unneeded check removed

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/service/wahlbeteiligung/WahlbeteiligungValidator.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/service/wahlbeteiligung/WahlbeteiligungValidator.java
@@ -16,7 +16,7 @@ public class WahlbeteiligungValidator {
     public void validDTOToSetOrThrow(WahlbeteiligungsMeldungDTO wahlbeteiligungToSet) {
         if (wahlbeteiligungToSet == null) {
             throw exceptionFactory.createFachlicheWlsException(
-                    de.muenchen.oss.wahllokalsystem.eaiservice.rest.common.exception.ExceptionConstants.DATENALLGEMEIN_PARAMETER_FEHLEN);
+                de.muenchen.oss.wahllokalsystem.eaiservice.rest.common.exception.ExceptionConstants.DATENALLGEMEIN_PARAMETER_FEHLEN);
         }
 
         if (StringUtils.isBlank(wahlbeteiligungToSet.wahlbezirkID())) {
@@ -27,7 +27,7 @@ public class WahlbeteiligungValidator {
             throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.SAVEWAHLBETEILIGUNG_WAHLID_FEHLT);
         }
 
-        if (wahlbeteiligungToSet.meldeZeitpunkt() == null || StringUtils.isBlank(wahlbeteiligungToSet.meldeZeitpunkt().toString())) {
+        if (wahlbeteiligungToSet.meldeZeitpunkt() == null) {
             throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.SAVEWAHLBETEILIGUNG_MELDEZEITPUNKT_FEHLT);
         }
     }

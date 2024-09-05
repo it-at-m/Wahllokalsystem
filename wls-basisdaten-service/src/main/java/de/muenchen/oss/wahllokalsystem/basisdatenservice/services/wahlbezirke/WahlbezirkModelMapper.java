0efc6a11996d21b2321f9bfa297ef8bb488b3e97
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-09-05 17:42:04 +0200

    remove unused mapping

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahlbezirke/WahlbezirkModelMapper.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahlbezirke/WahlbezirkModelMapper.java
@@ -1,19 +1,13 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlbezirke;
 
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Wahlbezirk;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.eai.aou.model.WahlbezirkDTO;
 import java.util.Collection;
 import java.util.List;
 import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED)
 public interface WahlbezirkModelMapper {
-
-    @Mapping(source = "identifikator", target = "wahlbezirkID")
-    @Mapping(source = "wahlbezirkArt", target = "wahlbezirkart")
-    Wahlbezirk fromDTOtoEntity(WahlbezirkDTO dto);
 
     Collection<Wahlbezirk> fromListOfWahlbezirkModeltoListOfWahlbezirkEntities(Collection<WahlbezirkModel> wahlbezirkModelList);
 

1074f1c4d6b2f5fd7aa9fb462732d61b43b72664
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-09-27 13:59:19 +0200

    Demo and TheEntity Elemente entfernt

--- wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/TestConstants.java
+++ wls-basisdaten-service/src/test/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/TestConstants.java
@@ -5,12 +5,7 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice;
 
 import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.springframework.hateoas.RepresentationModel;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TestConstants {
@@ -18,16 +13,5 @@ public final class TestConstants {
     public static final String SPRING_TEST_PROFILE = "test";
 
     public static final String SPRING_NO_SECURITY_PROFILE = "no-security";
-
-    @NoArgsConstructor
-    @Getter
-    @Setter
-    @EqualsAndHashCode(callSuper = true)
-    @ToString(callSuper = true)
-    public static class TheEntityDto extends RepresentationModel {
-
-        private String textAttribute;
-
-    }
 
 }

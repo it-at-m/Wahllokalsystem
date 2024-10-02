1bbd35f485ae4c4261f43cd8dab69b6fa59e5122
GerhardPx <gerhard.zollner@muenchen.de>
2024-10-02 14:09:52 +0200

    erstellungszeit table und mapper fixed

--- wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlergebnis/dto/ErgebnisDTO.java
+++ wls-eai-service/src/main/java/de/muenchen/oss/wahllokalsystem/eaiservice/rest/wahlergebnis/dto/ErgebnisDTO.java
@@ -2,13 +2,11 @@ package de.muenchen.oss.wahllokalsystem.eaiservice.rest.wahlergebnis.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 public record ErgebnisDTO(@NotNull String stimmenart,
                           @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long wahlvorschlagsordnungszahl,
                           @Schema(requiredMode = Schema.RequiredMode.REQUIRED) long ergebnis,
                           @NotNull String wahlvorschlagID,
-                          @NotNull String kandidatID,
-                          LocalDateTime erstellungszeit
+                          @NotNull String kandidatID
 ) {
 }

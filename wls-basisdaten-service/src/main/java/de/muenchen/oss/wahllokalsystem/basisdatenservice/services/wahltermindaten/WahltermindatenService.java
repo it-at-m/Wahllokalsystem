143ee92741de1ced7b21dc576e9d39fc2a1b925e
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-10-03 10:04:41 +0200

    improve implementation

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahltermindaten/WahltermindatenService.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahltermindaten/WahltermindatenService.java
@@ -1,7 +1,7 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahltermindaten;
 
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Wahlbezirk;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.WahlbezirkRepository;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.WahltagRepository;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.WahlvorschlaegeRepository;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.kopfdaten.KopfdatenRepository;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.referendumvorlagen.ReferendumvorlagenRepository;
@@ -9,22 +9,22 @@ import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.wahl.WahlReposit
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.exception.ExceptionConstants;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.kopfdaten.BasisstrukturdatenModel;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.kopfdaten.InitializeKopfdaten;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.kopfdaten.KopfdatenModelMapper;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.kopfdaten.WahldatenClient;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlbezirke.WahlbezirkModel;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlbezirke.WahlbezirkModelMapper;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlbezirke.WahlbezirkeClient;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlen.WahlModel;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlen.WahlModelMapper;
+import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahlen.WahltagWithNummer;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahltag.WahltagModel;
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahltag.WahltagModelMapper;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahltag.WahltageService;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.FachlicheWlsException;
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionFactory;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,24 +32,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+//TODO create Issue for Phase 3: this Service is a fassade for other services: wahlen, wahlbezirke, ...
 public class WahltermindatenService {
 
     private final WahltermindatenValidator wahltermindatenValidator;
     private final WahltageService wahltageService;
     private final WahldatenClient wahldatenClient;
-    private final WahlbezirkeClient wahlbezirkeClient;
 
-    private final WahltagModelMapper wahltagModelMapper;
     private final WahlModelMapper wahlModelMapper;
     private final WahlbezirkModelMapper wahlbezirkModelMapper;
-    private final KopfdatenModelMapper kopfdatenModelMapper;
 
     private final WahlRepository wahlRepository;
     private final WahlbezirkRepository wahlbezirkRepository;
     private final KopfdatenRepository kopfdatenRepository;
-    private final WahltagRepository wahltagRepository;
     private final WahlvorschlaegeRepository wahlvorschlaegeRepository;
     private final ReferendumvorlagenRepository referendumvorlagenRepository;
+    private final InitializeKopfdaten kopfDataInitializer;
 
     private final ExceptionFactory exceptionFactory;
 
@@ -57,19 +55,17 @@ public class WahltermindatenService {
     @Transactional
     public void putWahltermindaten(final String wahltagID) {
         log.info("#putWahltermindaten");
-        wahltermindatenValidator.validWahltagIDParamOrThrow(wahltagID, HttpMethod.PUT);
-        val wahltagModel = wahltageService.getWahltage().stream()
-                .filter(w -> w.wahltagID().equals(wahltagID))
-                .findAny().orElse(null);
-        wahltermindatenValidator.validateWahltagForSearchingWahltagID(wahltagModelMapper.toEntity(wahltagModel), HttpMethod.PUT);
+        wahltermindatenValidator.validateParameterToInitWahltermindaten(wahltagID);
+        val wahltagModel = getWahltagByIdOrThrow(wahltagID,
+                () -> exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_PUTWAHLTERMINDATEN_NO_WAHLTAG));
 
-        val basisdatenModel = wahldatenClient.loadBasisdaten(wahltagModel.wahltag(), wahltagModel.nummer());
+        val basisdatenModel = wahldatenClient.loadBasisdaten(new WahltagWithNummer(wahltagModel.wahltag(), wahltagModel.nummer()));
         if (null == basisdatenModel) {
             throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.GET_BASISDATEN_NO_DATA);
         }
-        val wahlModels = initWahlen(wahltagModel);
-        val wahlbezirkModels = initWahlbezirke(wahltagModel, wahlModels);
-        final InitializeKopfdaten kopfDataInitializer = new InitializeKopfdaten(exceptionFactory, kopfdatenRepository, kopfdatenModelMapper);
+
+        persistWahlen(basisdatenModel.wahlen().stream().toList());
+        persistWahlbezirke(basisdatenModel.wahlbezirke(), basisdatenModel.wahlen());
         kopfDataInitializer.initKopfdaten(basisdatenModel);
 
         // ToDo: add async classes, adapt EAI client, improve async handlöing?
@@ -84,35 +80,44 @@ public class WahltermindatenService {
     @Transactional
     public void deleteWahltermindaten(final String wahltagID) {
         log.info("#deleteWahltermindaten");
-        wahltermindatenValidator.validWahltagIDParamOrThrow(wahltagID, HttpMethod.DELETE);
-        val wahltag = wahltagRepository.findById(wahltagID).orElse(null);
-        wahltermindatenValidator.validateWahltagForSearchingWahltagID(wahltag, HttpMethod.DELETE);
+        wahltermindatenValidator.validateParameterToDeleteWahltermindaten(wahltagID);
+        val wahltag = getWahltagByIdOrThrow(wahltagID,
+                () -> exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_DELETEWAHLTERMINDATEN_PARAMETER_UNVOLLSTAENDIG));
 
-        val basisstrukturdaten = wahldatenClient.loadBasisdaten(wahltag.getWahltag(), wahltag.getNummer()).basisstrukturdaten();
+        val basisstrukturdaten = wahldatenClient.loadBasisdaten(new WahltagWithNummer(wahltag.wahltag(), wahltag.nummer())).basisstrukturdaten();
         val wahlIDs = basisstrukturdaten.stream().map(BasisstrukturdatenModel::wahlID).toList();
 
-        wahlbezirkRepository.deleteByWahltag(wahltag.getWahltag());
+        wahlbezirkRepository.deleteByWahltag(wahltag.wahltag());
         wahlIDs.forEach((wahlID) -> {
             kopfdatenRepository.deleteAllByBezirkUndWahlID_WahlID(wahlID);
             wahlRepository.deleteById(wahlID);
-            //ToDo: Test if "orphanRemoval= true" aus dem Repository funktioniert, also ob auch jeder entsprechende Wahlvorschlag gelöscht wird
             wahlvorschlaegeRepository.deleteAllByBezirkUndWahlID_WahlID(wahlID);
-            //ToDo: Test if "orphanRemoval= true" aus dem Repository funktioniert
             referendumvorlagenRepository.deleteAllByBezirkUndWahlID_WahlID(wahlID);
         });
     }
 
-    private List<WahlModel> initWahlen(WahltagModel wahltag) {
-        val wahlen = wahlModelMapper.fromListOfWahlModeltoListOfWahlEntities(
-                wahldatenClient.loadBasisdaten(wahltag.wahltag(), wahltag.nummer()).wahlen().stream().toList());
-        wahlRepository.saveAll(wahlen);
-        return wahlModelMapper.fromListOfWahlEntityToListOfWahlModel(wahlen);
+    private WahltagModel getWahltagByIdOrThrow(final String wahltagID, final Supplier<FachlicheWlsException> wlsExceptionSupplier) {
+        val wahltagModel = wahltageService.getWahltage().stream() //TODO wie wäre es mit getOptionalById oder findById aus dem WahltageService?
+                .filter(w -> w.wahltagID().equals(wahltagID))
+                .findAny();
+        return wahltagModel.orElseThrow(wlsExceptionSupplier);
     }
 
-    private List<WahlbezirkModel> initWahlbezirke(WahltagModel wahltagModel, List<WahlModel> wahlModels) {
-        val wahlbezirke = wahlbezirkModelMapper.fromListOfWahlbezirkModeltoListOfWahlbezirkEntities(
-                wahlbezirkeClient.loadWahlbezirke(wahltagModel.wahltag(), wahltagModel.nummer()).stream().toList());
+    private void persistWahlen(final List<WahlModel> wahlModelIterable) {
+        val wahlenEntities = wahlModelMapper.fromListOfWahlModeltoListOfWahlEntities(wahlModelIterable);
+        wahlRepository.saveAll(wahlenEntities);
+    }
+
+    private void persistWahlbezirke(Collection<WahlbezirkModel> wahlbezirkModelIterable, Collection<WahlModel> wahlModels) {
+        val wahlbezirke = wahlbezirkModelMapper.fromListOfWahlbezirkModeltoListOfWahlbezirkEntities(wahlbezirkModelIterable);
+        wahlbezirke.forEach(wahlbezirkEntity -> linkFirstMatchingWahl(wahlbezirkEntity, wahlModels));
         wahlbezirkRepository.saveAll(wahlbezirke);
-        return wahlbezirkModelMapper.fromListOfWahlbezirkEntityToListOfWahlbezirkModel(wahlbezirke);
+    }
+
+    private void linkFirstMatchingWahl(final Wahlbezirk wahlbezirk, final Collection<WahlModel> wahlen) {
+        val searchedWahl = wahlen.stream().filter(wahl -> wahlbezirk.getWahlnummer().equals(wahl.nummer())).findFirst().orElse(null);
+        if (null != searchedWahl) {
+            wahlbezirk.setWahlID(searchedWahl.wahlID());
+        }
     }
 }
--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahltermindaten/WahltermindatenValidator.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/services/wahltermindaten/WahltermindatenValidator.java
@@ -1,11 +1,9 @@
 package de.muenchen.oss.wahllokalsystem.basisdatenservice.services.wahltermindaten;
 
-import de.muenchen.oss.wahllokalsystem.basisdatenservice.domain.Wahltag;
 import de.muenchen.oss.wahllokalsystem.basisdatenservice.exception.ExceptionConstants;
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionFactory;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,21 +12,15 @@ public class WahltermindatenValidator {
 
     private final ExceptionFactory exceptionFactory;
 
-    public void validWahltagIDParamOrThrow(final String wahltagID, HttpMethod httpMethod) {
-        if (wahltagID == null || StringUtils.isBlank(wahltagID) || StringUtils.isEmpty(wahltagID)) {
-            switch (httpMethod.toString()) {
-            case "PUT" -> throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_PUTWAHLTERMINDATEN_PARAMETER_UNVOLLSTAENDIG);
-            case "DELETE" -> throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_DELETEWAHLTERMINDATEN_PARAMETER_UNVOLLSTAENDIG);
-            }
+    public void validateParameterToInitWahltermindaten(final String wahltagID) {
+        if (StringUtils.isBlank(wahltagID)) {
+            throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_PUTWAHLTERMINDATEN_PARAMETER_UNVOLLSTAENDIG);
         }
     }
 
-    public void validateWahltagForSearchingWahltagID(final Wahltag wahltag, HttpMethod httpMethod) {
-        if (null == wahltag || null == wahltag.getWahltag()) {
-            switch (httpMethod.toString()) {
-            case "PUT" -> throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_PUTWAHLTERMINDATEN_NO_WAHLTAG);
-            case "DELETE" -> throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_DELETEWAHLTERMINDATEN_LOESCHEN_UNVOLLSTAENDIG);
-            }
+    public void validateParameterToDeleteWahltermindaten(final String wahltagID) {
+        if (StringUtils.isBlank(wahltagID)) {
+            throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.CODE_DELETEWAHLTERMINDATEN_PARAMETER_UNVOLLSTAENDIG);
         }
     }
 

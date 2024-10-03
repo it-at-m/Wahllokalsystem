5c7dbd33e0388e4b9228e11932994898b5fca5cb
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-10-03 11:38:46 +0200

    implement async import of vorschlaege and vorlagen

---
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/configuration/AsyncConfiguration.java
@@ -0,0 +1,39 @@
+package de.muenchen.oss.wahllokalsystem.basisdatenservice.configuration;
+
+import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfiguration {
+
+    @Value("${app.async.corePoolSize}")
+    public int corePoolSize;
+
+    @Value("${app.async.maxPoolSize}")
+    public int maxPoolSize;
+
+    @Value("${app.async.queueCapacity}")
+    public int queueCapacity;
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        val executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("asyncTaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public DelegatingSecurityContextAsyncTaskExecutor taskExecutor(ThreadPoolTaskExecutor threadPoolTaskExecutor) {
+        return new DelegatingSecurityContextAsyncTaskExecutor(threadPoolTaskExecutor);
+    }
+}

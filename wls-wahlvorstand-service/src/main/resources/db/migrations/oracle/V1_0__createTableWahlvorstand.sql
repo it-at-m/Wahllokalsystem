447e0a095b5044132a55d45ed2b9aac33efefa76
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2024-10-08 15:59:24 +0200

    #138 add sql files

---
+++ wls-wahlvorstand-service/src/main/resources/db/migrations/oracle/V1_0__createTableWahlvorstand.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Wahlvorstand
+(
+    wahlbezirkID        VARCHAR(1024) NOT NULL,
+    anwesenheitbeginn   TIMESTAMP,
+
+    PRIMARY KEY (wahlbezirkID)
+)
+
+CREATE TABLE Wahlvorstandsmitglied
+(
+    wahlvorstand_wahlbezirkID       VARCHAR(1024) NOT NULL,
+    identifikator                   VARCHAR(1024) NOT NULL,
+    familienname                    VARCHAR(255) NOT NULL,
+    vorname                         VARCHAR(255) NOT NULL,
+    funktion                        VARCHAR(255) NOT NULL,
+    funktionsname                   VARCHAR(255),
+    anwesend                        NUMBER(1) NOT NULL,
+
+    FOREIGN KEY (wahlvorstand_wahlbezirkID) REFERENCES Wahlvorstand (wahlbezirkID)
+)

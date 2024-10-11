82026b50dc344d0a8043039dc2ae4d88a74a0139
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-10-11 18:13:59 +0200

    remove license header placeholder

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/Authority.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/Authority.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.muenchen.oss.wahllokalsystem.authservice.domain;
 
 import jakarta.persistence.CascadeType;

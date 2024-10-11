c267c47485deedc525970b05f83fc6fb4bc6579e
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2024-10-11 16:00:16 +0200

    #138 api client generiert

--- wls-wahlvorstand-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorstandservice/configuration/BasePathConfiguration.java
+++ wls-wahlvorstand-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorstandservice/configuration/BasePathConfiguration.java
@@ -1,7 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorstandservice.configuration;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@RequiredArgsConstructor
 public class BasePathConfiguration {
 
     @Value("${app.clients.eai.basePath}")
@@ -13,9 +18,14 @@ public class BasePathConfiguration {
     @Value("${app.clients.basisdaten.basePath}")
     String basisdatenBasePath;
 
-
     private final de.muenchen.oss.wahllokalsystem.wahlvorstandservice.eai.aou.ApiClient eaiApiClient;
     private final de.muenchen.oss.wahllokalsystem.wahlvorstandservice.eai.infomanagement.ApiClient infomanagementApiClient;
     private final de.muenchen.oss.wahllokalsystem.wahlvorstandservice.eai.basisdaten.ApiClient basisdatenApiClient;
 
+    @PostConstruct
+    public void updateBasePaths() {
+        eaiApiClient.setBasePath(eaiBasePath);
+        infomanagementApiClient.setBasePath(infomanagementBasePath);
+        basisdatenApiClient.setBasePath(basisdatenBasePath);
+    }
 }

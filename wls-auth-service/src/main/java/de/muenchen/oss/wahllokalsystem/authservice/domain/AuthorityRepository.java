b57428a24686053b4bec4f140ea37e8792b9ad60
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-10-14 09:31:32 +0200

    remove unchanged method overrides

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/AuthorityRepository.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/AuthorityRepository.java
@@ -1,31 +1,9 @@
 package de.muenchen.oss.wahllokalsystem.authservice.domain;
 
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AuthorityRepository extends CrudRepository<Authority, UUID> {
-
-    @Override
-    Iterable<Authority> findAll();
-
-    @Override
-    Optional<Authority> findById(UUID oid);
-
-    @Override
-    Authority save(Authority Authority);
-
-    @Override
-    void deleteById(UUID oid);
-
-    @Override
-    void delete(Authority entity);
-
-    @Override
-    void deleteAll(Iterable<? extends Authority> entities);
-
-    @Override
-    void deleteAll();
 
     Authority findByAuthority(String authority);
 

b4d576bece6c435d08dcac1045a0fe1bbf43446a
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-10-14 15:41:30 +0200

    removed unchanged method from super interface

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/PermissionRepository.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/PermissionRepository.java
@@ -6,27 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface PermissionRepository extends CrudRepository<Permission, UUID> {
 
-    @Override
-    Iterable<Permission> findAll();
-
-    @Override
-    Optional<Permission> findById(UUID oid);
-
-    @SuppressWarnings("unchecked")
-    @Override
-    Permission save(Permission Permission);
-
-    @Override
-    void deleteById(UUID oid);
-
-    @Override
-    void delete(Permission entity);
-
-    @Override
-    void deleteAll(Iterable<? extends Permission> entities);
-
-    @Override
-    void deleteAll();
-
     Optional<Permission> findByPermission(String permission);
 }

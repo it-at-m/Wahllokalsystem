0952bd7609b10472f6e8b11d671e60b848928dbf
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-10-17 12:03:51 +0200

    refactor login handler

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/rest/LoginController.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/rest/LoginController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,28 +16,34 @@ public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
-        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request, response);
-
         val loginView = new ModelAndView();
-        if (savedRequest.getParameterValues("admin") != null) {
+
+        if (hasParameterValue(request, response, "admin")) {
             loginView.setViewName("loginat");
-        } else if (savedRequest.getParameterValues("no") != null) {
+        } else if (hasParameterValue(request, response, "no")) {
             loginView.setViewName("nologin");
-        } else if (savedRequest.getParameterValues("amt") != null) {
-            loginView.setViewName("loginamt");
+        } else if (hasParameterValue(request, response, "error")) {
+            loginView.addObject("error", new RuntimeException("sth failed"));
+            loginView.setViewName("error");
         } else {
-            loginView.addObject("willkommensnachricht", "hello world");
-            loginView.setViewName("loginwls");
+            setupLoginWLS(loginView);
         }
 
         return loginView;
     }
 
-    @RequestMapping(value = "/loginAdmin", method = RequestMethod.GET)
-    public ModelAndView loginAt() {
-        val loginView = new ModelAndView();
-        loginView.setViewName("loginat");
+    private boolean hasParameterValue(final HttpServletRequest request, final HttpServletResponse response, final String parameterValue) {
+        val cacheHttpSessionRequest = new HttpSessionRequestCache().getRequest(request, response);
 
-        return loginView;
+        if (cacheHttpSessionRequest != null) {
+            return cacheHttpSessionRequest.getParameterValues(parameterValue) != null;
+        } else {
+            return request.getParameterValues(parameterValue) != null;
+        }
+    }
+
+    private void setupLoginWLS(final ModelAndView modelAndView) {
+        modelAndView.addObject("willkommensnachricht", "hello world");
+        modelAndView.setViewName("loginwls");
     }
 }

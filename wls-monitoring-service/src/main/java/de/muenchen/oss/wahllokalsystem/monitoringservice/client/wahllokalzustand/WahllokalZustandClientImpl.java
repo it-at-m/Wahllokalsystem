1de1d2809e978ff6fa56391b2f0d0e89a3beb34f
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-10-24 09:50:12 +0200

    remove Throwing - not expected

--- wls-monitoring-service/src/main/java/de/muenchen/oss/wahllokalsystem/monitoringservice/client/wahllokalzustand/WahllokalZustandClientImpl.java
+++ wls-monitoring-service/src/main/java/de/muenchen/oss/wahllokalsystem/monitoringservice/client/wahllokalzustand/WahllokalZustandClientImpl.java
@@ -30,8 +30,7 @@ public class WahllokalZustandClientImpl implements WahllokalZustandClient {
         try {
             wahllokalzustandControllerApi.saveWahllokalZustand(wahllokalZustandDTO);
         } catch (final Exception exception) {
-            log.info("exception on postWahllokalZustand from external", exception);
-            throw exceptionFactory.createTechnischeWlsException(ExceptionConstants.FAILED_COMMUNICATION_WITH_EAI);
+            log.info("Wahllokalzustand nicht gesendet. Exception: {}", exception.getMessage());
         }
     }
 }

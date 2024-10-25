c029d044ee142df4f10ae059e6745f9bc635cd1a
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2024-10-25 11:35:51 +0200

    #138 :construction: add beispielrequests + dummyClients

---
+++ wls-wahlvorstand-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorstandservice/clients/DummyClientImpl.java
@@ -0,0 +1,51 @@
+package de.muenchen.oss.wahllokalsystem.wahlvorstandservice.clients;
+
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.configuration.Profiles;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.FunktionModel;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.WahlvorstandsmitgliedModel;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.aoueaiClient.WahlvorstandEaiClient;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.aoueaiClient.WahlvorstandModel;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.basisdatenClient.Farbe;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.basisdatenClient.WahlModel;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.basisdatenClient.Wahlart;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.basisdatenClient.WahlenClient;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.infomanagementClient.KonfigurierterWahltagClient;
+import de.muenchen.oss.wahllokalsystem.wahlvorstandservice.service.wahlvorstand.infomanagementClient.KonfigurierterWahltagModel;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.WlsException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile(Profiles.DUMMY_CLIENTS) // todo: das profil dürfte hier eigentlich nicht manuell hinzugefügt werden
+public class DummyClientImpl implements WahlvorstandEaiClient, WahlenClient, KonfigurierterWahltagClient {
+    @Override
+    public WahlvorstandModel getWahlvorstand(String wahlbezirkID, LocalDate wahltag) {
+        return new WahlvorstandModel("wahlbezirkID", LocalDateTime.now(), List.of(
+                new WahlvorstandsmitgliedModel("id1", "müller", "max", FunktionModel.B, "Beisitzer*in", true),
+                new WahlvorstandsmitgliedModel("id2", "meier", "max", FunktionModel.W, "Wahlvorsteher*in", true),
+                new WahlvorstandsmitgliedModel("id3", "mustermann", "max", FunktionModel.SWB, "Stellvertretung Wahlvorsteher*in", true)));
+    }
+
+    @Override
+    public void postWahlvorstand(WahlvorstandModel wahlvorstand) {
+        log.info(wahlvorstand.toString());
+    }
+
+    @Override
+    public List<WahlModel> getWahlen(KonfigurierterWahltagModel wahltag) throws WlsException {
+        return List.of(
+                new WahlModel("wahl1", "remoteWahl 0", 1L, 1L, wahltag.wahltag(), Wahlart.BTW, new Farbe(0, 1, 2), "1"),
+                new WahlModel("wahl2", "remoteWahl 1", 2L, 1L, wahltag.wahltag(), Wahlart.EUW, new Farbe(3, 4, 5), "1"),
+                new WahlModel("wahl3", "remoteWahl 2", 3L, 1L, wahltag.wahltag(), Wahlart.LTW, new Farbe(6, 7, 8), "1"));
+    }
+
+    @Override
+    public KonfigurierterWahltagModel getKonfigurierterWahltag() throws WlsException {
+        return new KonfigurierterWahltagModel(LocalDate.now().plusMonths(1), "wahltagID1", true, "1");
+    }
+}

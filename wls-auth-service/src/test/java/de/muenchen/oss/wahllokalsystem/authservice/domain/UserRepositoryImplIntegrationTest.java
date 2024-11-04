7b74f6d3dd89f0b6bf136893d7ff40a6a2535a05
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-11-04 07:41:17 +0100

    creating UserRepositoryImplIntegrationTest

---
+++ wls-auth-service/src/test/java/de/muenchen/oss/wahllokalsystem/authservice/domain/UserRepositoryImplIntegrationTest.java
@@ -0,0 +1,144 @@
+package de.muenchen.oss.wahllokalsystem.authservice.domain;
+
+import de.muenchen.oss.wahllokalsystem.authservice.MicroServiceApplication;
+import de.muenchen.oss.wahllokalsystem.authservice.TestConstants;
+import jakarta.persistence.EntityManager;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.support.TransactionTemplate;
+
+@SpringBootTest(classes = MicroServiceApplication.class, properties = { "serviceauth.crypto.key=secret" })
+@ActiveProfiles(TestConstants.SPRING_TEST_PROFILE)
+class UserRepositoryImplIntegrationTest {
+
+    private static final String USERNAME_UNENCRYPTED = "username";
+    private static final String USERNAME_ENCRYPTED = "ENCRYPTED:AcZZ7iVGyYoE-DTb9rNwgQ==";
+
+    @Autowired
+    UserRepositoryImpl userRepository;
+
+    @Autowired
+    CrudUserRepository crudUserRepository;
+
+    @Autowired
+    AuthorityRepository authorityRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Autowired
+    TransactionTemplate transactionTemplate;
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @AfterEach
+    void tearDown() {
+        crudUserRepository.deleteAll();
+        authorityRepository.deleteAll();
+    }
+
+    @Nested
+    class Save {
+
+        @Test
+        void should_encryptUsername_when_savingUser() {
+            val userToSave = new User();
+            userToSave.setUsername(USERNAME_UNENCRYPTED);
+
+            val savedUser = transactionTemplate.execute(status -> userRepository.save(userToSave));
+
+            val result = (User) entityManager.createQuery("SELECT u FROM User u WHERE u.id = :id").setParameter("id", savedUser.getId()).getSingleResult();
+
+            Assertions.assertThat(result.getUsername()).isEqualTo(USERNAME_ENCRYPTED);
+        }
+
+        @Test
+        void should_returnUnencryptedUsername_when_savingUser() {
+            val userToSave = new User();
+            userToSave.setUsername("username");
+
+            val savedUser = transactionTemplate.execute(status -> userRepository.save(userToSave));
+
+            Assertions.assertThat(savedUser.getUsername()).isEqualTo(USERNAME_UNENCRYPTED);
+        }
+    }
+
+    @Nested
+    class FindByUsername {
+
+        @Test
+        void should_returnUser_when_searchingWithUnencryptedUsername() {
+            val userToSave = new User();
+            userToSave.setUsername(USERNAME_UNENCRYPTED);
+
+            val savedUser = transactionTemplate.execute(status -> userRepository.save(userToSave));
+
+            val result = userRepository.findByUsername(USERNAME_UNENCRYPTED);
+
+            Assertions.assertThat(result.get().getId()).isEqualTo(savedUser.getId());
+        }
+    }
+
+    @Nested
+    class Exists {
+
+        @Test
+        void should_returnTrue_when_searchingWithUnencryptedUsernameForExistingUser() {
+            val userToSave = new User();
+            userToSave.setUsername(USERNAME_UNENCRYPTED);
+
+            transactionTemplate.execute(status -> userRepository.save(userToSave));
+
+            val result = userRepository.exists(USERNAME_UNENCRYPTED);
+            Assertions.assertThat(result).isTrue();
+        }
+    }
+
+    @Nested
+    class DeleteUsersByWahltagID {
+
+        @Test
+        void should_do_sth() {
+            val wahltagID = "wahltagID";
+
+            val savedUsers = transactionTemplate.execute(status -> {
+                val permission1 = permissionRepository.save(new Permission("permission1"));
+                val permission2 = permissionRepository.save(new Permission("permission2"));
+                val permission3 = permissionRepository.save(new Permission("permission3"));
+
+                val authority = authorityRepository.save(new Authority("authority1", Set.of(permission1, permission2, permission3), Collections.emptySet()));
+
+                val user1 = userRepository.save(createUser("user1", wahltagID, authority));
+                val user2 = userRepository.save(createUser("user2", wahltagID, authority));
+                val user3 = userRepository.save(createUser("user3", wahltagID, authority));
+
+                return List.of(user1, user2, user3);
+            });
+
+            transactionTemplate.executeWithoutResult(status -> userRepository.deleteUsersByWahltagID(wahltagID));
+
+            Assertions.assertThat(savedUsers).allSatisfy(user -> Assertions.assertThat(crudUserRepository.existsById(user.getId())).isFalse());
+            Assertions.assertThat(authorityRepository.count()).isEqualTo(1);
+            Assertions.assertThat(permissionRepository.count()).isEqualTo(3);
+        }
+
+        private User createUser(final String username, final String wahltagID, final Authority authority) {
+            val user = new User();
+
+            user.setUsername(username);
+            user.setWahltagID(wahltagID);
+            user.setAuthorities(Set.of(authority));
+
+            return user;
+        }
+    }
+}

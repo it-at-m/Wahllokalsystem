71b1e9a70b54ea5feed3c13ac1e634c277ed673b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-11-04 14:49:52 +0100

    use model class for wahlbezirkart

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/service/WahllokalUserInfoModel.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/service/WahllokalUserInfoModel.java
@@ -1,13 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.authservice.service;
 
-import de.muenchen.oss.wahllokalsystem.authservice.rest.WahlbezirksartDTO;
 import java.time.LocalDate;
 
 public record WahllokalUserInfoModel(
         String wahlbezirknummer,
         LocalDate wahltag,
         String wahlbezirkID,
-        WahlbezirksartDTO wahlbezirksart,
+        WahlbezirksartModel wahlbezirksart,
         String wbid_wahlnummer
 ) {
 }

772f00f65e7a54d246bb561ac965e7efaa590023
GerhardPx <gerhard.zollner@muenchen.de>
2024-11-04 15:51:36 +0100

    manuelle Formatanpassung 2

--- wls-common/security/src/test/java/de/muenchen/oss/wahllokalsystem/wls/common/security/ProfilesTest.java
+++ wls-common/security/src/test/java/de/muenchen/oss/wahllokalsystem/wls/common/security/ProfilesTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 class ProfilesTest {
 
     @SpringBootTest(
-            classes = {TestConfiguration.class, AESEncryptionConfiguration.class, EncryptionBuilder.class, ServiceIDFormatter.class},
+            classes = { TestConfiguration.class, AESEncryptionConfiguration.class, EncryptionBuilder.class, ServiceIDFormatter.class },
             properties = {"app.crypto.key = 770A8A65DA156D24EE2A093277530142", "service.info.oid=My app name"}
     )
     @ActiveProfiles(Profiles.NO_BEZIRKS_ID_CHECK)
@@ -29,7 +29,7 @@ class ProfilesTest {
     }
 
     @SpringBootTest(
-            classes = {TestConfiguration.class, AESEncryptionConfiguration.class, EncryptionBuilder.class, ServiceIDFormatter.class},
+            classes = { TestConfiguration.class, AESEncryptionConfiguration.class, EncryptionBuilder.class, ServiceIDFormatter.class },
             properties = {"app.crypto.key = 770A8A65DA156D24EE2A093277530142", "service.info.oid=My app name"}
     )
     @Nested

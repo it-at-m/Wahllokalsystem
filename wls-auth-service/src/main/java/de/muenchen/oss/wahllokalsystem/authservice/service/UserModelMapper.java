ccdbd099f58558c1e47dbf0c180cb5e1ef63c1b7
Robert Jasny <robert.jasny@muenchen.de>
2024-11-08 16:27:28 +0100

    custom mapping von Authorities

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/service/UserModelMapper.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/service/UserModelMapper.java
@@ -1,13 +1,19 @@
 package de.muenchen.oss.wahllokalsystem.authservice.service;
 
+import de.muenchen.oss.wahllokalsystem.authservice.domain.Authority;
 import de.muenchen.oss.wahllokalsystem.authservice.domain.User;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserModelMapper {
 
-    // TODO: custom mapping
-    // @Mapping(target = "authorities", source = "authorities.authority")
     UserModel toModel(User user);
+
+    default String toModel(Authority authority) {
+        if (authority == null) {
+            return null;
+        }
+        return authority.getAuthority();
+    }
 
 }

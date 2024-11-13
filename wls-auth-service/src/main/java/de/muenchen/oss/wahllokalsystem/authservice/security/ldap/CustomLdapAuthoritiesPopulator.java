16130199a6613c7b87c97cd1aed6c24fde349632
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-11-13 19:59:28 +0100

    refactoring code

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/security/ldap/CustomLdapAuthoritiesPopulator.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/security/ldap/CustomLdapAuthoritiesPopulator.java
@@ -1,11 +1,9 @@
 package de.muenchen.oss.wahllokalsystem.authservice.security.ldap;
 
-import de.muenchen.oss.wahllokalsystem.authservice.domain.Authority;
+import de.muenchen.oss.wahllokalsystem.authservice.domain.User;
 import de.muenchen.oss.wahllokalsystem.authservice.domain.UserRepository;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
@@ -27,26 +25,19 @@ public class CustomLdapAuthoritiesPopulator implements LdapAuthoritiesPopulator 
     @Transactional(readOnly = true)
     public Collection<? extends GrantedAuthority> getGrantedAuthorities(
             DirContextOperations userData, String username) {
-        final Set<Authority> userAuthorities;
-
         val userByUsername = userRepository.findByUsername(username);
         if (userByUsername.isPresent()) {
             log.info("Found authorities for user {}.", username);
-            userAuthorities = userByUsername.get().getAuthorities();
         } else {
             log.info("Authorities for User {} not found.", username);
-            userAuthorities = Collections.emptySet();
         }
-        log.info("User {} got Authorities: {}", username, authorityCollectionToString(userAuthorities));
 
-        return userAuthorities.stream()
+        val userAuthorities = userByUsername.map(User::getAuthorities).orElse(Collections.emptySet());
+
+        val result = userAuthorities.stream()
                 .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
                 .toList();
-    }
-
-    private String authorityCollectionToString(final Collection<Authority> authorities) {
-        return authorities.stream()
-                .map(Authority::getAuthority)
-                .collect(Collectors.joining(","));
+        log.info("User {} got Authorities: {}", username, result);
+        return result;
     }
 }

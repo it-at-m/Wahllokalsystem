5749b6030ba40e875e696e6104fe35bab9ebd63d
Robert Jasny <robert.jasny@muenchen.de>
2024-11-13 16:52:06 +0100

    missing value 'userEnabled' added

--- wls-auth-service/src/main/resources/db/dummydata/h2/V1_2__insertDummyUser.sql
+++ wls-auth-service/src/main/resources/db/dummydata/h2/V1_2__insertDummyUser.sql
@@ -1,5 +1,5 @@
-INSERT INTO Wlsuser (id, username, accountNonLocked)
-VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c98', 'user', true),
-       ('68d798ca-ea64-49ce-a433-becd7ebb4c99', 'wls_all', true),
-       ('68d798ca-ea64-49ce-a433-becd7ebb4c9a', 'admin', true),
-       ('68d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', false);
+INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c98', 'user', true, true),
+       ('68d798ca-ea64-49ce-a433-becd7ebb4c99', 'wls_all', true, true),
+       ('68d798ca-ea64-49ce-a433-becd7ebb4c9a', 'admin', true, true),
+       ('68d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', true,false);

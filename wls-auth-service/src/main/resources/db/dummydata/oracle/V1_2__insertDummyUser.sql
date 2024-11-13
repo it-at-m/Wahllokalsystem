25af8728cdf415d80d8215ba9d1a4a2b2a76b0a8
Robert Jasny <robert.jasny@muenchen.de>
2024-11-13 16:46:30 +0100

    example http request + dummy data

---
+++ wls-auth-service/src/main/resources/db/dummydata/oracle/V1_2__insertDummyUser.sql
@@ -0,0 +1,8 @@
+INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c98', 'user', 0,0);
+INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c99', 'wls_all', 0,0);
+INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c9a', 'admin', 0,0);
+INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', 1,1);

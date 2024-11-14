92c1fda9ea207850c5a0e334a80c65a99410e9ab
Robert Jasny <robert.jasny@muenchen.de>
2024-11-14 14:48:05 +0100

    dummy data for oaracle'S boolean corrected and dummy data for login attempts added

--- wls-auth-service/src/main/resources/db/dummydata/h2/V1_2__insertDummyUser.sql
+++ wls-auth-service/src/main/resources/db/dummydata/h2/V1_2__insertDummyUser.sql
@@ -3,3 +3,6 @@ VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c98', 'user', true, true),
        ('68d798ca-ea64-49ce-a433-becd7ebb4c99', 'wls_all', true, true),
        ('68d798ca-ea64-49ce-a433-becd7ebb4c9a', 'admin', true, true),
        ('68d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', true,false);
+
+INSERT INTO LoginAttempt (ID, username, attempts)
+VALUES ('33d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', 5);
--- wls-auth-service/src/main/resources/db/dummydata/oracle/V1_2__insertDummyUser.sql
+++ wls-auth-service/src/main/resources/db/dummydata/oracle/V1_2__insertDummyUser.sql
@@ -1,8 +1,11 @@
 INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
-VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c98', 'user', 0,0);
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c98', 'user', 1,1);
 INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
-VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c99', 'wls_all', 0,0);
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c99', 'wls_all', 1,1);
 INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
-VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c9a', 'admin', 0,0);
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c9a', 'admin', 1,1);
 INSERT INTO Wlsuser (id, username, userEnabled, accountNonLocked)
-VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', 1,1);
+VALUES ('68d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', 1,0);
+
+INSERT INTO LoginAttempt (ID, username, attempts)
+VALUES ('33d798ca-ea64-49ce-a433-becd7ebb4c9b', 'wls_test', 5);

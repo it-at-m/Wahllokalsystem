b36a09180c4efeffb5deefe082155bb4fe6c9fc2
Robert Jasny <robert.jasny@muenchen.de>
2024-11-19 16:16:53 +0100

    script for H2 fixes

--- wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V0_1__createTableAWerte.sql
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V0_1__createTableAWerte.sql
@@ -1,9 +1,9 @@
 CREATE TABLE AWerte
 (
-    wahlID       VARCHAR(1024)  NOT NULL,
-    wahlbezirkID VARCHAR(255)   NOT NULL,
-    a1           BIGINT(19, 0)  NOT NULL,
-    a2           BIGINT(19, 0),
+    wahlID       VARCHAR(255) NOT NULL,
+    wahlbezirkID VARCHAR(255) NOT NULL,
+    a1           BIGINT       NOT NULL,
+    a2           BIGINT,
 
     PRIMARY KEY (wahlID, wahlbezirkID)
 );

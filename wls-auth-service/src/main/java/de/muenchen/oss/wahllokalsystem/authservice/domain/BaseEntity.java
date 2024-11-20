b575aa0e0d64ff4ab33728f537fed2ec7dd2dd2b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-11-20 13:33:52 +0100

    remove lombok equal-hashcode

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/BaseEntity.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/BaseEntity.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.util.UUID;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,7 +24,6 @@ import org.hibernate.annotations.JdbcTypeCode;
 @Getter
 @Setter
 @ToString
-@EqualsAndHashCode
 public abstract class BaseEntity implements Cloneable, Serializable {
 
     private static final long serialVersionUID = 1L;

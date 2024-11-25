0f636884380e0e943a5095026436c594486177b3
Robert Jasny <robert.jasny@muenchen.de>
2024-11-25 17:12:11 +0100

    admin authority added

--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/utils/Authorities.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/utils/Authorities.java
@@ -7,14 +7,15 @@ import lombok.NoArgsConstructor;
 public class Authorities {
 
     public static final String SERVICE_GET_AWERTE = "Ergebnismeldung_BUSINESSACTION_GetAWerte";
+    public static final String ADMIN_LOADWAHLTERMINDATEN = "Admin_BUSINESSACTION_LoadWahltermindaten";
 
     public static final String REPOSITORY_READ_AWERTE = "Ergebnismeldung_READ_AWerte";
     public static final String REPOSITORY_DELETE_AWERTE = "Ergebnismeldung_DELETE_AWerte";
     public static final String REPOSITORY_WRITE_AWERTE = "Ergebnismeldung_WRITE_AWerte";
 
     public static final String[] ALL_AUTHORITIES_GET_AWERTE = new String[] {
-            SERVICE_GET_AWERTE,
-            REPOSITORY_READ_AWERTE,
-            REPOSITORY_WRITE_AWERTE
+        SERVICE_GET_AWERTE,
+        REPOSITORY_READ_AWERTE,
+        REPOSITORY_WRITE_AWERTE
     };
 }

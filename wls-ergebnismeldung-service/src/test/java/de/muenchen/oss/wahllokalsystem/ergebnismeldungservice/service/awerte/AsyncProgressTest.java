3e6a8054021ce64e1b95ca47cb2bb450bc01a9d3
Robert Jasny <robert.jasny@muenchen.de>
2024-11-27 16:06:06 +0100

    AsyncProgressTest added

---
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/awerte/AsyncProgressTest.java
@@ -0,0 +1,37 @@
+package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.awerte;
+
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class AsyncProgressTest {
+
+    AsyncProgress unitUnderTest = new AsyncProgress();
+
+    @Nested
+    class IncAWerteFinished {
+
+        @Test
+        void should_increaseNumberOfaWerteFinishedAndLoadingIsNotActive_when_called() {
+            val numberOfFinishedBeforeCall = unitUnderTest.getAWerteFinished();
+            unitUnderTest.reset(0);
+            unitUnderTest.incAWerteFinished();
+            val numberOfFinishedAfterCall = unitUnderTest.getAWerteFinished();
+
+            Assertions.assertThat(numberOfFinishedAfterCall).isEqualTo(numberOfFinishedBeforeCall + 1);
+            Assertions.assertThat(unitUnderTest.isAWerteLoadingActive()).isFalse();
+        }
+
+        @Test
+        void should_increaseNumberOfaWerteFinishedAndIsLoadingActive_when_called() {
+            val numberOfFinishedBeforeCall = unitUnderTest.getAWerteFinished();
+            unitUnderTest.reset(5);
+            unitUnderTest.incAWerteFinished();
+            val numberOfFinishedAfterCall = unitUnderTest.getAWerteFinished();
+
+            Assertions.assertThat(numberOfFinishedAfterCall).isEqualTo(numberOfFinishedBeforeCall + 1);
+            Assertions.assertThat(unitUnderTest.isAWerteLoadingActive()).isTrue();
+        }
+    }
+}

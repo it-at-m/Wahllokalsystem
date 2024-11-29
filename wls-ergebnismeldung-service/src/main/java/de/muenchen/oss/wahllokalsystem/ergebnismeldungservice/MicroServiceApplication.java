94f447286c6f4b2cddd7a48d13bcb110503c1c68
Robert Jasny <robert.jasny@muenchen.de>
2024-11-29 16:08:59 +0100

    use @SpringBootApplication annotation

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/MicroServiceApplication.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/MicroServiceApplication.java
@@ -5,23 +5,14 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Application class for starting the micro-service.
  */
-@Configuration
-@ComponentScan(
-        basePackages = {
-                "org.springframework.data.jpa.convert.threeten",
-                "de.muenchen.oss.wahllokalsystem.ergebnismeldungservice",
-                "de.muenchen.oss.wahllokalsystem.wls.common.exception"
-        }
-)
+
 @EntityScan(
         basePackages = {
                 "org.springframework.data.jpa.convert.threeten",
@@ -33,7 +24,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                 "de.muenchen.oss.wahllokalsystem.ergebnismeldungservice"
         }
 )
-@EnableAutoConfiguration
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.springframework.data.jpa.convert.threeten",
+                "de.muenchen.oss.wahllokalsystem.ergebnismeldungservice",
+                "de.muenchen.oss.wahllokalsystem.wls.common.exception"
+        }
+)
 public class MicroServiceApplication {
 
     public static void main(String[] args) {

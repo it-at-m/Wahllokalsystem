f7b348dc9e10b8387919091aab9c6003f9cd17c9
Robert Jasny <robert.jasny@muenchen.de>
2024-11-29 10:20:47 +0100

    'app' replaced with 'service.config'

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/configuration/BasePathConfiguration.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/configuration/BasePathConfiguration.java
@@ -9,10 +9,10 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class BasePathConfiguration {
 
-    @Value("${app.clients.eai.basePath}")
+    @Value("${service.clients.eai.basePath}")
     String eaiBasePath;
 
-    @Value("${app.clients.infomanagement.basePath}")
+    @Value("${service.clients.infomanagement.basePath}")
     String infomanagementBasePath;
 
     private final de.muenchen.oss.wahllokalsystem.basisdatenservice.eai.aou.ApiClient eaiApiClient;

3df3fc1fe788adf4e4d47574a6717906cd3e1155
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-12-01 19:07:02 +0100

    extend type with scalar date

---
+++ experimental/wls-graphql-service/src/main/java/de/muenchen/oss/wahllokalsystem/wlsgraphqlservice/configuration/GraphQLConfiguration.java
@@ -0,0 +1,15 @@
+package de.muenchen.oss.wahllokalsystem.wlsgraphqlservice.configuration;
+
+import graphql.scalars.ExtendedScalars;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
+public class GraphQLConfiguration {
+
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date);
+    }
+}

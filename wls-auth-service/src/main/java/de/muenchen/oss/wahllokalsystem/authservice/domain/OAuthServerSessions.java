ddcff91a78adc7c0ca69b4addfb4ab5c87d328b3
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-12-02 17:57:14 +0100

    Lombok annotations to generate getters and setters automatically, improving code readability.

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/OAuthServerSessions.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/OAuthServerSessions.java
@@ -1,16 +1,11 @@
 package de.muenchen.oss.wahllokalsystem.authservice.domain;
 
 import java.util.List;
+import lombok.Data;
 
+@Data
 public class OAuthServerSessions {
 
     private List<OAuthServerSession> sessions;
 
-    public List<OAuthServerSession> getSessions() {
-        return sessions;
-    }
-
-    public void setSessions(List<OAuthServerSession> sessions) {
-        this.sessions = sessions;
-    }
 }

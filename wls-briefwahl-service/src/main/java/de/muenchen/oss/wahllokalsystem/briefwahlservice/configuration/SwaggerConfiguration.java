307a86e3d49607f1859fb41e78651c5abe07c2a2
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-12-02 18:34:18 +0100

    remove unnecessary additional schema definition

--- wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/configuration/SwaggerConfiguration.java
+++ wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/configuration/SwaggerConfiguration.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.media.MapSchema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import java.util.Map;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +13,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
-
-    public static final String SCHEMA_BEANSTANDETEWAHLBRIEFE_PROPERTY = "beanstandeteWahlbriefeProperty";
 
     @Value("${info.application.version:unknown}")
     String version;
@@ -42,15 +38,6 @@ public class SwaggerConfiguration {
                                 .addSecuritySchemes("bearerAuth", new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT"))
-                                .addSchemas(SCHEMA_BEANSTANDETEWAHLBRIEFE_PROPERTY,
-                                        new MapSchema().title("beanstandeteWahlbriefe as Map<String, Zurueckweisungsgrund_[]>")
-                                                .example(Map.of("BZW_2018",
-                                                        new String[] { "NICHT_WAHLBERECHTIGT", "UMSCHLAG_FEHLT", "LOSE_STIMMZETTEL", "ZUGELASSEN",
-                                                                "GEGENSTAND_IM_UMSCHLAG", "KEIN_ORIGINAL_SCHEIN" },
-                                                        "LTW_2018",
-                                                        new String[] { "WAHLBRIEF_UND_UMSCHLAG_OFFEN", "UNTERSCHRIFT_FEHLT", "UMSCHLAG_NICHT_AMTLICH",
-                                                                "UMSCHLAG_GEFAEHRDET_WAHLGEHEIMNIS", "SCHEINE_UNGLEICH_UMSCHLAEGE", "SCHEIN_UNGUELTIG" }))
-                                                .type("Map[String, Zurueckweisungsgrund_[]]")));
+                                        .bearerFormat("JWT")));
     }
 }

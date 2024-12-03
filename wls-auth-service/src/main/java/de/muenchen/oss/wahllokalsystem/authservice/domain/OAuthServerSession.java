101c46041c2a7928cbd7cde9fa45bfc4444285f9
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2024-12-03 15:07:57 +0100

    Consider enhancing the domain class with essential methods

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/OAuthServerSession.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/domain/OAuthServerSession.java
@@ -1,23 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.authservice.domain;
 
-public class OAuthServerSession {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public final class OAuthServerSession {
 
     private String username;
     private String sessionId;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
 }

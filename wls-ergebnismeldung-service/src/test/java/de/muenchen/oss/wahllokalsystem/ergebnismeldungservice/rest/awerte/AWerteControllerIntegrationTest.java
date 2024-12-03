2952dd648f2082a6b7edca32953c9f1c21d60e35
Robert Jasny <robert.jasny@muenchen.de>
2024-12-03 11:06:16 +0100

    test für initialiseAWerte hinzugefügt, WireMock entfernt, statt dessen Profiles.DUMMY_CLIENTS verwenden

--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/awerte/AWerteControllerIntegrationTest.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/awerte/AWerteControllerIntegrationTest.java
@@ -5,9 +5,9 @@ import static de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.TestConstan
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.client.WireMock;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.MicroServiceApplication;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.client.awerte.AWerteClientMapper;
+import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.configuration.Profiles;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.awerte.AWerteRepository;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.eai.aou.model.WahlberechtigteDTO;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.awerte.AWerteModelMapper;
@@ -17,22 +17,19 @@ import java.util.List;
 import lombok.val;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest(classes = MicroServiceApplication.class)
 @AutoConfigureMockMvc
-@AutoConfigureWireMock
-@ActiveProfiles(profiles = { SPRING_TEST_PROFILE, SPRING_NO_SECURITY_PROFILE })
+@ActiveProfiles(profiles = { SPRING_TEST_PROFILE, SPRING_NO_SECURITY_PROFILE, Profiles.DUMMY_CLIENTS })
 public class AWerteControllerIntegrationTest {
 
     @Autowired
@@ -59,22 +56,14 @@ public class AWerteControllerIntegrationTest {
         awerteRepository.deleteAll();
     }
 
-    @BeforeEach
-    void setup() {
-        WireMock.resetAllRequests();
-    }
-
     @Nested
     class GetAWerte {
 
         @Test
         void should_returnAWerteListFromEAI_when_dataFound() throws Exception {
             val wahlbezirkID = "wahlbezirkID1";
+            // same values as DummyClientImpl
             val eaiWahlberechtigte = createClientListOfAWahlberechtigteDTO(wahlbezirkID);
-            WireMock.stubFor(WireMock.get("/wahldaten/wahlbezirke/" + wahlbezirkID + "/wahlberechtigte")
-                    .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
-                            .withStatus(HttpStatus.OK.value())
-                            .withBody(objectMapper.writeValueAsBytes(eaiWahlberechtigte))));
 
             val request = MockMvcRequestBuilders.get("/businessActions/awerte/" + wahlbezirkID);
             val response = api.perform(request).andExpect(status().isOk()).andReturn();
@@ -90,11 +79,8 @@ public class AWerteControllerIntegrationTest {
         @Test
         void should_saveAWerteListFromEAIToRepo_when_dataFound() throws Exception {
             val wahlbezirkID = "wahlbezirkID1";
+            // same values as DummyClientImpl
             val eaiWahlberechtigte = createClientListOfAWahlberechtigteDTO(wahlbezirkID);
-            WireMock.stubFor(WireMock.get("/wahldaten/wahlbezirke/" + wahlbezirkID + "/wahlberechtigte")
-                    .willReturn(WireMock.aResponse().withHeader("Content-Type", "application/json")
-                            .withStatus(HttpStatus.OK.value())
-                            .withBody(objectMapper.writeValueAsBytes(eaiWahlberechtigte))));
 
             val request = MockMvcRequestBuilders.get("/businessActions/awerte/" + wahlbezirkID);
             api.perform(request).andExpect(status().isOk()).andReturn();
@@ -107,22 +93,32 @@ public class AWerteControllerIntegrationTest {
         }
     }
 
+    @Nested
+    class InitialiseAWerte {
+        @Test
+        void should_initialiseAWerteInRepo_when_wahlbezirkIDsAreGiven() throws Exception {
+            val wahlbezirkIDs = List.of("wahlbezirkID1", "wahlbezirkID2", "wahlbezirkID3");
+
+            val request = MockMvcRequestBuilders.post("/businessActions/awerte/init").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(wahlbezirkIDs));
+            api.perform(request).andExpect(status().isOk()).andReturn();
+
+            for (String wahlbezirkID : wahlbezirkIDs) {
+                val aWerteFromRepo = awerteRepository.findByBezirkUndWahlID_WahlbezirkID(wahlbezirkID);
+                Assertions.assertThat(aWerteFromRepo).hasSize(1);
+                Assertions.assertThat(aWerteFromRepo).allSatisfy(aWert -> {
+                    Assertions.assertThat(aWert.getBezirkUndWahlID().getWahlbezirkID()).isEqualTo(wahlbezirkID);
+                });
+            }
+        }
+    }
+
     private List<WahlberechtigteDTO> createClientListOfAWahlberechtigteDTO(String wahlbezirkID) {
         val wahlberechtigte1 = new WahlberechtigteDTO();
-        wahlberechtigte1.setWahlID("wahlID1");
+        wahlberechtigte1.setWahlID("wahlID01");
         wahlberechtigte1.setWahlbezirkID(wahlbezirkID);
-        wahlberechtigte1.setA1(2L);
-        wahlberechtigte1.setA2(3L);
-        val wahlberechtigte2 = new WahlberechtigteDTO();
-        wahlberechtigte2.setWahlID("wahlID2");
-        wahlberechtigte2.setWahlbezirkID(wahlbezirkID);
-        wahlberechtigte2.setA1(4L);
-        wahlberechtigte2.setA2(5L);
-        val wahlberechtigte3 = new WahlberechtigteDTO();
-        wahlberechtigte3.setWahlID("wahlID3");
-        wahlberechtigte3.setWahlbezirkID(wahlbezirkID);
-        wahlberechtigte3.setA1(6L);
-        wahlberechtigte3.setA2(7L);
-        return List.of(wahlberechtigte1, wahlberechtigte2, wahlberechtigte3);
+        wahlberechtigte1.setA1(25L);
+        wahlberechtigte1.setA2(26L);
+        return List.of(wahlberechtigte1);
     }
 }

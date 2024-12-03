61ea074b50ca9e80c90f5a314220ad89ed65b5b9
Robert Jasny <robert.jasny@muenchen.de>
2024-12-03 11:49:07 +0100

    Konstanten werden nicht benötigt, da im alten Code keine entsprechende Exception geworfen wird

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/exception/ExceptionConstants.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/exception/ExceptionConstants.java
@@ -1,17 +1,11 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.exception;
 
 import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionDataWrapper;
-import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionKonstanten;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionConstants {
-
-    public static ExceptionDataWrapper FAILED_COMMUNICATION_WITH_EAI = new ExceptionDataWrapper("100",
-            "Bei der Kommunikation mit dem Aoueai-Service ist ein Fehler aufgetreten. Es konnten daher keine Daten geladen werden.");
-
-    public static ExceptionDataWrapper NULL_FROM_CLIENT = new ExceptionDataWrapper(ExceptionKonstanten.CODE_ENTITY_NOT_FOUND, "not found");
 
     public static ExceptionDataWrapper GETAWERTE_PARAMETER_UNVOLLSTAENDIG = new ExceptionDataWrapper("600",
             "getAWerte: Parameter unvollstaendig");

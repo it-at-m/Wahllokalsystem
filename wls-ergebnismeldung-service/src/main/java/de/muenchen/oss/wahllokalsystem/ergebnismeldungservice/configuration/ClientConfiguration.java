42d2cb5af87b88019887b22708f4c2400aed6e7d
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-12-03 08:54:41 +0100

    added monitoring service api and defined client

---
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/configuration/ClientConfiguration.java
@@ -0,0 +1,48 @@
+package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.errorhandler.WlsResponseErrorHandler;
+import lombok.val;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.AbstractOAuth2Token;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ClientConfiguration {
+
+    //other configurations like the restTemplate bean factory method
+
+    @Bean
+    public WlsResponseErrorHandler wlsResponseErrorHandler(final ObjectMapper objectMapper) {
+        return new WlsResponseErrorHandler(objectMapper);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(final WlsResponseErrorHandler wlsResponseErrorHandler) {
+        val restTemplate = new RestTemplate();
+
+        /* definieren des Errorhandlers für Antworten vom externen Service */
+        restTemplate.setErrorHandler(wlsResponseErrorHandler);
+        /* Ergänzen eines Interceptors um den Bearer-Token an den nächsten Service weiter zu geben */
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null) {
+                return execution.execute(request, body);
+            }
+
+            if (!(authentication.getCredentials() instanceof AbstractOAuth2Token)) {
+                return execution.execute(request, body);
+            }
+
+            val token = (AbstractOAuth2Token) authentication.getCredentials();
+            request.getHeaders().setBearerAuth(token.getTokenValue());
+            return execution.execute(request, body);
+        });
+
+        return restTemplate;
+    }
+
+}

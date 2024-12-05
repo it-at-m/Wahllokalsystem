33bf974fb1862ea59463b85da5e57c43c5c82831
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-12-05 19:37:49 +0100

    🚧 some tests

---
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/status/StatusValidatorTest.java
@@ -0,0 +1,84 @@
+package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.status;
+
+import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.exception.ExceptionConstants;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.FachlicheWlsException;
+import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionFactory;
+import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlID;
+import java.util.stream.Stream;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class StatusValidatorTest {
+
+    @Mock
+    ExceptionFactory exceptionFactory;
+
+    @InjectMocks
+    StatusValidator unitUnderTest;
+
+    @Nested
+    class ValideBezirkUndWahlIdOrThrow {
+
+        final FachlicheWlsException providedException = FachlicheWlsException.withCode("").buildWithMessage("sth failed");
+
+        @Test
+        void should_notThrowException_when_bezirkUndWahlIDIsValid() {
+            val id = new BezirkUndWahlID("wahlID", "wahlbezirkID");
+
+            Assertions.assertThatNoException().isThrownBy(() -> unitUnderTest.valideBezirkUndWahlIdOrThrow(id, null));
+        }
+
+        @ParameterizedTest(name = "provided exception when {1}")
+        @MethodSource("invalidWahlbezirkArgumentsWithTestcaseNameAppendix")
+        void should_throwProvidedException_when_bezirkUndWahlIdIsNotValid(final ArgumentsAccessor arguments) {
+            Assertions.assertThatException()
+                    .isThrownBy(() -> unitUnderTest.valideBezirkUndWahlIdOrThrow(arguments.get(0, BezirkUndWahlID.class), providedException))
+                    .isSameAs(providedException);
+        }
+
+        public static Stream<Arguments> invalidWahlbezirkArgumentsWithTestcaseNameAppendix() {
+            return Stream.of(
+                    Arguments.of(null, "argument is null"),
+                    Arguments.of(new BezirkUndWahlID(null, "wahlbezirkID"), "wahlID is null"),
+                    Arguments.of(new BezirkUndWahlID("", "wahlbezirkID"), "wahlID is empty"),
+                    Arguments.of(new BezirkUndWahlID("   ", "wahlbezirkID"), "wahlID is blank"),
+                    Arguments.of(new BezirkUndWahlID("wahlID", null), "wahlbezirkID is null"),
+                    Arguments.of(new BezirkUndWahlID("wahlID", ""), "wahlbezirkID is is empty"),
+                    Arguments.of(new BezirkUndWahlID("wahlID", "   "), "wahlbezirkID is blank")
+            );
+        }
+    }
+
+    @Nested
+    class ValideStatusOrThrow {
+
+        @Test
+        void should_notThrowException_when_statusIsEmptyButNotNull() {
+            val statusModelToValidate = new StatusModel(null, null, null);
+
+            Assertions.assertThatNoException().isThrownBy(() -> unitUnderTest.valideStatusOrThrow(statusModelToValidate));
+        }
+
+        @Test
+        void should_throwFachlicheWlsException_when_statusIsNull() {
+            val mockedFachlicheWlsException = FachlicheWlsException.withCode("").buildWithMessage("sth failed");
+            Mockito.when(exceptionFactory.createFachlicheWlsException(ExceptionConstants.POST_STATUS_PARAMETER_UNVOLLSTAENDIG))
+                    .thenReturn(mockedFachlicheWlsException);
+
+            Assertions.assertThatException().isThrownBy(() -> unitUnderTest.valideStatusOrThrow(null)).isSameAs(mockedFachlicheWlsException);
+        }
+    }
+
+}

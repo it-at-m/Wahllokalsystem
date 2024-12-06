4dfdba3e871482158725da8ef88760f10a0b9f6f
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2024-12-06 15:43:45 +0100

    fix testname

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/status/sender/SchnellmeldungSendungsuhrzeitSender.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/status/sender/SchnellmeldungSendungsuhrzeitSender.java
@@ -3,18 +3,14 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.status.se
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.status.StatusModel;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.status.ValidierungsstatusModel;
 import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlID;
-import io.micrometer.tracing.annotation.DefaultNewSpanParser;
 import java.time.LocalDateTime;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SchnellmeldungSendungsuhrzeitSender extends AbstractStatusMonitoringSender {
 
-    private final DefaultNewSpanParser newSpanParser;
-
-    public SchnellmeldungSendungsuhrzeitSender(StatusClient monitoringClient, DefaultNewSpanParser newSpanParser) {
+    public SchnellmeldungSendungsuhrzeitSender(StatusClient monitoringClient) {
         super(monitoringClient);
-        this.newSpanParser = newSpanParser;
     }
 
     @Override

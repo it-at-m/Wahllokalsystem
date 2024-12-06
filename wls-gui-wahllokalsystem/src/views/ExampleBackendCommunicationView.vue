a69a1f4405cd5ddb5670134d66f12005b0af8ff9
viviane.johns <viviane.johns@muenchen.de>
2024-12-06 14:11:39 +0100

    #564 :art:

--- wls-gui-wahllokalsystem/src/views/ExampleBackendCommunicationView.vue
+++ wls-gui-wahllokalsystem/src/views/ExampleBackendCommunicationView.vue
@@ -5,20 +5,24 @@
     </v-col>
     <v-responsive class="mx-auto">
       <v-col class="text-center">
+        <h4>API Calls with fetch():</h4>
+        <br />
         <v-text-field
           class="ml-auto mr-auto"
           width="350"
-          v-model="messageInput"
+          v-model="messageInputFetch"
           clearable
           label="ID"
         ></v-text-field>
-        <v-btn @click="postMessage(['wbz-1', 'wbz-2'])"
+        <v-btn @click="postMessageFetch(['wbz-1', 'wbz-2'])"
           >post message with fetch utils
         </v-btn>
         <p v-if="errorPostFetch">{{ errorPostFetch }}</p>
         <br />
         <br />
-        <v-btn @click="getMessage('wbz-1')">get message with fetch utils</v-btn>
+        <v-btn @click="getMessageFetch('wbz-1')"
+          >get message with fetch utils</v-btn
+        >
         <pre v-if="messageFetch"> {{ messageFetch }} </pre>
         <p v-if="errorGetFetch">{{ errorGetFetch }}</p>
         <p v-if="errorReadFetch">{{ errorReadFetch }}</p>
@@ -48,14 +52,14 @@ import { STATUS_INDICATORS } from "@/constants";
 import { useSnackbarStore } from "@/stores/snackbar";
 
 const snackbarStore = useSnackbarStore();
-const messageInput = ref("Broadcast Message");
+const messageInputFetch = ref("Broadcast Message");
 const messageFetch = ref("Click Button to Load Message");
 const errorGetFetch = ref("");
 const errorPostFetch = ref("");
 const errorReadFetch = ref("");
-let messageFetchId = "";
+let messageIdFetch = "";
 
-function getMessage(wahlbezirkID: string) {
+function getMessageFetch(wahlbezirkID: string) {
   errorGetFetch.value = "";
   messageFetch.value = "";
   getBroadcastMessage(wahlbezirkID)
@@ -64,8 +68,8 @@ function getMessage(wahlbezirkID: string) {
     })
     .then((content: BroadcastMessageToRead) => {
       messageFetch.value = content.nachricht;
-      messageFetchId = content.oid;
-      broadcastMessageRead(messageFetchId).catch((e) => {
+      messageIdFetch = content.oid;
+      broadcastMessageRead(messageIdFetch).catch((e) => {
         errorReadFetch.value =
           "Es ist ein Fehler beim Lesen der Nachricht aufgetreten";
       });
@@ -76,12 +80,12 @@ function getMessage(wahlbezirkID: string) {
     });
 }
 
-function postMessage(wahlbezirkIDs: string[]) {
+function postMessageFetch(wahlbezirkIDs: string[]) {
   errorPostFetch.value = "";
-  postBroadcastMessage(wahlbezirkIDs, messageInput.value).catch((e) => {
+  postBroadcastMessage(wahlbezirkIDs, messageInputFetch.value).catch((e) => {
     errorPostFetch.value = e.message;
     snackbarStore.showMessage({ message: e, level: STATUS_INDICATORS.ERROR });
   });
-  messageInput.value = "";
+  messageInputFetch.value = "";
 }
 </script>

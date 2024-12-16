1b650e798e345a4429c4142f94d759dae655da20
Robert Jasny <robert.jasny@muenchen.de>
2024-12-16 17:22:18 +0100

    double slash removed

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/wahlscheine/WahlscheineController.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/wahlscheine/WahlscheineController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/businessActions//wahlscheine")
+@RequestMapping("/businessActions/wahlscheine")
 @RequiredArgsConstructor
 public class WahlscheineController {
 

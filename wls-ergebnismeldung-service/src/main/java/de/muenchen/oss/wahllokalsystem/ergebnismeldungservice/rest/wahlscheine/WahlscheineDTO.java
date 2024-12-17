e63f17ac81bc48ab60b1cdef889818bb9bac8d89
Robert Jasny <robert.jasny@muenchen.de>
2024-12-17 16:49:22 +0100

    checkstyle errors corrected

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/wahlscheine/WahlscheineDTO.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/wahlscheine/WahlscheineDTO.java
@@ -3,6 +3,6 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.wahlscheine;
 import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlID;
 import jakarta.validation.constraints.NotNull;
 
-public record WahlscheineDTO (@NotNull BezirkUndWahlID bezirkUndWahlID,
-                              @NotNull Long stimmabgabevermerke) {
+public record WahlscheineDTO(@NotNull BezirkUndWahlID bezirkUndWahlID,
+                             @NotNull Long stimmabgabevermerke) {
 }
--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/wahlscheine/WahlscheineModel.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/wahlscheine/WahlscheineModel.java
@@ -3,6 +3,6 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.wahlschei
 import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlID;
 import jakarta.validation.constraints.NotNull;
 
-public record WahlscheineModel (@NotNull BezirkUndWahlID bezirkUndWahlID,
-                                @NotNull Long stimmabgabevermerke){
+public record WahlscheineModel(@NotNull BezirkUndWahlID bezirkUndWahlID,
+                               @NotNull Long stimmabgabevermerke){
 }

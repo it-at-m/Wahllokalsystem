4503e4efef8b358cf98cf577358f87ba6051ca90
Robert Jasny <robert.jasny@muenchen.de>
2024-12-18 11:07:26 +0100

    Update wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V2_0__createTableWahlscheine.sql
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V2_0__createTableWahlscheine.sql
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V2_0__createTableWahlscheine.sql
@@ -2,7 +2,7 @@ CREATE TABLE Wahlscheine
 (
     wahlID                           VARCHAR(1024) NOT NULL,
     wahlbezirkID                     VARCHAR(1024) NOT NULL,
-    stimmabgabevermerke              BIGINT       NOT NULL,
+    stimmabgabevermerke              BIGINT        NOT NULL,
 
     PRIMARY KEY (wahlID, wahlbezirkID)
 );

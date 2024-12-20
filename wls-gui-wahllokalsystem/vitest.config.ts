1973c294a7aace21f46f4badb2f38cccba4e5d1a
viviane.johns <viviane.johns@muenchen.de>
2024-12-20 14:01:35 +0100

    #670 :bug: try to fix `module not found` error

--- wls-gui-wahllokalsystem/vitest.config.ts
+++ wls-gui-wahllokalsystem/vitest.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from "node:url";
 
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig, mergeConfig } from "vitest/config";
 
 import viteConfig from "./vite.config";
@@ -7,8 +8,16 @@ import viteConfig from "./vite.config";
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    plugins: [tsconfigPaths()],
+    resolve: {
+      alias: {
+        //"@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@": "./src",
+      },
+    },
     test: {
       environment: "jsdom",
+      include: ["./app/**/*.{test,spec}.{ts,tsx}"],
       root: fileURLToPath(new URL("./", import.meta.url)),
       server: {
         deps: {

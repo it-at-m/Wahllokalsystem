9908f85255bd121045b2c04c175a87099d3a90e3
viviane.johns <viviane.johns@muenchen.de>
2025-01-07 09:36:07 +0100

    #670 :bug: fix 2: update config files to successfully resolve alias

--- wls-gui-wahllokalsystem/vite.config.ts
+++ wls-gui-wahllokalsystem/vite.config.ts
@@ -1,13 +1,9 @@
-import * as node_fs from "fs";
-import * as fs from "node:fs";
-import path from "node:path";
+import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
-import { parse } from "jsonc-parser";
 import UnpluginFonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 const serverPort = 8400;
 
@@ -33,7 +29,6 @@ export default defineConfig({
         ],
       },
     }),
-    tsconfigPaths(),
   ],
   server: {
     port: serverPort,
@@ -47,52 +42,8 @@ export default defineConfig({
     },
   },
   resolve: {
-    //getPathsFromTsConfig(),
     alias: {
-      //"@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@": "./src",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
-
-/*
-// siehe https://stackoverflow.com/questions/77249074/how-do-i-use-typescript-path-aliases-in-vite/78658715#78658715
-interface TsConfig {
-  compilerOptions: {
-    paths: {
-      [key: string]: string[];
-    };
-  };
-}
-
-function get_paths_from_tsconfig(): Record<string, string> {
-  const tsconfig_s: string = node_fs
-    .readFileSync("./tsconfig.json", "utf-8")
-    .replace(/\/\/.*$/gm, ""); // Entfernen von Kommentaren
-  const tsconfig: TsConfig = JSON.parse(tsconfig_s);
-  const aliases: Record<string, string> = {};
-  for (const [key, value] of Object.entries(tsconfig.compilerOptions.paths)) {
-    if (Array.isArray(value) && value.length > 0) {
-      aliases[key] = path.resolve(__dirname, value[0]);
-    }
-  }
-  return aliases;
-}*/
-
-// siehe https://stackoverflow.com/questions/77249074/how-do-i-use-typescript-path-aliases-in-vite/79171421#79171421
-interface TsConfig {
-  compilerOptions: {
-    paths: Record<string, string[]>;
-  };
-}
-
-function getPathsFromTsConfig(): Record<string, string> {
-  const tsconfig: TsConfig = parse(fs.readFileSync("./tsconfig.json", "utf-8"));
-  const aliases: Record<string, string> = {};
-  for (const [key, value] of Object.entries(tsconfig.compilerOptions.paths)) {
-    const cleanKey = key.replace("/*", "");
-    const cleanValue = value[0].replace("/*", "");
-    aliases[cleanKey] = path.resolve(__dirname, cleanValue);
-  }
-  return aliases;
-}
--- wls-gui-wahllokalsystem/vitest.config.ts
+++ wls-gui-wahllokalsystem/vitest.config.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "node:url";
 
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig, mergeConfig } from "vitest/config";
 
 import viteConfig from "./vite.config";
@@ -8,16 +7,8 @@ import viteConfig from "./vite.config";
 export default mergeConfig(
   viteConfig,
   defineConfig({
-    plugins: [tsconfigPaths()],
-    resolve: {
-      alias: {
-        //"@": fileURLToPath(new URL("./src", import.meta.url)),
-        "@": "./src",
-      },
-    },
     test: {
       environment: "jsdom",
-      include: ["./app/**/*.{test,spec}.{ts,tsx}"],
       root: fileURLToPath(new URL("./", import.meta.url)),
       server: {
         deps: {

cecb8e45a96056773622e678b55e314323102ddf
viviane.johns <viviane.johns@muenchen.de>
2025-01-09 15:59:57 +0100

    #568 :white_check_mark: add unit tests for postMessage api

--- wls-gui-wahllokalsystem/tests/unit/broadcast-client.spec.ts
+++ wls-gui-wahllokalsystem/tests/unit/broadcast-client.spec.ts
@@ -3,61 +3,101 @@ import { afterEach, describe, expect, it, vi } from "vitest";
 import {
   BROADCAST_API_URL,
   getBroadcastMessage,
+  postBroadcastMessage,
 } from "@/api/wls-clients/broadcast-service/broadcast-client";
 
-describe("GetBroadcastMessage", () => {
+describe("Broadcast Service API", () => {
   afterEach(() => {
     vi.clearAllMocks();
   });
 
-  it("should_returnMessageForWahlbezirk_when_messageFoundAndCalledWithCorrectParams", async () => {
-    const wahlbezirkId = "id12345";
-    const mockedBroadcastMessage = {
-      oid: "123456789",
-      wahlbezirkIDs: [wahlbezirkId],
-      nachricht: "Neue Nachricht",
-    };
+  describe("GetBroadcastMessage", () => {
+    it("should_returnMessageForWahlbezirk_when_messageFoundAndCalledWithCorrectParams", async () => {
+      const wahlbezirkId = "id12345";
+      const mockedBroadcastMessage = {
+        oid: "123456789",
+        wahlbezirkIDs: [wahlbezirkId],
+        nachricht: "Neue Nachricht",
+      };
 
-    // gibt die gemockte response zurück, wenn die fetch funktion aufgerufen wird
-    global.fetch = vi.fn(() =>
-      Promise.resolve(
-        new Response(JSON.stringify(mockedBroadcastMessage), { status: 200 })
-      )
-    );
-
-    const response = await getBroadcastMessage(wahlbezirkId);
-    const result = await response.json();
-
-    expect(result).toEqual(mockedBroadcastMessage);
-    expect(fetch).toHaveBeenCalledWith(
-      `${BROADCAST_API_URL}getMessage/${wahlbezirkId}`,
-      expect.anything() // ignoriert den zweiten parameter, der an fetch übergeben wird
-    );
-  });
-
-  it("should_throwError_when_noMessageFound", async () => {
-    global.fetch = vi.fn(() =>
-      Promise.resolve(new Response(null, { status: 204 }))
-    );
-
-    await expect(getBroadcastMessage("123")).rejects.toThrow(
-      "Es konnten keine Daten gefunden werden"
-    );
-  });
-
-  it.each([
-    [""], // empty id
-    [" "], // blank id
-  ])(
-    'should_throwWlsError_when_calledWithWrongOrMissingWahlbezirkID: "%s"',
-    async (wahlbezirkID: string) => {
+      // gibt die gemockte response zurück, wenn die fetch funktion aufgerufen wird
       global.fetch = vi.fn(() =>
-        Promise.resolve(new Response(null, { status: 400 }))
+        Promise.resolve(
+          new Response(JSON.stringify(mockedBroadcastMessage), { status: 200 })
+        )
       );
 
-      await expect(getBroadcastMessage(wahlbezirkID)).rejects.toThrow(
-        "Ungültige Anfrage"
+      const response = await getBroadcastMessage(wahlbezirkId);
+      const result = await response.json();
+
+      expect(result).toEqual(mockedBroadcastMessage);
+      expect(fetch).toHaveBeenCalledWith(
+        `${BROADCAST_API_URL}getMessage/${wahlbezirkId}`,
+        expect.anything() // ignoriert den zweiten parameter, der an fetch übergeben wird
       );
-    }
-  );
+    });
+
+    it("should_throwError_when_noMessageFound", async () => {
+      global.fetch = vi.fn(() =>
+        Promise.resolve(new Response(null, { status: 204 }))
+      );
+
+      await expect(getBroadcastMessage("123")).rejects.toThrow(
+        "Es konnten keine Daten gefunden werden"
+      );
+    });
+
+    it.each([
+      [""], // empty id
+      [" "], // blank id
+    ])(
+      'should_throwWlsError_when_calledWithWrongOrMissingWahlbezirkID: "%s"',
+      async (wahlbezirkID: string) => {
+        global.fetch = vi.fn(() =>
+          Promise.resolve(new Response(null, { status: 400 }))
+        );
+
+        await expect(getBroadcastMessage(wahlbezirkID)).rejects.toThrow(
+          "Ungültige Anfrage"
+        );
+      }
+    );
+  });
+
+  describe("PostBroadcastMessage", () => {
+    it("should_postMessageSuccessfully_when_calledWithCorrectParams", async () => {
+      const mockedResponse = { error: null };
+      global.fetch = vi.fn(() =>
+        Promise.resolve(
+          new Response(JSON.stringify(mockedResponse), { status: 200 })
+        )
+      );
+      const response = await postBroadcastMessage(["id"], "message");
+      const result = await response.json();
+
+      expect(result).toEqual(mockedResponse);
+      expect(fetch).toHaveBeenCalledWith(
+        `${BROADCAST_API_URL}broadcast`,
+        expect.anything()
+      );
+    });
+
+    it.each([
+      [[""], "message"], // empty id
+      [[" "], "message"], // blank id
+      [["123"], ""], // empty message
+      [["123"], " "], // blank message
+    ])(
+      'should_throwWlsError_when_calledWithWrongOrMissingParams: "%s"',
+      async (wahlbezirkIDs: string[], message: string) => {
+        global.fetch = vi.fn(() =>
+          Promise.resolve(new Response(null, { status: 400 }))
+        );
+
+        await expect(
+          postBroadcastMessage(wahlbezirkIDs, message)
+        ).rejects.toThrow("Ungültige Anfrage");
+      }
+    );
+  });
 });

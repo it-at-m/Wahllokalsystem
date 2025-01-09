cae23b8bdee10101b157534ba2a039c2a17ea5d5
viviane.johns <viviane.johns@muenchen.de>
2025-01-09 14:57:41 +0100

    #568 :white_check_mark: refactor unit tests for getMessage api

--- wls-gui-wahllokalsystem/tests/unit/broadcast-client.spec.ts
+++ wls-gui-wahllokalsystem/tests/unit/broadcast-client.spec.ts
@@ -1,8 +1,9 @@
 import { afterEach, describe, expect, it, vi } from "vitest";
 
-import * as api from "@/api/wls-clients/broadcast-service/broadcast-client";
-
-global.fetch = vi.fn();
+import {
+  BROADCAST_API_URL,
+  getBroadcastMessage,
+} from "@/api/wls-clients/broadcast-service/broadcast-client";
 
 describe("GetBroadcastMessage", () => {
   afterEach(() => {
@@ -16,29 +17,30 @@ describe("GetBroadcastMessage", () => {
       wahlbezirkIDs: [wahlbezirkId],
       nachricht: "Neue Nachricht",
     };
-    // Erstellen einer gemockten Response
-    const mockedResponse = new Response(
-      JSON.stringify(mockedBroadcastMessage),
-      { status: 200 }
-    );
-    // gibt die gemockte response zurück, wenn die fetch funktion aufgerufen wird
-    fetch.mockResolvedValue(mockedResponse);
 
-    const response = await api.getBroadcastMessage(wahlbezirkId);
+    // gibt die gemockte response zurück, wenn die fetch funktion aufgerufen wird
+    global.fetch = vi.fn(() =>
+      Promise.resolve(
+        new Response(JSON.stringify(mockedBroadcastMessage), { status: 200 })
+      )
+    );
+
+    const response = await getBroadcastMessage(wahlbezirkId);
     const result = await response.json();
 
     expect(result).toEqual(mockedBroadcastMessage);
     expect(fetch).toHaveBeenCalledWith(
-      `${api.BROADCAST_API_URL}getMessage/${wahlbezirkId}`,
+      `${BROADCAST_API_URL}getMessage/${wahlbezirkId}`,
       expect.anything() // ignoriert den zweiten parameter, der an fetch übergeben wird
     );
   });
 
   it("should_throwError_when_noMessageFound", async () => {
-    const mockedResponse = new Response(null, { status: 204 });
-    fetch.mockResolvedValue(mockedResponse);
+    global.fetch = vi.fn(() =>
+      Promise.resolve(new Response(null, { status: 204 }))
+    );
 
-    await expect(api.getBroadcastMessage("123")).rejects.toThrow(
+    await expect(getBroadcastMessage("123")).rejects.toThrow(
       "Es konnten keine Daten gefunden werden"
     );
   });
@@ -49,9 +51,11 @@ describe("GetBroadcastMessage", () => {
   ])(
     'should_throwWlsError_when_calledWithWrongOrMissingWahlbezirkID: "%s"',
     async (wahlbezirkID: string) => {
-      // todo: damit läuft der test durch, aber ist das richtig? müsste es nicht auch ohne mocken einen fehler werfen?
-      fetch.mockResolvedValue(new Response(null, { status: 400 }));
-      await expect(api.getBroadcastMessage(wahlbezirkID)).rejects.toThrow(
+      global.fetch = vi.fn(() =>
+        Promise.resolve(new Response(null, { status: 400 }))
+      );
+
+      await expect(getBroadcastMessage(wahlbezirkID)).rejects.toThrow(
         "Ungültige Anfrage"
       );
     }

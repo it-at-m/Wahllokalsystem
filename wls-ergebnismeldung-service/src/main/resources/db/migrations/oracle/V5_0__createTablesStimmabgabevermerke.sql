ee8d75fc8ac07a3dcf66238c4b52ae5be3f956a5
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-01-10 16:45:09 +0100

    created flyway file for Stimmabgabevermerke

---
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/oracle/V5_0__createTablesStimmabgabevermerke.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Stimmabgabevermerke
+(
+    waehlerverzeichnisNummer NUMBER(19, 0) NOT NULL,
+    wahlbezirkid             VARCHAR(1024) NOT NULL,
+    anzahlblaetter           NUMBER(19, 0) NOT NULL,
+
+    PRIMARY KEY (waehlerverzeichnisNummer, wahlbezirkid)
+);
+
+CREATE TABLE Wahldaten
+(
+    id                       VARCHAR(36),
+    wahlid                   VARCHAR(1024) NOT NULL,
+    waehlerverzeichnisNummer NUMBER(19, 0),
+    wahlbezirkid             VARCHAR(1024),
+
+    CONSTRAINT fk_Wd
+        FOREIGN KEY (waehlerverzeichnisNummer, wahlbezirkid)
+            REFERENCES Stimmabgabevermerke (waehlerverzeichnisNummer, wahlbezirkid)
+                ON DELETE CASCADE,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE EingenommeneWahlscheine
+(
+    wahldatenID    VARCHAR(36),
+    anzahl         NUMBER(19, 0) NOT NULL,
+    stimmzettelart VARCHAR(255)  NOT NULL,
+
+    CONSTRAINT fk_Wahldaten_ew
+        FOREIGN KEY (wahldatenID)
+            REFERENCES Wahldaten (id)
+                ON DELETE CASCADE
+);
+
+CREATE TABLE Vermerk
+(
+    id          VARCHAR(36),
+    wahldatenID VARCHAR(36),
+    blattnummer NUMBER(19, 0) NOT NULL,
+
+    PRIMARY KEY (id),
+
+    CONSTRAINT fk_Wahldaten
+        FOREIGN KEY (wahldatenID)
+            REFERENCES Wahldaten (id)
+                ON DELETE CASCADE
+);
+
+CREATE TABLE Stimmzettel
+(
+    vermerkID      VARCHAR(36),
+    anzahl         NUMBER(19, 0) NOT NULL,
+    stimmzettelart VARCHAR(255)  NOT NULL,
+
+    CONSTRAINT fk_Vermerke
+        FOREIGN KEY (vermerkID)
+            REFERENCES Vermerk (id)
+);

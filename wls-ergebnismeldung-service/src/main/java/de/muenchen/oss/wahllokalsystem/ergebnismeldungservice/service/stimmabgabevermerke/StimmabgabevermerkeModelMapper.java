856e4f61f931af47eff3fa4e0c8b57ad07db4c1b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-01-10 17:02:12 +0100

    remove unnecessary mapping definition

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/stimmabgabevermerke/StimmabgabevermerkeModelMapper.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/stimmabgabevermerke/StimmabgabevermerkeModelMapper.java
@@ -1,15 +1,12 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.stimmabgabevermerke;
 
-import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.stimmabgabevermerke.EingenommenerWahlscheine;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.stimmabgabevermerke.Stimmabgabevermerke;
-import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.stimmabgabevermerke.Stimmzettel;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.stimmabgabevermerke.Vermerk;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.stimmabgabevermerke.Wahldaten;
-import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED)
+@Mapper
 public interface StimmabgabevermerkeModelMapper {
 
     StimmabgabevermerkeModel toModel(Stimmabgabevermerke entity);
@@ -21,8 +18,4 @@ public interface StimmabgabevermerkeModelMapper {
 
     @Mapping(target = "id", ignore = true)
     Vermerk toEntity(VermerkModel vermerkModel);
-
-    EingenommenerWahlscheine toEntity(EingenommenerWahlscheinModel eingenommenerWahlscheinModel);
-
-    Stimmzettel toEntity(StimmzettelModel stimmzettelModel);
 }

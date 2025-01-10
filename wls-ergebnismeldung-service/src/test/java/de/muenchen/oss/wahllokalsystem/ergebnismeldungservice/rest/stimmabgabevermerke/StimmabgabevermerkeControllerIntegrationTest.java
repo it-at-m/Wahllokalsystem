552cbcb3d15d0f49765e8a09774458f5e94da34b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-01-10 17:20:02 +0100

    remove wiremock

--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/stimmabgabevermerke/StimmabgabevermerkeControllerIntegrationTest.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/stimmabgabevermerke/StimmabgabevermerkeControllerIntegrationTest.java
@@ -27,14 +27,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest(classes = MicroServiceApplication.class)
 @AutoConfigureMockMvc
-@AutoConfigureWireMock
 @ActiveProfiles(
         profiles = { SPRING_TEST_PROFILE, SPRING_NO_SECURITY_PROFILE,
                 de.muenchen.oss.wahllokalsystem.wls.common.security.Profiles.NO_BEZIRKS_ID_CHECK }

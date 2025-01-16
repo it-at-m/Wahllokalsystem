7339cf7590b56696c04a47d596ceda65ec09d088
Robert Jasny <robert.jasny@muenchen.de>
2025-01-16 17:03:33 +0100

    some refactoring and bugs

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/ausdruck/AusdruckController.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/ausdruck/AusdruckController.java
@@ -2,18 +2,14 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.ausdruck;
 
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.ausdruck.Meldungsart;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.ausdruck.WahlUndBezirkIDUndMeldungsart;
-import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.exception.ExceptionConstants;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.ausdruck.AusdruckModel;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.ausdruck.AusdruckService;
-import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionFactory;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,8 +29,6 @@ public class AusdruckController {
 
     private final AusdruckWriteDTOMapper ausdruckWriteDTOMapper;
 
-    private final ExceptionFactory exceptionFactory;
-
     @GetMapping("{wahlID}/{wahlbezirkID}")
     public ResponseEntity<List<AusdruckReadDTO>> getAllAusdrucke(@PathVariable("wahlID") final String wahlID,
             @PathVariable("wahlbezirkID") final String wahlbezirkID) {
@@ -46,29 +40,24 @@ public class AusdruckController {
     public ResponseEntity<?> postHtmlAusdruck(@PathVariable("wahlID") final String wahlID,
             @PathVariable("wahlbezirkID") final String wahlbezirkID, @PathVariable("meldungsart") final Meldungsart meldungsart,
             @RequestBody final AusdruckWriteDTO ausdruck) {
-        AusdruckModel ausdruckModel = ausdruckWriteDTOMapper.toModel(ausdruck, new WahlUndBezirkIDUndMeldungsart(wahlID, wahlbezirkID, meldungsart),
+        AusdruckModel ausdruckModel = ausdruckWriteDTOMapper.toModel(ausdruck, new WahlUndBezirkIDUndMeldungsart(wahlbezirkID, wahlID, meldungsart),
                 Instant.now());
         ausdruckService.saveAusdruck(ausdruckModel);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("{wahlID}/{wahlbezirkID}/{meldungsart}/html")
-    public void getHtmlAusdruck(@PathVariable("wahlID") String wahlID,
-            @PathVariable("wahlbezirkID") String wahlbezirkID, @PathVariable("meldungsart") Meldungsart meldungsart,
-            HttpServletResponse response) {
-        AusdruckReadDTO result = ausdruckReadDTOMapper.toDTO(ausdruckService.getAusdruck(new WahlUndBezirkIDUndMeldungsart(wahlID, wahlbezirkID, meldungsart)));
+    public ResponseEntity<String> getHtmlAusdruck(@PathVariable("wahlID") String wahlID, @PathVariable("wahlbezirkID") String wahlbezirkID,
+            @PathVariable("meldungsart") Meldungsart meldungsart) {
+        AusdruckReadDTO result = ausdruckReadDTOMapper.toDTO(ausdruckService.getAusdruck(new WahlUndBezirkIDUndMeldungsart(wahlbezirkID, wahlID, meldungsart)));
         if (result == null) {
-            response.setStatus(HttpStatus.NOT_FOUND.value());
-            return;
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        response.setContentType(MediaType.TEXT_HTML_VALUE);
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        response.setStatus(HttpStatus.OK.value());
-        try {
-            response.getWriter().print(result.content());
-        } catch (IOException e) {
-            throw exceptionFactory.createFachlicheWlsException(ExceptionConstants.GET_AUSDRUCK_PARAMETER_UNVOLLSTAENDIG);
-        }
+
+        val responseHeaders = new HttpHeaders();
+        responseHeaders.add(HttpHeaders.CONTENT_TYPE, "text/html; charset=utf-8");
+
+        return new ResponseEntity<>(result.content(), responseHeaders, HttpStatus.OK);
     }
 
 }

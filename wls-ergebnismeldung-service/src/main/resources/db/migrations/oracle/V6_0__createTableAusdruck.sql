2ee8675bbf948720bdf2c85742ae7e2782bd59fa
Robert Jasny <robert.jasny@muenchen.de>
2025-01-22 09:56:19 +0100

    Update wls-ergebnismeldung-service/src/main/resources/db/migrations/oracle/V6_0__createTableAusdruck.sql
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-ergebnismeldung-service/src/main/resources/db/migrations/oracle/V6_0__createTableAusdruck.sql
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/oracle/V6_0__createTableAusdruck.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Ausdruck
 (
-    wahlID varchar(255) NOT NULL,
-    wahlbezirkID varchar(255) NOT NULL,
-    meldungsart varchar(255) NOT NULL,
+    wahlID VARCHAR(255) NOT NULL,
+    wahlbezirkID VARCHAR(255) NOT NULL,
+    meldungsart VARCHAR(255) NOT NULL,
     content CLOB,
     erstellt_am TIMESTAMP NOT NULL,
     PRIMARY KEY (wahlID, wahlbezirkID, meldungsart)

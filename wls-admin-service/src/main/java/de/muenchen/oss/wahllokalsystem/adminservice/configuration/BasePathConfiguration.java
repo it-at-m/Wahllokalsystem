16ce75224ba2633a931b55f9bc150f3237042b45
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-01-22 13:17:40 +0100

    add generation of clients for auth service and basisdaten service

---
+++ wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/configuration/BasePathConfiguration.java
@@ -0,0 +1,27 @@
+package de.muenchen.oss.wahllokalsystem.adminservice.configuration;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BasePathConfiguration {
+
+    /* Umgebungsvariable welche die Ziel-URL enthält, z.b. http//localhost:39146 */
+    @Value("${service.config.clients.auth.basePath}")
+    String authBasePath;
+
+    @Value("${service.config.clients.basisdaten.basePath}")
+    String basisdatenBasePath;
+
+    private final de.muenchen.oss.wahllokalsystem.adminservice.eai.auth.ApiClient authApiClient;
+    private final de.muenchen.oss.wahllokalsystem.adminservice.eai.basisdaten.ApiClient basisdatenApiClient;
+
+    @PostConstruct
+    public void updateBasePaths() {
+        basisdatenApiClient.setBasePath(basisdatenBasePath);
+        authApiClient.setBasePath(authBasePath);
+    }
+}

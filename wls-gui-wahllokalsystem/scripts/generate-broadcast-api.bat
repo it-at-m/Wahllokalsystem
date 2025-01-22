b6c5f1c6424cc5e0d6ec5b8b130c63a27ff9e4a5
viviane.johns <viviane.johns@muenchen.de>
2025-01-22 10:49:55 +0100

    #716 delete script files + add generator script to package.json

--- wls-gui-wahllokalsystem/scripts/generate-broadcast-api.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-openapi-generator-cli generate -i ../src/resources/openapis/openapi.broadcast.0.2.0.json -g typescript-fetch -o ../src/api/wls-clients/generated-broadcast-api --template-dir ../src/api/wls-clients/custom-openapi-template-files

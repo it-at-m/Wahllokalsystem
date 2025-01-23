56747a054e3e105c3239ecc6178f348cce14aff1
viviane.johns <viviane.johns@muenchen.de>
2025-01-23 20:38:29 +0100

    #565 add pwa config in vite.config.ts file

--- wls-gui-wahllokalsystem/vite.config.ts
+++ wls-gui-wahllokalsystem/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import UnpluginFonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 const serverPort = 8400;
@@ -27,6 +28,22 @@ export default defineConfig({
             styles: "wght@100;300;400;500;700;900",
           },
         ],
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ["**/*.{js,ts,css,html,ico,png,svg,json,vue,txt}"],
+      },
+      manifest: {
+        name: "Wahllokalsystem",
+        short_name: "WLS",
+        description: "",
+        theme_color: "#546e7a",
+        display: "standalone",
+        icons: [],
       },
     }),
   ],

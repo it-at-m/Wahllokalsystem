ca06a13fbc825fb8b6b06a48d533ef39390fc735
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2025-01-24 14:14:59 +0100

    Client Configuration Beans in Context

--- wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/configuration/ClientConfiguration.java
+++ wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/configuration/ClientConfiguration.java
@@ -12,20 +12,29 @@ import org.springframework.web.client.RestTemplate;
 public class ClientConfiguration {
 
     @Bean
-    public RestTemplate restTemplate(final WlsResponseErrorHandler wlsResponseErrorHandler, final OAuth2TokenInterceptor oAuth2TokenInterceptor) {
-        val restTemplate = new RestTemplate();
-
-        /* definieren des Errorhandlers für Antworten vom externen Service */
-        restTemplate.setErrorHandler(wlsResponseErrorHandler);
-        /* Ergänzen eines Interceptors um den Bearer-Token an den nächsten Service weiter zu geben */
-        restTemplate.getInterceptors().add(oAuth2TokenInterceptor);
-
-        return restTemplate;
-    }
-
-    @Bean
     public WlsResponseErrorHandler wlsResponseErrorHandler(final ObjectMapper objectMapper) {
         return new WlsResponseErrorHandler(objectMapper);
     }
 
+    @Bean
+    public RestTemplate restTemplate(final WlsResponseErrorHandler wlsResponseErrorHandler) {
+        val restTemplate = new RestTemplate();
+
+        restTemplate.setErrorHandler(wlsResponseErrorHandler);
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null) {
+                return execution.execute(request, body);
+            }
+
+            if (!(authentication.getCredentials() instanceof AbstractOAuth2Token token)) {
+                return execution.execute(request, body);
+            }
+
+            request.getHeaders().setBearerAuth(token.getTokenValue());
+            return execution.execute(request, body);
+        });
+
+        return restTemplate;
+    }
 }

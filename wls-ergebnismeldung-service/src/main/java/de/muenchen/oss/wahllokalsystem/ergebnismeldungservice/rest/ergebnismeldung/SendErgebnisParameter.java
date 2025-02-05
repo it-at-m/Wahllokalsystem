a9fec555dd4824a90d786c4f73d28fc8ff93b020
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-02-05 16:53:38 +0100

    add validation constraints

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/ergebnismeldung/SendErgebnisParameter.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/ergebnismeldung/SendErgebnisParameter.java
@@ -1,12 +1,13 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.ergebnismeldung;
 
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.common.MeldungsartDTO;
+import jakarta.validation.constraints.NotNull;
 
 public record SendErgebnisParameter(
-        String wahlID,
-        String wahlbezirkID,
-        Long waehlerverzeichnisNummer,
-        MeldungsartDTO meldungsart,
-        String hauptwahlbezirkID
+        @NotNull String wahlID,
+        @NotNull String wahlbezirkID,
+        @NotNull Long waehlerverzeichnisNummer,
+        @NotNull MeldungsartDTO meldungsart,
+        @NotNull String hauptwahlbezirkID
 ) {
 }

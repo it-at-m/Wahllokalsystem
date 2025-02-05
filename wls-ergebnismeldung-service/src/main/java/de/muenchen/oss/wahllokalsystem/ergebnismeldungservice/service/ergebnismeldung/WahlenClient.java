b2cf3d5cb97d87023eb7ef73b374360dc52f74e1
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-02-05 20:13:29 +0100

    add interface javadoc

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnismeldung/WahlenClient.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnismeldung/WahlenClient.java
@@ -2,5 +2,14 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.ergebnism
 
 public interface WahlenClient {
 
+    /**
+     * @param wahlID The unique identifier of the election on the current election day
+     * @return The election type model containing type information
+     * @throws de.muenchen.oss.wahllokalsystem.wls.common.exception.TechnischeWlsException when problem
+     *             on retrieving the information occurred
+     * @throws de.muenchen.oss.wahllokalsystem.wls.common.exception.FachlicheWlsException when current
+     *             election day is unknown or no election with wahlID
+     *             exists for the election day
+     */
     WahlartModel getWahlartOfCurrentWahltag(final String wahlID);
 }

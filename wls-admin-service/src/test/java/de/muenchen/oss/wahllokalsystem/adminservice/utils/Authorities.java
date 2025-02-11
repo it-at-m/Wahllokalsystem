16829deaa5908da4ed766716001f09f17e86fe94
Robert Jasny <robert.jasny@muenchen.de>
2025-02-11 09:25:02 +0100

    service security test added

---
+++ wls-admin-service/src/test/java/de/muenchen/oss/wahllokalsystem/adminservice/utils/Authorities.java
@@ -0,0 +1,11 @@
+package de.muenchen.oss.wahllokalsystem.adminservice.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Authorities {
+
+    public static final String ADMIN_LOADWAHLTERMINDATEN = "Admin_BUSINESSACTION_LoadWahltermindaten";
+
+}

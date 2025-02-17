6e594cacd634146d93a69764b8aa21061f76238c
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-02-17 13:21:46 +0100

    #565 add pwa config + custom sw file

--- wls-gui-wahllokalsystem/vite.config.ts
+++ wls-gui-wahllokalsystem/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import UnpluginFonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 const serverPort = 8400;
@@ -29,6 +30,26 @@ export default defineConfig({
         ],
       },
     }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      strategies: "injectManifest", // makes it possible to use own service worker
+      srcDir: "src", // custom sw file directory
+      filename: "wahl-worker.js", // custom sw file
+      injectManifest: {
+        // injectionPoint disabled, to avoid compilation errors, because precaching ist not used in wahl-worker.js
+        injectionPoint: undefined,
+        sourcemap: true,
+      },
+      manifest: {
+        name: "Wahllokalsystem",
+        short_name: "WLS",
+        description: "",
+        theme_color: "#546e7a",
+        display: "standalone",
+        icons: [],
+      },
+    }),
   ],
   server: {
     port: serverPort,
@@ -40,6 +61,10 @@ export default defineConfig({
     hmr: {
       clientPort: serverPort,
     },
+  },
+  // serves built solution from ./dist folder to preview (no hot reload)
+  preview: {
+    port: serverPort,
   },
   resolve: {
     alias: {

ed9246d160e4dfab149c883717a9a8f7cf47e879
Robert Jasny <robert.jasny@muenchen.de>
2025-02-19 10:02:44 +0100

    checkstyle error corrected

--- wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/rest/wahlen/FarbeDTO.java
+++ wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/rest/wahlen/FarbeDTO.java
@@ -2,5 +2,5 @@ package de.muenchen.oss.wahllokalsystem.adminservice.rest.wahlen;
 
 import jakarta.validation.constraints.NotNull;
 
-public record FarbeDTO (@NotNull long r, @NotNull long g, @NotNull long b) {
+public record FarbeDTO(@NotNull long r, @NotNull long g, @NotNull long b) {
 }

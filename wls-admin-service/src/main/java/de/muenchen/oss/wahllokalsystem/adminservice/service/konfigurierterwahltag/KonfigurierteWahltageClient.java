2228a7b5d6f7b1397d432c172b56a2a7141cb00e
GerhardPx <gerhard.zollner@muenchen.de>
2025-02-19 11:02:01 +0100

    spotless

--- wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/service/konfigurierterwahltag/KonfigurierteWahltageClient.java
+++ wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/service/konfigurierterwahltag/KonfigurierteWahltageClient.java
@@ -7,9 +7,13 @@ import java.util.List;
 public interface KonfigurierteWahltageClient {
 
     /**
-     * @return list of KonfigurierteWahltage {@link de.muenchen.oss.wahllokalsystem.adminservice.service.common.KonfigurierterWahltagModel}
-     * @throws WlsException {@link de.muenchen.oss.wahllokalsystem.wls.common.exception.FachlicheWlsException} if return would be null
-     *                      {@link de.muenchen.oss.wahllokalsystem.wls.common.exception.TechnischeWlsException} if there were trouble during communication
+     * @return list of KonfigurierteWahltage
+     *         {@link de.muenchen.oss.wahllokalsystem.adminservice.service.common.KonfigurierterWahltagModel}
+     * @throws WlsException
+     *             {@link de.muenchen.oss.wahllokalsystem.wls.common.exception.FachlicheWlsException} if
+     *             return would be null
+     *             {@link de.muenchen.oss.wahllokalsystem.wls.common.exception.TechnischeWlsException}
+     *             if there were trouble during communication
      */
     List<KonfigurierterWahltagModel> getKonfigurierteWahltage() throws WlsException;
 
--- wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/service/wahltermindaten/KonfigurierterWahltagClient.java
+++ wls-admin-service/src/main/java/de/muenchen/oss/wahllokalsystem/adminservice/service/wahltermindaten/KonfigurierterWahltagClient.java
@@ -6,7 +6,8 @@ public interface KonfigurierterWahltagClient {
 
     /**
      * @param konfigurierterWahltag filter for konfigurierten Wahltaa {@link KonfigurierterWahltagModel}
-     * @throws de.muenchen.oss.wahllokalsystem.wls.common.exception.TechnischeWlsException when problem on retrieving the information occurred
+     * @throws de.muenchen.oss.wahllokalsystem.wls.common.exception.TechnischeWlsException when problem
+     *             on retrieving the information occurred
      */
     void postKonfigurierterWahltag(KonfigurierterWahltagModel konfigurierterWahltag);
 }

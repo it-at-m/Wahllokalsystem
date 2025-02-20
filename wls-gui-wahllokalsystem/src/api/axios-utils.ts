8826663f00abf3927048734d49924bd816eb999a
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-02-20 13:39:29 +0100

    #565 add wls heartbeat

---
+++ wls-gui-wahllokalsystem/src/api/axios-utils.ts
@@ -0,0 +1,43 @@
+import type { AxiosRequestConfig } from "axios";
+
+import { AxiosHeaders } from "axios";
+
+export function basicGetConfig(
+  url: string,
+  swStrategy?: string
+): AxiosRequestConfig {
+  return {
+    url: url,
+    method: "get",
+    // "mode: 'cors'" from fetch is not necessary, because its included in axios
+    withCredentials: true, // equivalent to "credentials: 'include'" in fetch
+    // "redirect: 'manual'" from fetch is not necessary, because its icluded in axios
+    headers: getHeaders(swStrategy),
+  };
+}
+
+export function basicPostConfig(
+  url: string,
+  swStrategy?: string
+): AxiosRequestConfig {
+  return {
+    url: url,
+    method: "post",
+    withCredentials: true,
+    headers: getHeaders(swStrategy),
+  };
+}
+
+function getHeaders(strategy = "OFFLINE_FIRST"): AxiosHeaders {
+  const headers = new AxiosHeaders();
+  headers.set("X-XSRF-TOKEN", getCSRFToken());
+  headers.set("content-type", "application/json");
+  headers.set("X-WLS-SW-STRATEGY", strategy);
+
+  return headers;
+}
+
+function getCSRFToken() {
+  const token = document.cookie.match("(^|;)\\s*XSRF-TOKEN\\s*=\\s*([^;]+)");
+  return token ? token.pop() : "";
+}
---
+++ wls-gui-wahllokalsystem/src/components/wlsComponents/WlsHeartbeat.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-menu>
+    <template #activator="{ props: menu }">
+      <v-btn
+        v-bind="menu"
+        :icon="getIcon(isOffline)"
+        variant="text"
+        density="comfortable"
+        size="x-large"
+        :color="getColor(isOffline)"
+      ></v-btn>
+    </template>
+    <v-card
+      width="250"
+      max-height="200"
+    >
+      <v-list class="pt-0">
+        <v-list-item class="list-header">
+          <strong>Verbindungsstatus</strong>
+        </v-list-item>
+        <v-divider
+          thickness="2"
+          color="black"
+        ></v-divider>
+        <v-list-item> {{ getText(isOffline) }}</v-list-item>
+      </v-list>
+    </v-card>
+  </v-menu>
+</template>
+
+<script setup lang="ts">
+import axios from "axios";
+import {
+  VBtn,
+  VCard,
+  VDivider,
+  VList,
+  VListItem,
+  VMenu,
+} from "vuetify/components";
+
+import { basicPostConfig } from "@/api/axios-utils";
+import { useInterval } from "@/composables/useInterval";
+
+useInterval(() => {
+  heartbeat();
+}, 30000); // updates every 30 seconds
+defineProps({
+  isOffline: { type: Boolean, default: false },
+});
+const emit = defineEmits(["update:isOffline"]);
+function getIcon(isOffline: boolean) {
+  return isOffline ? "$signalOffline" : "$signalOnline";
+}
+function getText(isOffline: boolean) {
+  return isOffline
+    ? "Sie sind aktuell offline. Ihre Eingaben werden lokal gespeichert und synchronisiert, sobald Sie wieder online sind."
+    : "Sie sind aktuell online.";
+}
+function getColor(isOffline: boolean) {
+  return isOffline ? "error" : "white";
+}
+// todo: example implementation --> api logic should be transferred to composable
+async function heartbeat() {
+  return await axios
+    .request(
+      basicPostConfig(
+        "api/monitoring-service/businessActions/lastSeen/wbz-1",
+        "ONLINE_ONLY"
+      )
+    )
+    .then((response) => {
+      if (response.status == 200) {
+        emit("update:isOffline", false);
+      }
+    })
+    .catch(() => {
+      emit("update:isOffline", true);
+    });
+}
+</script>
+
+<style>
+.list-header {
+  background: #f0f0f0;
+}
+</style>

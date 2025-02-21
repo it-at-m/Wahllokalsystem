18e6a7ae2b3f08c12ad54ee23a4e964dda7fbb38
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-02-21 15:19:46 +0100

    add function to interact with wahlvorstand service

---
+++ wls-gui-wahllokalsystem/src/composables/wahlvorstand/wahlvorstandService.ts
@@ -0,0 +1,42 @@
+import type { Wahlvorstand } from "@/types/wahlvorstand/wahlvorstand";
+
+import {
+  Configuration,
+  WahlvorstandControllerApi,
+} from "@/api/wls-clients/generated-wahlvorstand-api";
+import { useWahlvorstandMapper } from "@/composables/wahlvorstand/wahlvorstandMapper";
+import { WAHLVORSTAND_SERVICE_API_URL } from "@/constants";
+
+const { toModel, toDto } = useWahlvorstandMapper();
+
+export function useWahlvorstandService() {
+  const wahlvorstandControllerApi = new WahlvorstandControllerApi(
+    new Configuration({
+      basePath: WAHLVORSTAND_SERVICE_API_URL,
+    })
+  );
+
+  function getWahlvorstand(wahlbezirkID: string): Promise<Wahlvorstand> {
+    return wahlvorstandControllerApi
+      .getWahlvorstand(wahlbezirkID)
+      .then((response) => toModel(response.data));
+  }
+
+  function saveWahlvorstand(wahlvorstand: Wahlvorstand): {
+    updateDatetime: Date;
+  } {
+    const now = new Date();
+    const wahlvorstandDto = toDto(wahlvorstand, now);
+
+    wahlvorstandControllerApi.postWahlvorstand("wahlbezirkID", wahlvorstandDto);
+
+    return {
+      updateDatetime: now,
+    };
+  }
+
+  return {
+    getWahlvorstand,
+    saveWahlvorstand,
+  };
+}

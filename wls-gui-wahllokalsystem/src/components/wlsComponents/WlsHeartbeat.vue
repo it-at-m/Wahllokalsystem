945acc175fa56223cb8a19d83b881afc75a470f8
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-02-21 16:38:41 +0100

    #565 disable periodic heartbeat but add statusCheck btn

--- wls-gui-wahllokalsystem/src/components/wlsComponents/WlsHeartbeat.vue
+++ wls-gui-wahllokalsystem/src/components/wlsComponents/WlsHeartbeat.vue
@@ -15,6 +15,16 @@
       max-height="280"
     >
       <v-list class="pt-0">
+        <v-list-item>
+          <v-btn
+            @click.stop="heartbeat"
+            :disabled="isCheckStatusBtnDisabled"
+            color="primary"
+            style="width: 100%"
+          >
+            {{ checkStatusBtnText }}
+          </v-btn>
+        </v-list-item>
         <v-list-item class="list-header">
           <v-row
             class="ma-1"
@@ -37,6 +47,7 @@
 
 <script setup lang="ts">
 import axios from "axios";
+import { ref } from "vue";
 import {
   VBtn,
   VCard,
@@ -50,28 +61,42 @@ import {
 
 import { basicPostConfig } from "@/api/axios-utils";
 import OfflineSyncer from "@/components/wlsComponents/OfflineSyncer.vue";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { useInterval } from "@/composables/useInterval";
 
+// todo: synchronizing activated via click. uncomment to activate periodically
+/*
 useInterval(() => {
   heartbeat();
 }, 30000); // updates every 30 seconds
+*/
+
 defineProps({
   isOffline: { type: Boolean, default: false },
 });
+
 const emit = defineEmits(["update:isOffline"]);
+const checkStatusBtnText = ref("Check online status");
+const isCheckStatusBtnDisabled = ref(false);
+
 function getIcon(isOffline: boolean) {
   return isOffline ? "$signalOffline" : "$signalOnline";
 }
+
 function getText(isOffline: boolean) {
   return isOffline
     ? "Sie sind aktuell offline. Ihre Eingaben werden lokal gespeichert und synchronisiert, sobald Sie wieder online sind."
     : "Sie sind aktuell online.";
 }
+
 function getColor(isOffline: boolean) {
   return isOffline ? "error" : "white";
 }
+
 // todo: example implementation --> api logic should be transferred to composable
 async function heartbeat() {
+  disableStatusBtn();
+  await new Promise((resolve) => setTimeout(resolve, 0o500)); // wait half a sec to show searching message
   return await axios
     .request(
       basicPostConfig(
@@ -82,11 +107,23 @@ async function heartbeat() {
     .then((response) => {
       if (response.status == 200) {
         emit("update:isOffline", false);
+        enableStatusBtn();
       }
     })
     .catch(() => {
       emit("update:isOffline", true);
+      enableStatusBtn();
     });
+}
+
+function disableStatusBtn() {
+  isCheckStatusBtnDisabled.value = true;
+  checkStatusBtnText.value = "searching signal...";
+}
+
+function enableStatusBtn() {
+  isCheckStatusBtnDisabled.value = false;
+  checkStatusBtnText.value = "Check online status";
 }
 </script>
 

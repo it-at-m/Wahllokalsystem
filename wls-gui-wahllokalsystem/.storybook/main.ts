2c6131423a77969f34f311f4d5ae2dc0f8ffbbb3
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-02-21 15:04:58 +0100

    add storybookJs and piniaTesting

---
+++ wls-gui-wahllokalsystem/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from "@storybook/vue3-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
+  },
+};
+export default config;
---
+++ wls-gui-wahllokalsystem/.storybook/preview.ts
@@ -0,0 +1,23 @@
+import type { Preview } from "@storybook/vue3";
+
+import { setup } from "@storybook/vue3";
+
+import { registerPlugins } from "../src/plugins";
+
+setup((app) => {
+  // Registers your app's plugins into Storybook
+  registerPlugins(app);
+});
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;

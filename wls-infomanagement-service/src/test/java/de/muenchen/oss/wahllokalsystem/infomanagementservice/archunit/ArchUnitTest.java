31c4cd5c224fda816ad1b338555eaddeea040db1
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-02-24 17:02:28 +0100

    #767 add/import rule to infomanagement-service

--- wls-infomanagement-service/src/test/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/archunit/ArchUnitTest.java
+++ wls-infomanagement-service/src/test/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/archunit/ArchUnitTest.java
@@ -1,27 +1,20 @@
 package de.muenchen.oss.wahllokalsystem.infomanagementservice.archunit;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
-
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.syntax.elements.MethodsShouldConjunction;
 import de.muenchen.oss.wahllokalsystem.infomanagementservice.MicroServiceApplication;
+import de.muenchen.oss.wahllokalsystem.wls.common.testing.archunit.rule.MethodRules;
+
 import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class ArchUnitTest {
-
-    private static final String REGEX_TEST_CASE_NAME = "^should_([a-z]+([A-Z]+[a-z]*)+)_when_([a-z]+([A-Z]+[a-z]*)+)$";
-
-    private static final MethodsShouldConjunction RULE_TEST_NAMING_CONVENTION_SHOULD_WHEN_MATCHED = methods()
-            .that().areAnnotatedWith(Test.class)
-            .should().haveNameMatching(REGEX_TEST_CASE_NAME);
 
     private JavaClasses serviceClasses;
 
@@ -38,6 +31,6 @@ public class ArchUnitTest {
 
     public static Stream<Arguments> rulesToVerify() {
         return Stream.of(
-                Arguments.of("TEST_NAMING_CONVENTION_RULE", RULE_TEST_NAMING_CONVENTION_SHOULD_WHEN_MATCHED));
+                Arguments.of("TEST_NAMING_CONVENTION_RULE", MethodRules.RULE_TEST_NAMING_CONVENTION_SHOULD_WHEN_MATCHED));
     }
 }

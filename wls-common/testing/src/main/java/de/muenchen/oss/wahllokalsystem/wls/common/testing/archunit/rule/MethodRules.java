cc88e40357d908acbcabb77eabb4222f2b95028e
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-02-25 09:28:28 +0100

    #767 add rule to wls common

--- wls-common/testing/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/testing/archunit/rule/MethodRules.java
+++ wls-common/testing/src/main/java/de/muenchen/oss/wahllokalsystem/wls/common/testing/archunit/rule/MethodRules.java
@@ -3,9 +3,12 @@ package de.muenchen.oss.wahllokalsystem.wls.common.testing.archunit.rule;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.elements.MethodsShouldConjunction;
 import de.muenchen.oss.wahllokalsystem.wls.common.testing.archunit.condition.RequestMappingMethodParameterAnnotationCondition;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,4 +18,8 @@ public class MethodRules {
             .that().areAnnotatedWith(RequestMapping.class)
             .or().areMetaAnnotatedWith(RequestMapping.class)
             .should(new RequestMappingMethodParameterAnnotationCondition());
+
+    public static final MethodsShouldConjunction RULE_TEST_NAMING_CONVENTION_SHOULD_WHEN_MATCHED = methods()
+            .that().areAnnotatedWith(Test.class).or().areAnnotatedWith(ParameterizedTest.class)
+            .should().haveNameMatching("should_[a-z].*_when_[a-z].*");
 }

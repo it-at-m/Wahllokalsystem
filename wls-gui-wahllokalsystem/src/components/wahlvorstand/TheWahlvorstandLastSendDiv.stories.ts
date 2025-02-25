f42728994c98f73ac4f07ead3abd949b4376700b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-02-25 14:41:46 +0100

    add example story for component with store interaction

---
+++ wls-gui-wahllokalsystem/src/components/wahlvorstand/TheWahlvorstandLastSendDiv.stories.ts
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import { createPinia, setActivePinia } from "pinia";
+
+import TheWahlvorstandLastSendDiv from "@/components/wahlvorstand/TheWahlvorstandLastSendDiv.vue";
+import pinia from "@/plugins/pinia";
+import { useWahlvorstandStore } from "@/stores/wahlvorstandStore";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  component: TheWahlvorstandLastSendDiv,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  args: {},
+  decorators: [
+    (story) => {
+      const pinia = createPinia();
+      setActivePinia(pinia);
+      return {
+        component: { story },
+        template: "<story />",
+      };
+    },
+  ],
+} satisfies Meta<typeof TheWahlvorstandLastSendDiv>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LastSendingIsNow: Story = {
+  async beforeEach() {
+    const store = useWahlvorstandStore(pinia);
+    store.lastSending = new Date();
+  },
+};
+
+export const LastSendingWithLeadingZeros: Story = {
+  async beforeEach() {
+    const store = useWahlvorstandStore(pinia);
+    store.lastSending = new Date("2025-02-01T01:02:03.004");
+  },
+};
+
+export const LastsendingIsNull: Story = {
+  beforeEach() {
+    const store = useWahlvorstandStore(pinia);
+    store.lastSending = null;
+  },
+};

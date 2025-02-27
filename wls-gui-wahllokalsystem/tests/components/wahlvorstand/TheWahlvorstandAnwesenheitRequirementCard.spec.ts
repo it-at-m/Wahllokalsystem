7ceb7bc78c8dbbc37e312fff4cfe826a712fb91c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-02-27 17:52:10 +0100

    lint fix

--- wls-gui-wahllokalsystem/tests/components/wahlvorstand/TheWahlvorstandAnwesenheitRequirementCard.spec.ts
+++ wls-gui-wahllokalsystem/tests/components/wahlvorstand/TheWahlvorstandAnwesenheitRequirementCard.spec.ts
@@ -63,7 +63,7 @@ describe("TheWahlvorstandAnwesenheitRequirementCard.vue", () => {
         getSnapshotFilename(context)
       );
     });
-    
+
     it("should_showErrorText_when_schriftfuehrerIsNotAnwesend", async (context) => {
       const wahlvorstandStore = useWahlvorstandStore();
 
@@ -78,7 +78,7 @@ describe("TheWahlvorstandAnwesenheitRequirementCard.vue", () => {
         getSnapshotFilename(context)
       );
     });
-    
+
     it("should_showErrorText_when_wahlvorsteherIsNotAnwesend", async (context) => {
       const wahlvorstandStore = useWahlvorstandStore();
 

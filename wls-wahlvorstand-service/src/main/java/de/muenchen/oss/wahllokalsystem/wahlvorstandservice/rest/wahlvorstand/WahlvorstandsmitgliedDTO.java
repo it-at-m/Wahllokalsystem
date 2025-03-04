d0ea5496e48c08144301854b5a16b77b31e882d6
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-04 10:26:47 +0100

    added NotNull annotations

--- wls-wahlvorstand-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorstandservice/rest/wahlvorstand/WahlvorstandsmitgliedDTO.java
+++ wls-wahlvorstand-service/src/main/java/de/muenchen/oss/wahllokalsystem/wahlvorstandservice/rest/wahlvorstand/WahlvorstandsmitgliedDTO.java
@@ -1,12 +1,13 @@
 package de.muenchen.oss.wahllokalsystem.wahlvorstandservice.rest.wahlvorstand;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
-public record WahlvorstandsmitgliedDTO(String identifikator,
-                                       String familienname,
-                                       String vorname,
-                                       FunktionDTO funktion,
+public record WahlvorstandsmitgliedDTO(@NotNull String identifikator,
+                                       @NotNull String familienname,
+                                       @NotNull String vorname,
+                                       @NotNull FunktionDTO funktion,
                                        String funktionsname,
-                                       boolean anwesend) {
+                                       @NotNull boolean anwesend) {
 }

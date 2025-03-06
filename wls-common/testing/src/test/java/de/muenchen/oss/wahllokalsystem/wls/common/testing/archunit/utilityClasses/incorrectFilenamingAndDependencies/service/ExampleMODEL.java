3d2bec0aaffabe1368201aeecc011bd00de7b5e1
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-06 13:33:11 +0100

    #770 spotless:apply

--- wls-common/testing/src/test/java/de/muenchen/oss/wahllokalsystem/wls/common/testing/archunit/utilityClasses/incorrectFilenamingAndDependencies/service/ExampleMODEL.java
+++ wls-common/testing/src/test/java/de/muenchen/oss/wahllokalsystem/wls/common/testing/archunit/utilityClasses/incorrectFilenamingAndDependencies/service/ExampleMODEL.java
@@ -3,8 +3,4 @@ package de.muenchen.oss.wahllokalsystem.wls.common.testing.archunit.utilityClass
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
-public record ExampleMODEL() {
-    @Id
-    private static Long id;
-}
+@Entity public record ExampleMODEL(){@Id private static Long id;}

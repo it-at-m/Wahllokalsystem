17d5367c759788e4657ef2e6ba03c2efb605798b
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-07 08:55:38 +0100

    #767 update broadcast service

--- wls-broadcast-service/src/test/java/de/muenchen/oss/wahllokalsystem/broadcastservice/archunit/ArchUnitTest.java
+++ wls-broadcast-service/src/test/java/de/muenchen/oss/wahllokalsystem/broadcastservice/archunit/ArchUnitTest.java
@@ -5,30 +5,29 @@ import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
 import de.muenchen.oss.wahllokalsystem.broadcastservice.MicroServiceApplication;
 import de.muenchen.oss.wahllokalsystem.wls.common.testing.archunit.rule.MethodRules;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
 public class ArchUnitTest {
 
-    private JavaClasses serviceClasses;
+    private JavaClasses allServiceClasses;
 
     @BeforeEach
     void setUp() {
-        serviceClasses = new ClassFileImporter().importPackages(MicroServiceApplication.class.getPackage().getName());
+        allServiceClasses = new ClassFileImporter().importPackages(MicroServiceApplication.class.getPackage().getName());
     }
 
     @ParameterizedTest(name = "{0}")
-    @MethodSource("rulesToVerify")
+    @MethodSource("allServiceClassesRulesToVerify")
     void should_verifyArchUnitRuleForAllClassesOfService_when_running(final ArgumentsAccessor arguments) {
-        arguments.get(1, ArchRule.class).check(serviceClasses);
+        arguments.get(1, ArchRule.class).check(allServiceClasses);
     }
 
-    public static Stream<Arguments> rulesToVerify() {
+    public static Stream<Arguments> allServiceClassesRulesToVerify() {
         return Stream.of(
                 Arguments.of("TEST_NAMING_CONVENTION_RULE", MethodRules.RULE_TEST_NAMING_CONVENTION_SHOULD_WHEN_MATCHED));
     }

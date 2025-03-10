fd62a2f532057e0a16a9419778d0d31e30a75ddc
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-10 14:32:58 +0100

    Apply suggestions from code review
    Co-authored-by: Viviane Johns <145964798+vjohnslhm@users.noreply.github.com>

--- wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/rest/wahlbezirke/WahlbezirkeController.java
+++ wls-basisdaten-service/src/main/java/de/muenchen/oss/wahllokalsystem/basisdatenservice/rest/wahlbezirke/WahlbezirkeController.java
@@ -21,7 +21,8 @@ public class WahlbezirkeController {
     private final WahlbezirkDTOMapper wahlbezirkDTOMapper;
 
     @Operation(
-            description = "Laden der Liste der Wahlbezirke, die einem vorgegebenen Wahltag (Parameter wahltagID) entsprechen.", responses = {
+            description = "Laden der Liste der Wahlbezirke, die einem vorgegebenen Wahltag (Parameter wahltagID) entsprechen.", 
+            responses = {
                     @ApiResponse(
                             responseCode = "200", description = "Wahlbezirke erfolgreich zurückgegeben."
                     )

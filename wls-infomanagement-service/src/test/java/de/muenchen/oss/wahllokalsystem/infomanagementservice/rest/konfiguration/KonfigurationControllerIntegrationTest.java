33eee9b76014926c9ccb275e1c5585510c27fa64
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-10 16:53:49 +0100

    #772 add aftereach rule to infomanagement service

--- wls-infomanagement-service/src/test/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/rest/konfiguration/KonfigurationControllerIntegrationTest.java
+++ wls-infomanagement-service/src/test/java/de/muenchen/oss/wahllokalsystem/infomanagementservice/rest/konfiguration/KonfigurationControllerIntegrationTest.java
@@ -57,7 +57,7 @@ public class KonfigurationControllerIntegrationTest {
     KonfigurationModelValidator konfigurationModelValidator;
 
     @AfterEach
-    void tearDown() {
+    void teardown() {
         SecurityUtils.runWith(Authorities.REPOSITORY_DELETE_KONFIGURATION);
         konfigurationRepository.deleteAll();
     }
@@ -211,7 +211,7 @@ public class KonfigurationControllerIntegrationTest {
             val responseBodyDTO = objectMapper.readValue(response.getResponse().getContentAsString(), KennbuchstabenListenDTO.class);
 
             val expectedResponseDTO = new KennbuchstabenListenDTO(List.of(new KennbuchstabenListeDTO(
-                    List.of(new KennbuchstabenDTO(List.of("a", "b", " c")), new KennbuchstabenDTO(List.of("A", "B", "C")))),
+                            List.of(new KennbuchstabenDTO(List.of("a", "b", " c")), new KennbuchstabenDTO(List.of("A", "B", "C")))),
                     new KennbuchstabenListeDTO(List.of(new KennbuchstabenDTO(List.of("1", "2")), new KennbuchstabenDTO(List.of("11", "12"))))));
 
             Assertions.assertThat(responseBodyDTO).isEqualTo(expectedResponseDTO);

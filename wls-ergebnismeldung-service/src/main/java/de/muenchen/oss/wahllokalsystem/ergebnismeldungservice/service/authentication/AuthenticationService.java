f7618912700119eea2cb1b2d440801cc619b7dbe
Nic12345678 <162564162+Nic12345678@users.noreply.github.com>
2025-03-11 14:43:52 +0100

    Variablen Namen gemäß dem Objekt umbenennen authenticationHandler zu authDetailRetriver

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/authentication/AuthenticationService.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/authentication/AuthenticationService.java
@@ -18,12 +18,12 @@ public class AuthenticationService {
     private static final String WAHLBEZIRK_ART_USER_DETAIL_KEY = "wahlbezirksArt";
 
     private final ExceptionFactory exceptionFactory;
-    private final Collection<AuthDetailRetriever> authenticationHandlers;
+    private final Collection<AuthDetailRetriever> authDetailRetrivers;
 
     public WahlbezirkArtModel getWahlbezirkArtOfCurrentAuthenticationOrThrow() throws FachlicheWlsException {
         val currentAuthentication = SecurityContextHolder.getContext().getAuthentication();
-        val authenticationHandler = authenticationHandlers.stream().filter(handler -> handler.canHandle(currentAuthentication)).findFirst();
-        val wahlbezirkOfUser = authenticationHandler
+        val authDetailRetriver = authDetailRetrivers.stream().filter(handler -> handler.canHandle(currentAuthentication)).findFirst();
+        val wahlbezirkOfUser = authDetailRetriver
                 .flatMap(handler -> handler.getDetail(WAHLBEZIRK_ART_USER_DETAIL_KEY, currentAuthentication))
                 .map(WahlbezirkArtModel::valueOf);
 

5d3dc94c83cd74d073c8b7a98d7d217f7e59c4d4
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-13 17:03:55 +0100

    #769 add tests end with test rule to admin service

--- wls-admin-service/src/test/java/de/muenchen/oss/wahllokalsystem/adminservice/archunit/ArchUnitTest.java
+++ wls-admin-service/src/test/java/de/muenchen/oss/wahllokalsystem/adminservice/archunit/ArchUnitTest.java
@@ -71,7 +71,9 @@ public class ArchUnitTest {
     private static Stream<Arguments> allServiceClassesRulesToVerify() {
         return Stream.of(
                 Arguments.of("RULE_NESTED_TESTSUITE_HAS_CORRESPONDING_PUBLIC_METHOD_CONVENTION_MATCHED",
-                        ClassRules.RULE_NESTED_TESTSUITE_HAS_CORRESPONDING_PUBLIC_METHOD_CONVENTION_MATCHED));
+                        ClassRules.RULE_NESTED_TESTSUITE_HAS_CORRESPONDING_PUBLIC_METHOD_CONVENTION_MATCHED),
+                Arguments.of("RULE_TESTCLASSES_END_WITH_TEST_CONVENTION_MATCHED",
+                        MethodRules.RULE_TESTCLASSES_END_WITH_TEST_CONVENTION_MATCHED));
     }
 
     public static Stream<Arguments> allClassesOfRestRulesToVerify() {

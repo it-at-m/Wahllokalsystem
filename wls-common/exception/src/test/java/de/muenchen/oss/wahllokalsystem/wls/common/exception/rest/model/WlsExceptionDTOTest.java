48191e729619dd971b351c3dd0e1cd91be11bd00
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-14 10:06:02 +0100

    #769 undo changes in WlsExceptionDTOTest

--- wls-common/exception/src/test/java/de/muenchen/oss/wahllokalsystem/wls/common/exception/rest/model/WlsExceptionDTOTest.java
+++ wls-common/exception/src/test/java/de/muenchen/oss/wahllokalsystem/wls/common/exception/rest/model/WlsExceptionDTOTest.java
@@ -4,80 +4,87 @@ import de.muenchen.oss.wahllokalsystem.wls.common.exception.util.ExceptionKonsta
 import de.muenchen.oss.wahllokalsystem.wls.common.testing.LoggerExtension;
 import lombok.val;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 class WlsExceptionDTOTest {
 
-    // -- constructor --
-    @Test
-    void should_onlyHaveDefinedConstructorWithNullHandling_when_checkingAvailableConstructors() {
-        Assertions.assertThat(WlsExceptionDTO.class.getConstructors().length).withFailMessage("es sollte nur den überschrieben Konstruktur geben")
-                .isEqualTo(1);
-    }
+    @Nested
+    class Constructor {
 
-    @Test
-    void should_useDefaultValues_when_isCalledWithNullArguments() {
-        val result = new WlsExceptionDTO(null, null, null, null);
+        @Test
+        void should_onlyHaveDefinedConstructorWithNullHandling_when_checkingAvailableConstructors() {
+            Assertions.assertThat(WlsExceptionDTO.class.getConstructors().length).withFailMessage("es sollte nur den überschrieben Konstruktur geben")
+                    .isEqualTo(1);
+        }
 
-        val expectedResult = new WlsExceptionDTO(WlsExceptionCategory.T, ExceptionKonstanten.CODE_ALLGEMEIN_UNBEKANNT,
-                ExceptionKonstanten.SERVICE_UNBEKANNT,
-                ExceptionKonstanten.MESSAGE_UNBEKANNTER_FEHLER);
+        @Test
+        void should_useDefaultValues_when_isCalledWithNullArguments() {
+            val result = new WlsExceptionDTO(null, null, null, null);
 
-        Assertions.assertThat(result).isEqualTo(expectedResult);
-    }
+            val expectedResult = new WlsExceptionDTO(WlsExceptionCategory.T, ExceptionKonstanten.CODE_ALLGEMEIN_UNBEKANNT,
+                    ExceptionKonstanten.SERVICE_UNBEKANNT,
+                    ExceptionKonstanten.MESSAGE_UNBEKANNTER_FEHLER);
 
-    @Test
-    void should_assignParametersCorrectly_when_calledWithParameters() {
-        val category = WlsExceptionCategory.I;
-        val code = "089233";
-        val service = "WLS3.0 TestService";
-        val message = "Lorem ipsum";
+            Assertions.assertThat(result).isEqualTo(expectedResult);
+        }
 
-        val result = new WlsExceptionDTO(category, code, service, message);
+        @Test
+        void should_assignParametersCorrectly_when_calledWithParameters() {
+            val category = WlsExceptionCategory.I;
+            val code = "089233";
+            val service = "WLS3.0 TestService";
+            val message = "Lorem ipsum";
 
-        Assertions.assertThat(result.category()).isEqualTo(category);
-        Assertions.assertThat(result.code()).isEqualTo(code);
-        Assertions.assertThat(result.message()).isEqualTo(message);
-        Assertions.assertThat(result.service()).isEqualTo(service);
-    }
+            val result = new WlsExceptionDTO(category, code, service, message);
 
-    // -- logging events --
-    @RegisterExtension
-    public LoggerExtension loggerExtension = new LoggerExtension();
+            Assertions.assertThat(result.category()).isEqualTo(category);
+            Assertions.assertThat(result.code()).isEqualTo(code);
+            Assertions.assertThat(result.message()).isEqualTo(message);
+            Assertions.assertThat(result.service()).isEqualTo(service);
+        }
 
-    @Test
-    void should_creteNoLoggEvents_when_allParametersArNonNull() {
-        new WlsExceptionDTO(WlsExceptionCategory.T, "code", "service", "message");
+        @Nested
+        class TestLoggingEvents {
 
-        Assertions.assertThat(loggerExtension.getFormattedMessages().isEmpty()).isTrue();
-    }
+            @RegisterExtension
+            public LoggerExtension loggerExtension = new LoggerExtension();
 
-    @Test
-    void should_createWarn_when_categoryIsNull() {
-        new WlsExceptionDTO(null, "code", "service", "message");
+            @Test
+            void should_creteNoLoggEvents_when_allParametersArNonNull() {
+                new WlsExceptionDTO(WlsExceptionCategory.T, "code", "service", "message");
 
-        Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
-    }
+                Assertions.assertThat(loggerExtension.getFormattedMessages().isEmpty()).isTrue();
+            }
 
-    @Test
-    void should_createWarn_when_codeIsNull() {
-        new WlsExceptionDTO(WlsExceptionCategory.T, null, "service", "message");
+            @Test
+            void should_createWarn_when_categoryIsNull() {
+                new WlsExceptionDTO(null, "code", "service", "message");
 
-        Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
-    }
+                Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
+            }
 
-    @Test
-    void should_createWarn_when_serviceIsNull() {
-        new WlsExceptionDTO(WlsExceptionCategory.T, "code", null, "message");
+            @Test
+            void should_createWarn_when_codeIsNull() {
+                new WlsExceptionDTO(WlsExceptionCategory.T, null, "service", "message");
 
-        Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
-    }
+                Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
+            }
 
-    @Test
-    void should_createWarn_when_messageIsNull() {
-        new WlsExceptionDTO(WlsExceptionCategory.T, "code", "service", null);
+            @Test
+            void should_createWarn_when_serviceIsNull() {
+                new WlsExceptionDTO(WlsExceptionCategory.T, "code", null, "message");
 
-        Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
+                Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
+            }
+
+            @Test
+            void should_createWarn_when_messageIsNull() {
+                new WlsExceptionDTO(WlsExceptionCategory.T, "code", "service", null);
+
+                Assertions.assertThat(loggerExtension.getFormattedMessages().size()).isEqualTo(1);
+            }
+        }
     }
 }

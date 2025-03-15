ac0c712413f552e60323c448decfe11219c72ede
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-15 17:23:07 +0100

    added uuid dependency

--- wls-gui-wahllokalsystem/src/stores/useUserNotificationStore.ts
+++ wls-gui-wahllokalsystem/src/stores/useUserNotificationStore.ts
@@ -1,9 +1,8 @@
-import { randomUUID } from "node:crypto";
-
 import type { UserNotification } from "@/types/wlsTypes/UserNotification.ts";
 import type { UserNotificationTypeEnum } from "@/types/wlsTypes/UserNotificationTypeEnum.ts";
 
 import { defineStore } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 
 export const useUserNotificationStore = defineStore("notification", {
   state: () => ({
@@ -12,7 +11,7 @@ export const useUserNotificationStore = defineStore("notification", {
 
   actions: {
     addNotification(message: string, category: UserNotificationTypeEnum) {
-      const id = randomUUID();
+      const id = uuidv4();
       const newNotification: UserNotification = {
         message,
         category,

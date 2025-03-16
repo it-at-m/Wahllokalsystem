67f875108ce182b4ebf33f7b9639b1facc6f1714
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-16 14:33:07 +0100

    changes ui and added description

--- wls-gui-wahllokalsystem/src/views/ExampleToastView.vue
+++ wls-gui-wahllokalsystem/src/views/ExampleToastView.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>
-    <v-btn @click="toastErfolg">Erfolg</v-btn>
-    <v-btn @click="toastWarnung">WARNUNG</v-btn>
-    <v-btn @click="toastError">FEHLER</v-btn>
-  </div>
+  <v-container class="d-flex flex-column align-center">
+    <p>This view shows how the different Toast-Notifications look and work.</p>
+    <v-container
+      class="d-flex flex-column align-center"
+      min-height="200"
+      min-width="300"
+    >
+      <v-btn
+        class="mb-5"
+        @click="toastErfolg"
+        >Erfolg
+      </v-btn>
+      <v-btn
+        class="mb-5"
+        @click="toastWarnung"
+        >WARNUNG
+      </v-btn>
+      <v-btn @click="toastError">FEHLER</v-btn>
+    </v-container>
+  </v-container>
 </template>
 <script setup lang="ts">
-import { VBtn } from "vuetify/components";
+import { VBtn, VContainer } from "vuetify/components";
 
 import { useUserNotificationStore } from "@/stores/useUserNotificationStore.ts";
 import { UserNotificationCategoryEnum } from "@/types/wlsTypes/UserNotificationCategoryEnum.ts";

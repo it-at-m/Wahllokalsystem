7f332d8f4357030bcdd3929ae542251ae63fa14a
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-16 13:25:29 +0100

    refactoring

--- wls-gui-wahllokalsystem/src/main.ts
+++ wls-gui-wahllokalsystem/src/main.ts
@@ -1,5 +1,3 @@
-import type { ToastContainerOptions } from "vue3-toastify";
-
 import { createApp } from "vue";
 import Vue3Toastify from "vue3-toastify";
 
@@ -8,9 +6,7 @@ import "vue3-toastify/dist/index.css";
 import App from "@/App.vue";
 import { registerPlugins } from "@/plugins";
 
-const app = createApp(App).use(Vue3Toastify, {
-  autoClose: 4000,
-} as ToastContainerOptions);
+const app = createApp(App).use(Vue3Toastify);
 
 registerPlugins(app);
 
--- wls-gui-wahllokalsystem/src/stores/useUserNotificationStore.ts
+++ wls-gui-wahllokalsystem/src/stores/useUserNotificationStore.ts
@@ -1,9 +1,11 @@
 import type { UserNotification } from "@/types/wlsTypes/UserNotification.ts";
-import type { UserNotificationCategoryEnum } from "@/types/wlsTypes/UserNotificationCategoryEnum.ts";
 
 import { defineStore } from "pinia";
 import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
+import { toast } from "vue3-toastify";
+
+import { UserNotificationCategoryEnum } from "@/types/wlsTypes/UserNotificationCategoryEnum.ts";
 
 export const useUserNotificationStore = defineStore("userNotification", () => {
   const userNotifications = ref([] as UserNotification[]);
@@ -19,6 +21,37 @@ export const useUserNotificationStore = defineStore("userNotification", () => {
       id,
     };
     userNotifications.value.push(newUserNotification);
+    displayToast(newUserNotification);
+  }
+
+  function displayToast(newUserNotification: UserNotification) {
+    const toastPosition = toast.POSITION.BOTTOM_LEFT;
+    switch (newUserNotification.category) {
+      case UserNotificationCategoryEnum.ERFOLG:
+        toast.success(newUserNotification.message, {
+          autoClose: 5000,
+          toastId: newUserNotification.id,
+          position: toastPosition,
+          onClose: () => removeNotification(newUserNotification.id),
+        });
+        break;
+      case UserNotificationCategoryEnum.WARNUNG:
+        toast.warning(newUserNotification.message, {
+          autoClose: 5000,
+          toastId: newUserNotification.id,
+          position: toastPosition,
+          onClose: () => removeNotification(newUserNotification.id),
+        });
+        break;
+      case UserNotificationCategoryEnum.FEHLER:
+        toast.error(newUserNotification.message, {
+          autoClose: false,
+          toastId: newUserNotification.id,
+          position: toastPosition,
+          onClose: () => removeNotification(newUserNotification.id),
+        });
+        break;
+    }
   }
 
   function removeNotification(id: string) {

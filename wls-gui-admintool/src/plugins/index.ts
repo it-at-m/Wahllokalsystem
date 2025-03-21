8dad0ff7e61a456954e44f5b8d8e940498661094
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-21 09:39:40 +0100

    moved from admin to wahllokalsystem

--- wls-gui-admintool/src/plugins/index.ts
+++ wls-gui-admintool/src/plugins/index.ts
@@ -1,11 +1,9 @@
 import type { App } from "vue";
 
-import Vue3Toastify from "vue3-toastify";
-
 import pinia from "@/plugins/pinia";
 import router from "@/plugins/router";
 import vuetify from "@/plugins/vuetify";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify).use(router).use(pinia).use(Vue3Toastify);
+  app.use(vuetify).use(router).use(pinia);
 }

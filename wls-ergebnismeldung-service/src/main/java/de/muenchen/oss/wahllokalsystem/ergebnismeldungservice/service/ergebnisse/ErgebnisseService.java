234411cc044a656d580fa9f0a2b8267bca921fd5
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-03-21 12:31:15 +0100

    fix log message to match the methodname

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnisse/ErgebnisseService.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnisse/ErgebnisseService.java
@@ -45,7 +45,7 @@ public class ErgebnisseService {
                 + " and @bezirkIdPermissionEvaluator.tokenUserBezirkIdMatches(#wahlbezirkID, authentication)"
     )
     public List<ErgebnisseModel> getAllErgebnisse(@NotNull final String wahlID, @P("wahlbezirkID") @NotNull final String wahlbezirkID) {
-        log.info("#getErgebnisse");
+        log.info("#getAllErgebnisse");
 
         ergebnisseValidator.validIDOrThrow(wahlID, wahlbezirkID,
                 exceptionFactory.createFachlicheWlsException(ExceptionConstants.GET_ERGEBNISSE_PARAMETER_UNVOLLSTAENDIG));

38e1690e6bed1d8be5464257b988fc194b741ad4
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-21 08:38:39 +0100

    moved use of toastify

--- wls-gui-wahllokalsystem/src/main.ts
+++ wls-gui-wahllokalsystem/src/main.ts
@@ -6,8 +6,8 @@ import "vue3-toastify/dist/index.css";
 import App from "@/App.vue";
 import { registerPlugins } from "@/plugins";
 
-const app = createApp(App).use(Vue3Toastify, {});
+const app = createApp(App);
 
-registerPlugins(app);
+registerPlugins(app.use(Vue3Toastify, {}));
 
 app.mount("#app");

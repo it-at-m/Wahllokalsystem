3e1c3e0178502527b556a9407f6a81ad21a51ad3
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-24 16:20:57 +0100

    #904 update storybook config

--- wls-gui-admintool/.storybook/main.ts
+++ wls-gui-admintool/.storybook/main.ts
@@ -1,7 +1,11 @@
+import * as path from "node:path";
+
 import type { StorybookConfig } from "@storybook/vue3-vite";
 
+import { mergeConfig } from "vitest/config";
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
@@ -12,5 +16,19 @@ const config: StorybookConfig = {
     name: "@storybook/vue3-vite",
     options: {},
   },
+  core: {
+    builder: "@storybook/builder-vite",
+    disableTelemetry: true,
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "../src"),
+        },
+      },
+    });
+  },
 };
+
 export default config;
--- wls-gui-admintool/.storybook/preview.ts
+++ wls-gui-admintool/.storybook/preview.ts
@@ -1,6 +1,16 @@
 import type { Preview } from "@storybook/vue3";
 
+import { setup } from "@storybook/vue3";
+
+import { registerPlugins } from "../src/plugins";
+
+setup((app) => {
+  // Registers your app's plugins into Storybook
+  registerPlugins(app);
+});
+
 const preview: Preview = {
+  tags: ["autodocs"],
   parameters: {
     controls: {
       matchers: {

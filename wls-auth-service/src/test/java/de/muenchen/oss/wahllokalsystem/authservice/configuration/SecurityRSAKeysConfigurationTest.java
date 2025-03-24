aa5db246bf2b8ffff955027f62188dedeaa7eb8b
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-03-24 15:05:32 +0100

    renamerename

--- wls-auth-service/src/test/java/de/muenchen/oss/wahllokalsystem/authservice/configuration/SecurityRSAKeysConfigurationTest.java
+++ wls-auth-service/src/test/java/de/muenchen/oss/wahllokalsystem/authservice/configuration/SecurityRSAKeysConfigurationTest.java
@@ -9,13 +9,13 @@ import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.boot.SpringApplication;
 
-public class SecurityConfigurationRSAKeysTest {
+class SecurityRSAKeysConfigurationTest {
 
     @Nested
-    class RSAKeyConfiguredTests {
+    class RSAKeyConfigured {
 
         @Test
-        void should_fail_when_privateKeyIsMissing() {
+        void should_failStartup_when_privateKeyIsMissing() {
             System.setProperty("spring.profiles.active", "test,dummy.ldap");
             System.setProperty("service.config.oauth2.jwk.rsa.init.seed", "seed");
             System.setProperty("service.config.rsa.rsaKeySetting", "STATIC_KEY");
@@ -26,7 +26,7 @@ public class SecurityConfigurationRSAKeysTest {
         }
 
         @Test
-        void should_fail_when_publicKeyIsMissing() {
+        void should_failStartup_when_publicKeyIsMissing() {
             System.setProperty("spring.profiles.active", "test,dummy.ldap");
             System.setProperty("service.config.oauth2.jwk.rsa.init.seed", "seed");
             System.setProperty("service.config.rsa.rsaKeySetting", "STATIC_KEY");
@@ -37,7 +37,7 @@ public class SecurityConfigurationRSAKeysTest {
         }
 
         @Test
-        void should_fail_when_publicAndPrivateKeyAreMissing() {
+        void should_failStartup_when_publicAndPrivateKeyAreMissing() {
             System.setProperty("spring.profiles.active", "test,dummy.ldap");
             System.setProperty("service.config.oauth2.jwk.rsa.init.seed", "seed");
             System.setProperty("service.config.rsa.rsaKeySetting", "STATIC_KEY");
@@ -69,9 +69,9 @@ public class SecurityConfigurationRSAKeysTest {
     }
 
     @Nested
-    class RSAKeyNotConfiguredTests {
+    class RSAKeyNotConfigured {
         @Test
-        void should_fail_when_defaultRSASettingsAreUsed() {
+        void should_failStartup_when_defaultRSASettingsAreUsed() {
             System.setProperty("spring.profiles.active", "dummy.ldap,testWithoutRSAKeys");
             System.setProperty("service.config.oauth2.jwk.rsa.init.seed", "seed");
             Throwable exception = Assertions.catchThrowableOfType(

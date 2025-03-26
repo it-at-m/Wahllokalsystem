ecd06e5504da9042c82fbc8326b5b00cd156480a
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-26 16:54:19 +0100

    #970 :bulb:

--- wls-gui-admintool/src/components/common/BaseAutocompleteWahltag.vue
+++ wls-gui-admintool/src/components/common/BaseAutocompleteWahltag.vue
@@ -14,9 +14,6 @@ import type { WahltagDTO } from "@/api/wls-clients/generated-admin-api";
 import { VAutocomplete } from "vuetify/components";
 
 const props = defineProps({
-  /**
-   * Wahltage to select
-   */
   items: {
     type: Array<WahltagDTO>,
     required: true,

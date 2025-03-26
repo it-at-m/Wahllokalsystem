7445eba58f153aca2db08440a5a7f3d34d8149b9
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-26 16:09:35 +0100

    #970 :white_check_mark: check for emitted wahltag + update snapshots

--- wls-gui-admintool/tests/components/common/BaseAutocompleteWahltag.spec.ts
+++ wls-gui-admintool/tests/components/common/BaseAutocompleteWahltag.spec.ts
@@ -26,8 +26,26 @@ describe("BaseAutocompleteWahltag.vue", () => {
     wrapper = mount(BaseAutocompleteWahltag, {
       global: { plugins: [pinia, vuetify] },
       props: {
-        items: [],
-        multiple: true,
+        items: [
+          {
+            wahltagID: "1",
+            wahltag: "27.02.1988",
+            beschreibung: "Sebastian",
+            nummer: "1.1",
+          },
+          {
+            wahltagID: "2",
+            wahltag: "31.01.1998",
+            beschreibung: "Daniel",
+            nummer: "2.1",
+          },
+          {
+            wahltagID: "3",
+            wahltag: "04.04.2000",
+            beschreibung: "Viviane",
+            nummer: "3.1",
+          },
+        ],
       },
     });
   });
@@ -36,6 +54,23 @@ describe("BaseAutocompleteWahltag.vue", () => {
 
   describe("visual logic", () => {
     it("should_renderAutocompleteWithCorrectLabel_when_componentIsMounted", async (context) => {
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+  });
+
+  describe("behavioral logic", () => {
+    it("should_emitWahltag_when_elementIsSelected", async (context) => {
+      await wrapper.setProps({
+        modelValue: {
+          wahltagID: "2",
+          wahltag: "31.01.1998",
+          beschreibung: "Daniel",
+          nummer: "2.1",
+        },
+      });
+
       await expect(wrapper.html()).toMatchFileSnapshot(
         getSnapshotFilename(context)
       );

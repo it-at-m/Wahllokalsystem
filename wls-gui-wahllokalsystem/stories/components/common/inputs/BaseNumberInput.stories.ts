2de6334600a8de6bbc16abe0065159c4af957964
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-28 14:47:59 +0100

    #1078 add rules for min + max number + reformat

--- wls-gui-wahllokalsystem/stories/components/common/inputs/BaseNumberInput.stories.ts
+++ wls-gui-wahllokalsystem/stories/components/common/inputs/BaseNumberInput.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 import { fn } from "@storybook/test";
 
 import BaseNumberInput from "@/components/common/inputs/BaseNumberInput.vue";
-import { REQUIRED } from "@/util/rules.ts";
+import { MAX_NUMBER, MIN_NUMBER, REQUIRED } from "@/util/rules.ts";
 
 const meta: Meta<typeof BaseNumberInput> = {
   component: BaseNumberInput,
@@ -57,6 +57,21 @@ export const Required: Story = {
   args: {
     ...Default.args,
     rules: [REQUIRED],
+  },
+};
+
+/**
+ * Input muss zwischen 5 und 10 liegen
+ * ```
+ * const rules = [MIN_NUMBER(5), MAX_NUMBER(10)]
+ * const MIN_NUMBER = (min: number) => (value: number) => value >= min || "Eingabe darf nicht kleiner als ${min} sein."
+ * const MAX_NUMBER (max: number) => (value: number) => value <= max || "Eingabe darf nicht größer als ${max} sein."
+ * ```
+ */
+export const InputRange: Story = {
+  args: {
+    ...Default.args,
+    rules: [MAX_NUMBER(10), MIN_NUMBER(5)],
   },
 };
 

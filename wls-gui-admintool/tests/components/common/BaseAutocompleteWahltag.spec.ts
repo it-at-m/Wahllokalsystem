e3003db989f4d8399e0b8977498b5f79d6d85b95
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-03-28 09:59:01 +0100

    #970 :white_check_mark: update tests

--- wls-gui-admintool/tests/components/common/BaseAutocompleteWahltag.spec.ts
+++ wls-gui-admintool/tests/components/common/BaseAutocompleteWahltag.spec.ts
@@ -3,11 +3,33 @@ import { createPinia } from "pinia";
 import { afterEach, beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
+import { VAutocomplete } from "vuetify/components";
 import * as directives from "vuetify/directives";
 
 import BaseAutocompleteWahltag from "@/components/common/BaseAutocompleteWahltag.vue";
 import pinia from "@/plugins/pinia";
 import { getSnapshotFilename } from "../../utils/testutils.ts";
+
+const wahltage = [
+  {
+    wahltagID: "1",
+    wahltag: "24.12.2025",
+    beschreibung: "Weihnachten",
+    nummer: "1.1",
+  },
+  {
+    wahltagID: "2",
+    wahltag: "01.01.2025",
+    beschreibung: "Neujahr",
+    nummer: "2.1",
+  },
+  {
+    wahltagID: "3",
+    wahltag: "20.03.2025",
+    beschreibung: "Frühlingsanfang",
+    nummer: "3.1",
+  },
+];
 
 describe("BaseAutocompleteWahltag.vue", () => {
   let vuetify: ReturnType<typeof createVuetify>;
@@ -26,44 +48,7 @@ describe("BaseAutocompleteWahltag.vue", () => {
     wrapper = mount(BaseAutocompleteWahltag, {
       global: { plugins: [pinia, vuetify] },
       props: {
-        items: [
-          {
-            wahltagID: "1",
-            wahltag: "27.02.1988",
-            beschreibung: "S",
-            nummer: "1.1",
-          },
-          {
-            wahltagID: "2",
-            wahltag: "31.01.1998",
-            beschreibung: "D",
-            nummer: "2.1",
-          },
-          {
-            wahltagID: "3",
-            wahltag: "04.04.2000",
-            beschreibung: "V",
-            nummer: "3.1",
-          },
-          {
-            wahltagID: "4",
-            wahltag: "31.10.1999",
-            beschreibung: "G",
-            nummer: "4.1",
-          },
-          {
-            wahltagID: "5",
-            wahltag: "05.11.",
-            beschreibung: "R",
-            nummer: "5.1",
-          },
-          {
-            wahltagID: "6",
-            wahltag: "14.05.1977",
-            beschreibung: "N",
-            nummer: "6.1",
-          },
-        ],
+        items: wahltage,
       },
     });
   });
@@ -80,36 +65,26 @@ describe("BaseAutocompleteWahltag.vue", () => {
 
   describe("visual logic", () => {
     it("should_renderDateOfWahltag_when_elementIsSelected", async (context) => {
-      const wahltag = {
-        wahltagID: "2",
-        wahltag: "31.01.1998",
-        beschreibung: "D",
-        nummer: "2.1",
-      };
-      await wrapper.setProps({ modelValue: wahltag });
+      const tag = wahltage[0];
+      await wrapper.setProps({ modelValue: tag });
 
       await expect(wrapper.html()).toMatchFileSnapshot(
         getSnapshotFilename(context)
       );
-      expect(wrapper.html()).toContain(wahltag.wahltag);
-      expect(wrapper.html()).not.toContain(wahltag.beschreibung);
+      expect(wrapper.html()).toContain(tag.wahltag);
+      expect(wrapper.html()).not.toContain(tag.beschreibung);
     });
   });
 
   describe("behavioral logic", () => {
-    it("should_emitWahltag_when_elementIsSelected", async (context) => {
-      await wrapper.setProps({
-        modelValue: {
-          wahltagID: "2",
-          wahltag: "31.01.1998",
-          beschreibung: "D",
-          nummer: "2.1",
-        },
-      });
+    it("should_emitWahltag_when_elementIsSelected", async () => {
+      const tag = wahltage[2];
 
-      await expect(wrapper.html()).toMatchFileSnapshot(
-        getSnapshotFilename(context)
-      );
+      const autocomplete = wrapper.findComponent(VAutocomplete);
+      await autocomplete.setValue(tag);
+
+      expect(wrapper.emitted()).toHaveProperty("update:modelValue");
+      expect(wrapper.emitted("update:modelValue")).toEqual([[tag]]);
     });
   });
 });

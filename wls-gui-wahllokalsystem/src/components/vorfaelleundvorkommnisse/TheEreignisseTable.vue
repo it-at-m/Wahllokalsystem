5c7cca2a39814658cdcf39915551b90e342170fb
Robert Jasny <robert.jasny@muenchen.de>
2025-03-31 18:06:29 +0200

    draft: view and table for ereignisse

---
+++ wls-gui-wahllokalsystem/src/components/vorfaelleundvorkommnisse/TheEreignisseTable.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-table data-test="tableWahlvorstandsMitglieder">
+    <thead>
+      <tr>
+        <th>Uhrzeit</th>
+        <th>Beschreibung</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr
+        v-for="(ereignis, index) in wahlbezirkEreignisse.ereigniseintraege"
+        :key="index"
+      >
+        <td data-test="textUhrzeit">{{ ereignis.uhrzeit }}</td>
+        <td data-test="textBeschreibung">{{ ereignis.beschreibung }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { VTable } from "vuetify/components";
+
+import { useEreignisStore } from "@/stores/vorfaelleundvorkommnisseStore.ts";
+
+const ereignisStore = useEreignisStore();
+const wahlbezirkEreignisse = computed(() => ereignisStore.wahlbezirkEreignisse);
+</script>
---
+++ wls-gui-wahllokalsystem/src/views/EreignisseView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="text-h1">Dokumentation eingetretener Ereignisse</div>
+
+    <the-ereignisse-table />
+
+    <base-button-save @click="ereignisStore.sendEreignisse()" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
+import TheEreignisseTable from "@/components/vorfaelleundvorkommnisse/TheEreignisseTable.vue";
+import { useEreignisStore } from "@/stores/vorfaelleundvorkommnisseStore.ts";
+
+const ereignisStore = useEreignisStore();
+
+onMounted(() => {
+  loadEreignisse();
+});
+
+/**
+ * Loads Ereignisse from the backend and sets it in the store.
+ */
+function loadEreignisse(): void {
+  ereignisStore.loadEreignisse();
+}
+</script>

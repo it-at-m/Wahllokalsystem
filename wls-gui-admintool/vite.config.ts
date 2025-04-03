88824598c8a3920b05e7c454b4535e5ad5b6f8b1
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-04-03 16:20:52 +0200

    added second alias for tests directory

--- wls-gui-admintool/vite.config.ts
+++ wls-gui-admintool/vite.config.ts
@@ -50,6 +50,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@tests": fileURLToPath(new URL("./tests", import.meta.url)),
       },
     },
   };

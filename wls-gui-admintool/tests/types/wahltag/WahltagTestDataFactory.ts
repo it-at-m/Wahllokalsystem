222f27d7e196f1df3a5c5cd723d18c08a6b72d8a
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-04-03 18:24:59 +0200

    created tests for wahltagService

--- wls-gui-admintool/tests/types/wahltag/WahltagTestDataFactory.ts
+++ wls-gui-admintool/tests/types/wahltag/WahltagTestDataFactory.ts
@@ -1,4 +1,6 @@
 import type { WahltagDTO } from "@/api/wls-clients/generated-admin-api";
+import type { Wahltag } from "@/types/wahltag/Wahltag.ts";
+import type { WahltagEvent } from "@/types/wahltag/WahltagEvent.ts";
 import type { Builder } from "@tests/common/Builder.ts";
 
 import { proxyBuilder } from "@tests/common/Builder.ts";
@@ -17,7 +19,28 @@ export function useWahltagTestDataFactory() {
     });
   }
 
+  function createWahltagComplete(countEvents: number = 3): Wahltag {
+    const wahltagEvents: WahltagEvent[] = [];
+    for (let i = 0; i < countEvents; i++) {
+      wahltagEvents.push(createWahltagEventComplete());
+    }
+    return {
+      wahltag: generateDateRandomAsString(),
+      events: wahltagEvents,
+    };
+  }
+
+  /* internal only */
+  function createWahltagEventComplete(): WahltagEvent {
+    return {
+      nummer: `${generateNumberRandom(2)}`,
+      wahltagID: `wahltagID${generateNumberRandom(3)}`,
+      beschreibung: `beschreibung${generateNumberRandom(3)}`,
+    };
+  }
+
   return {
+    createWahltagComplete,
     createWahltagDtoComplete,
   };
 }

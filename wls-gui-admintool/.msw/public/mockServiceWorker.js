c300617e85b6fd958407ec2ada8fc85dd2462a7f
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-04-07 18:20:50 +0200

    use other worker directory for msw

--- wls-gui-admintool/.msw/public/mockServiceWorker.js
+++ wls-gui-admintool/.msw/public/mockServiceWorker.js
@@ -132,20 +132,20 @@ async function handleRequest(event, requestId) {
       const responseClone = response.clone()
 
       sendToClient(
-          client,
-          {
-            type: 'RESPONSE',
-            payload: {
-              requestId,
-              isMockedResponse: IS_MOCKED_RESPONSE in response,
-              type: responseClone.type,
-              status: responseClone.status,
-              statusText: responseClone.statusText,
-              body: responseClone.body,
-              headers: Object.fromEntries(responseClone.headers.entries()),
-            },
+        client,
+        {
+          type: 'RESPONSE',
+          payload: {
+            requestId,
+            isMockedResponse: IS_MOCKED_RESPONSE in response,
+            type: responseClone.type,
+            status: responseClone.status,
+            statusText: responseClone.statusText,
+            body: responseClone.body,
+            headers: Object.fromEntries(responseClone.headers.entries()),
           },
-          [responseClone.body],
+        },
+        [responseClone.body],
       )
     })()
   }
@@ -173,15 +173,15 @@ async function resolveMainClient(event) {
   })
 
   return allClients
-      .filter((client) => {
-        // Get only those clients that are currently visible.
-        return client.visibilityState === 'visible'
-      })
-      .find((client) => {
-        // Find the client ID that's recorded in the
-        // set of clients that have registered the worker.
-        return activeClientIds.has(client.id)
-      })
+    .filter((client) => {
+      // Get only those clients that are currently visible.
+      return client.visibilityState === 'visible'
+    })
+    .find((client) => {
+      // Find the client ID that's recorded in the
+      // set of clients that have registered the worker.
+      return activeClientIds.has(client.id)
+    })
 }
 
 async function getResponse(event, client, requestId) {
@@ -203,7 +203,7 @@ async function getResponse(event, client, requestId) {
     if (acceptHeader) {
       const values = acceptHeader.split(',').map((value) => value.trim())
       const filteredValues = values.filter(
-          (value) => value !== 'msw/passthrough',
+        (value) => value !== 'msw/passthrough',
       )
 
       if (filteredValues.length > 0) {
@@ -232,27 +232,27 @@ async function getResponse(event, client, requestId) {
   // Notify the client that a request has been intercepted.
   const requestBuffer = await request.arrayBuffer()
   const clientMessage = await sendToClient(
-      client,
-      {
-        type: 'REQUEST',
-        payload: {
-          id: requestId,
-          url: request.url,
-          mode: request.mode,
-          method: request.method,
-          headers: Object.fromEntries(request.headers.entries()),
-          cache: request.cache,
-          credentials: request.credentials,
-          destination: request.destination,
-          integrity: request.integrity,
-          redirect: request.redirect,
-          referrer: request.referrer,
-          referrerPolicy: request.referrerPolicy,
-          body: requestBuffer,
-          keepalive: request.keepalive,
-        },
+    client,
+    {
+      type: 'REQUEST',
+      payload: {
+        id: requestId,
+        url: request.url,
+        mode: request.mode,
+        method: request.method,
+        headers: Object.fromEntries(request.headers.entries()),
+        cache: request.cache,
+        credentials: request.credentials,
+        destination: request.destination,
+        integrity: request.integrity,
+        redirect: request.redirect,
+        referrer: request.referrer,
+        referrerPolicy: request.referrerPolicy,
+        body: requestBuffer,
+        keepalive: request.keepalive,
       },
-      [requestBuffer],
+    },
+    [requestBuffer],
   )
 
   switch (clientMessage.type) {
@@ -281,8 +281,8 @@ function sendToClient(client, message, transferrables = []) {
     }
 
     client.postMessage(
-        message,
-        [channel.port2].concat(transferrables.filter(Boolean)),
+      message,
+      [channel.port2].concat(transferrables.filter(Boolean)),
     )
   })
 }
--- wls-gui-admintool/.storybook/main.ts
+++ wls-gui-admintool/.storybook/main.ts
@@ -20,7 +20,7 @@ const config: StorybookConfig = {
     builder: "@storybook/builder-vite",
     disableTelemetry: true,
   },
-  staticDirs: ["msw/gen/public"],
+  staticDirs: ["../.msw/public"],
   async viteFinal(config) {
     return mergeConfig(config, {
       resolve: {

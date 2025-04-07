0aee2feb2c24fb923dbdc12af4c58c5a1e22fe3c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-04-07 09:28:54 +0200

    add stepper to init view to init wahltermindaten for all wahltag events

--- wls-gui-admintool/src/views/InitWahltagView.vue
+++ wls-gui-admintool/src/views/InitWahltagView.vue
@@ -4,23 +4,45 @@
     <v-card-text>
       <div class="d-flex justify-between flex-wrap">
         <base-autocomplete-wahltag
-          v-model="wahltagSelected"
+          :model-value="wahltagSelected"
           :items="wahltage"
           :loading="wahltageAreLoading"
+          @update:model-value="onSelectedWahltagChanged"
         />
         <base-icon-button-refresh
           class="ml-2"
           @click="onRefreshWahltageClicked"
         />
       </div>
-      <div v-if="wahltagSelected">
-        <div
-          v-for="event in wahltagSelected.events"
-          :key="event.wahltagID"
+      <v-stepper
+        v-if="steps.length > 0"
+        v-model="activeStep"
+        :items="steps"
+        item-title="nummer"
+      >
+        <template #title="{ step }"
+          ><div class="mb-1">
+            Wahlnummer: {{ steps[step - 1].nummer }}
+          </div></template
         >
-          {{ event.wahltagID }} - {{ event.beschreibung }} - {{ event.nummer }}
-        </div>
-      </div>
+        <template #subtitle="{ step }">
+          <div>{{ steps[step - 1].beschreibung }}</div>
+        </template>
+        <v-stepper-window>
+          <v-stepper-window-item
+            v-for="(step, index) in steps"
+            :key="step.wahltagID"
+            :value="index + 1"
+          >
+            <base-step-wahltag-init
+              :wahltag-event="step"
+              :wahltermin-daten-exists="
+                konfigurierteWahltage.get(step.wahltagID)
+              "
+            />
+          </v-stepper-window-item>
+        </v-stepper-window>
+      </v-stepper>
     </v-card-text>
   </v-card>
 </template>
@@ -29,18 +51,33 @@
 import type { Wahltag } from "@/types/wahltag/Wahltag.ts";
 import type { Ref } from "vue";
 
-import { onMounted, ref } from "vue";
-import { VCard, VCardText, VCardTitle } from "vuetify/components";
+import { computed, onMounted, ref } from "vue";
+import {
+  VCard,
+  VCardText,
+  VCardTitle,
+  VStepper,
+  VStepperWindow,
+  VStepperWindowItem,
+} from "vuetify/components";
 
 import BaseAutocompleteWahltag from "@/components/common/BaseAutocompleteWahltag.vue";
 import BaseIconButtonRefresh from "@/components/common/BaseIconButtonRefresh.vue";
+import BaseStepWahltagInit from "@/components/wahltag/BaseStepWahltagInit.vue";
 import useWahltagService from "@/composables/wahltag/wahltagService.ts";
 
-const { getWahltage } = useWahltagService();
+const { getWahltage, isKonfigurierterWahltag } = useWahltagService();
 
 const wahltage: Ref<Wahltag[]> = ref([]);
 const wahltagSelected: Ref<Wahltag | undefined> = ref(undefined);
 const wahltageAreLoading = ref(false);
+
+const konfigurierteWahltage: Ref<Map<string, boolean | undefined>> = ref(
+  new Map<string, boolean | undefined>()
+);
+
+const steps = computed(() => wahltagSelected.value?.events ?? []);
+const activeStep = ref(0);
 
 onMounted(() => {
   loadWahltage();
@@ -50,7 +87,26 @@ function onRefreshWahltageClicked() {
   loadWahltage();
 }
 
+function onSelectedWahltagChanged(newSelectedWahltag?: Wahltag) {
+  wahltagSelected.value = newSelectedWahltag;
+  activeStep.value = 0;
+
+  if (newSelectedWahltag) {
+    reloadKonfigurierteWahltageOfDate(newSelectedWahltag);
+  }
+}
+
 async function loadWahltage(): Promise<void> {
   wahltage.value = await getWahltage(wahltageAreLoading);
+}
+
+function reloadKonfigurierteWahltageOfDate(wahltag: Wahltag) {
+  konfigurierteWahltage.value = new Map<string, boolean>();
+
+  wahltag.events.forEach((event) => {
+    isKonfigurierterWahltag(event.wahltagID).then((result) =>
+      konfigurierteWahltage.value.set(event.wahltagID, result)
+    );
+  });
 }
 </script>

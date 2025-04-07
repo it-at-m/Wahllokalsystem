745540499a5391567850675001d59b870d2a4257
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-04-07 22:42:31 +0200

    add tests for BaseDialogWahltagOverrideWahlterminConfirmation

---
+++ wls-gui-admintool/tests/components/wahltag/BaseDialogWahltagOverrideWahlterminConfirmation.spec.ts
@@ -0,0 +1,155 @@
+import { getSnapshotFilename } from "@tests/utils/testutils.ts";
+import { mount, VueWrapper } from "@vue/test-utils";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { nextTick } from "vue";
+
+import BaseDialogWahltagOverrideWahlterminConfirmation from "@/components/wahltag/BaseDialogWahltagOverrideWahlterminConfirmation.vue";
+import vuetify from "@/plugins/vuetify.ts";
+
+describe("BaseDialogWahltagOverrideWahlterminConfirmation.vue", () => {
+  let wrapper: VueWrapper<
+    InstanceType<typeof BaseDialogWahltagOverrideWahlterminConfirmation>
+  >;
+
+  beforeEach(() => {
+    wrapper = setupWrapper();
+  });
+
+  afterEach(() => {
+    cleanUp(wrapper);
+  });
+
+  describe("visual logic", () => {
+    it("should_nothingVisible_when_mountedUnchanged", async (context) => {
+      await expect(document.body.innerHTML).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+    it("should_renderDialog_when_showWasCalled", async (context) => {
+      wrapper.vm.show();
+
+      await wrapper.vm.$nextTick();
+
+      await expect(document.body.innerHTML).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+
+    it("should_hideDialog_when_hideWasCalledAfterShow", async (context) => {
+      wrapper.vm.show();
+      await wrapper.vm.$nextTick();
+      wrapper.vm.hide();
+      await wrapper.vm.$nextTick();
+
+      await expect(document.body.innerHTML).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+
+    it("should_renderValues_when_propertiesAreSet", async (context) => {
+      cleanUp(wrapper);
+
+      wrapper = setupWrapper({
+        requiredConfirmText: "requiredConfirmText",
+      });
+
+      wrapper.vm.show();
+
+      await wrapper.vm.$nextTick();
+
+      await expect(document.body.innerHTML).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+  });
+
+  describe("behavior logic", () => {
+    it("should_emitConfirmDelete_when_confirmButtonClicked", async () => {
+      wrapper.vm.show();
+
+      await wrapper.vm.$nextTick();
+
+      const confirmButton = document.body.querySelector(
+        '[data-test="button-confirm"]'
+      );
+
+      if (!confirmButton) {
+        throw new Error("No confirm button found.");
+      }
+
+      confirmButton.dispatchEvent(new Event("click"));
+
+      await nextTick();
+
+      expect(wrapper.emitted("confirmDelete")).toEqual([[]]);
+    });
+
+    it("should_emitCancelDelete_when_cancelButtonClicked", async () => {
+      wrapper.vm.show();
+
+      await wrapper.vm.$nextTick();
+
+      const confirmButton = document.body.querySelector(
+        '[data-test="button-cancel"]'
+      );
+
+      if (!confirmButton) {
+        throw new Error("No confirm button found.");
+      }
+
+      confirmButton.dispatchEvent(new Event("click"));
+
+      await nextTick();
+
+      expect(wrapper.emitted("cancelDelete")).toEqual([[]]);
+    });
+
+    it("should_enableConfirmButton_when_requiredConfirmTextIsEntered", async () => {
+      const confirmText = "confirmText";
+
+      cleanUp(wrapper);
+
+      wrapper = setupWrapper({
+        requiredConfirmText: confirmText,
+      });
+      wrapper.vm.show();
+      await wrapper.vm.$nextTick();
+
+      const confirmTextField = document.body
+        .querySelector('[data-test="textfield-confirm-text"]')
+        ?.querySelector("input");
+
+      if (!confirmTextField) {
+        throw new Error("No confirm text field found.");
+      }
+
+      confirmTextField.value = confirmText;
+
+      const confirmButton = document.body.querySelector(
+        '[data-test="button-cancel"]'
+      );
+
+      await nextTick();
+
+      if (!confirmButton) {
+        throw new Error("No confirm button found.");
+      }
+
+      expect(confirmButton.hasAttribute("disabled")).toStrictEqual(false);
+    });
+  });
+});
+
+function setupWrapper(props?: Record<string, unknown>) {
+  return mount(BaseDialogWahltagOverrideWahlterminConfirmation, {
+    attachTo: document.body,
+    props: props,
+    global: { plugins: [vuetify] },
+  });
+}
+
+function cleanUp(wrapper: VueWrapper) {
+  wrapper.unmount();
+  document.body.innerHTML = "";
+  document.head.innerHTML = "";
+}

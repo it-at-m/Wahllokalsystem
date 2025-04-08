4a60328cb73a642a38c0e91f66c3a460f900781c
Robert Jasny <robert.jasny@muenchen.de>
2025-04-08 15:07:55 +0200

    declare private methods outside useEreignisStore

--- wls-gui-wahllokalsystem/src/stores/vorfaelleundvorkommnisseStore.ts
+++ wls-gui-wahllokalsystem/src/stores/vorfaelleundvorkommnisseStore.ts
@@ -1,3 +1,4 @@
+import type { Ereignis } from "@/types/vorfaelleundvorkommnisse/Ereignis.ts";
 import type { WahlbezirkEreignisse } from "@/types/vorfaelleundvorkommnisse/WahlbezirkEreignisse.ts";
 
 import { defineStore } from "pinia";
@@ -10,6 +11,21 @@ import { WahlbezirkEreignisseBuilder } from "@/types/vorfaelleundvorkommnisse/Wa
 const { getEreignisse, saveEreignisse } = useEreignisService();
 
 export const storeID = "vorfaelleundvorkommnisse";
+
+// Funktion zum Sortieren der Ereignisse
+export function sortEreignisse(ereigniseintraege: Ereignis[] | undefined) {
+  return ereigniseintraege?.sort((a, b) => {
+    const timeA = a.uhrzeit ? new Date(a.uhrzeit).getTime() : Infinity;
+    const timeB = b.uhrzeit ? new Date(b.uhrzeit).getTime() : Infinity;
+
+    return timeA - timeB;
+  });
+}
+
+// Funktion zum Abrufen der Wahlbezirk-ID des Benutzers
+export function getUsersWahlbezirkID(userStore: any): string | undefined {
+  return userStore.getUser?.wahlbezirkID;
+}
 
 export const useEreignisStore = defineStore(storeID, () => {
   const userStore = useUserStore();
@@ -27,33 +43,19 @@ export const useEreignisStore = defineStore(storeID, () => {
   }
 
   async function loadEreignisse() {
-    const currentUserWahlbezirkID = getUsersWahlbezirkID();
+    const currentUserWahlbezirkID = getUsersWahlbezirkID(userStore);
     if (currentUserWahlbezirkID) {
       wahlbezirkEreignisse.value = await getEreignisse(currentUserWahlbezirkID);
-      sortEreignisse();
+      sortEreignisse(wahlbezirkEreignisse.value.ereigniseintraege);
     }
   }
 
   async function sendEreignisse() {
-    const currentUserWahlbezirkID = getUsersWahlbezirkID();
+    const currentUserWahlbezirkID = getUsersWahlbezirkID(userStore);
     if (currentUserWahlbezirkID) {
-      sortEreignisse();
+      sortEreignisse(wahlbezirkEreignisse.value.ereigniseintraege);
       await saveEreignisse(currentUserWahlbezirkID, wahlbezirkEreignisse.value);
     }
-  }
-
-  function getUsersWahlbezirkID(): string | undefined {
-    return userStore.getUser?.wahlbezirkID;
-  }
-
-  // Funktion zum Sortieren der Ereignisse
-  function sortEreignisse() {
-    return wahlbezirkEreignisse.value.ereigniseintraege?.sort((a, b) => {
-      const timeA = a.uhrzeit ? new Date(a.uhrzeit).getTime() : Infinity;
-      const timeB = b.uhrzeit ? new Date(b.uhrzeit).getTime() : Infinity;
-
-      return timeA - timeB;
-    });
   }
 
   return {

0c0010c6f9e1c635b191f1d56aa3495b9f228a96
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-04-23 17:09:15 +0200

    #984 update card component to show text input + save button

--- wls-gui-wahllokalsystem/src/components/wahlvorbereitung/BaseWahlschliessungCard.vue
+++ wls-gui-wahllokalsystem/src/components/wahlvorbereitung/BaseWahlschliessungCard.vue
@@ -1,13 +1,46 @@
 <template>
   <v-card>
     <v-card-title>Wahlscheine und Uhrzeit Wahlende</v-card-title>
-    <v-card-text
-      >Bitte geben Sie hier die Uhrzeit ein, zu der die Wahl für geschlossen
-      erklärt wurde.</v-card-text
-    >
+    <v-card-text class="text-center mt-2 pb-0">
+      Bitte geben Sie hier die Uhrzeit ein, zu der die Wahl für geschlossen
+      erklärt wurde.
+      <v-form v-model="schliessungsuhrzeitValidForm">
+        <div class="d-flex justify-center mt-2">
+          <base-time-input></base-time-input>
+        </div>
+      </v-form>
+    </v-card-text>
+    <v-card-actions class="justify-center mb-0">
+      <!-- todo: wird noch nicht richtig aktiviert -->
+      <base-button-save
+        color="primary"
+        :disabled="isSaveButtonDisabled"
+        @click="wahlbezirkStore.sendSchliessungsuhrzeit()"
+      ></base-button-save>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { VCard, VCardText, VCardTitle } from "vuetify/components";
+import type { Ref } from "vue";
+
+import { computed, ref } from "vue";
+import {
+  VCard,
+  VCardActions,
+  VCardText,
+  VCardTitle,
+  VForm,
+} from "vuetify/components";
+
+import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
+import BaseTimeInput from "@/components/common/inputs/BaseTimeInput.vue";
+import { useWahlbezirkStore } from "@/stores/wahlbezirkStore.ts";
+
+const schliessungsuhrzeitValidForm: Ref<null | boolean> = ref(null);
+
+const wahlbezirkStore = useWahlbezirkStore();
+const isSaveButtonDisabled = computed(
+  () => schliessungsuhrzeitValidForm.value !== true
+);
 </script>

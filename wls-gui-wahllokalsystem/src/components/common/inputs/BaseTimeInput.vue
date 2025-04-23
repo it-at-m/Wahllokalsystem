3163a6880e697f0d76ff4fa42e7592de7d03721b
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-04-23 17:06:52 +0200

    #984 add time input component

---
+++ wls-gui-wahllokalsystem/src/components/common/inputs/BaseTimeInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-text-field
+    :v-model="toHhMm(schliessungsuhrzeit)"
+    :rules="[REQUIRED]"
+    label="Uhrzeit"
+    type="time"
+    clearable
+    max-width="200"
+    class="centered-input"
+  ></v-text-field>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from "vue";
+
+import { VTextField } from "vuetify/components";
+
+import { useFormatter } from "@/composables/common/formatter.ts";
+import { REQUIRED } from "@/util/rules.ts";
+
+const { toHhMm } = useFormatter();
+
+const schliessungsuhrzeit = defineModel({
+  type: Object as PropType<Date>,
+});
+
+// todo: add rule: schliessungsuhrzeit darf nicht in der zukunft liegen (anderes issue??)
+</script>
+
+<style scoped>
+.centered-input :deep(input) {
+  text-align: center;
+}
+</style>

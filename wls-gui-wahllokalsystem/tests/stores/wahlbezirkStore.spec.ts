babaf47304ab8352db7f8bdca8e1796eafddccf0
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-04-29 15:14:47 +0200

    #984 :white_check_mark: add tests wahlbezirkStore

---
+++ wls-gui-wahllokalsystem/tests/stores/wahlbezirkStore.spec.ts
@@ -0,0 +1,71 @@
+import { createPinia, setActivePinia } from "pinia";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+
+import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlbezirkStore } from "@/stores/wahlbezirkStore.ts";
+import { User } from "@/types/User";
+import { UrnenwahlSchliessungsuhrzeitBuilder } from "@/types/wahlvorbereitung/UrnenwahlSchliessungsuhrzeit.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  postUrnenwahlSchliessungsuhrzeit: vi.fn(),
+}));
+
+vi.mock("@/composables/wahlvorbereitung/wahlvorbereitungService", () => ({
+  useWahlvorbereitungService: () => ({
+    postUrnenwahlSchliessungsuhrzeit:
+      mockDefinitions.postUrnenwahlSchliessungsuhrzeit,
+  }),
+}));
+
+const mockedNow = new Date();
+
+describe("wahlbezirkStore.ts", () => {
+  let unitUnderTest: ReturnType<typeof useWahlbezirkStore>;
+
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    vi.useFakeTimers({
+      now: mockedNow,
+    });
+    unitUnderTest = useWahlbezirkStore();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.useRealTimers();
+  });
+
+  describe("sendSchliessungsuhrzeit", () => {
+    it("should_sendSchliessungsuhrzeit_when_inputAndWahlbezirkIDIsGiven", async () => {
+      const userStore = useUserStore();
+      const wahlbezirkID = "wahlbezirkID";
+      const user = new User();
+      user.wahlbezirkID = wahlbezirkID;
+      userStore.setUser(user);
+
+      const time = mockedNow.toISOString();
+
+      await unitUnderTest.sendSchliessungsuhrzeit(time);
+      expect(
+        mockDefinitions.postUrnenwahlSchliessungsuhrzeit
+      ).toHaveBeenCalledWith(
+        wahlbezirkID,
+        UrnenwahlSchliessungsuhrzeitBuilder.createWithSchliessungsuhrzeit(time)
+      );
+    });
+
+    it("should_sendSchliessungsuhrzeit_when_wahlbezirkIDIsNotGiven", async () => {
+      const userStore = useUserStore();
+      const user = new User();
+      user.wahlbezirkID = undefined;
+      userStore.setUser(user);
+
+      const time = mockedNow.toISOString();
+
+      await unitUnderTest.sendSchliessungsuhrzeit(time);
+      expect(mockDefinitions.postUrnenwahlSchliessungsuhrzeit).toBeCalledTimes(
+        0
+      );
+    });
+  });
+});

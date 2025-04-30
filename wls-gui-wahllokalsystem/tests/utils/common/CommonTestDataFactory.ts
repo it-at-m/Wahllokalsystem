40131e2ca752cdd1d9528f98f8211b69cbb595b5
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-04-30 19:03:51 +0200

    removed unused common testdata factory functions

--- wls-gui-wahllokalsystem/tests/utils/common/CommonTestDataFactory.ts
+++ wls-gui-wahllokalsystem/tests/utils/common/CommonTestDataFactory.ts
@@ -1,18 +1,6 @@
 export function useCommonTestDataFactory() {
-  function generateRandomBoolean() {
-    return Math.random() < 0.5;
-  }
-
   function generateRandomNumber(length: number): number {
     return Math.floor(Math.random() * Math.pow(10, length));
-  }
-
-  function generateRandomDateAsString(): string {
-    const date = new Date();
-
-    date.setDate(date.getDate() - Math.trunc(Math.random() * 100));
-
-    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
   }
 
   function generateRandomDateTimeAsString() {
@@ -23,19 +11,8 @@ export function useCommonTestDataFactory() {
     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}T${date.getHours().toString().padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}`;
   }
 
-  function generateRandomDate(): Date {
-    const date = new Date();
-
-    date.setDate(date.getDate() - Math.trunc(Math.random() * 100));
-
-    return date;
-  }
-
   return {
-    generateRandomBoolean,
     generateRandomNumber,
-    generateRandomDate,
-    generateRandomDateAsString,
     generateRandomDateTimeAsString,
   };
 }

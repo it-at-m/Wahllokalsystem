1392a77a2b942b63b47907910943ee675dcf57d4
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-05-02 15:25:57 +0200

    use store to refs and replace null by undefined check

--- wls-gui-wahllokalsystem/src/components/vorfaelleundvorkommnisse/TheEreignisseNoEventsCheckboxes.vue
+++ wls-gui-wahllokalsystem/src/components/vorfaelleundvorkommnisse/TheEreignisseNoEventsCheckboxes.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <v-checkbox
-      v-model="ereignisStore.wahlbezirkEreignisse.keineVorfaelle"
+      v-model="wahlbezirkEreignisse.keineVorfaelle"
       :disabled="isCheckboxNoVorfaelleDisabled"
       label="Besondere Vorfälle während der Wahlhandlung waren nicht zu verzeichnen."
       hide-details
       data-test="checkboxKeineVorfaelle"
     ></v-checkbox>
     <v-checkbox
-      v-model="ereignisStore.wahlbezirkEreignisse.keineVorkommnisse"
+      v-model="wahlbezirkEreignisse.keineVorkommnisse"
       :disabled="isCheckboxNoVorkommnisseDisabled"
       label="Besondere Vorkommnisse während der Auszählung waren nicht zu verzeichnen."
       hide-details
@@ -18,21 +18,19 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import { computed } from "vue";
 import { VCheckbox } from "vuetify/components";
 
 import { useEreignisStore } from "@/stores/ereignisStore.ts";
 import { useWahlbezirkStore } from "@/stores/wahlbezirkStore.ts";
 
-const ereignisStore = useEreignisStore();
-const wahlbezirkStore = useWahlbezirkStore();
+const { schliessungsUhrzeitSent } = storeToRefs(useWahlbezirkStore());
+const { wahlbezirkEreignisse, hasVorkommnisse, hasVorfaelle } =
+  storeToRefs(useEreignisStore());
 
-const isCheckboxNoVorfaelleDisabled = computed(
-  () => ereignisStore.hasVorfaelle
-);
+const isCheckboxNoVorfaelleDisabled = computed(() => hasVorfaelle.value);
 const isCheckboxNoVorkommnisseDisabled = computed(
-  () =>
-    ereignisStore.hasVorkommnisse ||
-    wahlbezirkStore.schliessungsUhrzeitSent === null
+  () => hasVorkommnisse.value || schliessungsUhrzeitSent.value === undefined
 );
 </script>

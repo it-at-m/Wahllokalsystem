3b530fe6d7c36dd88b05295bee1ac6469791ed26
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-06 15:17:41 +0200

    moved load user

--- wls-gui-wahllokalsystem/src/App.vue
+++ wls-gui-wahllokalsystem/src/App.vue
@@ -165,7 +165,6 @@ import {
   VTooltip,
 } from "vuetify/components";
 
-import { getUser } from "@/api/user-client";
 import TheBroadcastReadConfirmationDialog from "@/components/broadcast/TheBroadcastReadConfirmationDialog.vue";
 import WlsHeartbeat from "@/components/wlsComponents/WlsHeartbeat.vue";
 import { useBroadcastCronjobService } from "@/composables/broadcast/broadcastCronjobService.ts";
@@ -178,20 +177,12 @@ import {
   ROUTE_WAHLVORSTAND,
   TOAST,
 } from "@/constants";
-import { useUserStore } from "@/stores/userStore.ts";
-import { useWahlvorstandStore } from "@/stores/wahlvorstandStore";
-import { User, UserLocalDevelopment } from "@/types/User";
 
-const userStore = useUserStore();
-const wahlvorstandStore = useWahlvorstandStore();
-const { startBroadcastMessageInterval, stopBroadcastMessageInterval } =
-  useBroadcastCronjobService();
 const [drawer, toggleDrawer] = useToggle();
 const isOffline = ref(false);
+const { stopBroadcastMessageInterval } = useBroadcastCronjobService();
 
 onMounted(() => {
-  loadUser();
-
   // config for service worker indexed db (same config as in wahl-worker.js !)
   localforage.config({
     driver: localforage.INDEXEDDB,
@@ -205,26 +196,6 @@ onMounted(() => {
 onUnmounted(() => {
   stopBroadcastMessageInterval();
 });
-
-/**
- * Loads UserInfo from the backend and sets it in the store.
- */
-function loadUser(): void {
-  getUser()
-    .then((user: User) => userStore.setUser(user))
-    .catch(() => {
-      // No user info received, so fallback
-      if (import.meta.env.DEV) {
-        userStore.setUser(UserLocalDevelopment());
-      } else {
-        userStore.setUser(null);
-      }
-    })
-    .then(() => {
-      wahlvorstandStore.loadWahlvorstand();
-      startBroadcastMessageInterval();
-    });
-}
 </script>
 
 <style>
--- wls-gui-wahllokalsystem/src/views/HomeView.vue
+++ wls-gui-wahllokalsystem/src/views/HomeView.vue
@@ -20,16 +20,45 @@
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import { VCol, VContainer, VIcon, VRow } from "vuetify/components";
+import { VContainer } from "vuetify/components";
 
 import { checkHealth } from "@/api/health-client";
+import { getUser } from "@/api/user-client.ts";
+import { useBroadcastCronjobService } from "@/composables/broadcast/broadcastCronjobService.ts";
+import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlenStore } from "@/stores/wahlenStore.ts";
+import { useWahlvorstandStore } from "@/stores/wahlvorstandStore.ts";
 import { HealthState } from "@/types/HealthState";
+import { User, UserLocalDevelopment } from "@/types/User.ts";
 
+const wahlvorstandStore = useWahlvorstandStore();
+const { startBroadcastMessageInterval } = useBroadcastCronjobService();
 const status = ref("DOWN");
+const wahlenStore = useWahlenStore();
+const userStore = useUserStore();
 
 onMounted(() => {
+  loadUser();
   checkHealth().then((content: HealthState) => (status.value = content.status));
 });
+
+function loadUser(): void {
+  getUser()
+    .then((user: User) => userStore.setUser(user))
+    .catch(() => {
+      // No user info received, so fallback
+      if (import.meta.env.DEV) {
+        userStore.setUser(UserLocalDevelopment());
+      } else {
+        userStore.setUser(null);
+      }
+    })
+    .then(() => {
+      wahlvorstandStore.loadWahlvorstand();
+      startBroadcastMessageInterval();
+      wahlenStore.loadWahlen();
+    });
+}
 </script>
 
 <style scoped>

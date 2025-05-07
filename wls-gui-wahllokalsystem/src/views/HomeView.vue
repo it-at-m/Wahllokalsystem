867b4cae941b85e52cec7692003c51e35a932d7e
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-07 11:00:19 +0200

    added progress bars

--- wls-gui-wahllokalsystem/src/views/HomeView.vue
+++ wls-gui-wahllokalsystem/src/views/HomeView.vue
@@ -1,41 +1,64 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="text-h3 font-weight-bold mb-3">
-          Willkommen beim Wahllokalsystem
-        </h1>
-        <v-icon
-          icon="$home"
-          size="large"
-        ></v-icon>
-        <p>
-          Das API-Gateway ist:
-          <span :class="status">{{ status }}</span>
-        </p>
-      </v-col>
-    </v-row>
+  <v-container max-width="800px">
+    <h1 v-if="progressBarActive">Offline-Daten werden heruntergeladen...</h1>
+    <h1 v-else>Offline-Daten wurden heruntergeladen.</h1>
+    <p v-if="currentlyRunningTask">{{ currentlyRunningTask.name }}</p>
+    <p v-else>
+      Herunterladen der Daten abgeschlossen ({{
+        numberOfSuccessfullTasks + numberOfFailedTasks
+      }}
+      / {{ numberOfTasksToRun }})
+    </p>
+    <v-progress-linear
+      color="primary"
+      :striped="progressBarActive"
+      :max="numberOfTasksToRun"
+      :model-value="numberOfSuccessfullTasks + numberOfFailedTasks"
+    />
+    <p>
+      Erfolgreich heruntergeladen ({{ numberOfSuccessfullTasks }} /
+      {{ numberOfTasksToRun }})
+    </p>
+    <v-progress-linear
+      color="primary"
+      :striped="progressBarActive"
+      :max="numberOfTasksToRun"
+      :model-value="numberOfSuccessfullTasks"
+    />
+    <p>Fehlgeschlagen ({{ numberOfFailedTasks }} / {{ numberOfTasksToRun }})</p>
+    <v-progress-linear
+      color="primary"
+      :striped="progressBarActive"
+      :max="numberOfTasksToRun"
+      :model-value="numberOfFailedTasks"
+    />
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import { VContainer } from "vuetify/components";
+import { computed, onMounted, ref } from "vue";
+import { VContainer, VProgressLinear } from "vuetify/components";
 
 import { checkHealth } from "@/api/health-client";
 import { getUser } from "@/api/user-client.ts";
 import { useBroadcastCronjobService } from "@/composables/broadcast/broadcastCronjobService.ts";
 import { useUserStore } from "@/stores/userStore.ts";
-import { useWahlenStore } from "@/stores/wahlenStore.ts";
 import { useWahlvorstandStore } from "@/stores/wahlvorstandStore.ts";
 import { HealthState } from "@/types/HealthState";
 import { User, UserLocalDevelopment } from "@/types/User.ts";
+import { useTaskManager } from "@/util/taskManager.ts";
 
 const wahlvorstandStore = useWahlvorstandStore();
 const { startBroadcastMessageInterval } = useBroadcastCronjobService();
 const status = ref("DOWN");
-const wahlenStore = useWahlenStore();
 const userStore = useUserStore();
+const {
+  initTasks,
+  numberOfSuccessfullTasks,
+  numberOfFailedTasks,
+  numberOfTasksToRun,
+  currentlyRunningTask,
+} = useTaskManager();
 
 onMounted(() => {
   loadUser();
@@ -46,7 +69,6 @@ function loadUser(): void {
   getUser()
     .then((user: User) => userStore.setUser(user))
     .catch(() => {
-      // No user info received, so fallback
       if (import.meta.env.DEV) {
         userStore.setUser(UserLocalDevelopment());
       } else {
@@ -56,9 +78,16 @@ function loadUser(): void {
     .then(() => {
       wahlvorstandStore.loadWahlvorstand();
       startBroadcastMessageInterval();
-      wahlenStore.loadWahlen();
+      initTasks();
     });
 }
+
+const progressBarActive = computed(() => {
+  return (
+    numberOfSuccessfullTasks.value + numberOfFailedTasks.value !=
+    numberOfTasksToRun.value
+  );
+});
 </script>
 
 <style scoped>

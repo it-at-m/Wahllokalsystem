443e81f87ce6d802409be7409efef88e86ce4b0f
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-07 11:01:15 +0200

    wahlen persist in store

--- wls-gui-wahllokalsystem/src/stores/wahlenStore.ts
+++ wls-gui-wahllokalsystem/src/stores/wahlenStore.ts
@@ -3,20 +3,21 @@ import type { Wahl } from "@/types/wahl/Wahl.ts";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-import { useWahlService } from "@/composables/wahlen/wahlenService.ts";
+import { useWahlService } from "@/composables/wahl/wahlService.ts";
 import { useUserStore } from "@/stores/userStore.ts";
 
 export const storeID = "wahlen";
 const wahlenService = useWahlService();
-const userStore = useUserStore();
+
 export const useWahlenStore = defineStore(storeID, () => {
+  const userStore = useUserStore();
   const wahlen = ref<Wahl[]>();
   const wahlenReady = ref(false);
 
   async function loadWahlen() {
     const currentWahltag = userStore.currentUserWahltagID;
     if (currentWahltag) {
-      await wahlenService.loadWahlen(currentWahltag);
+      wahlen.value = await wahlenService.loadWahlen(currentWahltag);
       wahlenReady.value = true;
     }
   }

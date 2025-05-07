e97df76b440c3ed2f0f5eec59afff891d6902c24
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-07 15:43:27 +0200

    added tests

---
+++ wls-gui-wahllokalsystem/tests/stores/wahlStore.spec.ts
@@ -0,0 +1,52 @@
+import { useCommonTestDataFactory } from "@tests/utils/common/CommonTestDataFactory.ts";
+import { useWahlTestDataFactory } from "@tests/utils/wahl/WahlTestDataFactory.ts";
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlStore } from "@/stores/wahlStore.ts";
+import { User } from "@/types/User.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  getWahlen: vi.fn(),
+}));
+
+vi.mock("@/composables/wahl/wahlService.ts", () => ({
+  useWahlService: () => ({
+    getWahlen: mockDefinitions.getWahlen,
+  }),
+}));
+
+const { createWahl, prepareWahl } = useWahlTestDataFactory();
+const { generateRandomString } = useCommonTestDataFactory();
+
+describe("wahlStore.ts", () => {
+  let unitUnderTest: ReturnType<typeof useWahlStore>;
+
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    unitUnderTest = useWahlStore();
+  });
+
+  it("should_loadWahlen_when_calledWithCorrectWahltagID", async () => {
+    const wahltagID = generateRandomString(10);
+    const userStore = useUserStore();
+    const user = new User();
+    user.wahltagID = wahltagID;
+    userStore.setUser(user);
+
+    const expectedWahlArray = [createWahl()];
+    const wahl = Promise.resolve(expectedWahlArray);
+    mockDefinitions.getWahlen.mockReturnValue(wahl);
+
+    await unitUnderTest.loadWahlen();
+
+    expect(unitUnderTest.wahlen).toStrictEqual(expectedWahlArray);
+  });
+
+  it("should_notLoadWahlen_when_calledWithNullWahltagId", async () => {
+    await unitUnderTest.loadWahlen();
+
+    expect(unitUnderTest.wahlen).toStrictEqual(undefined);
+  });
+});

34f3f89193463939ed0dcf8284a74fba497fe054
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-07 18:11:46 +0200

    added test for taskMangerStore

---
+++ wls-gui-wahllokalsystem/tests/stores/taskManagerStore.spec.ts
@@ -0,0 +1,82 @@
+import type { Task } from "@/types/Task.ts";
+
+import { createPinia, setActivePinia } from "pinia";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { useTaskManagerStore } from "@/stores/taskManagerStore.ts";
+import { useWahlStore } from "@/stores/wahlStore.ts";
+import { WahlWahlartEnum } from "@/types/wahl/wahlWahlartEnum.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  loadWahlen: vi.fn(),
+}));
+
+vi.mock("@/store/wahl/wahlStore.ts", () => ({
+  useWahlStore: () => ({
+    loadWahlen: mockDefinitions.loadWahlen,
+  }),
+}));
+
+describe("taskManagerStore.ts", () => {
+  let unitUnderTest: ReturnType<typeof useTaskManagerStore>;
+  let wahlStore: ReturnType<typeof useWahlStore>;
+
+  beforeEach(() => {
+    setActivePinia(createPinia());
+    unitUnderTest = useTaskManagerStore();
+    wahlStore = useWahlStore();
+  });
+
+  it("should_setTaskSuccessful_when_noErrorsWhileRunningTask", async () => {
+    const exampleTaskList: Task[] = [
+      {
+        name: "Wahlen",
+        wahlbezirksart: undefined,
+        forWahlen: [
+          WahlWahlartEnum.Obw,
+          WahlWahlartEnum.Bzw,
+          WahlWahlartEnum.Srw,
+        ],
+        forAllWVZs: undefined,
+        callback: () => {
+          return Promise.resolve();
+        },
+      },
+    ];
+    unitUnderTest.taskList = exampleTaskList;
+
+    await unitUnderTest.initTasks();
+
+    expect(unitUnderTest.successfullyTasks.length).toStrictEqual(1);
+    expect(unitUnderTest.failedTasks.length).toStrictEqual(0);
+    expect(unitUnderTest.numberOfTasksToRun).toStrictEqual(1);
+  });
+
+  it("should_setTaskFailed_when_errorWhileRunningTask", async () => {
+    const exampleTaskList: Task[] = [
+      {
+        name: "Wahlen",
+        wahlbezirksart: undefined,
+        forWahlen: [
+          WahlWahlartEnum.Obw,
+          WahlWahlartEnum.Bzw,
+          WahlWahlartEnum.Srw,
+        ],
+        forAllWVZs: undefined,
+        callback: () => {
+          return wahlStore.loadWahlen();
+        },
+      },
+    ];
+    unitUnderTest.taskList = exampleTaskList;
+    mockDefinitions.loadWahlen.mockRejectedValue(
+      new Error("Failed to load Wahlen")
+    );
+
+    await unitUnderTest.initTasks();
+
+    expect(unitUnderTest.successfullyTasks.length).toStrictEqual(0);
+    expect(unitUnderTest.failedTasks.length).toStrictEqual(1);
+    expect(unitUnderTest.numberOfTasksToRun).toStrictEqual(1);
+  });
+});

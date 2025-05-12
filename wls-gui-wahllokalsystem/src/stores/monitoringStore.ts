fb386ecb94171fc586660781e3c1e37625f80ccf
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-12 15:40:51 +0200

    #943 add store

---
+++ wls-gui-wahllokalsystem/src/stores/monitoringStore.ts
@@ -0,0 +1,45 @@
+import type { Waehleranzahl } from "@/types/monitoring/Waehleranzahl.ts";
+
+import { defineStore, storeToRefs } from "pinia";
+import { ref } from "vue";
+
+import { useMonitoringService } from "@/composables/monitoring/monitoringService.ts";
+import { useUserStore } from "@/stores/userStore.ts";
+
+const { getWahlbeteiligung, postWahlbeteiligung } = useMonitoringService();
+
+export const storeID = "monitoring";
+
+export const useMonitoringStore = defineStore(storeID, () => {
+  const { currentUserWahlbezirkID, currentUserHauptWahlID } =
+    storeToRefs(useUserStore());
+
+  const waehler = ref<number>(0);
+
+  function addWaehler() {
+    waehler.value++;
+  }
+
+  async function loadWaehler() {
+    const wahlbezirkID = currentUserWahlbezirkID.value;
+    const wahlID = currentUserHauptWahlID.value;
+    if (wahlbezirkID && wahlID) {
+      await getWahlbeteiligung(wahlID, wahlbezirkID);
+    }
+  }
+
+  async function sendWaehler() {
+    const wahlbezirkID = currentUserWahlbezirkID.value;
+    const wahlID = currentUserHauptWahlID.value;
+    const wahlbeteiligung: Waehleranzahl = {
+      anzahlWaehler: waehler.value,
+      uhrzeit: new Date(),
+    };
+
+    if (wahlbezirkID && wahlID) {
+      await postWahlbeteiligung(wahlID, wahlbezirkID, wahlbeteiligung);
+    }
+  }
+
+  return { waehler, addWaehler, loadWaehler, sendWaehler };
+});

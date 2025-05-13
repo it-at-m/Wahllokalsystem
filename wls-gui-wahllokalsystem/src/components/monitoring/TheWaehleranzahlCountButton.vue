e4d36ee3c52d31217a984e772decec6985632858
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-13 11:10:13 +0200

    #943 add component

---
+++ wls-gui-wahllokalsystem/src/components/monitoring/TheWaehleranzahlCountButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn
+    class="bg-white mr-5"
+    color="primary"
+    @click="onWaehleranzahlClicked"
+    prepend-icon="$plus"
+  >
+    {{ waehler }} Wähler*innen
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { VBtn } from "vuetify/components";
+
+import { useMonitoringStore } from "@/stores/monitoringStore.ts";
+
+const { waehler } = storeToRefs(useMonitoringStore());
+const { addWaehler } = useMonitoringStore();
+
+function onWaehleranzahlClicked() {
+  addWaehler();
+}
+</script>

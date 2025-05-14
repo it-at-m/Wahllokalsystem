10c645b957f93e11c95d21c89064ef97841660c9
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-14 16:25:39 +0200

    #1230 add print functionality to button

--- wls-gui-wahllokalsystem/src/components/wahlvorstand/TheNachbesetzungDruckenButton.vue
+++ wls-gui-wahllokalsystem/src/components/wahlvorstand/TheNachbesetzungDruckenButton.vue
@@ -9,7 +9,21 @@
 </template>
 
 <script setup lang="ts">
+import type { NachbesetzungsDruckInput } from "@/types/wahlvorstand/NachbesetzungsDruckInput.ts";
+
+import { storeToRefs } from "pinia";
 import { VBtn } from "vuetify/components";
+
+import { useWahlvorstandNachbesetzungsDruck } from "@/composables/wahlvorstand/wahlvorstandNachbesetzungsDruck.ts";
+import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlvorstandStore } from "@/stores/wahlvorstandStore.ts";
+
+const { loadWahlvorstand, sendWahlvorstand } = useWahlvorstandStore();
+const { buildTemplateFromData } = useWahlvorstandNachbesetzungsDruck();
+
+const { currentUserWahltagID, currentUserWahlbezirkNummer } =
+  storeToRefs(useUserStore());
+const { wahlvorstand } = storeToRefs(useWahlvorstandStore());
 
 const props = defineProps({
   disabled: {
@@ -20,6 +34,28 @@ const props = defineProps({
 });
 
 function onNachbesetzungDruckenClicked() {
-  console.log("NachbesetzungDruckenClicked");
+  sendWahlvorstand();
+  loadWahlvorstand();
+  _openPrintDialog();
+}
+
+function _openPrintDialog() {
+  const data: NachbesetzungsDruckInput = {
+    wahlbezirknummer: currentUserWahlbezirkNummer.value,
+    wahltag: currentUserWahltagID.value,
+    wahlvorstaende: wahlvorstand.value.wahlvorstandsmitglieder,
+  };
+
+  const printWindow = window.open(
+    "",
+    "",
+    "left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0"
+  );
+
+  if (printWindow) {
+    printWindow.document.body.innerHTML = buildTemplateFromData(data);
+    printWindow.print();
+    printWindow.close();
+  }
 }
 </script>

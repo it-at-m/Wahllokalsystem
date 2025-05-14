f6a8dffb92f8dfc915a46cb7ffc5d7171bdbbaea
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-14 14:57:44 +0200

    #1230 use storeToRefs

--- wls-gui-wahllokalsystem/src/views/WahlvorstandAnwesenheitView.vue
+++ wls-gui-wahllokalsystem/src/views/WahlvorstandAnwesenheitView.vue
@@ -4,11 +4,11 @@
       <span>Anwesenheit prüfen</span>
       <div class="d-flex flex-column align-start">
         <the-wahlvorstand-latest-load-div
-          :datetime="wahlvorstandStore.lastLoading"
+          :datetime="lastLoading"
           class="text-subtitle-2 mb-1"
         />
         <the-wahlvorstand-last-send-div
-          :datetime="wahlvorstandStore.lastSending"
+          :datetime="lastSending"
           class="text-subtitle-2"
         />
       </div>
@@ -16,21 +16,22 @@
     <v-card-text>
       <the-wahlvorstand-mitglieder-table />
       <the-wahlvorstand-anwesenheit-requirement-card
-        v-show="!wahlvorstandStore.isWahlvorstandAusreichendAnwesend"
+        v-show="!isWahlvorstandAusreichendAnwesend"
       />
     </v-card-text>
     <v-card-actions>
-      <base-button-refresh @click="wahlvorstandStore.loadWahlvorstand()" />
+      <base-button-refresh @click="loadWahlvorstand()" />
       <base-button-save
-        :disabled="!wahlvorstandStore.isWahlvorstandAusreichendAnwesend"
+        :disabled="!isWahlvorstandAusreichendAnwesend"
         active
-        @click="wahlvorstandStore.sendWahlvorstand()"
+        @click="sendWahlvorstand()"
       />
     </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
 import { VCard, VCardActions, VCardText, VCardTitle } from "vuetify/components";
 
 import BaseButtonRefresh from "@/components/common/buttons/BaseButtonRefresh.vue";
@@ -41,5 +42,8 @@ import TheWahlvorstandLatestLoadDiv from "@/components/wahlvorstand/TheWahlvorst
 import TheWahlvorstandMitgliederTable from "@/components/wahlvorstand/TheWahlvorstandMitgliederTable.vue";
 import { useWahlvorstandStore } from "@/stores/wahlvorstandStore";
 
-const wahlvorstandStore = useWahlvorstandStore();
+const { loadWahlvorstand, sendWahlvorstand } = useWahlvorstandStore();
+
+const { isWahlvorstandAusreichendAnwesend, lastLoading, lastSending } =
+  storeToRefs(useWahlvorstandStore());
 </script>

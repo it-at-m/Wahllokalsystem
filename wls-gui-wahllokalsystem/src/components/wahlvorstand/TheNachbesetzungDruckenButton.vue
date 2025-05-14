bfff064625415cad5a30438587b5e9463ebf5d56
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-14 14:58:30 +0200

    #1230 add button component

---
+++ wls-gui-wahllokalsystem/src/components/wahlvorstand/TheNachbesetzungDruckenButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn
+    prepend-icon="$printer"
+    @click="onNachbesetzungDruckenClicked"
+    :disabled="props.disabled"
+  >
+    Nachbesetzung drucken
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { VBtn } from "vuetify/components";
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+});
+
+function onNachbesetzungDruckenClicked() {
+  console.log("NachbesetzungDruckenClicked");
+}
+</script>

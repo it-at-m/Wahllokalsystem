1e60ed93021d4d726f1161a65224658c1ffd81c5
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-14 17:13:06 +0200

    #1230 adjust document styling

--- wls-gui-wahllokalsystem/src/composables/wahlvorstand/wahlvorstandNachbesetzungsDruck.ts
+++ wls-gui-wahllokalsystem/src/composables/wahlvorstand/wahlvorstandNachbesetzungsDruck.ts
@@ -7,11 +7,11 @@ export function useWahlvorstandNachbesetzungsDruck() {
         <html lang="de">
         <head>
           <meta charset="utf-8"/>
-          <style type="text/css">
+          <style>
             /****** Print Header ******/
             @page {
-                size: auto; /* von A$ geändert in auto */
-                margin-top: 1cm; /* von 0.0 geändert in 1cm */
+                size: auto;
+                margin-top: 1cm;
                 margin-left: 0;
                 margin-right: 0;
             }
@@ -24,11 +24,11 @@ export function useWahlvorstandNachbesetzungsDruck() {
                     z-index: 0;
                 }
             }
-           /****** Default Value Tags ******/
+            /****** Default Value Tags ******/
             body {
                 max-width: 21cm;
                 margin: 0;
-                font-size: 12pt; /* von 9 geändert in 12 */
+                font-size: 9pt;
                 writing-mode: lr-tb;
                 text-align: left;
                 font-family: Arial, serif;
@@ -36,9 +36,12 @@ export function useWahlvorstandNachbesetzungsDruck() {
             html, body {
                 overflow-x: hidden;
             }
+            /****** Container ******/
             .bodycontainer {
                 max-width: 20cm;
                 height: 26cm;
+                margin-left: 1cm;
+                margin-right: 1cm;
             }
             .horizontal {
                 display: flex;
@@ -46,12 +49,6 @@ export function useWahlvorstandNachbesetzungsDruck() {
             .vertical {
                 display: flex;
                 flex-direction: column;
-            }
-            .flexCenter {   /* todo: unused */
-                justify-content: center;
-            }
-            .flexEnd {  /* todo: unused */
-                justify-content: flex-end;
             }
             .verticalContainer {
                 display: flex;
@@ -68,31 +65,16 @@ export function useWahlvorstandNachbesetzungsDruck() {
                 align-items: flex-start;
                 justify-content: space-between;
             }
-            .border_bold {
-                border-width: 1.5px;
-                border-style: solid;
-                border-color: #000000;
-            }
-           /****** Table ******/
+            /****** Table ******/
             table {
                 border-collapse: collapse;
                 font-size: 11pt;
             }
-            table.uncollapsed { /* todo: unused */
-                border-collapse: separate;
-                border-spacing: 0px;
-            }
-            table.borderless { /* todo: unused */
-                border-width: 0px;
-            }
             table.borderless th {
-                border-width: 0px;
+                border-width: 0;
             }
             table.borderless td {
-                border-width: 0px ;
-            }
-            table.backandborder { /* todo: unused */
-                border: 1px solid #546e7a;
+                border-width: 0 ;
             }
             table.backandborder th {
                 border: 1px solid #546e7a;
@@ -112,15 +94,20 @@ export function useWahlvorstandNachbesetzungsDruck() {
                 border: 1px solid #000000;
                 font-size: 11pt;
             }
-            .grayBorders { /* todo: unused */
-                border: 1px solid #d3d3d3;
-            }         
+            /****** Margin/Padding ******/
             .marginCheckbox {
                 margin-top: -0.05cm;
                 margin-right: 0.2cm;
             }
-            .paddingRight_03 {
+            .marginTop3mm {
+                margin-top: 3mm
+            }
+            .paddingRight03cm {
                 padding-right: 0.3cm;
+            }
+            /****** Text ******/
+            .fontSize_10 {
+                font-size: 12pt;
             }
             .fontSize_12 {
                 font-size: 12pt;
@@ -128,30 +115,37 @@ export function useWahlvorstandNachbesetzungsDruck() {
             .fontSize_14 {
                 font-size: 14pt;
             }
-            .widthCalc {
-                width: calc(100% - 0.0cm);
-            }
-            .blueGrayBG {
-                background-color: #e6e6ff;
+            .fontSize_16 {
+                font-size: 16pt;
             }
             .textAlignCenter {
                 text-align: center;
             }
-            .backendData { /* todo: unused */
+            /****** Width/Height ******/
+            .widthCalc {
+                width: calc(100% - 0.0cm);
+            }
+            .height04cm {
+                height: 0.4cm;
+            }
+            /****** Other ******/
+            .backendData {
                 font-weight: bold;
                 font-style: italic;
                 color: #546e7a;
             }
-            .blackData { /* todo: unused */
+            .blackData {
                 font-weight: bold;
                 font-style: italic;
                 color: #000000;
             }
-            .marginTop { /* todo: unused */
-                margin-top: 0.1cm;
+            .blueGrayBG {
+                background-color: #e6e6ff;
             }
-            .marginLeft_10 {
-                margin-left: 1cm;
+            .border_bold {
+                border-width: 1.5px;
+                border-style: solid;
+                border-color: #000000;
             }
           </style>
           <title>Besetzung und Anwesenheit Briefwahlvorstand ${data.wahlbezirknummer}</title>
@@ -159,35 +153,37 @@ export function useWahlvorstandNachbesetzungsDruck() {
         <body>                    
           <div class="verticalContainer bodycontainer" style="margin-top: 6.5mm">
             <div class="vertical">
-              <div class="horizontalContainer" style="font-size: 16pt">
-                <div style="font-size: 16pt;"><b>Besetzung und Anwesenheit Briefwahlvorstand</b></div>
-                <div style="font-size: 16pt;"><b>${data.wahlbezirknummer}</b></div>
+              <div class="horizontalContainer fontSize_16">
+                <div class="fontSize_16"><b>Besetzung und Anwesenheit Briefwahlvorstand</b></div>
+                <div class="fontSize_16"><b>${data.wahlbezirknummer}</b></div>
               </div>
-              <div class="horizontalTeilContainer" style="margin-top: 3mm; font-size: 16pt">
+              <div class="horizontalTeilContainer fontSize_16 marginTop3mm">
                 <div>Briefwahltisch-Nummer:</div>
                 <div style="min-width: 40%;">
                   <div class="border_bold" style="height: 25pt;"></div>
-                  <div style="font-size: 10pt"><i>(- bitte Tisch-Nummer eingeben)</i></div>
+                  <div class="fontSize_10"><i>(- bitte Tisch-Nummer eingeben)</i></div>
                 </div>
               </div>
-              <div class="horizontal" style="margin-top: 3mm; font-size: 16pt">
+              <div class="horizontal fontSize_16 marginTop3mm">
                 <div>Europawahl 2024 – Auszählung am 9. Juni 2024</div>
               </div>
-              <div class="horizontal" style="margin-top: 1mm; font-size: 12pt">
-                <div><b>Wichtig:</b> Füllen Sie dieses Formular vollständig aus und geben es bei Ihrer Tischbetreuung ab. Notieren Sie, ob das Gremium vollständig ist, Rollen verändert wurden und/oder Sie noch eine oder mehrere Personen brauchen. 
+              <div class="horizontal fontSize_12" style="margin: 0.5cm 0 0.5cm 0;">
+                <div><b>Wichtig:</b> Füllen Sie dieses Formular vollständig aus und geben es bei Ihrer Tischbetreuung ab. 
+                                     Notieren Sie, ob das Gremium vollständig ist, Rollen verändert wurden und/oder Sie 
+                                     noch eine oder mehrere Personen brauchen. 
                                      Ihre Tischbetreuung unterstützt Sie gern beim Befüllen dieses Blattes.
                 </div>
               </div>
-              <div class="horizontal" style="margin-top: 3mm; font-size: 14pt">
+              <div class="horizontal fontSize_14 marginTop3mm">
                 <div><b>Besetzung Wahlvorstand – Änderungen</b></div>
               </div>
               <div class="horizontal fontSize_12" style="margin-top: 1mm;">
                 <div class="fontSize_14 marginCheckbox">&#9744;</div>
-                <div class="paddingRight_03">Team vollständig</div>
+                <div class="paddingRight03cm">Team vollständig</div>
                 <div class="fontSize_14 marginCheckbox">&#9744;</div>
-                <div class="paddingRight_03">Rollen verändert</div>
+                <div class="paddingRight03cm">Rollen verändert</div>
                 <div class="fontSize_14 marginCheckbox">&#9744;</div>
-                <div class="paddingRight_03">Es werden noch __ Person/en benötigt</div>
+                <div class="paddingRight03cm">Es werden noch __ Person/en benötigt</div>
               </div>
               <div class="horizontal">
                 <table class="table widthCalc">
@@ -211,7 +207,7 @@ export function useWahlvorstandNachbesetzungsDruck() {
                           .map((mitglied) => {
                             return `<tr>
                       <td class="backendData">${mitglied && mitglied.funktionsname ? mitglied.funktionsname : ""}</td>
-                      <td class="blackData" style="height: 0.4cm;">${mitglied && mitglied.familienname ? mitglied.familienname : ""}</td>
+                      <td class="blackData height04cm">${mitglied && mitglied.familienname ? mitglied.familienname : ""}</td>
                       <td class="blackData">${mitglied && mitglied.vorname ? mitglied.vorname : ""}</td>
                       <td class="backendData textAlignCenter">${mitglied && mitglied.anwesend ? "" : "<b>X</b>"}</td>
                       <td></td>
@@ -223,9 +219,9 @@ export function useWahlvorstandNachbesetzungsDruck() {
                 </table>
               </div>                      
             </div>
-            <div style="margin-top: 3mm;">
+            <div style="margin-top: 1cm;">
               <div class="fontSize_14"><b>Nachbesetzung Wahlvorstand</b></div> 
-              <div style="font-size: 10pt"><i>(wird vom Briefwahlserviceteam ausgefüllt)</i></div>
+              <div class="fontSize_10"><i>(wird vom Briefwahlserviceteam ausgefüllt)</i></div>
             </div>
             <div class="horizontal">
               <table class="table widthCalc">
@@ -240,22 +236,22 @@ export function useWahlvorstandNachbesetzungsDruck() {
                   <td class="blueGrayBG textAlignCenter">Vorname</td>
                 </tr>
                 <tr>
-                  <td style="height: 0.4cm;">Beisitzer*in</td>
+                  <td class="height04cm">Beisitzer*in</td>
                   <td></td>
                   <td></td>
                 </tr>
                 <tr>
-                  <td style="height: 0.4cm;">Beisitzer*in</td>
+                  <td class="height04cm">Beisitzer*in</td>
                   <td></td>
                   <td></td>
                 </tr>
                 <tr>
-                  <td style="height: 0.4cm;">Beisitzer*in</td>
+                  <td class="height04cm">Beisitzer*in</td>
                   <td></td>
                   <td></td>
                 </tr>
               </table>
-              <div class="horizontal marginLeft_10">
+              <div class="horizontal" style="margin-left: 1cm">
                 <div class="fontSize_14 marginCheckbox">&#9744;</div>
                 <div class="fontSize_12">an WOS-Team per Webex übermittelt</div>    
               </div>

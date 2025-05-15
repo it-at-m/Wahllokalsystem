f1f94b20aa653cd919f43344e5ae610bc94fd5d5
Viviane Johns <145964798+vjohnslhm@users.noreply.github.com>
2025-05-15 10:12:42 +0200

    update test name
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-gui-wahllokalsystem/tests/stores/monitoringStore.spec.ts
+++ wls-gui-wahllokalsystem/tests/stores/monitoringStore.spec.ts
@@ -75,7 +75,7 @@ describe("monitoringStore.ts", () => {
       expect(unitUnderTest.waehler).toBe(0);
     });
 
-    it("should_notLoadWaehleranzahl_when_usersHauptWahlIdIsUndefined", async () => {
+    it("should_notLoadWaehleranzahl_when_usersHauptWahlIDIsUndefined", async () => {
       userStore.setUser(createUserWithRandomWahlbezirkID());
       // @ts-expect-error: cannot set readonly
       userStore.currentUserHauptWahlID = undefined;

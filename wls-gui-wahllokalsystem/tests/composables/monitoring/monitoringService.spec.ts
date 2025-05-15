cb2ec6b917a46c2ea222ff6d280b34c44134c993
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-15 09:28:50 +0200

    #943 review: add tests for getWahlbeteiligung

--- wls-gui-wahllokalsystem/tests/composables/monitoring/monitoringService.spec.ts
+++ wls-gui-wahllokalsystem/tests/composables/monitoring/monitoringService.spec.ts
@@ -1,6 +1,7 @@
 import type { WaehleranzahlDTO } from "@/api/wls-clients/generated-monitoring-api";
 import type { Waehleranzahl } from "@/types/monitoring/Waehleranzahl.ts";
 
+import { useCommonTestDataFactory } from "@tests/utils/common/CommonTestDataFactory.ts";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
 import { useMonitoringService } from "@/composables/monitoring/monitoringService.ts";
@@ -9,12 +10,15 @@ import { UserNotificationCategoryEnum } from "@/types/userNotification/UserNotif
 const mockDefinitions = vi.hoisted(() => ({
   addNotification: vi.fn(),
   toDto: vi.fn(),
+  toModel: vi.fn(),
   postWahlbeteiligung: vi.fn(),
+  getWahlbeteiligung: vi.fn(),
 }));
 
 vi.mock("@/api/wls-clients/generated-monitoring-api", () => ({
   WaehleranzahlControllerApi: vi.fn().mockImplementation(() => ({
     postWahlbeteiligung: mockDefinitions.postWahlbeteiligung,
+    getWahlbeteiligung: mockDefinitions.getWahlbeteiligung,
   })),
   Configuration: vi.fn(),
 }));
@@ -26,10 +30,12 @@ vi.mock("@/composables/userNotification/userNotificationService.ts", () => ({
 vi.mock("@/composables/monitoring/wahlbeteiligungMapper.ts", () => ({
   useWahlbeteiligungMapper: () => ({
     toDto: mockDefinitions.toDto,
+    toModel: mockDefinitions.toModel,
   }),
 }));
 
-const { postWahlbeteiligung } = useMonitoringService();
+const { generateRandomString } = useCommonTestDataFactory();
+const { postWahlbeteiligung, getWahlbeteiligung } = useMonitoringService();
 const mockedNow = new Date();
 
 describe("monitoringService.ts", () => {
@@ -40,6 +46,73 @@ describe("monitoringService.ts", () => {
     vi.useFakeTimers({
       now: mockedNow,
     });
+  });
+
+  describe("getWahlbeteiligung", () => {
+    it("should_returnWaehleranzahl_when_givenWahlIdAndWahlbezirkId", async () => {
+      const wahlbezirkID = generateRandomString(10);
+      const wahlID = generateRandomString(10);
+
+      const mockedWaehleranzahlDTO: WaehleranzahlDTO = {
+        anzahlWaehler: 5,
+        uhrzeit: "2025-05-05T12:00:00",
+      };
+      const mockedWaehleranzahlModel: Waehleranzahl = {
+        anzahlWaehler: 5,
+        uhrzeit: new Date("2025-05-05T12:00:00"),
+      };
+
+      mockDefinitions.getWahlbeteiligung.mockResolvedValue(
+        mockedWaehleranzahlDTO
+      );
+      mockDefinitions.toModel.mockReturnValue(mockedWaehleranzahlModel);
+
+      const result = await getWahlbeteiligung(wahlID, wahlbezirkID);
+
+      expect(mockDefinitions.getWahlbeteiligung).toHaveBeenCalledWith(
+        wahlID,
+        wahlbezirkID
+      );
+      expect(result).toEqual(mockedWaehleranzahlModel);
+    });
+
+    it.each([
+      {
+        when: "wahlIdEmpty",
+        wahlID: "",
+        wahlbezirkID: generateRandomString(10),
+      },
+      {
+        when: "wahlIdBlank",
+        wahlID: "  ",
+        wahlbezirkID: generateRandomString(10),
+      },
+      {
+        when: "wahlbezirkIdEmpty",
+        wahlID: generateRandomString(10),
+        wahlbezirkID: "",
+      },
+      {
+        when: "wahlbezirkIdBlank",
+        wahlID: generateRandomString(10),
+        wahlbezirkID: "  ",
+      },
+    ])(
+      "should_notReturnWaehleranzahl_when_$when",
+      async ({ wahlID, wahlbezirkID }) => {
+        mockDefinitions.getWahlbeteiligung.mockRejectedValue(
+          new Error("mocked api call failed")
+        );
+
+        await getWahlbeteiligung(wahlID, wahlbezirkID);
+
+        expect(mockDefinitions.getWahlbeteiligung).toHaveBeenCalledWith(
+          wahlID,
+          wahlbezirkID
+        );
+        expect(mockDefinitions.toModel).not.toHaveBeenCalled();
+      }
+    );
   });
 
   describe("postWahlbeteiligung", () => {

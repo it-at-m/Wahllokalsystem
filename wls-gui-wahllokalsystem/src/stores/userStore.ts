9e3bf0c91662d8507f328cd743f4f8800a5ececd
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-17 16:32:04 +0200

    added default user

--- wls-gui-wahllokalsystem/src/stores/userStore.ts
+++ wls-gui-wahllokalsystem/src/stores/userStore.ts
@@ -2,13 +2,31 @@ import { acceptHMRUpdate, defineStore } from "pinia";
 import { computed, ref } from "vue";
 
 import { User } from "@/types/User";
+import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
 
 export interface UserState {
   user: User | null;
 }
 
 export const useUserStore = defineStore("user", () => {
-  const user = ref<User | null>(null);
+  const user = ref<User>({
+    authorities: [],
+    department: "",
+    displayName: "",
+    email: "",
+    givenname: "",
+    lhmObjectID: "",
+    memberof: [],
+    preferred_username: "",
+    sub: "",
+    surname: "",
+    telephoneNumber: "",
+    user_roles: [],
+    username: "",
+    wahlbezirkID: "",
+    wahlbezirksArt: WahlbezirksArtEnum.UWB,
+    wahltagID: "",
+  });
 
   const currentUserWahlbezirkID = computed((): string | undefined => {
     return user.value?.wahlbezirkID;
@@ -18,15 +36,25 @@ export const useUserStore = defineStore("user", () => {
     return user.value?.wahltagID;
   });
 
+  const currentUserWahlbezirkArt = computed(() => {
+    return user.value.wahlbezirksArt;
+  });
+
   const getUser = computed((): User | null => {
     return user.value;
   });
 
-  function setUser(payload: User | null): void {
+  function setUser(payload: User): void {
     user.value = payload;
   }
 
-  return { getUser, setUser, currentUserWahlbezirkID, currentUserWahltagID };
+  return {
+    getUser,
+    setUser,
+    currentUserWahlbezirkID,
+    currentUserWahltagID,
+    currentUserWahlbezirkArt,
+  };
 });
 
 if (import.meta.hot) {

481ed98be69da235f700f2fe1daffaa124e955ef
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-19 16:17:37 +0200

    #1230 :white_check_mark: add component event test

--- wls-gui-wahllokalsystem/tests/components/wahlvorstand/TheNachbesetzungDruckenButton.spec.ts
+++ wls-gui-wahllokalsystem/tests/components/wahlvorstand/TheNachbesetzungDruckenButton.spec.ts
@@ -2,6 +2,7 @@ import type { VueWrapper } from "@vue/test-utils";
 
 import { createTestingPinia } from "@pinia/testing";
 import {
+  COMPONENT_EVENT_TESTS,
   COMPONENT_RENDER_TESTS,
   getSnapshotFilename,
 } from "@tests/utils/testutils.ts";
@@ -20,10 +21,12 @@ import {
 import { nextTick } from "vue";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
+import { VBtn } from "vuetify/components";
 import * as directives from "vuetify/directives";
 
 import TheNachbesetzungDruckenButton from "@/components/wahlvorstand/TheNachbesetzungDruckenButton.vue";
 import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlvorstandStore } from "@/stores/wahlvorstandStore.ts";
 import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
 
 const { prepareUser } = useUserTestDataFactory();
@@ -91,6 +94,38 @@ describe("TheNachbesetzungDruckenButton.vue", () => {
       await expect(wrapper.html()).toMatchFileSnapshot(
         getSnapshotFilename(context)
       );
+    });
+  });
+
+  describe(COMPONENT_EVENT_TESTS, () => {
+    describe("onNachbesetzungDruckenClicked", () => {
+      it("should_saveAndLoadWahlvorstandAndOpenNewWindow_when_clicked", async () => {
+        const userStore = useUserStore();
+        const wahlvorstandStore = useWahlvorstandStore();
+
+        userStore.setUser(
+          prepareUser().wahlbezirksArt(WahlbezirksArtEnum.BWB).build()
+        );
+        await nextTick();
+
+        const originalWindow = window.open;
+        const mockedWindow = {
+          document: { body: { innerHTML: "" } },
+          print: vi.fn(),
+          close: vi.fn(),
+        };
+        window.open = vi.fn().mockReturnValue(mockedWindow);
+
+        const button = wrapper.findComponent(VBtn);
+        await button.trigger("click");
+
+        expect(wahlvorstandStore.sendWahlvorstand).toHaveBeenCalled();
+        expect(wahlvorstandStore.loadWahlvorstand).toHaveBeenCalled();
+        expect(window.open).toHaveBeenCalled();
+        expect(mockedWindow.print).toHaveBeenCalled();
+
+        window.open = originalWindow; // restore previous state
+      });
     });
   });
 });

0c8cb925db372098c39faec69a51e9d547559dc8
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-05-19 15:42:26 +0200

    refactored test to differntiate between uwb and bwb

--- wls-gui-wahllokalsystem/tests/types/vorfaelleundvorkommnisse/Ereignisart.spec.ts
+++ wls-gui-wahllokalsystem/tests/types/vorfaelleundvorkommnisse/Ereignisart.spec.ts
@@ -1,13 +1,33 @@
-import { describe, expect, it } from "vitest";
+import { createTestingPinia } from "@pinia/testing";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 
+import { useUserStore } from "@/stores/userStore.ts";
+import { User } from "@/types/User.ts";
 import {
   EreignisartEnum,
   getEreignisArtForDateRelatedToSchliessungsuhrzeit,
 } from "@/types/vorfaelleundvorkommnisse/Ereignisart.ts";
+import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
 
 describe("Ereignisart.ts", () => {
-  describe("getEreignisArtForDateRelatedToSchliessungsuhrzeit", () => {
-    it("should_returnVorfall_when_schliessungsuhrzeitIsNotSet", () => {
+  let userStore: ReturnType<typeof useUserStore>;
+
+  beforeEach(() => {
+    const testPinia = createTestingPinia({
+      stubActions: false,
+      createSpy: vi.fn,
+    });
+    userStore = useUserStore(testPinia);
+  });
+
+  describe("getEreignisArtForDateRelatedToSchliessungsuhrzeitForUWB", () => {
+    beforeEach(() => {
+      const user = new User();
+      user.wahlbezirksArt = WahlbezirksArtEnum.UWB;
+      userStore.setUser(user);
+    });
+
+    it("should_returnVorfall_when_schliessungsuhrzeitIsNotSetAndUwb", () => {
       const result = getEreignisArtForDateRelatedToSchliessungsuhrzeit(
         new Date(),
         undefined
@@ -34,6 +54,53 @@ describe("Ereignisart.ts", () => {
       );
 
       expect(result).toStrictEqual(EreignisartEnum.Vorfall);
+    });
+
+    it("should_returnVorkommnis_when_schliesssungsuhrzeitIsBeforeEreignisDate", () => {
+      const schliessungsuhrzeit = new Date();
+      const result = getEreignisArtForDateRelatedToSchliessungsuhrzeit(
+        new Date(schliessungsuhrzeit.getTime() + 1),
+        schliessungsuhrzeit
+      );
+
+      expect(result).toStrictEqual(EreignisartEnum.Vorkommnis);
+    });
+  });
+
+  describe("getEreignisArtForDateRelatedToSchliessungsuhrzeitForBWB", () => {
+    beforeEach(() => {
+      const user = new User();
+      user.wahlbezirksArt = WahlbezirksArtEnum.BWB;
+      userStore.setUser(user);
+    });
+
+    it("should_returnVorkomniss_when_schliessungsuhrzeitIsNotSetAnd", () => {
+      const result = getEreignisArtForDateRelatedToSchliessungsuhrzeit(
+        new Date(),
+        undefined
+      );
+
+      expect(result).toStrictEqual(EreignisartEnum.Vorkommnis);
+    });
+
+    it("should_returnVorkomniss_when_schliessungsuhrzeitIsAfterEreignisDate", () => {
+      const schliessungsuhrzeit = new Date();
+      const result = getEreignisArtForDateRelatedToSchliessungsuhrzeit(
+        new Date(schliessungsuhrzeit.getTime() - 1),
+        schliessungsuhrzeit
+      );
+
+      expect(result).toStrictEqual(EreignisartEnum.Vorkommnis);
+    });
+
+    it("should_returnVorkomniss_when_schliessungsuhrzeitIsEqualEreignisDate", () => {
+      const schliessungsuhrzeit = new Date();
+      const result = getEreignisArtForDateRelatedToSchliessungsuhrzeit(
+        schliessungsuhrzeit,
+        schliessungsuhrzeit
+      );
+
+      expect(result).toStrictEqual(EreignisartEnum.Vorkommnis);
     });
 
     it("should_returnVorkommnis_when_schliesssungsuhrzeitIsBeforeEreignisDate", () => {

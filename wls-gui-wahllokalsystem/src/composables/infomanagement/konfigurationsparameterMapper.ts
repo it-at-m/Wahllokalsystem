d0828d41a8762377bf49abfbea8964e8ce17aff0
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-21 14:46:37 +0200

    #1137 add konfigurationsparameter mapper

---
+++ wls-gui-wahllokalsystem/src/composables/infomanagement/konfigurationsparameterMapper.ts
@@ -0,0 +1,21 @@
+import type { KonfigurationDTO } from "@/api/wls-clients/generated-infomanagement-api";
+import type { Konfigurationsparameter } from "@/types/infomanagement/Konfigurationsparameter.ts";
+
+export function useKonfigurationsparameterMapper() {
+  function toModel(dtos: KonfigurationDTO[]): Konfigurationsparameter[] {
+    const konfigurationsparameter: Konfigurationsparameter[] = [];
+
+    for (const dto of dtos) {
+      const model: Konfigurationsparameter = {
+        beschreibung: dto.beschreibung ?? "",
+        schluessel: dto.schluessel ?? "",
+        standardwert: dto.standardwert ?? "",
+        wert: dto.wert ?? "",
+      };
+      konfigurationsparameter.push(model);
+    }
+    return konfigurationsparameter;
+  }
+
+  return { toModel };
+}

08c2a01497f32e1fb22bf31b1405b0e2c749dda5
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-23 17:53:53 +0200

    #1338 add missing mapper tests wahlvorbereitung

---
+++ wls-gui-wahllokalsystem/tests/composables/wahlvorbereitung/wahlvorbereitungMapper.spec.ts
@@ -0,0 +1,61 @@
+import type {
+  UrnenwahlSchliessungsUhrzeitDTO,
+  UrnenwahlSchliessungsUhrzeitWriteDTO,
+} from "@/api/wls-clients/generated-wahlvorbereitung-api";
+import type { UrnenwahlSchliessungsuhrzeit } from "@/types/wahlvorbereitung/UrnenwahlSchliessungsuhrzeit.ts";
+
+import { describe, expect, it, vi } from "vitest";
+
+import { useWahlvorbereitungMapper } from "@/composables/wahlvorbereitung/wahlvorbereitungMapper.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  applyLocalTimezoneOffset: vi.fn(),
+}));
+
+vi.mock("@/composables/common/dateTimeFormatter.ts", () => ({
+  useDateTimeFormatter: () => ({
+    applyLocalTimezoneOffset: mockDefinitions.applyLocalTimezoneOffset,
+  }),
+}));
+
+describe("wahlvorbereitungMapper.ts", () => {
+  const { toModel, toDTO } = useWahlvorbereitungMapper();
+
+  describe("toDTO", () => {
+    it("should_returnDTO_when_schliessungsUhrzeitIsGiven", () => {
+      const dateToMap = new Date("2025-05-23T18:00:00+02:00");
+      const mockedApplyLocalTimezoneOffsetResponse = new Date(
+        "2025-05-23T18:00:00Z"
+      );
+      const expectedResult: UrnenwahlSchliessungsUhrzeitWriteDTO = {
+        schliessungsuhrzeit:
+          mockedApplyLocalTimezoneOffsetResponse.toISOString(),
+      };
+
+      mockDefinitions.applyLocalTimezoneOffset.mockReturnValue(
+        mockedApplyLocalTimezoneOffsetResponse
+      );
+
+      const result = toDTO(dateToMap);
+
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+
+  describe("toModel", () => {
+    it("should_returnModel_when_schliessungsUhrzeitDtoIsGiven", () => {
+      const schliessungsuhrzeit = "2025-05-23T18:00:00Z";
+      const dateToMap: UrnenwahlSchliessungsUhrzeitDTO = {
+        wahlbezirkID: "id",
+        schliessungsuhrzeit: schliessungsuhrzeit,
+      };
+      const expectedResult: UrnenwahlSchliessungsuhrzeit = {
+        schliessungsuhrzeit: schliessungsuhrzeit,
+      };
+
+      const result = toModel(dateToMap);
+
+      expect(result).toStrictEqual(expectedResult);
+    });
+  });
+});

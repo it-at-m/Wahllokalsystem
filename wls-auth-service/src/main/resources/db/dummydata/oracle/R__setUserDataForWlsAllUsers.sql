1551fa223b35e8b7e574119ebae68c5a4d921f0c
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-05-30 12:00:25 +0200

    #1270 update userData

--- wls-auth-service/src/main/resources/db/dummydata/oracle/R__setUserDataForWlsAllUsers.sql
+++ wls-auth-service/src/main/resources/db/dummydata/oracle/R__setUserDataForWlsAllUsers.sql
@@ -1,8 +1,27 @@
--- give all wls_all*-Users the same wahlbezirkID, wahltagID, wahlbezirkNummer and wbid_wahlnummer
+-- update for user wls_all
 UPDATE Wlsuser
-SET wahlbezirkID     = 'wahlbezirkID',
-    wahltagID        = 'wahltagID1',
+SET wahlbezirkID     = '', -- no wahlbezirksart either
+    wahltagID        = '5b4a73dd-c67b-490e-a09d-d2023bd91565',
+    -- TODO wahltag korrekt eintragen für alle user
+    -- wahltag          = TO_DATE('01.01.2026', 'DD.MM.YYYY'), // '01.01.2026', // '2026-01-01', // TO_TIMESTAMP('01-01-2026', 'MM-DD-YYYY'), macht ein array draus... --> klappt alles nicht
     wahlbezirkNummer = '1234',
-    wbid_wahlnummer = '{"wbid_wahlnummer": [{"wahlbezirkID": "wahlbezirkID", "wahlnummer": "2", "wahlID": "00000000-0000-0000-0000-000000000001"},{"wahlbezirkID": "wahlbezirkID", "wahlnummer": "1", "wahlID": "wahltagID"}]}'
-WHERE id in ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002',
-             '00000000-0000-0000-0000-000000000003');
+    wbid_wahlnummer  = '{"wbid_wahlnummer": [{"wahlbezirkID": "", "wahlnummer": "0", "wahlID": "9dbae57b-84d1-4a9a-b176-caf838d42f7b"}]}'
+WHERE id in ('00000000-0000-0000-0000-000000000001');
+
+-- update for user wls_all_bwb
+UPDATE Wlsuser
+SET wahlbezirkID     = '9081145c-dc16-4a9b-8255-5c49ddb98c14',
+    wahltagID        = '827e9e6a-ebe9-483f-a9cd-162b26f348e8',
+    -- wahltag          = TO_DATE('01.01.2026', 'DD.MM.YYYY'),
+    wahlbezirkNummer = '5678',
+    wbid_wahlnummer  = '{"wbid_wahlnummer": [{"wahlbezirkID": "9081145c-dc16-4a9b-8255-5c49ddb98c14", "wahlnummer": "0", "wahlID": "28039463-3d3d-4ce5-acc1-0bef931a4409"}]}'
+WHERE id in ('00000000-0000-0000-0000-000000000002');
+
+-- update for user wls_all_uwb
+UPDATE Wlsuser
+SET wahlbezirkID     = '9899cae4-df9a-4ffc-a940-f20cf2280171',
+    wahltagID        = '4526485e-2a3f-43d6-bd57-d0a58fc330cb',
+    -- wahltag          = TO_DATE('01.01.2026', 'DD.MM.YYYY'),
+    wahlbezirkNummer = '9012',
+    wbid_wahlnummer  = '{"wbid_wahlnummer": [{"wahlbezirkID": "9899cae4-df9a-4ffc-a940-f20cf2280171", "wahlnummer": "0", "wahlID": "d8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1"}]}'
+WHERE id in ('00000000-0000-0000-0000-000000000003');

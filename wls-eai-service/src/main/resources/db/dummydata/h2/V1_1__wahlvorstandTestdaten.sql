cdad0fff3d6ba32ce7c88914b6c87d9e74442089
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-03 13:14:36 +0200

    #1270 undo unwanted changes in h2 files

--- wls-eai-service/src/main/resources/db/dummydata/h2/V1_1__wahlvorstandTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V1_1__wahlvorstandTestdaten.sql
@@ -2,38 +2,38 @@
 INSERT INTO wahlvorstand (id, wahlbezirkID)
 VALUES ('00000000-0000-0000-0001-000000000001', '9899cae4-df9a-4ffc-a940-f20cf2280171');
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0001-000000000001', 'Homer', 'Simpson', 'W', 0, '00000000-0000-0000-0001-000000000001',
+VALUES ('00000000-0000-0000-0001-000000000001', 'Homer', 'Simpson', 'W', false, '00000000-0000-0000-0001-000000000001',
         null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0001-000000000002', 'Marge', 'Simpson', 'SB', 0, '00000000-0000-0000-0001-000000000001',
+VALUES ('00000000-0000-0000-0001-000000000002', 'Marge', 'Simpson', 'SB', false, '00000000-0000-0000-0001-000000000001',
         null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0001-000000000003', 'Bart', 'Simpson', 'SWB', 0,
+VALUES ('00000000-0000-0000-0001-000000000003', 'Bart', 'Simpson', 'SWB', false,
         '00000000-0000-0000-0001-000000000001', null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0001-000000000004', 'Lisa', 'Simpson', 'SSB', 0, '00000000-0000-0000-0001-000000000001',
+VALUES ('00000000-0000-0000-0001-000000000004', 'Lisa', 'Simpson', 'SSB', false, '00000000-0000-0000-0001-000000000001',
         null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0001-000000000005', 'Maggie', 'Simpson', 'B', 0, '00000000-0000-0000-0001-000000000001',
+VALUES ('00000000-0000-0000-0001-000000000005', 'Maggie', 'Simpson', 'B', false, '00000000-0000-0000-0001-000000000001',
         null);
 
 -- insert wahlvorstand for user wls_all_bwb
 INSERT INTO wahlvorstand (id, wahlbezirkID)
 VALUES ('00000000-0000-0000-0001-000000000002', '9081145c-dc16-4a9b-8255-5c49ddb98c14');
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0002-000000000001', 'Peter', 'Griffin', 'W', 0, '00000000-0000-0000-0001-000000000002',
+VALUES ('00000000-0000-0000-0002-000000000001', 'Peter', 'Griffin', 'W', false, '00000000-0000-0000-0001-000000000002',
         null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0002-000000000002', 'Lois', 'Griffin', 'SB', 0, '00000000-0000-0000-0001-000000000002',
+VALUES ('00000000-0000-0000-0002-000000000002', 'Lois', 'Griffin', 'SB', false, '00000000-0000-0000-0001-000000000002',
         null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0002-000000000003', 'Megan', 'Griffin', 'SWB', 0,
+VALUES ('00000000-0000-0000-0002-000000000003', 'Megan', 'Griffin', 'SWB', false,
         '00000000-0000-0000-0001-000000000002', null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0002-000000000004', 'Christopher', 'Griffin', 'SSB', 0,
+VALUES ('00000000-0000-0000-0002-000000000004', 'Christopher', 'Griffin', 'SSB', false,
         '00000000-0000-0000-0001-000000000002', null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
-VALUES ('00000000-0000-0000-0002-000000000005', 'Stewie', 'Griffin', 'B', 0, '00000000-0000-0000-0001-000000000002',
+VALUES ('00000000-0000-0000-0002-000000000005', 'Stewie', 'Griffin', 'B', false, '00000000-0000-0000-0001-000000000002',
         null);
 INSERT INTO wahlvorstandsmitglied (id, vorname, nachname, funktion, anwesend, wahlvorstandID, anwesenheitUpdatedOn)
 VALUES ('00000000-0000-0000-0002-000000000006', 'Brian', 'Griffin', 'B', 0, '00000000-0000-0000-0001-000000000002',
--- wls-eai-service/src/main/resources/db/dummydata/h2/V2_1__wahlvorschlagTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V2_1__wahlvorschlagTestdaten.sql
@@ -1,90 +1,88 @@
 -- for user wls_all_uwb
 INSERT INTO wahlvorschlaegeliste (id, wahltag, wahlID)
-VALUES ('00000001-0000-0000-0000-000000000001', TO_TIMESTAMP('2026-01-01', 'YYYY-MM-DD'),
-        'd8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1');
+VALUES ('00000001-0000-0000-0000-000000000001', '2026-01-01', 'd8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1');
 
-INSERT INTO wahlvorschlaege (id, wahlbezirkID, wahlID, stimmzettelgebietID, wahlvorschlaegelisteID) -- TODO wahlvorschlaege für uwb werden nicht in db eingetragen
+INSERT INTO wahlvorschlaege (id, wahlbezirkID, wahlID, stimmzettelgebietID, wahlvorschlaegelisteID)
 VALUES ('00000000-0000-0000-0000-000000000001', '9899cae4-df9a-4ffc-a940-f20cf2280171',
         'd8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1', '00000000-0000-0000-0000-000000000001',
         '00000001-0000-0000-0000-000000000001');
 
 INSERT INTO wahlvorschlag (id, ordnungszahl, kurzname, erhaeltStimmen, wahlvorschlaegeID)
-VALUES ('00000000-0000-0000-0001-000000000001', 1, 'Die Besten', 1,
+VALUES ('00000000-0000-0000-0001-000000000001', 1, 'Die Besten', true,
         '00000000-0000-0000-0000-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000001', 'Clapton', 1, 0, 10, 0,
+VALUES ('00000000-0000-0000-0001-000000000001', 'Clapton', 1, false, 10, false,
         '00000000-0000-0000-0001-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000002', 'Jagger', 2, 0, 11, 0,
+VALUES ('00000000-0000-0000-0001-000000000002', 'Jagger', 2, false, 11, false,
         '00000000-0000-0000-0001-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000003', 'Prince', 3, 0, 15, 0,
+VALUES ('00000000-0000-0000-0001-000000000003', 'Prince', 3, false, 15, false,
         '00000000-0000-0000-0001-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000004', 'Mac', 4, 0, 16, 1,
+VALUES ('00000000-0000-0000-0001-000000000004', 'Mac', 4, false, 16, true,
         '00000000-0000-0000-0001-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000005', 'Cool', 5, 1, 25, 1,
+VALUES ('00000000-0000-0000-0001-000000000005', 'Cool', 5, true, 25, true,
         '00000000-0000-0000-0001-000000000001');
 
 INSERT INTO wahlvorschlag (id, ordnungszahl, kurzname, erhaeltStimmen, wahlvorschlaegeID)
-VALUES ('00000000-0000-0000-0001-000000000002', 2, 'Die Zweitbesten', 1,
+VALUES ('00000000-0000-0000-0001-000000000002', 2, 'Die Zweitbesten', true,
         '00000000-0000-0000-0000-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000006', 'Bush', 1, 0, 10, 0,
+VALUES ('00000000-0000-0000-0001-000000000006', 'Bush', 1, false, 10, false,
         '00000000-0000-0000-0001-000000000002');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000007', 'Clinton', 2, 0, 11, 0,
+VALUES ('00000000-0000-0000-0001-000000000007', 'Clinton', 2, false, 11, false,
         '00000000-0000-0000-0001-000000000002');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000008', 'Biden', 3, 0, 15, 0,
+VALUES ('00000000-0000-0000-0001-000000000008', 'Biden', 3, false, 15, false,
         '00000000-0000-0000-0001-000000000002');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000009', 'Lincoln', 4, 0, 16, 1,
+VALUES ('00000000-0000-0000-0001-000000000009', 'Lincoln', 4, false, 16, true,
         '00000000-0000-0000-0001-000000000002');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000010', 'Reagan', 5, 1, 25, 1,
+VALUES ('00000000-0000-0000-0001-000000000010', 'Reagan', 5, true, 25, true,
         '00000000-0000-0000-0001-000000000002');
 
 INSERT INTO wahlvorschlag (id, ordnungszahl, kurzname, erhaeltStimmen, wahlvorschlaegeID)
-VALUES ('00000000-0000-0000-0001-000000000003', 3, 'Die Drittbesten', 1,
+VALUES ('00000000-0000-0000-0001-000000000003', 3, 'Die Drittbesten', true,
         '00000000-0000-0000-0000-000000000001');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000011', 'Bush', 1, 0, 10, 0,
+VALUES ('00000000-0000-0000-0001-000000000011', 'Bush', 1, false, 10, false,
         '00000000-0000-0000-0001-000000000003');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000012', 'Clinton', 2, 0, 11, 0,
+VALUES ('00000000-0000-0000-0001-000000000012', 'Clinton', 2, false, 11, false,
         '00000000-0000-0000-0001-000000000003');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000013', 'Biden', 3, 0, 15, 0,
+VALUES ('00000000-0000-0000-0001-000000000013', 'Biden', 3, false, 15, false,
         '00000000-0000-0000-0001-000000000003');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000014', 'Lincoln', 4, 0, 16, 1,
+VALUES ('00000000-0000-0000-0001-000000000014', 'Lincoln', 4, false, 16, true,
         '00000000-0000-0000-0001-000000000003');
 INSERT INTO kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber,
                       wahlvorschlagID)
-VALUES ('00000000-0000-0000-0001-000000000015', 'Reagan', 5, 1, 25, 1,
+VALUES ('00000000-0000-0000-0001-000000000015', 'Reagan', 5, true, 25, true,
         '00000000-0000-0000-0001-000000000003');
 
 
 -- for user wls_all_bwb
 INSERT INTO wahlvorschlaegeliste (id, wahltag, wahlID)
-VALUES ('00000001-0000-0000-0000-000000000002', TO_TIMESTAMP('2026-01-01', 'YYYY-MM-DD'),
-        '28039463-3d3d-4ce5-acc1-0bef931a4409');
+VALUES ('00000001-0000-0000-0000-000000000002', '2026-01-01', '28039463-3d3d-4ce5-acc1-0bef931a4409');
 
 INSERT INTO wahlvorschlaege (id, wahlbezirkID, wahlID, stimmzettelgebietID, wahlvorschlaegelisteID)
 VALUES ('00000000-0000-0000-0000-000000000005', '9081145c-dc16-4a9b-8255-5c49ddb98c14',

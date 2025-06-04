0b6fcc3e885c43d3a7c8dd1404835692b4117775
Robert Jasny <robert.jasny@muenchen.de>
2025-06-04 18:34:58 +0200

    test data for wahlvorbereitung added

--- wls-gui-wahllokalsystem/tests/utils/wahlvorbereitung/WahlvorbereitungTestDataFactory.ts
+++ wls-gui-wahllokalsystem/tests/utils/wahlvorbereitung/WahlvorbereitungTestDataFactory.ts
@@ -1,4 +1,11 @@
-import type { EroeffnungsUhrzeitWriteDTO } from "@/api/wls-clients/generated-wahlvorbereitung-api";
+import type {
+  EroeffnungsUhrzeitWriteDTO,
+  UrnenwahlvorbereitungDTO,
+  UrnenwahlvorbereitungWriteDTO,
+  WahlurneDTO,
+} from "@/api/wls-clients/generated-wahlvorbereitung-api";
+import type { Urnenwahlvorbereitung } from "@/types/wahlvorbereitung/Urnenwahlvorbereitung.ts";
+import type { Wahlurne } from "@/types/wahlvorbereitung/Wahlurne.ts";
 
 import { useCommonTestDataFactory } from "@tests/utils/common/CommonTestDataFactory.ts";
 
@@ -11,7 +18,59 @@ export function useWahlvorbereitungTestDataFactory() {
     };
   }
 
+  function createUrnenwahlvorbereitungWriteDTO(): UrnenwahlvorbereitungWriteDTO {
+    return {
+      anzahlWahlkabinen: 1,
+      anzahlWahltische: 1,
+      anzahlNebenraeume: 1,
+      urnenAnzahl: generateWahlurneDTOArray(),
+    };
+  }
+
+  function createUrnenwahlvorbereitungDTO(): UrnenwahlvorbereitungDTO {
+    return {
+      wahlbezirkID: "wahlbezirkID1",
+      anzahlWahlkabinen: 1,
+      anzahlWahltische: 1,
+      anzahlNebenraeume: 1,
+      urnenAnzahl: generateWahlurneDTOArray(),
+    };
+  }
+
+  function generateWahlurneDTOArray(): WahlurneDTO[] {
+    return [
+      {
+        wahlID: "wahlID1",
+        anzahl: 1,
+        urneVersiegelt: true,
+      },
+    ];
+  }
+
+  function createUrnenwahlvorbereitung(): Urnenwahlvorbereitung {
+    return {
+      wahlbezirkID: "wahlbezirkID1",
+      anzahlWahlkabinen: 1,
+      anzahlWahltische: 1,
+      anzahlNebenraeume: 1,
+      urnenAnzahl: generateWahlurneArray(),
+    };
+  }
+
+  function generateWahlurneArray(): Wahlurne[] {
+    return [
+      {
+        wahlID: "wahlID1",
+        anzahl: 1,
+        urneVersiegelt: true,
+      },
+    ];
+  }
+
   return {
     createEroeffnungsUhrzeitWriteDTO,
+    createUrnenwahlvorbereitungWriteDTO,
+    createUrnenwahlvorbereitung,
+    createUrnenwahlvorbereitungDTO,
   };
 }

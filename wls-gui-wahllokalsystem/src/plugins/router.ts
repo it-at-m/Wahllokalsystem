44ea768ef180d074c8eb9baff24e85232412852e
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-12 13:09:50 +0200

    #944 delete unused components

--- wls-gui-wahllokalsystem/src/plugins/router.ts
+++ wls-gui-wahllokalsystem/src/plugins/router.ts
@@ -4,24 +4,16 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import ExampleDynamicComponent from "@/components/ExampleDynamicComponent.vue";
 import {
   EXAMPLE_ROUTES_DYNAMIC,
-  EXAMPLE_ROUTES_NEWROUTE,
   EXAMPLE_ROUTES_NOTFOUND,
-  EXAMPLE_VALIDATION,
-  PRINT_EXAMPLE,
   ROUTE_BEGINN_STIMMABGABE,
   ROUTE_EREIGNISSE,
   ROUTE_WAHLSCHLIESSUNG,
   ROUTE_WAHLVORSTAND,
   ROUTES_HOME,
-  TOAST,
 } from "@/constants";
 import { useTaskManagerStore } from "@/stores/taskManagerStore.ts";
 import EreignisseView from "@/views/EreignisseView.vue";
 import ExampleError404View from "@/views/ExampleError404View.vue";
-import ExampleNewRouteView from "@/views/ExampleNewRouteView.vue";
-import ExamplePrintView from "@/views/ExamplePrintView.vue";
-import ExampleToastView from "@/views/ExampleToastView.vue";
-import ExampleValidation from "@/views/ExampleValidation.vue";
 import HomeView from "@/views/HomeView.vue";
 import WahleroeffnungView from "@/views/wahlvorbereitung/WahleroeffnungView.vue";
 import WahlschliessungView from "@/views/wahlvorbereitung/WahlschliessungView.vue";
@@ -57,11 +49,6 @@ const routes = [
     meta: {},
   },
   {
-    path: "/newroute",
-    name: EXAMPLE_ROUTES_NEWROUTE,
-    component: ExampleNewRouteView,
-  },
-  {
     path: "/dynamic/:wahlid",
     name: EXAMPLE_ROUTES_DYNAMIC,
     component: ExampleDynamicComponent,
@@ -71,21 +58,6 @@ const routes = [
     name: EXAMPLE_ROUTES_NOTFOUND,
     component: ExampleError404View,
   }, // CatchAll route
-  {
-    path: "/validation-example",
-    name: EXAMPLE_VALIDATION,
-    component: ExampleValidation,
-  },
-  {
-    path: "/toast-example",
-    name: TOAST,
-    component: ExampleToastView,
-  },
-  {
-    path: "/print-example",
-    name: PRINT_EXAMPLE,
-    component: ExamplePrintView,
-  },
 ];
 
 const router = createRouter({

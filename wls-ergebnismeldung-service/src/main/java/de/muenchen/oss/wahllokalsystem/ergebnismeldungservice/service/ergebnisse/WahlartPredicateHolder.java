159e9fe52056a033361eb6c68dac96fc9a1976f0
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-17 13:11:40 +0200

    #1007 records in service package end with `Model` (invalidergebnissePredicate)

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnisse/WahlartPredicateHolder.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnisse/WahlartPredicateHolder.java
@@ -11,29 +11,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class WahlartPredicateHolder {
 
-    private final Map<WahlartModel, InvalidErgebnissePredicate> predicatesForWahlarten = Map.ofEntries(
-            Map.entry(WahlartModel.BAW, new InvalidErgebnissePredicate(List.of(Stapelart.SRW_BAW_D_UNGUELTIG))),
-            Map.entry(WahlartModel.BEB, new InvalidErgebnissePredicate(List.of(Stapelart.VE_BE_D_UNGUELTIG, Stapelart.VE_BE_CBA_UNGUELTIG))),
+    private final Map<WahlartModel, InvalidErgebnissePredicateModel> predicatesForWahlarten = Map.ofEntries(
+            Map.entry(WahlartModel.BAW, new InvalidErgebnissePredicateModel(List.of(Stapelart.SRW_BAW_D_UNGUELTIG))),
+            Map.entry(WahlartModel.BEB, new InvalidErgebnissePredicateModel(List.of(Stapelart.VE_BE_D_UNGUELTIG, Stapelart.VE_BE_CBA_UNGUELTIG))),
             Map.entry(WahlartModel.BTW,
-                    new InvalidErgebnissePredicate(
+                    new InvalidErgebnissePredicateModel(
                             List.of(Stapelart.BTW_B_II_UNGUELTIG, Stapelart.BTW_B_I_UNGUELTIG, Stapelart.BTW_C_UNGEKENNZEICHNET,
                                     Stapelart.BTW_C_LEER,
                                     Stapelart.BTW_D_I_UNGUELTIG, Stapelart.BTW_D_II_UNGUELTIG))),
             Map.entry(WahlartModel.BZW,
-                    new InvalidErgebnissePredicate(
+                    new InvalidErgebnissePredicateModel(
                             List.of(Stapelart.LTW_BZW_C_UNGUELTIG, Stapelart.LTW_BZW_F_UNGUELTIG, Stapelart.LTW_BZW_B, Stapelart.LTW_BZW_E,
                                     Stapelart.LTW_BZW_G_BEIDE, Stapelart.LTW_BZW_G_GROSS, Stapelart.LTW_BZW_G_KLEIN))),
             Map.entry(WahlartModel.EUW,
-                    new InvalidErgebnissePredicate(List.of(Stapelart.EUW_C_UNGUELTIG, Stapelart.EUW_B_LEER, Stapelart.EUW_B_UNGEKENNZEICHNET))),
+                    new InvalidErgebnissePredicateModel(List.of(Stapelart.EUW_C_UNGUELTIG, Stapelart.EUW_B_LEER, Stapelart.EUW_B_UNGEKENNZEICHNET))),
             Map.entry(WahlartModel.LTW,
-                    new InvalidErgebnissePredicate(
+                    new InvalidErgebnissePredicateModel(
                             List.of(Stapelart.LTW_BZW_C_UNGUELTIG, Stapelart.LTW_BZW_F_UNGUELTIG, Stapelart.LTW_BZW_B, Stapelart.LTW_BZW_E,
                                     Stapelart.LTW_BZW_G_BEIDE, Stapelart.LTW_BZW_G_GROSS, Stapelart.LTW_BZW_G_KLEIN))),
-            Map.entry(WahlartModel.MBW, new InvalidErgebnissePredicate(List.of(Stapelart.MBW_D_UNGUELTIG))),
+            Map.entry(WahlartModel.MBW, new InvalidErgebnissePredicateModel(List.of(Stapelart.MBW_D_UNGUELTIG))),
             Map.entry(WahlartModel.OBW,
-                    new InvalidErgebnissePredicate(List.of(Stapelart.OBW_C_UNGUELTIG, Stapelart.OBW_B_LEER, Stapelart.OBW_B_UNGEKENNZEICHNET))),
-            Map.entry(WahlartModel.SRW, new InvalidErgebnissePredicate(List.of(Stapelart.SRW_BAW_D_UNGUELTIG))),
-            Map.entry(WahlartModel.VE, new InvalidErgebnissePredicate(List.of(Stapelart.VE_BE_D_UNGUELTIG))));
+                    new InvalidErgebnissePredicateModel(List.of(Stapelart.OBW_C_UNGUELTIG, Stapelart.OBW_B_LEER, Stapelart.OBW_B_UNGEKENNZEICHNET))),
+            Map.entry(WahlartModel.SRW, new InvalidErgebnissePredicateModel(List.of(Stapelart.SRW_BAW_D_UNGUELTIG))),
+            Map.entry(WahlartModel.VE, new InvalidErgebnissePredicateModel(List.of(Stapelart.VE_BE_D_UNGUELTIG))));
 
     public Predicate<Stapelart> getPredicateForStapelWithInvalidErgebnisse(final WahlartModel wahlart) {
         if (!predicatesForWahlarten.containsKey(wahlart)) {
@@ -43,7 +43,7 @@ public class WahlartPredicateHolder {
         return predicatesForWahlarten.get(wahlart);
     }
 
-    private record InvalidErgebnissePredicate(Collection<Stapelart> stapelRepresentingInvalid) implements Predicate<Stapelart> {
+    private record InvalidErgebnissePredicateModel(Collection<Stapelart> stapelRepresentingInvalid) implements Predicate<Stapelart> {
 
     @Override
     public boolean test(final Stapelart stapelart) {

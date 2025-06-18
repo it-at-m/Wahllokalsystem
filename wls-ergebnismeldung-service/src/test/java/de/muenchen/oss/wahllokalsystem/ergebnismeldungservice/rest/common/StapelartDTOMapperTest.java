3217d19a7fb20a1bc9b3255a208b563bc993db8b
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-18 13:14:35 +0200

    #1007 delete empty lines

--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/common/StapelartDTOMapperTest.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/common/StapelartDTOMapperTest.java
@@ -49,6 +49,5 @@ public class StapelartDTOMapperTest {
 
             Assertions.assertEquals(expectedResult, result);
         }
-
     }
 }
--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/common/StapelartModelMapperTest.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/common/StapelartModelMapperTest.java
@@ -49,6 +49,5 @@ public class StapelartModelMapperTest {
 
             Assertions.assertEquals(expectedResult, result);
         }
-
     }
 }

73f992884a2fe338d89e213c941678d18ec1b1a9
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-06-20 12:25:54 +0200

    add names to foreign key constraints

--- wls-eai-service/src/main/resources/db/migrations/oracle/V6_0__createTableWahllokalZustand.sql
+++ wls-eai-service/src/main/resources/db/migrations/oracle/V6_0__createTableWahllokalZustand.sql
@@ -7,18 +7,17 @@ CREATE TABLE WahllokalZustand
 
     PRIMARY KEY (id),
 
-    FOREIGN KEY (wahlbezirkID) REFERENCES Wahlbezirk(id)
+    FOREIGN KEY (wahlbezirkID) REFERENCES Wahlbezirk (id)
 );
 
 CREATE TABLE Druckzustand
 (
-    wahllokalzustandID          VARCHAR2(36) NOT NULL,
-    wahlID                      VARCHAR2(36) NOT NULL,
+    wahllokalzustandID          VARCHAR2(36) NOT NULL
+        CONSTRAINT FK_Wahllokalzustand REFERENCES WahllokalZustand (id),
+    wahlID                      VARCHAR2(36) NOT NULL
+        CONSTRAINT FK_Wahl REFERENCES Wahl (id),
     schnellmeldungSendenUhrzeit TIMESTAMP,
     niederschriftSendenUhrzeit  TIMESTAMP,
     schnellmeldungDruckUhrzeit  TIMESTAMP,
-    niederschriftDruckUhrzeit   TIMESTAMP,
-
-    FOREIGN KEY (wahllokalzustandID) REFERENCES WahllokalZustand(id),
-    FOREIGN KEY (wahlID) REFERENCES Wahl(id)
+    niederschriftDruckUhrzeit   TIMESTAMP
 );

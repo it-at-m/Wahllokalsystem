0bce8bf20c1f9cb7e1b9fbad52e09d22bdc6fb70
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-24 08:23:30 +0200

    #1304 remove not null from optional attributes for admin users

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/rest/UserDTO.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/rest/UserDTO.java
@@ -8,15 +8,15 @@ import lombok.Builder;
 
 @Builder
 public record UserDTO(@NotNull String username,
-                      @NotNull String email,
-                      @NotNull boolean userEnabled,
-                      @NotNull String wahltagID,
+                      String email,
+                      boolean userEnabled,
+                      String wahltagID,
                       @JsonFormat(pattern = "yyyy-MM-dd") // to avoid date being converted to array
-                      @NotNull LocalDate wahltag,
-                      @NotNull String wahlbezirkID,
-                      @NotNull String wahlbezirkNummer,
-                      @NotNull WahlbezirksartDTO wahlbezirksArt,
-                      @NotNull String pin,
+                      LocalDate wahltag,
+                      String wahlbezirkID,
+                      String wahlbezirkNummer,
+                      WahlbezirksartDTO wahlbezirksArt,
+                      String pin,
                       @NotNull Set<String> authorities,
-                      @NotNull String wbid_wahlnummer) {
+                      String wbid_wahlnummer) {
 }

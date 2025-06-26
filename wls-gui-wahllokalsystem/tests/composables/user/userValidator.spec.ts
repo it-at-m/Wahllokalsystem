a19e5b31be98f8082487d25228656f2481e9d6d5
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-26 11:34:37 +0200

    #1302 add test for every user prop

--- wls-gui-wahllokalsystem/tests/composables/user/userValidator.spec.ts
+++ wls-gui-wahllokalsystem/tests/composables/user/userValidator.spec.ts
@@ -1,3 +1,5 @@
+import type { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
+
 import { useUserTestDataFactory } from "@tests/utils/user/UserTestDataFactory.ts";
 import { describe, expect, it } from "vitest";
 
@@ -20,13 +22,141 @@ describe("userValidator.ts", () => {
     { invalid: "Blank", value: " " },
     { invalid: "Null", value: null },
     { invalid: "Undefined", value: undefined },
-  ])("should_throwError_when_userHas'$invalid'Field", async ({ value }) => {
+  ])(`should_throwError_when_userHas'$invalid'Username`, async ({ value }) => {
     const userDto = prepareUserDTO()
       .username(value as string)
       .build();
 
     expect(() => validUserDtoOrThrow(userDto)).toThrow(
       "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: username"
+    );
+  });
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])(`should_notThrowError_when_userHas'$invalid'Email`, async ({ value }) => {
+    const userDto = prepareUserDTO()
+      .email(value as string)
+      .build();
+
+    const result = validUserDtoOrThrow(userDto);
+
+    expect(result).toStrictEqual(userDto);
+  });
+
+  it.each([
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])(
+    `should_throwError_when_userHas'$invalid'userEnabled`,
+    async ({ value }) => {
+      const userDto = prepareUserDTO()
+        .userEnabled(value as unknown as boolean)
+        .build();
+
+      expect(() => validUserDtoOrThrow(userDto)).toThrow(
+        "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: userEnabled"
+      );
+    }
+  );
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])("should_throwError_when_userHas'$invalid'WahltagId", async ({ value }) => {
+    const userDto = prepareUserDTO()
+      .wahltagID(value as string)
+      .build();
+
+    expect(() => validUserDtoOrThrow(userDto)).toThrow(
+      "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: wahltagID"
+    );
+  });
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])("should_throwError_when_userHas'$invalid'Wahltag", async ({ value }) => {
+    const userDto = prepareUserDTO()
+      .wahltag(value as string)
+      .build();
+
+    expect(() => validUserDtoOrThrow(userDto)).toThrow(
+      "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: wahltag"
+    );
+  });
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])(
+    "should_throwError_when_userHas'$invalid'WahlbezirkID",
+    async ({ value }) => {
+      const userDto = prepareUserDTO()
+        .wahlbezirkID(value as string)
+        .build();
+
+      expect(() => validUserDtoOrThrow(userDto)).toThrow(
+        "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: wahlbezirkID"
+      );
+    }
+  );
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])(
+    "should_throwError_when_userHas'$invalid'WahlbezirkNummer",
+    async ({ value }) => {
+      const userDto = prepareUserDTO()
+        .wahlbezirkNummer(value as string)
+        .build();
+
+      expect(() => validUserDtoOrThrow(userDto)).toThrow(
+        "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: wahlbezirkNummer"
+      );
+    }
+  );
+
+  it.each([
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])(
+    `should_throwError_when_userHas'$invalid'WahlbezirksArt`,
+    async ({ value }) => {
+      const userDto = prepareUserDTO()
+        .wahlbezirksArt(value as unknown as WahlbezirksArtEnum)
+        .build();
+
+      expect(() => validUserDtoOrThrow(userDto)).toThrow(
+        "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: wahlbezirksArt"
+      );
+    }
+  );
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])("should_throwError_when_userHas'$invalid'Pin", async ({ value }) => {
+    const userDto = prepareUserDTO()
+      .pin(value as string)
+      .build();
+
+    expect(() => validUserDtoOrThrow(userDto)).toThrow(
+      "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: pin"
     );
   });
 
@@ -37,4 +167,22 @@ describe("userValidator.ts", () => {
       "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: authorities"
     );
   });
+
+  it.each([
+    { invalid: "Empty", value: "" },
+    { invalid: "Blank", value: " " },
+    { invalid: "Null", value: null },
+    { invalid: "Undefined", value: undefined },
+  ])(
+    "should_throwError_when_userHas'$invalid'WbidWahlnummer",
+    async ({ value }) => {
+      const userDto = prepareUserDTO()
+        .wbid_wahlnummer(value as string)
+        .build();
+
+      expect(() => validUserDtoOrThrow(userDto)).toThrow(
+        "Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: wbid_wahlnummer"
+      );
+    }
+  );
 });

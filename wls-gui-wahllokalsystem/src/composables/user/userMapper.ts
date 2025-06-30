c185ed7aff486138dbc06f002f06bea85317b986
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-06-30 10:53:23 +0200

    #1302 refactor validation

--- wls-gui-wahllokalsystem/src/composables/user/userMapper.ts
+++ wls-gui-wahllokalsystem/src/composables/user/userMapper.ts
@@ -2,84 +2,112 @@ import type { UserDTO } from "@/api/wls-clients/generated-auth-api";
 import type { User } from "@/types/User.ts";
 import type { WahlMetaData } from "@/types/wlsTypes/WahlMetaData.ts";
 
-import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
-
 export function useUserMapper() {
   function validateDtoAndMapToModel(userDto: UserDTO): User {
-    const undefinedOrEmptyFields = [
-      ...(userDto.username == null || !_isValidString(userDto.username)
-        ? ["username"]
-        : []),
-      ...(userDto.userEnabled == null ? ["userEnabled"] : []),
-      ...(userDto.wahltagID == null || !_isValidString(userDto.wahltagID)
-        ? ["wahltagID"]
-        : []),
-      ...(userDto.wahltag == null || !_isValidString(userDto.wahltag)
-        ? ["wahltag"]
-        : []),
-      ...(userDto.wahlbezirkID == null || !_isValidString(userDto.wahlbezirkID)
-        ? ["wahlbezirkID"]
-        : []),
-      ...(userDto.wahlbezirkNummer == null ||
-      !_isValidString(userDto.wahlbezirkNummer)
-        ? ["wahlbezirkNummer"]
-        : []),
-      ...(userDto.wahlbezirksArt == null ||
-      !(userDto.wahlbezirksArt in WahlbezirksArtEnum)
-        ? ["wahlbezirksArt"]
-        : []),
-      ...(userDto.pin == null || !_isValidString(userDto.pin) ? ["pin"] : []),
-      ...(userDto.authorities == null || !_isValidSet(userDto.authorities)
-        ? ["authorities"]
-        : []),
-      ...(userDto.wbid_wahlnummer == null ||
-      !_isValidString(userDto.wbid_wahlnummer)
-        ? ["wbid_wahlnummer"]
-        : []),
-    ];
+    const errors: Error[] = [];
 
-    if (undefinedOrEmptyFields.length > 0) {
+    _assertIsNotBlank(userDto.username, "username", errors);
+    _assertIsNotUndefinedOrNull(userDto.userEnabled, "userEnabled", errors);
+    _assertIsNotBlank(userDto.wahltagID, "wahltagID", errors);
+    _assertIsNotBlank(userDto.wahltag, "wahltag", errors);
+    _assertIsNotBlank(userDto.wahlbezirkID, "wahlbezirkID", errors);
+    _assertIsNotBlank(userDto.wahlbezirkNummer, "wahlbezirkNummer", errors);
+    _assertIsNotBlank(userDto.wahlbezirksArt, "wahlbezirksArt", errors);
+    _assertIsNotBlank(userDto.pin, "pin", errors);
+    _assertIsNotEmpty(userDto.authorities, "authorities", errors);
+    _assertIsNotBlank(userDto.wbid_wahlnummer, "wbid_wahlnummer", errors);
+
+    if (errors.length > 0) {
       throw new Error(
-        `Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: ${undefinedOrEmptyFields.join(", ")}`
+        `Laden des Users fehlgeschlagen. Folgende Pflichtfelder sind nicht befüllt: ${errors.join(", ")}`
       );
-    } else {
-      return {
-        username: userDto.username,
-        email: userDto.email,
-        userEnabled: userDto.userEnabled,
-        /* eslint-disable @typescript-eslint/no-non-null-assertion -- Temporarily disabling because check is done in validation above */
-        wahltagID: userDto.wahltagID!,
-        wahltag: userDto.wahltag!,
-        wahlbezirkID: userDto.wahlbezirkID!,
-        wahlbezirkNummer: userDto.wahlbezirkNummer!,
-        wahlbezirksArt: userDto.wahlbezirksArt!,
-        pin: userDto.pin!,
-        /* eslint-enable @typescript-eslint/no-non-null-assertion */
-        authorities: userDto.authorities,
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        wahlMetaData: _parseWbIdWahlnummer(userDto.wbid_wahlnummer!),
-      } as User;
+    }
+
+    return {
+      username: userDto.username,
+      email: userDto.email,
+      userEnabled: userDto.userEnabled,
+      wahltagID: userDto.wahltagID,
+      wahltag: userDto.wahltag,
+      wahlbezirkID: userDto.wahlbezirkID,
+      wahlbezirkNummer: userDto.wahlbezirkNummer,
+      wahlbezirksArt: userDto.wahlbezirksArt,
+      pin: userDto.pin,
+      authorities: userDto.authorities,
+      wahlMetaData: _parseWbIdWahlnummer(userDto.wbid_wahlnummer),
+    } as User;
+  }
+
+  function _assertIsNotBlank(
+    value: string | undefined | null,
+    propName: string,
+    errors?: Error[]
+  ): asserts value is string {
+    if (value === undefined || value == null || value.trim() === "") {
+      if (errors) {
+        errors.push(new Error(`${propName} is blank`));
+      } else {
+        throw new Error(`${propName} is blank`);
+      }
     }
   }
 
-  function _isValidString(value: string): boolean {
-    return value.trim() !== "";
+  function _assertIsNotEmpty(
+    value: Set<string> | null | undefined,
+    propName: string,
+    errors?: Error[]
+  ): asserts value is Set<string> {
+    if (value === undefined || value === null || value.size === 0) {
+      if (errors) {
+        errors.push(new Error(`${propName} is empty`));
+      } else {
+        throw new Error(`${propName} is empty`);
+      }
+    }
   }
 
-  function _isValidSet(value: Set<string>): boolean {
-    return value.size !== 0;
+  function _assertIsNotUndefinedOrNull(
+    value: boolean | undefined | null,
+    propName: string,
+    errors?: Error[]
+  ): asserts value is boolean {
+    if (value === undefined || value == null) {
+      if (errors) {
+        errors.push(new Error(`${propName} is undefined`));
+      } else {
+        throw new Error(`${propName} is undefined`);
+      }
+    }
   }
 
   function _parseWbIdWahlnummer(value: string): WahlMetaData[] {
-    try {
-      const parsed_wbid_wahlnummer = value ? JSON.parse(value) : undefined;
-      return parsed_wbid_wahlnummer?.wbid_wahlnummer
-        ? parsed_wbid_wahlnummer.wbid_wahlnummer
-        : undefined;
-    } catch {
-      console.debug("failed to parse JSON wbid_wahlnummer");
-      return [];
+    const parsedObject = JSON.parse(value);
+    if (!_isWbidWahlnummer(parsedObject)) {
+      throw new Error();
     }
+
+    return parsedObject.wbid_wahlnummer;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function _isWbidWahlnummer(value: any): value is {
+    wbid_wahlnummer: WahlMetaData[];
+  } {
+    return (
+      value.wbid_wahlnummer !== undefined &&
+      Array.isArray(value.wbid_wahlnummer) &&
+      _isArrayOfWahlMetaData(value.wbid_wahlnummer)
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function _isArrayOfWahlMetaData(values: any[]): values is WahlMetaData[] {
+    return values.every(
+      (value) =>
+        value.wahlbezirkID !== undefined &&
+        value.wahlnummer !== undefined &&
+        value.wahlID !== undefined
+    );
   }
 
   return { validateDtoAndMapToModel };

ae59f3d70944e95af62f1f9e33ac868f967b6dd5
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-06-30 17:33:23 +0200

    changed Date

--- wls-gui-wahllokalsystem/src/components/wlsComponents/WlsClock.vue
+++ wls-gui-wahllokalsystem/src/components/wlsComponents/WlsClock.vue
@@ -5,6 +5,7 @@
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
+        hour12: false,
       })
     }}
   </div>
--- wls-gui-wahllokalsystem/tests/components/wlsComponents/TheWlsAppBar.spec.ts
+++ wls-gui-wahllokalsystem/tests/components/wlsComponents/TheWlsAppBar.spec.ts
@@ -27,7 +27,7 @@ import { useUserStore } from "@/stores/userStore.ts";
 
 describe("TheWlsAppBar.vue", () => {
   let wrapper: VueWrapper;
-  const mockedDate = new Date("2025-05-23T07:30:00");
+  const mockedDate = new Date("2025-05-23T07:30:00").getUTCDate();
 
   const ResizeObserverMock = vi.fn(() => ({
     observe: vi.fn(),

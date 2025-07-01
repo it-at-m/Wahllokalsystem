69ce66564a79419275a5a0c7e957c3ba12c95ced
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-07-01 15:13:37 +0200

    fix wrong prop binding

--- wls-gui-wahllokalsystem/src/components/wahlvorstand/TheWahlvorstandAnwesenheitsCheckPopupDialog.vue
+++ wls-gui-wahllokalsystem/src/components/wahlvorstand/TheWahlvorstandAnwesenheitsCheckPopupDialog.vue
@@ -3,7 +3,7 @@
     :visible="visible"
     dialogtitle="Anwesenheit kontrollieren"
     :confirmtext="BUTTON_TITLE_CONFIRM"
-    canceltext="BUTTON_TITLE_CANCEL"
+    :canceltext="BUTTON_TITLE_CANCEL"
     icon="$information"
     @confirm="onConfirmClicked"
     @cancel="onCancelClicked"

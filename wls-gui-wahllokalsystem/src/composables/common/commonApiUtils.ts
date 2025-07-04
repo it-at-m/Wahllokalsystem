18ff7f58ae181bf98c64e6a6a342797308798cbe
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-07-04 11:39:29 +0200

    simplify generic name

--- wls-gui-wahllokalsystem/src/composables/common/commonApiUtils.ts
+++ wls-gui-wahllokalsystem/src/composables/common/commonApiUtils.ts
@@ -1,9 +1,9 @@
 import type { AxiosResponse } from "axios";
 
 export function useCommonApiUtils() {
-  function getNullOn204OrElseResponseData<dataType>(
-    response: AxiosResponse<dataType>
-  ): dataType | null {
+  function getNullOn204OrElseResponseData<T>(
+    response: AxiosResponse<T>
+  ): T | null {
     return response.status === 204 ? null : response.data;
   }
 

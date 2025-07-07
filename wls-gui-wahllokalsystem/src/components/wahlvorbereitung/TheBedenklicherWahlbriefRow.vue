2f8a473802ea9b890b20ffbb178d5bbdb7f3f517
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-07-07 17:54:54 +0200

    #1313 delete helper function

--- wls-gui-wahllokalsystem/src/components/wahlvorbereitung/TheBedenklicherWahlbriefRow.vue
+++ wls-gui-wahllokalsystem/src/components/wahlvorbereitung/TheBedenklicherWahlbriefRow.vue
@@ -30,26 +30,9 @@
 </template>
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
-import { onMounted } from "vue";
 import { VBtn, VCol, VRow } from "vuetify/components";
 
-import { useBriefwahlService } from "@/composables/briefwahl/briefwahlService.ts";
 import { useWahlenStore } from "@/stores/wahlenStore.ts";
 
 const { wahlen } = storeToRefs(useWahlenStore());
-const { getWaehlerverzeichnisnummerOrUndefinedById } = useWahlenStore();
-const { getBeanstandeteWahlbriefe } = useBriefwahlService();
-
-// todo: am anfang müssen die nicht geladen werden!! die werden ja in der anwendung erst erstellt das heißt am anfang ist es noch ein leeres objekt
-onMounted(async () => {
-  if (wahlen.value) {
-    for (const wahl of wahlen.value) {
-      const wvzNr = getWaehlerverzeichnisnummerOrUndefinedById(wahl.wahlID);
-      if (wvzNr) {
-        const briefe = await getBeanstandeteWahlbriefe(wvzNr);
-        console.log(briefe);
-      }
-    }
-  }
-});
 </script>

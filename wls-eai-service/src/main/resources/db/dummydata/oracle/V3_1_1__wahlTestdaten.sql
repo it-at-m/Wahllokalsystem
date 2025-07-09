365baa37525976fb17ed1b903c6d3f53068e0eff
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-07-09 11:27:01 +0200

    #1389 replace wahltagid 4526485e-2a3f-43d6-bd57-d0a58fc330cb with 827e9e6a-ebe9-483f-a9cd-162b26f348e8

--- wls-eai-service/src/main/resources/db/dummydata/oracle/V3_1_1__wahlTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/oracle/V3_1_1__wahlTestdaten.sql
@@ -1,6 +1,6 @@
 -- for user wls_all_uwb
 INSERT INTO wahl (id, name, wahlart, wahltagID)
-values ('d8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1', 'EU-Wahl 24', 'EUW', '4526485e-2a3f-43d6-bd57-d0a58fc330cb');
+values ('d8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1', 'EU-Wahl 24', 'EUW', '827e9e6a-ebe9-483f-a9cd-162b26f348e8');
 
 -- for user wls_all_bwb
 INSERT INTO wahl (id, name, wahlart, wahltagID)

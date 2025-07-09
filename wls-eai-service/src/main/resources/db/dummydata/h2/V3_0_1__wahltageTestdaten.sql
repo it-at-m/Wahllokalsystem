5264f6a516e9b5c83d053196cef340e3fd3435e7
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-07-09 11:27:01 +0200

    #1389 replace wahltagid 4526485e-2a3f-43d6-bd57-d0a58fc330cb with 827e9e6a-ebe9-483f-a9cd-162b26f348e8

--- wls-eai-service/src/main/resources/db/dummydata/h2/V3_0_1__wahltageTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V3_0_1__wahltageTestdaten.sql
@@ -1,7 +1,3 @@
--- for user wls_all_uwb
-INSERT INTO wahltag (id, tag, beschreibung, nummer)
-values ('4526485e-2a3f-43d6-bd57-d0a58fc330cb', '2026-01-01', 'zehnte Direktwahl zum Europäischen Parlament', '0');
-
--- for user wls_all_bwb
+-- for user wls_all_bwb + wls_all_uwb
 INSERT INTO wahltag (id, tag, beschreibung, nummer)
 values ('827e9e6a-ebe9-483f-a9cd-162b26f348e8', '2026-01-01', 'Kommunalwahl 2026', '0');

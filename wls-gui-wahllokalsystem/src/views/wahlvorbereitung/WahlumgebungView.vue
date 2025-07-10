4373b8cb60775a99da7932f9f35c7bb49697f35f
Robert Jasny <robert.jasny@muenchen.de>
2025-07-10 14:50:53 +0200

    condition for Wahlbezirksart added

--- wls-gui-wahllokalsystem/src/views/wahlvorbereitung/WahlumgebungView.vue
+++ wls-gui-wahllokalsystem/src/views/wahlvorbereitung/WahlumgebungView.vue
@@ -1,7 +1,19 @@
 <template>
-  <wahlumgebung-uwb-card />
+  <wahlumgebung-uwb-card
+    v-if="currentUserWahlbezirksArt == WahlbezirksArtEnum.UWB"
+  />
+  <wahlumgebung-bwb-card
+    v-if="currentUserWahlbezirksArt == WahlbezirksArtEnum.BWB"
+  />/>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
+
+import WahlumgebungBwbCard from "@/components/wahlvorbereitung/WahlumgebungBwbCard.vue";
 import WahlumgebungUwbCard from "@/components/wahlvorbereitung/WahlumgebungUwbCard.vue";
+import { useUserStore } from "@/stores/userStore.ts";
+import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
+
+const { currentUserWahlbezirksArt } = storeToRefs(useUserStore());
 </script>

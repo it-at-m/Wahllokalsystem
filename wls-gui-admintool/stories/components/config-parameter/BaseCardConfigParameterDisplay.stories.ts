d05663fb791df0125c2f13dd7f440577275240e6
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2025-07-15 12:31:40 +0200

    🔥|🎨 Refactoring - Codeoptimierung sowie - Bereinigung und Umbenennung Komponente (BaseCardConfigParameterDisplay) und Interface (InfomanagementConfigParameterModel); Anwendung von Prettier für einheitliche Formatierung

---
+++ wls-gui-admintool/stories/components/config-parameter/BaseCardConfigParameterDisplay.stories.ts
@@ -0,0 +1,58 @@
+import type { InfomanagementConfigParameter } from "@/types/config/InfomanagementConfigParameter.ts";
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import { fn } from "@storybook/test";
+
+import BaseCardConfigParameterDisplay from "@/components/config-parameter/BaseCardConfigParameterDisplay.vue";
+
+const meta: Meta<typeof BaseCardConfigParameterDisplay> = {
+  component: BaseCardConfigParameterDisplay,
+  argTypes: {
+    configParameter: {
+      description: "Konfigurationsparameter Objekt",
+      control: "object",
+    },
+    onConfirmEdit: {
+      description:
+        "Wird ausgelöst wenn der Edit-Button geklickt wird und gibt den Namen des ConfigParameters zurück",
+      action: "confirmEdit",
+      name: "confirmEdit",
+      table: {
+        category: "Events",
+      },
+    },
+    confirmEdit: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  args: {
+    configParameter: {
+      name: "Willkommenstext",
+      beschreibung: "Begrüßungstext auf der Anmeldemaske",
+      wert: "Herzlich willkommen zur Wahl!",
+      defaultValue: "Herzlich willkommen zur Testwahl!",
+    } as InfomanagementConfigParameter,
+
+    onConfirmEdit: fn(),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BaseCardConfigParameterDisplay>;
+
+export const WithValue: Story = {};
+
+export const NoValue: Story = {
+  args: {
+    configParameter: {
+      name: "Willkommenstext",
+      beschreibung: "Begrüßungstext auf der Anmeldemaske",
+      defaultValue: "Herzlich willkommen zur Testwahl!",
+    } as InfomanagementConfigParameter,
+
+    onConfirmEdit: fn(),
+  },
+};

c431ea51b4cf43c42e6d16ef12c2d636b2075615
Robert Jasny <robert.jasny@muenchen.de>
2025-07-17 16:27:11 +0200

    rule validation tests added

--- wls-gui-wahllokalsystem/tests/components/wahlvorbereitung/BaseWahlumgebungWahlurnenDiv.spec.ts
+++ wls-gui-wahllokalsystem/tests/components/wahlvorbereitung/BaseWahlumgebungWahlurnenDiv.spec.ts
@@ -6,7 +6,7 @@ import {
   COMPONENT_RENDER_TESTS,
   getSnapshotFilename,
 } from "@tests/utils/testutils.ts";
-import { mount } from "@vue/test-utils";
+import { flushPromises, mount } from "@vue/test-utils";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 import { VNumberInput } from "vuetify/components";
 
@@ -81,6 +81,150 @@ describe("BaseWahlumgebungWahlurnenDiv.vue", () => {
       await expect(wrapper.html()).toMatchFileSnapshot(
         getSnapshotFilename(context)
       );
+    });
+
+    it("should_renderNoErrorMessage_when_anzahlWahlurnenIsValid", async (context) => {
+      const twoWahlenWahlVorbereitungAnzahlNull = {
+        wahlbezirkID: "wahlbezirkID1",
+        urneVersiegelt: false,
+        urnenAnzahl: [
+          { wahlID: "wahlID1", anzahl: null },
+          { wahlID: "wahlID2", anzahl: null },
+        ],
+      };
+
+      const wrapper = mount(BaseWahlumgebungWahlurnenDiv, {
+        global: {
+          plugins: [testPinia, vuetify],
+        },
+        props: {
+          wahlVorbereitung: twoWahlenWahlVorbereitungAnzahlNull,
+        },
+      });
+
+      const inputs = wrapper.findAllComponents(VNumberInput);
+      expect(inputs.length).toBe(2);
+
+      // Teste die Validierung mit ungültigem Wert
+      await inputs[0].setValue(1);
+      await inputs[1].setValue(99);
+      await flushPromises();
+      expect(wrapper.vm.wahlVorbereitung.urnenAnzahl[0].anzahl).toBe(1);
+      expect(wrapper.vm.wahlVorbereitung.urnenAnzahl[1].anzahl).toBe(99);
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+      // Überprüfen, dass keine Fehlermeldung angezeigt wird
+      expect(wrapper.findAll(".v-messages__message").length).toBe(0);
+    });
+
+    it("should_renderErrorMessage_when_ruleMinIsViolated", async (context) => {
+      const errorMessage = "Eingabe darf nicht kleiner als 1 sein.";
+      const twoWahlenWahlVorbereitungAnzahlNull = {
+        wahlbezirkID: "wahlbezirkID1",
+        urneVersiegelt: false,
+        urnenAnzahl: [
+          { wahlID: "wahlID1", anzahl: null },
+          { wahlID: "wahlID2", anzahl: null },
+        ],
+      };
+
+      const wrapper = mount(BaseWahlumgebungWahlurnenDiv, {
+        global: {
+          plugins: [testPinia, vuetify],
+        },
+        props: {
+          wahlVorbereitung: twoWahlenWahlVorbereitungAnzahlNull,
+        },
+      });
+
+      const inputs = wrapper.findAllComponents(VNumberInput);
+      expect(inputs.length).toBe(2);
+
+      // Teste die Validierung mit ungültigem Wert
+      await inputs[0].setValue(0);
+      await flushPromises();
+      expect(wrapper.vm.wahlVorbereitung.urnenAnzahl[0].anzahl).toBe(0);
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+      expect(wrapper.html()).toContain(errorMessage);
+    });
+
+    it("should_renderErrorMessage_when_ruleMaxIsViolated", async (context) => {
+      const errorMessage = "Eingabe darf nicht größer als 99 sein.";
+      const twoWahlenWahlVorbereitungAnzahlNull = {
+        wahlbezirkID: "wahlbezirkID1",
+        urneVersiegelt: false,
+        urnenAnzahl: [
+          { wahlID: "wahlID1", anzahl: null },
+          { wahlID: "wahlID2", anzahl: null },
+        ],
+      };
+
+      const wrapper = mount(BaseWahlumgebungWahlurnenDiv, {
+        global: {
+          plugins: [testPinia, vuetify],
+        },
+        props: {
+          wahlVorbereitung: twoWahlenWahlVorbereitungAnzahlNull,
+        },
+      });
+
+      const inputs = wrapper.findAllComponents(VNumberInput);
+      expect(inputs.length).toBe(2);
+
+      // Teste die Validierung mit ungültigem Wert
+      await inputs[0].setValue(100);
+      await flushPromises();
+      expect(wrapper.vm.wahlVorbereitung.urnenAnzahl[0].anzahl).toBe(100);
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+      expect(wrapper.html()).toContain(errorMessage);
+    });
+
+    it.skip("should_renderErrorMessage_when_ruleRequiredIsViolated", async (context) => {
+      const twoWahlenWahlVorbereitungAnzahlNull = {
+        wahlbezirkID: "wahlbezirkID1",
+        urneVersiegelt: false,
+        urnenAnzahl: [
+          { wahlID: "wahlID1", anzahl: null },
+          { wahlID: "wahlID2", anzahl: null },
+        ],
+      };
+
+      const wrapper = mount(BaseWahlumgebungWahlurnenDiv, {
+        global: {
+          plugins: [testPinia, vuetify],
+        },
+        props: {
+          wahlVorbereitung: twoWahlenWahlVorbereitungAnzahlNull,
+        },
+      });
+
+      const inputs = wrapper.findAllComponents(VNumberInput);
+      expect(inputs.length).toBe(2);
+      await flushPromises();
+
+      // Teste die Validierung mit gültigem Wert (z.B. 77)
+      await inputs[0].setValue(77);
+      await flushPromises();
+      expect(wrapper.vm.wahlVorbereitung.urnenAnzahl[0].anzahl).toBe(77);
+
+      // zurück auf null
+      await inputs[0].setValue(null);
+      await flushPromises();
+      expect(wrapper.vm.wahlVorbereitung.urnenAnzahl[0].anzahl).toBe(null);
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+      // Überprüfen, ob eine Fehlermeldung angezeigt wird
+      expect(wrapper.findAll(".v-messages__message").length).toBeGreaterThan(0);
     });
   });
 

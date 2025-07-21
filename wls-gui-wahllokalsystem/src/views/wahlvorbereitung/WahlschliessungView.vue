b0476c373f8f8e37debdbf0a394bf88ecde10fc0
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-07-21 17:02:30 +0200

    add new card to wahlhandlung/wahlschliessung view

--- wls-gui-wahllokalsystem/src/views/wahlvorbereitung/WahlschliessungView.vue
+++ wls-gui-wahllokalsystem/src/views/wahlvorbereitung/WahlschliessungView.vue
@@ -1,7 +1,9 @@
 <template>
+  <the-unguetilge-wahlscheine-verify-card />
   <base-wahlschliessung-card />
 </template>
 
 <script setup lang="ts">
 import BaseWahlschliessungCard from "@/components/wahlvorbereitung/BaseWahlschliessungCard.vue";
+import TheUnguetilgeWahlscheineVerifyCard from "@/components/wahlvorbereitung/TheUnguetilgeWahlscheineVerifyCard.vue";
 </script>

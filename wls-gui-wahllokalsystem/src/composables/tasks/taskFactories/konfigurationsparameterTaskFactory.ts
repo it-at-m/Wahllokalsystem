fd1ce47780d49420974e39ec9c3dcad9f30cbf1b
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-07-24 08:40:35 +0200

    Update wls-gui-wahllokalsystem/src/composables/tasks/taskFactories/konfigurationsparameterTaskFactory.ts
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-gui-wahllokalsystem/src/composables/tasks/taskFactories/konfigurationsparameterTaskFactory.ts
+++ wls-gui-wahllokalsystem/src/composables/tasks/taskFactories/konfigurationsparameterTaskFactory.ts
@@ -1,12 +1,10 @@
 import type { TaskFactory } from "@/composables/tasks/TaskFactory.ts";
-import type { TaskFactoryContext } from "@/composables/tasks/TaskFactoryContext.ts";
 import type { Task } from "@/types/tasks/Task.ts";
 
 import { useInfomanagementStore } from "@/stores/infomanagementStore.ts";
 
 export function useKonfigurationsparameterTaskFactory(): TaskFactory {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  function createTasks(taskFactoryData: TaskFactoryContext): Task[] {
+  function createTasks(): Task[] {
     return [_createTask()];
   }
 

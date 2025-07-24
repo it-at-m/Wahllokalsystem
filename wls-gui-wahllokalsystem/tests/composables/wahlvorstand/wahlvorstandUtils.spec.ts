34333e9877a760c2e584307ff99a0a1a3f58bd9a
Robert Jasny <robert.jasny@muenchen.de>
2025-07-24 15:21:11 +0200

    use TestDataFactory

--- wls-gui-wahllokalsystem/tests/composables/wahlvorstand/wahlvorstandUtils.spec.ts
+++ wls-gui-wahllokalsystem/tests/composables/wahlvorstand/wahlvorstandUtils.spec.ts
@@ -1,26 +1,29 @@
+import { useWahlvorstandTestDataFactory } from "@tests/utils/wahlvorstand/WahlvorstandTestDataFactory.ts";
 import { describe, expect, it } from "vitest";
 
 import { useWahlvorstandComparators } from "@/composables/wahlvorstand/wahlvorstandUtils.ts";
 import { WahlvorstandsmitgliedFunktionEnum } from "@/types/wahlvorstand/WahlvorstandsmitgliedFunktion.ts";
 
+const { prepareWahlvorstandsmitglied } = useWahlvorstandTestDataFactory();
+
 describe("useWahlvorstandComparators", () => {
   const { compareWahlvorstandsMitglieder } = useWahlvorstandComparators();
 
   it("should_sortByFunktion_when_funktionIsDifferent", () => {
-    const mitgliedA = {
-      identifikator: "1",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Xaver",
-      familienname: "Zinfandel",
-    };
-    const mitgliedB = {
-      identifikator: "2",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.Sb,
-      vorname: "Anna",
-      familienname: "Bauer",
-    };
+    const mitgliedA = prepareWahlvorstandsmitglied()
+      .identifikator("1")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Xaver")
+      .familienname("Zinfandel")
+      .build();
+    const mitgliedB = prepareWahlvorstandsmitglied()
+      .identifikator("2")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.Sb)
+      .vorname("Anna")
+      .familienname("Bauer")
+      .build();
 
     expect(compareWahlvorstandsMitglieder(mitgliedA, mitgliedB)).toBeLessThan(
       0
@@ -31,20 +34,20 @@ describe("useWahlvorstandComparators", () => {
   });
 
   it("should_sortByFamilienname_when_funktionIsTheSame", () => {
-    const mitgliedA = {
-      identifikator: "1",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Xaver",
-      familienname: "Zinfandel",
-    };
-    const mitgliedB = {
-      identifikator: "2",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Anna",
-      familienname: "Bauer",
-    };
+    const mitgliedA = prepareWahlvorstandsmitglied()
+      .identifikator("1")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Xaver")
+      .familienname("Zinfandel")
+      .build();
+    const mitgliedB = prepareWahlvorstandsmitglied()
+      .identifikator("2")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Anna")
+      .familienname("Bauer")
+      .build();
 
     expect(
       compareWahlvorstandsMitglieder(mitgliedA, mitgliedB)
@@ -55,20 +58,20 @@ describe("useWahlvorstandComparators", () => {
   });
 
   it("should_sortByVorname_when_funktionAndFamiliennameAreTheSame", () => {
-    const mitgliedA = {
-      identifikator: "1",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Anna",
-      familienname: "Müller",
-    };
-    const mitgliedB = {
-      identifikator: "2",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Xaver",
-      familienname: "Müller",
-    };
+    const mitgliedA = prepareWahlvorstandsmitglied()
+      .identifikator("1")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Anna")
+      .familienname("Müller")
+      .build();
+    const mitgliedB = prepareWahlvorstandsmitglied()
+      .identifikator("2")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Xaver")
+      .familienname("Müller")
+      .build();
 
     expect(compareWahlvorstandsMitglieder(mitgliedA, mitgliedB)).toBeLessThan(
       0
@@ -79,20 +82,20 @@ describe("useWahlvorstandComparators", () => {
   });
 
   it("should_handleCase_when_funktionAndFamiliennameAndVornameAreTheSame", () => {
-    const mitgliedA = {
-      identifikator: "1",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Max",
-      familienname: "Mustermann",
-    };
-    const mitgliedB = {
-      identifikator: "2",
-      anwesend: true,
-      funktion: WahlvorstandsmitgliedFunktionEnum.W,
-      vorname: "Max",
-      familienname: "Mustermann",
-    };
+    const mitgliedA = prepareWahlvorstandsmitglied()
+      .identifikator("1")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Max")
+      .familienname("Mustermann")
+      .build();
+    const mitgliedB = prepareWahlvorstandsmitglied()
+      .identifikator("2")
+      .anwesend(true)
+      .funktion(WahlvorstandsmitgliedFunktionEnum.W)
+      .vorname("Max")
+      .familienname("Mustermann")
+      .build();
 
     expect(compareWahlvorstandsMitglieder(mitgliedA, mitgliedB)).toBe(0);
   });

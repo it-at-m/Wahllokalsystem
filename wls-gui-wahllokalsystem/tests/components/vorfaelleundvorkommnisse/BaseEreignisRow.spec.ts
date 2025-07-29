09ce9d4a88ebf21905f693e42a3fc3e37f237c23
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-07-29 19:59:24 +0200

    creat tests for base ereignis row

---
+++ wls-gui-wahllokalsystem/tests/components/vorfaelleundvorkommnisse/BaseEreignisRow.spec.ts
@@ -0,0 +1,100 @@
+import type { Ereignis } from "@/types/vorfaelleundvorkommnisse/Ereignis.ts";
+import type { VueWrapper } from "@vue/test-utils";
+
+import {
+  COMPONENT_EVENT_TESTS,
+  COMPONENT_RENDER_TESTS,
+  getSnapshotFilename,
+} from "@tests/utils/testutils.ts";
+import { useVorfaelleundvorkommnisseTestDataFactory } from "@tests/utils/vorfaelleundvorkommnisse/VorfaelleundvorkommnisseTestDataFactory.ts";
+import { flushPromises, mount } from "@vue/test-utils";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { nextTick } from "vue";
+import { VTextarea, VTextField } from "vuetify/components";
+
+import BaseDateInput from "@/components/common/inputs/BaseDateInput.vue";
+import BaseTimeInput from "@/components/common/inputs/BaseTimeInput.vue";
+import YesNoDialog from "@/components/common/YesNoDialog.vue";
+import BaseEreignisRow from "@/components/vorfaelleundvorkommnisse/BaseEreignisRow.vue";
+import vuetify from "@/plugins/vuetify.ts";
+import { useEreignisStore } from "@/stores/ereignisStore.ts";
+import { EreignisBuilder } from "@/types/vorfaelleundvorkommnisse/Ereignis.ts";
+
+const { createEreignis, prepareEreignis } =
+  useVorfaelleundvorkommnisseTestDataFactory();
+
+describe("BaseEreignisRow.vue", () => {
+  const ResizeObserverMock = vi.fn(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn(),
+  }));
+  // Stub the global ResizeObserver
+  vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+
+  let wrapper: VueWrapper;
+  beforeEach(() => {
+    wrapper = mount(BaseEreignisRow, {
+      global: {
+        plugins: [vuetify],
+      },
+      props: {
+        lineNumber: 1,
+        modelValue: createEreignis(),
+      },
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  describe(COMPONENT_RENDER_TESTS, () => {
+    it("should_renderRowWithData_when_mounted", async (context) => {
+      await wrapper.setProps({
+        lineNumber: 42,
+        modelValue: prepareEreignis()
+          .beschreibung("dies ist eine Beschreibung")
+          .uhrzeit(new Date("2025-07-29T15:36:42.23"))
+          .build(),
+      });
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+
+    it("should_renderRowWithErrorMessages_when_mountedWithoutAnyData", async (context) => {
+      await wrapper.setProps({
+        lineNumber: 42,
+        modelValue: prepareEreignis()
+          .beschreibung(undefined)
+          .uhrzeit(undefined)
+          .build(),
+      });
+
+      wrapper.findAllComponents(VTextField).forEach((component) => {
+        component.vm.validate();
+      });
+      wrapper.findComponent(VTextarea).vm.validate();
+
+      await flushPromises();
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+  });
+
+  describe(COMPONENT_EVENT_TESTS, () => {
+    it("should_openYesNoDialog_when_deleteIconIsClicked", async () => {
+      const deleteIcon = wrapper.findComponent(
+        '[data-test="delete-ereignis-icon"]'
+      );
+      expect(deleteIcon.exists()).toBe(true);
+
+      await deleteIcon.trigger("click");
+
+      expect(wrapper.emitted("delete")).toEqual([[]]);
+    });
+  });
+});

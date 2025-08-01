c2207ebda2f6de3a34d038843e281e00ec75ca94
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-08-01 15:25:07 +0200

    add as const to fetch strategies enum

--- wls-gui-wahllokalsystem/src/types/api/FetchStrategiesEnum.ts
+++ wls-gui-wahllokalsystem/src/types/api/FetchStrategiesEnum.ts
@@ -2,7 +2,7 @@ export const FetchStrategiesEnum = {
   STRATEGY_OFFLINE_FIRST: "STRATEGY_OFFLINE_FIRST",
   STRATEGY_ONLINE_ONLY: "STRATEGY_ONLINE_ONLY",
   STRATEGY_ONLINE_FIRST: "STRATEGY_ONLINE_FIRST",
-};
+} as const;
 
 export type FetchStrategiesEnum =
   (typeof FetchStrategiesEnum)[keyof typeof FetchStrategiesEnum];

36b8ab9447aa95e06e5cdc49c40d511e2b1e97b0
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-08-03 10:12:18 +0200

    added component and render tests

---
+++ wls-gui-wahllokalsystem/tests/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable.spec.ts
@@ -0,0 +1,108 @@
+import type { TestingPinia } from "@pinia/testing";
+
+import { createTestingPinia } from "@pinia/testing";
+import { useStimmabgabevermerkeTestDataFactory } from "@tests/utils/stimmabgabevermerke/StimmabgabevermerkeTestDataFactory.ts";
+import {
+  COMPONENT_EVENT_TESTS,
+  COMPONENT_RENDER_TESTS,
+  getSnapshotFilename,
+} from "@tests/utils/testutils.ts";
+import { mount } from "@vue/test-utils";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { VNumberInput } from "vuetify/components";
+
+import TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable from "@/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable.vue";
+import vuetify from "@/plugins/vuetify.ts";
+import { useStimmabgabevermerkeStore } from "@/stores/stimmabgabevermerkeStore.ts";
+import { EingenommenerWahlscheinStimmzettelartEnum } from "@/types/stimmabgabermerke/EingenommenerWahlscheinStimmzettelartEnum.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  getWahlNameOrBlankStringById: vi.fn(),
+}));
+vi.mock("@/stores/wahlenStore.ts", () => ({
+  useWahlenStore: () => ({
+    getWahlNameOrBlankStringById: mockDefinitions.getWahlNameOrBlankStringById,
+  }),
+}));
+
+describe("TheUwbStimmabgabevermerkeEingenommeneWahlscheineTable", () => {
+  const { createStimmabgabevermerke } = useStimmabgabevermerkeTestDataFactory();
+  let stimmabgabevermerkeStore: ReturnType<typeof useStimmabgabevermerkeStore>;
+  let testPinia: TestingPinia;
+
+  beforeEach(() => {
+    testPinia = createTestingPinia({
+      stubActions: false,
+      createSpy: vi.fn,
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe(COMPONENT_RENDER_TESTS, () => {
+    it("should_renderTableWithThreeEntries_when_stimmabgabevermerkeHasThreeWahldatenEntries", async (context) => {
+      const stimmabgabevermerke = createStimmabgabevermerke();
+      stimmabgabevermerkeStore = useStimmabgabevermerkeStore(testPinia);
+      stimmabgabevermerkeStore.stimmabgabevermerke = stimmabgabevermerke;
+
+      mockDefinitions.getWahlNameOrBlankStringById.mockReturnValue("Wahlname");
+
+      const wrapper = mount(
+        TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable,
+        {
+          global: {
+            plugins: [testPinia, vuetify],
+          },
+        }
+      );
+
+      expect(wrapper.findAllComponents(VNumberInput).length).toBe(3);
+      expect(wrapper.findAll("th").length).toBe(3);
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+  });
+
+  describe(COMPONENT_EVENT_TESTS, () => {
+    it("should_changeValuesInStore_when_numberInputValueChanges", async (context) => {
+      const stimmabgabevermerke = createStimmabgabevermerke();
+      stimmabgabevermerkeStore = useStimmabgabevermerkeStore(testPinia);
+      stimmabgabevermerkeStore.stimmabgabevermerke = stimmabgabevermerke;
+
+      mockDefinitions.getWahlNameOrBlankStringById.mockReturnValue("Wahlname");
+      const newNumberInputValue = 42;
+
+      const wrapper = mount(
+        TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable,
+        {
+          global: {
+            plugins: [testPinia, vuetify],
+          },
+        }
+      );
+
+      const numberInputs = wrapper.findAllComponents(VNumberInput);
+      numberInputs.forEach((numberInput) => {
+        numberInput.setValue(newNumberInputValue);
+      });
+
+      stimmabgabevermerkeStore.stimmabgabevermerke.wahldaten.forEach(
+        (wahldatenEntries) => {
+          expect(
+            wahldatenEntries.eingenommeneWahlscheine.get(
+              EingenommenerWahlscheinStimmzettelartEnum.Klein
+            )
+          ).toBe(newNumberInputValue);
+        }
+      );
+
+      await expect(wrapper.html()).toMatchFileSnapshot(
+        getSnapshotFilename(context)
+      );
+    });
+  });
+});

76002f90d4bc136fde90afd6bc0a20ebff2ef328
Robert Jasny <robert.jasny@muenchen.de>
2025-08-04 17:53:58 +0200

    component added to count Stimmzettelumschlaege for Wahl

--- wls-gui-wahllokalsystem/src/components/ergebnisermittlung/TheErgebnisermittlungStimmzettelumschlaegeCard.vue
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/TheErgebnisermittlungStimmzettelumschlaegeCard.vue
@@ -10,6 +10,7 @@
           <div class="d-flex flex-wrap justify-start">
             <div>
               <v-number-input
+                v-model="wahl.stimmzettelumschlaege.anzahlWaehler"
                 class="mr-4"
                 :rules="[REQUIRED, MIN_NUMBER(0), MAX_NUMBER(999)]"
                 min-width="30rem"
@@ -24,8 +25,8 @@
       <v-card-actions>
         <base-button-save
           active
-          :disabled="isSaveButtonDisabled"
           :loading="isStimmzettelumschlaegeSaving"
+          :disabled="isSaveButtonDisabled"
           @click="onSaveAnzahlStimmzettelClicked"
         />
       </v-card-actions>
@@ -34,18 +35,6 @@
 </template>
 
 <script setup lang="ts">
-// TODO:
-// Objekt 'stimmzettelumschlaege' mit wahlID und wahlbezirkID übergeben
-// Anzahl Stimmzettel eingeben, beim Speichern checken gegen Anzahl an Stimmabgabevermerken
-// -> bei Ungleichheit Dialog anzeigen mit Begründung
-
-// initial falls vorhanden stimmzettelvorschlaege vorbelegen
-// GET /businessActions/stimmzettelumschlaege/{wahlID}/{wahlbezirkID}
-
-// initial falls vorhanden Begruendung für von der Anzahl an Stimmabgabevermerken
-// abweichenden Zahl an Stimmzetteln vorbelegen
-// GET /businessActions/begruendung/{wahlbezirkID}/{wahlID}/{stapelart}
-
 import { computed, ref } from "vue";
 import {
   VCard,
@@ -61,17 +50,25 @@ import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
 import { useWahlenStore } from "@/stores/wahlenStore.ts";
 import { MAX_NUMBER, MIN_NUMBER, REQUIRED } from "@/util/rules.ts";
 
-const anzahlStimmzettelValidForm = ref<null | boolean>(null);
-const anzahlStimmzettelForm = ref<HTMLFormElement>();
+const props = defineProps<{
+  wahlID: string;
+}>();
 
-const { saveStimmzettelumschlaege, isStimmzettelumschlaegeSaving } =
-  useWahlenStore();
+const {
+  getWahlOrUndefinedById,
+  saveStimmzettelumschlaege,
+  isStimmzettelumschlaegeSaving,
+} = useWahlenStore();
+
+const wahl = getWahlOrUndefinedById(props.wahlID);
+
+const anzahlStimmzettelValidForm = ref<null | boolean>(null);
 
 const isSaveButtonDisabled = computed(() => {
   return anzahlStimmzettelValidForm.value !== true;
 });
 
 function onSaveAnzahlStimmzettelClicked() {
-  saveStimmzettelumschlaege();
+  saveStimmzettelumschlaege(wahl.wahlID);
 }
 </script>

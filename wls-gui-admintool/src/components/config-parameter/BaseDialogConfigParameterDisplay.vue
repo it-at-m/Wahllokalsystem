c053ebeaafb6250835264b7ccaeedbe0f02e886c
xGnouHQ <181658429+xGnouHQ@users.noreply.github.com>
2025-08-07 12:57:51 +0200

    :sparkles: Vollständige Implementierung der ConfigParamDialog-Komponente inkl. Events cancelEdit, commitEdit

--- wls-gui-admintool/src/components/config-parameter/BaseDialogConfigParameterDisplay.vue
+++ wls-gui-admintool/src/components/config-parameter/BaseDialogConfigParameterDisplay.vue
@@ -1,80 +1,71 @@
 <template>
-  <base-card-config-parameter-display
-    :config-parameter="configParameter"
-    @click="showDialog"
-  />
-  <div
-    ref="dialogRef"
-    v-if="visible"
+  <v-dialog
+    v-model="visible"
+    data-test="dialog-configparameter-override-wert"
   >
     <v-confirm-edit
       :model-value="visible"
-      persistent
-      max-width="648px"
-      data-test="dialog-configparameter-override-wert"
-      update:model-Value="updateVisible"
+      hide-actions
     >
-      <template v-slot:default="{ actions }">
+      <template v-slot:default>
         <v-card
-          max-width="600"
-          style="background-color: #ffb74d"
-          :title="`Dialog Konfigurationsparameter ${configParameter.name}`"
+          :title="`Konfigurationsparameter '${configParameter.name}' Bearbeiten`"
         >
           <template v-slot:text>
             <v-row class="my-2 align-center">
               <v-col cols="6">Standardwert:</v-col>
               <v-col cols="6">{{ configParameter?.defaultValue }}</v-col>
             </v-row>
-            <v-row class="my-2 align-center">
-              <v-col cols="6">Wert:</v-col>
-              <v-col cols="6"
-                >{{ configParameter?.wert }}
-                <v-card-actions>
-                  <v-btn @click="resetValue">Zurücksetzen</v-btn>
-                </v-card-actions>
-              </v-col>
-            </v-row>
-            <v-spacer/>
             <v-text-field
               v-model="model"
-              @input="isChanged = true"
-              messages="Bitte geben Sie einen Text in das Eingabefeld ein und bestätigen Sie, ob Sie den neuen Wert übernehmen wollen."
+              data-test="config-value-input"
+              variant="underlined"
+              label="Konfigurationsparameterwert"
+              hint="Bitte Wert eingeben und Änderung übernehmen oder verwerfen"
             />
           </template>
           <template v-slot:actions>
             <base-button-cancel
               color="red"
-              :disabled="!isChanged"
               @click="onConfigParameterEditCanceled"
+              data-test="cancel-edit-button"
             />
             <base-button-confirm
+              color="success"
               :disabled="!isChanged"
               @click="onConfigParameterEditCommited"
+              data-test="commit-edit-button"
             />
+            <v-btn
+              @click="resetToDefaultValue"
+              class="ml-auto"
+              data-test="reset-button"
+              >Auf Standardwert Zurücksetzen
+            </v-btn>
           </template>
         </v-card>
       </template>
     </v-confirm-edit>
-  </div>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
 import type { InfomanagementConfigParameter } from "@/types/config/InfomanagementConfigParameter.ts";
 import type { PropType } from "vue";
 
-import { defineEmits, defineProps, ref, shallowRef } from "vue";
+import { computed, ref } from "vue";
 import {
   VBtn,
   VCard,
   VCol,
   VConfirmEdit,
+  VDialog,
   VRow,
   VTextField,
 } from "vuetify/components";
 
 import BaseButtonCancel from "@/components/common/BaseButtonCancel.vue";
 import BaseButtonConfirm from "@/components/common/BaseButtonConfirm.vue";
-import BaseCardConfigParameterDisplay from "@/components/config-parameter/BaseCardConfigParameterDisplay.vue";
 
 const props = defineProps({
   configParameter: {
@@ -83,10 +74,9 @@ const props = defineProps({
   },
 });
 
-const model = shallowRef(props.configParameter.wert);
+const model = ref(props.configParameter.wert);
 const visible = ref(false);
-const isChanged = ref(false);
-const dialogRef = ref();
+const isChanged = computed(() => model.value !== props.configParameter.wert);
 
 const emit = defineEmits<{
   cancelEdit: [wert: string];
@@ -94,34 +84,28 @@ const emit = defineEmits<{
 }>();
 
 defineExpose({
-  showDialog,
-  hideDialog,
-  resetValue,
+  showDialog() {
+    visible.value = true;
+  },
+  hideDialog() {
+    visible.value = true;
+  },
+  resetToDefaultValue,
 });
 
 function onConfigParameterEditCanceled() {
   emit("cancelEdit", "Kein Payload, Keine Änderung");
-  resetModel(); // Setze isChanged zurück
+  resetModel();
   hideDialog();
 }
 
 function onConfigParameterEditCommited() {
-  emit(
-    "commitEdit",
-    `Neue Daten für den Konfigurationsparameter ${props.configParameter.name} => '${model.value}'`
-  );
-  props.configParameter.wert = model.value;
-  hideDialog(); // Schließe den Dialog
+  emit("commitEdit", `Neue Daten für den Konfigurationsparameter ${props.configParameter.name} => '${model.value}'`);
+  hideDialog();
 }
 
-function resetValue() {
-  model.value = props.configParameter.defaultValue; // Setze den Wert auf den Standardwert
-  props.configParameter.wert = model.value; // Aktualisiere den Wert des Konfigurationsparameters
-  isChanged.value = true; // Setze isChanged auf true, um den Zustand zu reflektieren
-}
-
-function showDialog() {
-  visible.value = true;
+function resetToDefaultValue() {
+  model.value = props.configParameter.defaultValue;
 }
 
 function hideDialog() {
@@ -129,7 +113,6 @@ function hideDialog() {
 }
 
 function resetModel() {
-  model.value = props.configParameter.wert; // Setze das Modell zurück
-  isChanged.value = false; // Setze isChanged zurück
+  model.value = props.configParameter.wert;
 }
 </script>

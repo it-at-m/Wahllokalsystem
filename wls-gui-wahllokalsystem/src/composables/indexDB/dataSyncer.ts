8278309eb0fcef088e7c360e755da09cb0d9f676
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-08-10 21:38:22 +0200

    rework offline syncer component

--- wls-gui-wahllokalsystem/src/composables/indexDB/dataSyncer.ts
+++ wls-gui-wahllokalsystem/src/composables/indexDB/dataSyncer.ts
@@ -1,29 +1,28 @@
 import axios from "axios";
 
 import { basicPostConfig } from "@/api/axios-utils.ts";
-import { useLogging } from "@/composables/common/logging.ts";
 import { useIndexDB } from "@/composables/indexDB/indexDB.ts";
 import { FetchStrategiesEnum } from "@/types/api/FetchStrategiesEnum.ts";
 
 const { getDirtyItems } = useIndexDB();
-const { logDebug } = useLogging(`useDataSyncer`);
 
 export function useDataSyncer() {
-  async function syncDirtyData() {
+  async function getSyncTasks() {
     const itemsToSync = await getDirtyItems();
-    for (const item of itemsToSync) {
-      logDebug(`syncing item with key ${item.key}`);
-      await axios.request(
-        basicPostConfig(
-          item.key,
-          FetchStrategiesEnum.STRATEGY_ONLINE_FIRST,
-          JSON.parse(item.item.data)
-        )
-      );
-    }
+    return itemsToSync.map((item) => ({
+      name: item.key,
+      callback: () =>
+        axios.request(
+          basicPostConfig(
+            item.key,
+            FetchStrategiesEnum.STRATEGY_ONLINE_FIRST,
+            JSON.parse(item.item.data)
+          )
+        ),
+    }));
   }
 
   return {
-    syncDirtyData,
+    getSyncTasks,
   };
 }

ee298999f4c74af3267434f08782bec852d41861
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-08-11 18:53:40 +0200

    created tests for create response functions

--- wls-gui-wahllokalsystem/tests/composables/common/commonApiUtils.spec.ts
+++ wls-gui-wahllokalsystem/tests/composables/common/commonApiUtils.spec.ts
@@ -1,11 +1,22 @@
 import { useAxiosTestDataFactory } from "@tests/utils/common/AxiosTestDataFactory.ts";
+import { useIndexDBValueTestDataFactory } from "@tests/utils/indexDB/IndexDBValueTestDataFactory.ts";
+import { HttpStatusCode } from "axios";
 import { describe, expect, it } from "vitest";
 
 import { useCommonApiUtils } from "@/composables/common/commonApiUtils.ts";
+import { HTTP_HEADER_CONTENT_TYPE } from "@/constants.ts";
+
+const { createIndexDBValue, prepareIndexDBValue } =
+  useIndexDBValueTestDataFactory();
 
 describe("commonApiUtils.ts", () => {
   const { createAxiosResponse } = useAxiosTestDataFactory();
-  const { getNullOn204OrElseResponseData } = useCommonApiUtils();
+  const {
+    getNullOn204OrElseResponseData,
+    createResponseOfIndexDBValue,
+    createResponseOkWithoutResponseBody,
+    createResponseNotFound,
+  } = useCommonApiUtils();
 
   describe("getNullOn204OrElseResponseData", () => {
     it.each([205, 203])(
@@ -36,5 +47,57 @@ describe("commonApiUtils.ts", () => {
         expect(result).toStrictEqual(null);
       }
     );
+  });
+
+  describe("createResponseOfIndexDBValue", () => {
+    it("should_createResponseWithIndexDBValueHttpStatus_when_indexDBValuesHasHttpStatusCode", async () => {
+      const indexDBValue = createIndexDBValue();
+
+      const result = createResponseOfIndexDBValue(indexDBValue);
+
+      //have to compare field by field cause strictEqual won't work with response
+      expect(result.status).toEqual(indexDBValue.httpStatus);
+      expect(result.statusText).toEqual("fetched from idb");
+      expect(result.headers.get(HTTP_HEADER_CONTENT_TYPE)).toEqual(
+        indexDBValue.contentType
+      );
+      expect(await result.text()).toEqual(indexDBValue.data);
+    });
+
+    it("should_createResponseWithFallbackHttpStatusCode_when_indexDBValuesHasNoHttpStatusCode", async () => {
+      const indexDBValue = prepareIndexDBValue().httpStatus(undefined).build();
+
+      const fallbackHttpStatusCode = 401;
+      const result = createResponseOfIndexDBValue(
+        indexDBValue,
+        fallbackHttpStatusCode
+      );
+
+      //have to compare field by field cause strictEqual won't work with response
+      expect(result.status).toEqual(fallbackHttpStatusCode);
+      expect(result.statusText).toEqual("fetched from idb");
+      expect(result.headers.get(HTTP_HEADER_CONTENT_TYPE)).toEqual(
+        indexDBValue.contentType
+      );
+      expect(await result.text()).toEqual(indexDBValue.data);
+    });
+  });
+
+  describe("createResponseOkWithoutResponseBody", () => {
+    it("should_createResponseWithoutBodyAndWithStatusOk_when_called", async () => {
+      const result = createResponseOkWithoutResponseBody();
+      expect(result).toStrictEqual(
+        new Response(null, { status: HttpStatusCode.Ok })
+      );
+    });
+  });
+
+  describe("createResponseNotFound", () => {
+    it("should_createResponseWithoutBodyAndWithStatusNotFound_when_called", async () => {
+      const result = createResponseNotFound();
+      expect(result).toStrictEqual(
+        new Response(null, { status: HttpStatusCode.NotFound })
+      );
+    });
   });
 });

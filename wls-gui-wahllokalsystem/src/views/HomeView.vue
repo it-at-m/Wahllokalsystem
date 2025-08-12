85b1c72245aade3c8c3e35281e4205cb58612b64
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-08-12 09:49:08 +0200

    revert unnecessary changes

--- wls-gui-wahllokalsystem/src/views/HomeView.vue
+++ wls-gui-wahllokalsystem/src/views/HomeView.vue
@@ -5,22 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { useRegisterSW } from "virtual:pwa-register/vue";
 import { VContainer } from "vuetify/components";
 
 import BaseOfflineLoading from "@/components/wlsComponents/BaseOfflineLoading.vue";
-import { useLogging } from "@/composables/common/logging.ts";
-import HomeView from "@/views/HomeView.vue";
-
-const { log } = useLogging(HomeView.__name ?? "");
-
-useRegisterSW({
-  immediate: true,
-  onNeedRefresh: () => log("onNeedRefresh"),
-  onOfflineReady: () => log("onOfflineReady"),
-  onRegisteredSW: () => {
-    log("onRegisteredSW");
-  },
-  onRegisterError: () => log("onRegisterError"),
-});
 </script>

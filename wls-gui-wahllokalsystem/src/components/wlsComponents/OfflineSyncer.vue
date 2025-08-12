9b2e954b47b8b923730860fbaf5137f468df7e56
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-08-12 08:42:20 +0200

    created component tests for OfflineSyncer

--- wls-gui-wahllokalsystem/src/components/wlsComponents/OfflineSyncer.vue
+++ wls-gui-wahllokalsystem/src/components/wlsComponents/OfflineSyncer.vue
@@ -14,6 +14,7 @@
             size="x-small"
             color="primary"
             :loading="isSyncInProgress"
+            data-test="button-sync-offline-data"
             @click="synchronizeOfflineData"
           />
         </template>

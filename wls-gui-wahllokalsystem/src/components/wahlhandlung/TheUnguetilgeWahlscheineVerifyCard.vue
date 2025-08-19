dba6ed53334ed7b877be0a21f5891341182526cd
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-08-19 10:37:28 +0200

    #1657 add input to enter votes and save as ereignis

--- wls-gui-wahllokalsystem/src/components/wahlhandlung/TheUnguetilgeWahlscheineVerifyCard.vue
+++ wls-gui-wahllokalsystem/src/components/wahlhandlung/TheUnguetilgeWahlscheineVerifyCard.vue
@@ -41,19 +41,38 @@
           :title="titleFeedbackWahlscheinUngueltig"
           :type="InputFeedbackTypeEnum.error"
         >
-          <div>
-            <ul>
-              <li>
-                Die Person darf mit diesem Wahlschein keine Stimme abgeben!
-              </li>
-              <li>Behalten Sie den Wahlschein ein.</li>
-              <li>Fassen Sie einen Beschluss über die Zurückweisung.</li>
-              <li>
-                Erfassen Sie dies als besonderes Vorkommnis unter dem Punkt
-                "Ereignisse".
-              </li>
-            </ul>
-          </div>
+          <ul>
+            <li>Die Person darf mit diesem Wahlschein keine Stimme abgeben!</li>
+            <li>Behalten Sie den Wahlschein ein.</li>
+            <li>Fassen Sie einen Beschluss über die Zurückweisung.</li>
+            <li>Erfassen Sie dies als besonderes Vorkommnis:</li>
+          </ul>
+          <template #additionalFeedback>
+            <v-row>
+              <v-col cols="4">
+                {{ ereignisBeschreibungWahlscheinUnguelttig }}
+              </v-col>
+              <v-col>
+                <v-form v-model="isAbstimmungsergebnisFormValid">
+                  <v-number-input
+                    v-model="stimmenZurueckweisung"
+                    label="Stimmen für die Zurückwesiung"
+                    width="350"
+                    :rules="[required]"
+                  />
+                </v-form>
+              </v-col>
+              <v-col>
+                <base-button-save
+                  active
+                  class="mt-2 ml-5"
+                  :disabled="!isAbstimmungsergebnisFormValid"
+                  @click="onSaveAbstimmungsergebnisClicked"
+                />
+              </v-col>
+              <v-spacer />
+            </v-row>
+          </template>
         </base-input-feedback-card>
         <v-img
           position="left"
@@ -104,14 +123,19 @@ import { computed, ref, useTemplateRef } from "vue";
 
 import wahlscheinExampleImage from "@/assets/previewWahlscheinnummerOnWahlschein.png";
 import BaseButtonRefresh from "@/components/common/buttons/BaseButtonRefresh.vue";
+import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
 import BaseInputFeedbackCard from "@/components/common/cards/BaseInputFeedbackCard.vue";
 import { useRules } from "@/composables/common/rules.ts";
+import { useEreignisStore } from "@/stores/ereignisStore.ts";
 import { useWahlbezirkStore } from "@/stores/wahlbezirkStore.ts";
 import { InputFeedbackTypeEnum } from "@/types/common/InputFeedbackTypeEnum.ts";
 
 const { maxNumber, minNumber, required } = useRules();
+const { addEreignis, sendEreignisse } = useEreignisStore();
 
 const isFormValid = ref<boolean | null>(null);
+const isAbstimmungsergebnisFormValid = ref<boolean | null>(null);
+
 const isSearchButtonDisabled = computed(() => !isFormValid.value);
 const wahlscheinValidationForm = useTemplateRef(
   "wahlscheinValidationForm"
@@ -128,6 +152,7 @@ const wahlscheinnummer = ref<null | number>(null);
 const ungueltigerWahlschein = ref<null | undefined | UngueltigerWahlschein>(
   undefined
 );
+const stimmenZurueckweisung = ref<number | null>(null);
 
 const feedbackNoDataAvailableIsVisible = computed(
   () =>
@@ -152,6 +177,9 @@ const searchButtonLabel = computed(() =>
 const titleFeedbackWahlscheinUngueltig = computed(() => {
   return `Wahlschein ${ungueltigerWahlschein.value?.wahlscheinnummer ?? ""} für ${ungueltigerWahlschein.value?.vorname} ${ungueltigerWahlschein.value?.familienname} ist ungültig`;
 });
+const ereignisBeschreibungWahlscheinUnguelttig = computed(() => {
+  return `${titleFeedbackWahlscheinUngueltig.value}. Die Person wurde zurückgewiesen. Abstimmungsergebnis: `;
+});
 
 function onRefreshClicked() {
   ungueltigeWahlscheineActions.loadUngueltigeWahlscheine();
@@ -167,6 +195,16 @@ function onSearchClicked() {
         `${wahlscheinnummer.value}`
       );
   }
+}
+
+function onSaveAbstimmungsergebnisClicked() {
+  addEreignis({
+    uhrzeit: new Date(),
+    beschreibung: `${ereignisBeschreibungWahlscheinUnguelttig.value} ${stimmenZurueckweisung.value}`,
+  });
+  sendEreignisse();
+
+  stimmenZurueckweisung.value = null;
 }
 
 function onWahlscheinnummerChanged(newValue: number) {

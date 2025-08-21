a48ff63b4b0c5d9cbbc48dceee809b549191df3e
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-08-21 14:58:44 +0200

    script und template verschieben

--- wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeErfassenCard.vue
+++ wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeErfassenCard.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
-import { VCard, VCardTitle } from "vuetify/components";
-
-import TheUWBStimmabgabevermerkeErfassenTable from "@/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeErfassenTable.vue";
-</script>
-
 <template>
   <v-card>
     <v-card-title> Anzahl der Blätter im Wählerverzeichnis</v-card-title>
     <the-u-w-b-stimmabgabevermerke-erfassen-table />
   </v-card>
 </template>
+
+<script setup lang="ts">
+import { VCard, VCardTitle } from "vuetify/components";
+
+import TheUWBStimmabgabevermerkeErfassenTable from "@/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeErfassenTable.vue";
+</script>
 
 <style scoped></style>

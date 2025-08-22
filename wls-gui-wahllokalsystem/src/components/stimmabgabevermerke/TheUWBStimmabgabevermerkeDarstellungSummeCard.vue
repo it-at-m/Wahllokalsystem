8503b389ccbf950c589564c9f24dfa60f1f54132
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-08-22 17:34:42 +0200

    1549 stimmabgabevermerke uwb   darstellung summen der erfassten daten (#1780)
    * added sum of Vermerke and eingenommene wahlscheine
    
    * renaming
    
    * fix
    
    * anmerkungen eingearbeitet

---
+++ wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeDarstellungSummeCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    <v-card-title> Anzahl Stimmabgabevermerke zusammen</v-card-title>
+    <v-card-text
+      ><the-u-w-b-stimmabgabevermerke-darstellung-summe-table />
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+import TheUWBStimmabgabevermerkeDarstellungSummeTable from "@/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeDarstellungSummeTable.vue";
+</script>
--- wls-gui-wahllokalsystem/tests/stores/stimmabgabevermerkeStore.spec.ts
+++ wls-gui-wahllokalsystem/tests/stores/stimmabgabevermerkeStore.spec.ts
@@ -3,6 +3,7 @@ import { useStimmabgabevermerkeTestDataFactory } from "@tests/utils/stimmabgabev
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
 import { useStimmabgabevermerkeStore } from "@/stores/stimmabgabevermerkeStore.ts";
+import { StimmzettelStimmzettelartEnum } from "@/types/stimmabgabevermerke/StimmzettelStimmzettelartEnum.ts";
 
 describe("stimmabgabevermerkeStore.ts", () => {
   let unitUnderTest: ReturnType<typeof useStimmabgabevermerkeStore>;
@@ -164,6 +165,60 @@ describe("stimmabgabevermerkeStore.ts", () => {
       const result = unitUnderTest.stimmabgabevermerkeTableTotalEachWahldaten;
 
       expect(result).toStrictEqual([6, 26]);
+    });
+  });
+
+  describe("sumEingenommeneWahlscheineAndStimmabgabevermerkeForEachWahl", () => {
+    it("should_returnMapWithCalculatedValuesForEachWahl_when_givenStimmabgabevermerke", () => {
+      const wahldatenOne = prepareWahldaten()
+        .vermerke([
+          prepareVermerk()
+            .blattnummer(2)
+            .stimmzettel([prepareStimmzettel().anzahl(10).build()])
+            .build(),
+          prepareVermerk()
+            .blattnummer(3)
+            .stimmzettel([prepareStimmzettel().anzahl(20).build()])
+            .build(),
+          prepareVermerk()
+            .blattnummer(4)
+            .stimmzettel([prepareStimmzettel().anzahl(30).build()])
+            .build(),
+        ])
+        .eingenommeneWahlscheine(
+          new Map([[StimmzettelStimmzettelartEnum.Klein, 20]])
+        )
+        .build();
+
+      const wahldatenTwo = prepareWahldaten()
+        .vermerke([
+          prepareVermerk()
+            .blattnummer(2)
+            .stimmzettel([prepareStimmzettel().anzahl(5).build()])
+            .build(),
+          prepareVermerk()
+            .blattnummer(3)
+            .stimmzettel([prepareStimmzettel().anzahl(15).build()])
+            .build(),
+          prepareVermerk()
+            .blattnummer(4)
+            .stimmzettel([prepareStimmzettel().anzahl(25).build()])
+            .build(),
+        ])
+        .eingenommeneWahlscheine(
+          new Map([[StimmzettelStimmzettelartEnum.Klein, 40]])
+        )
+        .build();
+
+      unitUnderTest.stimmabgabevermerke = prepareStimmabgabevermerke()
+        .wahldaten(new Set([wahldatenOne, wahldatenTwo]))
+        .build();
+
+      const result =
+        unitUnderTest.sumEingenommeneWahlscheineAndStimmabgabevermerkeForEachWahl;
+
+      expect(result.get(wahldatenOne.wahlID)).toStrictEqual(80);
+      expect(result.get(wahldatenTwo.wahlID)).toStrictEqual(85);
     });
   });
 });

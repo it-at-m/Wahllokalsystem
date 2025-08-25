67af379dcd4f99132070f9562080e747da74aeaa
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-08-25 19:12:26 +0200

    #1686 :white_check_mark: add service test

---
+++ wls-gui-wahllokalsystem/tests/composables/wahlvorschlaege/wahlvorschlaegeService.spec.ts
@@ -0,0 +1,53 @@
+import { useCommonTestDataFactory } from "@tests/utils/common/CommonTestDataFactory.ts";
+import { useWahlvorschlaegeTestDataFactory } from "@tests/utils/wahlvorschlaege/WahlvorschlaegeTestDataFactory.ts";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { useWahlvorschlaegeService } from "@/composables/wahlvorschlaege/wahlvorschlaegeService.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  getWahlvorschlaege: vi.fn(),
+  toModel: vi.fn(),
+  configurationConstructor: vi.fn().mockImplementation(() => ({})),
+}));
+
+vi.mock("@/api/wls-clients/generated-basisdaten-api", () => ({
+  WahlvorschlaegeControllerApi: vi.fn().mockImplementation(() => ({
+    getWahlvorschlaege: mockDefinitions.getWahlvorschlaege,
+  })),
+  Configuration: mockDefinitions.configurationConstructor,
+}));
+vi.mock("@/composables/wahlvorschlaege/wahlvorschlaegeMapper.ts", () => ({
+  useWahlvorschlaegeMapper: () => ({
+    toModel: mockDefinitions.toModel,
+  }),
+}));
+
+const { generateRandomString } = useCommonTestDataFactory();
+const { createWahlvorschlaege, createWahlvorschlaegeDto } =
+  useWahlvorschlaegeTestDataFactory();
+
+describe("wahlvorschlaegeService.ts", () => {
+  const { getWahlvorschlaege } = useWahlvorschlaegeService();
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+    vi.clearAllMocks();
+  });
+
+  describe("getWahlvorschlaege", () => {
+    it("should_returnWahlvorschlaege_when_wahlIDAndWahlbezirkIdGiven", async () => {
+      const wahlID = generateRandomString(10);
+      const wahlbezirkID = generateRandomString(10);
+      const mockedWahlvorschlaegeModel = createWahlvorschlaege();
+
+      mockDefinitions.getWahlvorschlaege.mockReturnValue(
+        Promise.resolve({ status: 200, data: createWahlvorschlaegeDto() })
+      );
+      mockDefinitions.toModel.mockReturnValue(mockedWahlvorschlaegeModel);
+
+      const result = await getWahlvorschlaege(wahlID, wahlbezirkID);
+
+      expect(result).toEqual(mockedWahlvorschlaegeModel);
+    });
+  });
+});

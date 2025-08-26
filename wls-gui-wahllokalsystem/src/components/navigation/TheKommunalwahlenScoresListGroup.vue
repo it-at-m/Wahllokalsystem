9e5479daa8bdd6e8bcadf5fb33de42c87e440aed
Viviane Johns <145964798+vjohnslhm@users.noreply.github.com>
2025-08-26 11:48:56 +0200

    1688 erweiterung navigation um ergebnisermittlung (#1788)
    * #1688 add list group allgemein
    
    * #1688 add uwb navigation
    
    * #1688 add bwb navigation
    
    * #1688 fix snapshots
    
    * #1688 move logic to script

---
+++ wls-gui-wahllokalsystem/src/components/navigation/TheKommunalwahlenScoresListGroup.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list-group value="Ergebnisse">
+    <template #activator="{ props }">
+      <v-list-item
+        v-bind="props"
+        title="Ergebnisermittlung"
+      />
+    </template>
+    <v-list-item :title="title" />
+    <the-o-b-w-scores-list-group />
+    <the-s-r-w-scores-list-group />
+    <the-b-a-w-scores-list-group />
+  </v-list-group>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+
+import TheBAWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten/TheBAWScoresListGroup.vue";
+import TheOBWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten/TheOBWScoresListGroup.vue";
+import TheSRWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten/TheSRWScoresListGroup.vue";
+import { useUserStore } from "@/stores/userStore.ts";
+
+const { isBWB } = storeToRefs(useUserStore());
+
+const title = computed(() =>
+  isBWB.value ? "Wahlscheine" : "Stimmabgabevermerke"
+);
+</script>
---
+++ wls-gui-wahllokalsystem/src/components/navigation/auszaehlung_wahlarten/TheBAWScoresListGroup.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-list-group value="BAW_Scores">
+    <template #activator="{ props }">
+      <v-list-item
+        v-bind="props"
+        title="🚧 Wahl des Bezirksausschusses"
+      />
+    </template>
+    <v-list-item title="Zählen der Stimmzettel" />
+    <v-list-item title="Ungültige Stimmzettel" />
+    <v-list-item title="Gültige Stimmzettel" />
+    <v-list-item title="Schnellmeldung" />
+    <v-list-item title="Kandidatinnen- und Kandidatenstimmen" />
+    <v-list-item title="Niederschrift" />
+  </v-list-group>
+</template>

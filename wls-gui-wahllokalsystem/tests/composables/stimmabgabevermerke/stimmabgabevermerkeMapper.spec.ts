66d809d75ca8d52b9f40cbf019ac533ac9dcb25a
DanielOber <145556033+DanielOber@users.noreply.github.com>
2025-08-27 17:33:41 +0200

    added tests

---
+++ wls-gui-wahllokalsystem/tests/composables/stimmabgabevermerke/stimmabgabevermerkeMapper.spec.ts
@@ -0,0 +1,83 @@
+import type { Stimmabgabevermerke } from "@/types/stimmabgabevermerke/Stimmabgabevermerke.ts";
+
+import { useStimmabgabevermerkeTestDataFactory } from "@tests/utils/stimmabgabevermerke/StimmabgabevermerkeTestDataFactory.ts";
+import { describe, expect, it } from "vitest";
+
+import {
+  EingenommenerWahlscheinDTOStimmzettelartEnum,
+  StimmzettelDTOStimmzettelartEnum,
+} from "@/api/wls-clients/generated-ergebnismeldung-api";
+import { useStimmabgabevermerkeMapper } from "@/composables/stimmabgabevermerke/stimmabgabevermerkeMapper.ts";
+import { EingenommenerWahlscheinStimmzettelartEnum } from "@/types/stimmabgabevermerke/EingenommenerWahlscheinStimmzettelartEnum.ts";
+import { StimmzettelStimmzettelartEnum } from "@/types/stimmabgabevermerke/StimmzettelStimmzettelartEnum.ts";
+
+const { toModel } = useStimmabgabevermerkeMapper();
+
+const {
+  prepareStimmabgabevermerkeDTO,
+  prepareWahldatenDTO,
+  prepareStimmzettelDTO,
+  prepareVermerkDTO,
+} = useStimmabgabevermerkeTestDataFactory();
+
+describe("stimmabgabevermerkeMapper.ts", () => {
+  describe("toModel", () => {
+    it("should_returnModel_when_dtoIsGiven", () => {
+      const stimmzettelDTO = prepareStimmzettelDTO()
+        .stimmzettelart(StimmzettelDTOStimmzettelartEnum.Klein)
+        .build();
+
+      const vermerkDTO = prepareVermerkDTO()
+        .stimmzettel(new Set([stimmzettelDTO]))
+        .build();
+
+      const eingenommenerWahlscheinDTO = {
+        stimmzettelart: EingenommenerWahlscheinDTOStimmzettelartEnum.Klein,
+        anzahl: 4,
+      };
+
+      const wahldatenDTO = prepareWahldatenDTO()
+        .vermerke(new Set([vermerkDTO]))
+        .eingenommeneWahlscheine(new Set([eingenommenerWahlscheinDTO]))
+        .build();
+
+      const dto = prepareStimmabgabevermerkeDTO()
+        .wahldaten(new Set([wahldatenDTO]))
+        .build();
+
+      const model: Stimmabgabevermerke = {
+        anzahlBlaetter: dto.anzahlBlaetter,
+        waehlerverzeichnisNummer: dto.waehlerverzeichnisNummer,
+        wahlbezirkID: dto.wahlbezirkID,
+        wahldaten: [
+          {
+            wahlID: wahldatenDTO.wahlID,
+            waehlerverzeichnisNummer: wahldatenDTO.waehlerverzeichnisNummer,
+            vermerke: [
+              {
+                blattnummer: vermerkDTO.blattnummer,
+                stimmzettel: [
+                  {
+                    anzahl: stimmzettelDTO.anzahl,
+                    stimmzettelart: StimmzettelStimmzettelartEnum.Klein,
+                  },
+                ],
+              },
+            ],
+            wahlbezirkID: wahldatenDTO.wahlbezirkID,
+            eingenommeneWahlscheine: new Map([
+              [
+                EingenommenerWahlscheinStimmzettelartEnum.Klein,
+                eingenommenerWahlscheinDTO.anzahl,
+              ],
+            ]),
+          },
+        ],
+      };
+
+      const result = toModel(dto);
+
+      expect(result).toStrictEqual(model);
+    });
+  });
+});

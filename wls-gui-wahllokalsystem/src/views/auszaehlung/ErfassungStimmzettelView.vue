bcedd5376d6bd152853760ecb97fd0bd787c32b2
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-09-01 16:27:44 +0200

    #1692 add watcher for url

--- wls-gui-wahllokalsystem/src/views/auszaehlung/ErfassungStimmzettelView.vue
+++ wls-gui-wahllokalsystem/src/views/auszaehlung/ErfassungStimmzettelView.vue
@@ -7,14 +7,18 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import { useRoute } from "vue-router";
+import { computed, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 import TheErgebnisermittlungStimmzettelumschlaegeCard from "@/components/ergebnisermittlung/TheErgebnisermittlungStimmzettelumschlaegeCard.vue";
+import { ROUTE_AUSZAEHLUNG_STIMMZETTEL } from "@/constants.ts";
 import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlenStore } from "@/stores/wahlenStore.ts";
 
 const route = useRoute();
+const router = useRouter();
 const { isUWB } = useUserStore();
+const { getWahlOrUndefinedById } = useWahlenStore();
 
 const wahlID = computed(() => route.params.wahlId as string);
 
@@ -23,4 +27,18 @@ const title = computed(() => {
     ? "Wahlurne öffnen und Stimmzettel zählen"
     : "Wahlurne öffnen und Stimmzettelumschläge zählen";
 });
+
+watch(
+  () => route.params.wahlId,
+  (newId) => {
+    const wahl = getWahlOrUndefinedById(newId as string);
+
+    if (wahl) {
+      router.push({
+        name: ROUTE_AUSZAEHLUNG_STIMMZETTEL,
+        params: { wahlId: wahl.wahlID },
+      });
+    }
+  }
+);
 </script>

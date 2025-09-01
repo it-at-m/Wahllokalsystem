a9f635627990522a882cd9e738e8c0d19ef7fb3e
Viviane Johns <145964798+vjohnslhm@users.noreply.github.com>
2025-09-01 08:07:13 +0200

    1687 laden der erfassten ergebnisse bei initialisierung (#1793)
    * #1687 rename ereignismeldung to ergebnismeldung
    
    * #1687 add types
    
    * #1687 add mapper
    
    * #1687 add service
    
    * #1687 add store
    
    * #1687 add tasks
    
    * #1687 add testDataFactory
    
    * #1687 fix mapper
    
    * #1687 remove wahlbezirkID from store function
    
    * #1687 coderabbit: fix parameter order in service
    
    * #1687 nitpick: prefer immutable construction for tasks list
    
    * #1687 nitpick: avoid double wrapping promise
    
    * #1687 nitpick: add service test case for api returning 204
    
    * #1687 nitpick: simplyfy async error assertion
    
    * #1687 nitpick: add enum mapping coverage
    
    * #1687 nitpick: await task callbacks
    
    * #1687 nitpick: use forEach instead of filter
    
    * #1687 nitpick: add test skip stapel OwbBLeer for uwb users
    
    * #1687 nitpick: use strict equality
    
    * #1687 nitpick: simplify ergebnisse dto to model mapping by using .map instead of .forEach
    
    * #1687 nitpick: take stapelart as param in testdata factory functions
    
    * #1687 nitpick: rename stapelart to stapelArt
    
    * #1687 nitpick: fix import
    
    * #1687 refactor task creation

---
+++ wls-gui-wahllokalsystem/src/types/ergebnismeldung/StapelArtEnum.ts
@@ -0,0 +1,14 @@
+export const StapelArtEnum = {
+  ObwA: "OBW_A",
+  ObwBLeer: "OBW_B_LEER",
+  ObwBUngekennzeichnet: "OBW_B_UNGEKENNZEICHNET",
+  ObwCGueltig: "OBW_C_GUELTIG",
+  ObwCUngueltig: "OBW_C_UNGUELTIG",
+  SrwBawA: "SRW_BAW_A",
+  SrwBawB: "SRW_BAW_B",
+  SrwBawAB: "SRW_BAW_A_B",
+  SrwBawDUngueltig: "SRW_BAW_D_UNGUELTIG",
+  SrwBawBC: "SRW_BAW_B_C",
+} as const;
+
+export type StapelArtEnum = (typeof StapelArtEnum)[keyof typeof StapelArtEnum];

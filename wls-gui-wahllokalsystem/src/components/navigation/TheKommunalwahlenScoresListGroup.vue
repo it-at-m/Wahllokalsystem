caa02ffdcad98b90ed53f0893c1be2ef5615706c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-09-05 08:17:09 +0200

    Revert "use to list items for navigation step and fix url to stimmabgabevermerke"
    This reverts commit e6289f8cabab0603b87fcfd6fe101e44e5fdfafd.

--- wls-gui-wahllokalsystem/src/components/navigation/TheKommunalwahlenScoresListGroup.vue
+++ wls-gui-wahllokalsystem/src/components/navigation/TheKommunalwahlenScoresListGroup.vue
@@ -7,14 +7,8 @@
       />
     </template>
     <v-list-item
-      v-if="isBWB"
-      title="Wahlscheine"
-      to="/Wahlscheine"
-    />
-    <v-list-item
-      v-if="isUWB"
-      title="Stimmabgabevermerke"
-      :to="'/' + ROUTE_STIMMABGABEVERMERKE"
+      :title="title"
+      :to="route"
     />
     <the-o-b-w-scores-list-group :title-stimmen-zaehlen="titleStimmenZaehlen" />
     <the-s-r-w-scores-list-group :title-stimmen-zaehlen="titleStimmenZaehlen" />
@@ -32,8 +26,15 @@ import TheSRWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten
 import { ROUTE_STIMMABGABEVERMERKE } from "@/constants.ts";
 import { useUserStore } from "@/stores/userStore.ts";
 
-const { isBWB, isUWB } = storeToRefs(useUserStore());
+const { isBWB } = storeToRefs(useUserStore());
 
+const title = computed(() =>
+  isBWB.value ? "Wahlscheine" : "Stimmabgabevermerke"
+);
+
+const route = computed(() =>
+  isBWB.value ? "Wahlscheine" : ROUTE_STIMMABGABEVERMERKE
+);
 const titleStimmenZaehlen = computed(() => {
   return isBWB.value
     ? "Zählen der Stimmzettelumschläge"

cb4a764194a78cbe1fbbae89b53cef13057f3a67
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-09-09 13:28:48 +0200

    #1834 change card title depending on wahlen.length

--- wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineCard.vue
+++ wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-card-title>
-      Anzahl Stimmabgabevermerke bei den eingenommenen Wahlscheinen
+      {{ title }}
     </v-card-title>
     <v-card-text>
       <the-u-w-b-stimmabgabevermerke-eingenommene-wahlscheine-table />
@@ -10,5 +10,21 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+
 import TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable from "@/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable.vue";
+import { useWahlenStore } from "@/stores/wahlenStore.ts";
+
+const { wahlenState } = storeToRefs(useWahlenStore());
+
+const title = computed(() => {
+  if (wahlenState.value.wahlen) {
+    return wahlenState.value.wahlen.length > 1
+      ? "Anzahl Stimmabgabevermerke bei den eingenommenen Wahlscheinen"
+      : "Anzahl der eingenommenen Wahlscheine";
+  } else {
+    return "";
+  }
+});
 </script>

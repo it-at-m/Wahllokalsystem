b58ef49b7ab3601dcef6cabb211134a2aca43e57
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-09-09 13:29:37 +0200

    #1834 removed `:min="0"`

--- wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable.vue
+++ wls-gui-wahllokalsystem/src/components/stimmabgabevermerke/TheUWBStimmabgabevermerkeEingenommeneWahlscheineTable.vue
@@ -28,7 +28,6 @@
           >
             <v-number-input
               max-width="15rem"
-              :min="0"
               :rules="[required]"
               :model-value="
                 getMapValue(

cbf59a36115d3862c75e1f64848f9f9df1d54b34
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-09-11 08:08:28 +0200

    remove init of ergebnisse when non where loaded

--- wls-gui-wahllokalsystem/src/stores/ergebnismeldungStore.ts
+++ wls-gui-wahllokalsystem/src/stores/ergebnismeldungStore.ts
@@ -1,6 +1,5 @@
 import type { Ergebnisse } from "@/types/ergebnismeldung/Ergebnisse.ts";
 import type { StapelArtEnum } from "@/types/ergebnismeldung/StapelArtEnum.ts";
-import type { Wahlvorschlaege } from "@/types/wahlvorschlaege/Wahlvorschlaege.ts";
 
 import { defineStore } from "pinia";
 import { ref } from "vue";
@@ -8,7 +7,6 @@ import { ref } from "vue";
 import { useHmrUpdate } from "@/composables/common/hmrUpdate.ts";
 import { useErgebnisService } from "@/composables/ergebnismeldung/ergebnisService.ts";
 import { useUserStore } from "@/stores/userStore.ts";
-import { useWahlvorschlaegeStore } from "@/stores/wahlvorschlaegeStore.ts";
 
 const { registerStoreHMR } = useHmrUpdate();
 
@@ -17,7 +15,6 @@ const storeID = "ergebnismeldung";
 export const useErgebnismeldungStore = defineStore(storeID, () => {
   const { getWahlbezirkIdFromWahlMetaDataByWahlId } = useUserStore();
   const { getErgebnisse, postErgebnisse } = useErgebnisService();
-  const wahlvorschlaegeStore = useWahlvorschlaegeStore();
 
   const ergebnisse = ref<Ergebnisse[]>([]);
 
@@ -35,9 +32,18 @@ export const useErgebnismeldungStore = defineStore(storeID, () => {
           stapelArt
         );
         if (loadedErgebnisse) {
-          _addOrUpdateErgebnisse(loadedErgebnisse);
-        } else {
-          _initErgebnisse(wahlID, wahlbezirkID, stapelArt);
+          const existingErgebnisseIndexForStapelart =
+            ergebnisse.value.findIndex(
+              (ergebnisse) =>
+                ergebnisse.bezirkUndWahlIDStapelart.wahlID === wahlID &&
+                ergebnisse.bezirkUndWahlIDStapelart.stapelArt === stapelArt
+            );
+          if (existingErgebnisseIndexForStapelart >= 0) {
+            ergebnisse.value[existingErgebnisseIndexForStapelart] =
+              loadedErgebnisse;
+          } else {
+            ergebnisse.value.push(loadedErgebnisse);
+          }
         }
       }
     } catch {
@@ -77,61 +83,6 @@ export const useErgebnismeldungStore = defineStore(storeID, () => {
         ergebnisse.bezirkUndWahlIDStapelart.stapelArt === stapelArt &&
         ergebnisse.bezirkUndWahlIDStapelart.wahlID === wahlID
     );
-  }
-
-  function _addOrUpdateErgebnisse(ergebnisseToSet: Ergebnisse) {
-    const existingErgebnisseIndexForStapelart = ergebnisse.value.findIndex(
-      (ergebnisse) =>
-        ergebnisse.bezirkUndWahlIDStapelart.wahlID ===
-          ergebnisseToSet.bezirkUndWahlIDStapelart.wahlID &&
-        ergebnisse.bezirkUndWahlIDStapelart.stapelArt ===
-          ergebnisseToSet.bezirkUndWahlIDStapelart.stapelArt
-    );
-    if (existingErgebnisseIndexForStapelart >= 0) {
-      ergebnisse.value[existingErgebnisseIndexForStapelart] = ergebnisseToSet;
-    } else {
-      ergebnisse.value.push(ergebnisseToSet);
-    }
-  }
-
-  function _initErgebnisse(
-    wahlID: string,
-    wahlbezirkID: string,
-    stapelArt: StapelArtEnum
-  ) {
-    const wahlvorschlaege =
-      wahlvorschlaegeStore.getWahlvorschlaegeByWahlIDAndWahlbezirkID(
-        wahlID,
-        wahlbezirkID
-      );
-    if (wahlvorschlaege) {
-      _initErgebnisseForWahlvorschlaege(wahlvorschlaege, stapelArt);
-    }
-  }
-
-  function _initErgebnisseForWahlvorschlaege(
-    wahlvorschlaege: Wahlvorschlaege,
-    stapel: StapelArtEnum
-  ) {
-    const key = {
-      wahlbezirkID: wahlvorschlaege.wahlbezirkID,
-      wahlID: wahlvorschlaege.wahlID,
-      stapelArt: stapel,
-    };
-    const result: Ergebnisse = {
-      bezirkUndWahlIDStapelart: key,
-      ergebnisse: [],
-    };
-    [...wahlvorschlaege.wahlvorschlaege].forEach((wahlvorschlag, index) => {
-      result.ergebnisse.push({
-        numIndex: index + 1,
-        wahlvorschlagsOrdnungszahl: wahlvorschlag.ordnungszahl,
-        ergebnis: null,
-        kandidatID: null,
-        wahlvorschlagID: wahlvorschlag.identifikator,
-      });
-    });
-    _addOrUpdateErgebnisse(result);
   }
 
   return {

33b77940f99e4618ff7b22208124306cd4bd0c7c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-09-17 12:37:07 +0200

    use loaders instead of beforeEach

--- wls-gui-wahllokalsystem/stories/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.stories.ts
+++ wls-gui-wahllokalsystem/stories/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.stories.ts
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 
 import { useErgebnisseTestDataFactory } from "@tests/utils/ergebnismeldung/ergebnisseTestDataFactory.ts";
 import { useWahlvorschlaegeTestDataFactory } from "@tests/utils/wahlvorschlaege/WahlvorschlaegeTestDataFactory.ts";
-import { createPinia, setActivePinia } from "pinia";
 
 import TheOBWStapelCErfassungCard from "@/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.vue";
 import pinia from "@/plugins/pinia";
@@ -17,16 +16,6 @@ const { prepareKandidat, prepareWahlvorschlaege, prepareWahlvorschlag } =
 const meta = {
   component: TheOBWStapelCErfassungCard,
   args: {},
-  decorators: [
-    (story) => {
-      const pinia = createPinia();
-      setActivePinia(pinia);
-      return {
-        component: { story },
-        template: "<story />",
-      };
-    },
-  ],
 } satisfies Meta<typeof TheOBWStapelCErfassungCard>;
 
 const wahlID = "wahlID";
@@ -35,73 +24,75 @@ const wahlbezirkID = "wahlbezirkID";
 export default meta;
 type Story = StoryObj<typeof meta>;
 export const Default: Story = {
-  async beforeEach() {
-    const ergebnismeldungsStore = useErgebnismeldungStore(pinia);
+  loaders: [
+    async () => {
+      const ergebnismeldungsStore = useErgebnismeldungStore(pinia);
 
-    ergebnismeldungsStore.ergebnisse = [
-      prepareErgebnisse()
-        .bezirkUndWahlIDStapelart({
-          wahlID,
-          wahlbezirkID,
-          stapelArt: StapelArtEnum.ObwCUngueltig,
-        })
-        .ergebnisse([
-          prepareErgebnis().ergebnis(1).numIndex(1).build(),
-          prepareErgebnis().ergebnis(1).numIndex(3).build(),
-        ])
-        .build(),
-      prepareErgebnisse()
-        .bezirkUndWahlIDStapelart({
-          wahlID,
-          wahlbezirkID,
-          stapelArt: StapelArtEnum.ObwCGueltig,
-        })
-        .ergebnisse([
-          prepareErgebnis()
-            .ergebnis(1)
-            .wahlvorschlagID("wahlvorschlag1")
-            .numIndex(2)
-            .build(),
-          prepareErgebnis()
-            .ergebnis(1)
-            .wahlvorschlagID("wahlvorschlag2")
-            .numIndex(4)
-            .build(),
-          prepareErgebnis()
-            .ergebnis(1)
-            .wahlvorschlagID("wahlvorschlag1")
-            .numIndex(5)
-            .build(),
-        ])
-        .build(),
-    ];
-
-    const wahlvorschlaegStore = useWahlvorschlaegeStore(pinia);
-    wahlvorschlaegStore.wahlvorschlaege = [
-      prepareWahlvorschlaege()
-        .wahlID(wahlID)
-        .wahlbezirkID(wahlbezirkID)
-        .wahlvorschlaege(
-          new Set([
-            prepareWahlvorschlag()
-              .identifikator("wahlvorschlag1")
-              .kurzname("Wahlvorschlag1")
-              .kandidaten(
-                new Set([prepareKandidat().name("Kandidat 11").build()])
-              )
+      ergebnismeldungsStore.ergebnisse = [
+        prepareErgebnisse()
+          .bezirkUndWahlIDStapelart({
+            wahlID,
+            wahlbezirkID,
+            stapelArt: StapelArtEnum.ObwCUngueltig,
+          })
+          .ergebnisse([
+            prepareErgebnis().ergebnis(1).numIndex(1).build(),
+            prepareErgebnis().ergebnis(1).numIndex(3).build(),
+          ])
+          .build(),
+        prepareErgebnisse()
+          .bezirkUndWahlIDStapelart({
+            wahlID,
+            wahlbezirkID,
+            stapelArt: StapelArtEnum.ObwCGueltig,
+          })
+          .ergebnisse([
+            prepareErgebnis()
+              .ergebnis(1)
+              .wahlvorschlagID("wahlvorschlag1")
+              .numIndex(2)
               .build(),
-            prepareWahlvorschlag()
-              .identifikator("wahlvorschlag2")
-              .kurzname("Wahlvorschlag2")
-              .kandidaten(
-                new Set([prepareKandidat().name("Kandidat 21").build()])
-              )
+            prepareErgebnis()
+              .ergebnis(1)
+              .wahlvorschlagID("wahlvorschlag2")
+              .numIndex(4)
+              .build(),
+            prepareErgebnis()
+              .ergebnis(1)
+              .wahlvorschlagID("wahlvorschlag1")
+              .numIndex(5)
               .build(),
           ])
-        )
-        .build(),
-    ];
-  },
+          .build(),
+      ];
+
+      const wahlvorschlaegStore = useWahlvorschlaegeStore(pinia);
+      wahlvorschlaegStore.wahlvorschlaege = [
+        prepareWahlvorschlaege()
+          .wahlID(wahlID)
+          .wahlbezirkID(wahlbezirkID)
+          .wahlvorschlaege(
+            new Set([
+              prepareWahlvorschlag()
+                .identifikator("wahlvorschlag1")
+                .kurzname("Wahlvorschlag1")
+                .kandidaten(
+                  new Set([prepareKandidat().name("Kandidat 11").build()])
+                )
+                .build(),
+              prepareWahlvorschlag()
+                .identifikator("wahlvorschlag2")
+                .kurzname("Wahlvorschlag2")
+                .kandidaten(
+                  new Set([prepareKandidat().name("Kandidat 21").build()])
+                )
+                .build(),
+            ])
+          )
+          .build(),
+      ];
+    },
+  ],
   args: {
     wahlID: wahlID,
     wahlbezirkID: wahlbezirkID,

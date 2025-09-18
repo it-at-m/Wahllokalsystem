aeb36c2fd70a42181088e90f1c9a28013c4fee55
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-09-18 08:53:14 +0200

    add confirm dialog

--- wls-gui-wahllokalsystem/src/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.vue
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.vue
@@ -1,64 +1,77 @@
 <template>
-  <v-card>
-    <v-card-title>
-      Stapel c - Stimmzettel, die Anlass zu Bedenken geben
-    </v-card-title>
-    <v-card-text>
-      <v-form v-model="isChangeRowCountFormValid">
-        <div class="d-flex">
-          <base-number-input
-            v-model="countRows"
-            :rules="[minNumber(0), maxNumber(9999), required]"
-            label="Anzahl"
-            max-width="15rem"
-          />
-          <v-btn
-            active
-            :disabled="isApplyRowCountDisabled"
-            class="ml-4 mt-3"
-            @click="onApplyRowCountClicked"
-            >Übernehmen</v-btn
-          >
-        </div>
-      </v-form>
-      <v-form v-model="isFormValid">
-        <v-table>
-          <thead>
-            <tr>
-              <th />
-              <th>Ungültig</th>
-              <th>Gültig für Wahlvorschlag</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            <base-row-stapel-c
-              v-for="(
-                ergebnisAndStapel, index
-              ) in stapelCErgebnisseOrdereByNumIndex"
-              :key="
-                ergebnisAndStapel.ergebnis.numIndex ??
-                `${ergebnisAndStapel.stapelArt}-${index}`
-              "
-              :wahlvorschlaege="wahlvorschlaege"
-              :stapel-art="ergebnisAndStapel.stapelArt"
-              :model-value="ergebnisAndStapel.ergebnis"
-              :index="index + 1"
-              @selection-changed="
-                onGueltigkeitOfRowChanged($event, ergebnisAndStapel)
-              "
+  <div>
+    <v-card>
+      <v-card-title>
+        Stapel c - Stimmzettel, die Anlass zu Bedenken geben
+      </v-card-title>
+      <v-card-text>
+        <v-form v-model="isChangeRowCountFormValid">
+          <div class="d-flex">
+            <base-number-input
+              v-model="countRows"
+              :rules="[minNumber(0), maxNumber(9999), required]"
+              label="Anzahl"
+              max-width="15rem"
             />
-          </tbody>
-        </v-table>
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <base-button-save
-        :disabled="!areStapelCGueltigeErgebnisseValid"
-        @click="onSaveClicked"
-      />
-    </v-card-actions>
-  </v-card>
+            <v-btn
+              active
+              :disabled="isApplyRowCountDisabled"
+              class="ml-4 mt-3"
+              @click="onApplyRowCountClicked"
+              >Übernehmen</v-btn
+            >
+          </div>
+        </v-form>
+        <v-form v-model="isFormValid">
+          <v-table>
+            <thead>
+              <tr>
+                <th />
+                <th>Ungültig</th>
+                <th>Gültig für Wahlvorschlag</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              <base-row-stapel-c
+                v-for="(
+                  ergebnisAndStapel, index
+                ) in stapelCErgebnisseOrdereByNumIndex"
+                :key="
+                  ergebnisAndStapel.ergebnis.numIndex ??
+                  `${ergebnisAndStapel.stapelArt}-${index}`
+                "
+                :wahlvorschlaege="wahlvorschlaege"
+                :stapel-art="ergebnisAndStapel.stapelArt"
+                :model-value="ergebnisAndStapel.ergebnis"
+                :index="index + 1"
+                @selection-changed="
+                  onGueltigkeitOfRowChanged($event, ergebnisAndStapel)
+                "
+              />
+            </tbody>
+          </v-table>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <base-button-save
+          :disabled="!areStapelCGueltigeErgebnisseValid"
+          @click="onSaveClicked"
+        />
+      </v-card-actions>
+    </v-card>
+    <base-dialog
+      :visible="isConfirmDialogVisible"
+      :dialogtitle="CONFIRM_DIALOG_TITLE"
+      confirmtext="Hinweis schließen"
+      icon="$information"
+      @confirm="onConfirmDialogConfirmClicked"
+    >
+      Sie haben die Anzahl der bedenklichen Stimmzettel verändert. Zeilen können
+      nur gelöscht werden, wenn für einen Stimmzettel noch kein Beschluss im
+      System gespeichert worden ist. Bitte überprüfen Sie Ihre Eingaben.
+    </base-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -69,6 +82,7 @@ import type { Ergebnis } from "@/types/ergebnismeldung/Ergebnis.ts";
 import { computed, onMounted, ref } from "vue";
 
 import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
+import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
 import BaseNumberInput from "@/components/common/inputs/BaseNumberInput.vue";
 import BaseRowStapelC from "@/components/ergebnisermittlung/OBW/stapelC/BaseRowStapelC.vue";
 import { useRules } from "@/composables/common/rules.ts";
@@ -76,6 +90,8 @@ import { useOBWStapelCUtils } from "@/composables/ergebnisermittlung/obwStapelCU
 import { useErgebnisUtils } from "@/composables/ergebnismeldung/ergebnisUtils.ts";
 import { useErgebnismeldungStore } from "@/stores/ergebnismeldungStore.ts";
 import { StapelArtEnum } from "@/types/ergebnismeldung/StapelArtEnum.ts";
+
+const CONFIRM_DIALOG_TITLE = "Reduzierung der Anzahl bedenklicher Stimmzettel";
 
 const ergebnismeldungsStore = useErgebnismeldungStore();
 const { orderedByNumIndexWithNullAtEnd } = useErgebnisUtils();
@@ -114,6 +130,7 @@ const stapelCErgebnisseOrdereByNumIndex = computed(() => {
 });
 
 const isFormValid = ref<boolean | null>(null);
+const isConfirmDialogVisible = ref(false);
 const isChangeRowCountFormValid = ref<boolean | null>(null);
 const countRows = ref<number | null>(null);
 
@@ -139,26 +156,24 @@ function onApplyRowCountClicked() {
   }
 
   if (countRows.value < (getMaxNumIndexWithValueSet() || 0)) {
-    console.log(`kann nicht reduziert werden`);
+    showConfirmDialog();
     return;
   }
 
   if (countRows.value !== stapelCErgebnisseOrdereByNumIndex.value.length) {
     const maxUsedNumIndex = getMaxNumIndex();
     if (maxUsedNumIndex === null) {
-      console.log(`increasing countRows to ${countRows.value}`);
       addGueltigErgebnisse(countRows.value);
     } else if (countRows.value > maxUsedNumIndex) {
-      console.log(
-        `increasing countRows to ${countRows.value}`,
-        maxUsedNumIndex
-      );
       addGueltigErgebnisse(countRows.value);
     } else {
-      console.log(`decreasing countRows to ${countRows.value}`);
       deleteErgebnisseWithNumIndexAbove(countRows.value);
     }
   }
+}
+
+function onConfirmDialogConfirmClicked() {
+  hideConfirmDialog();
 }
 
 function onSaveClicked() {
@@ -193,6 +208,10 @@ function hasErgebnisNumIndex(
   return ergebnisAndStapelArt.ergebnis.numIndex !== null;
 }
 
+function hideConfirmDialog() {
+  isConfirmDialogVisible.value = false;
+}
+
 function removeWahlvorschlagIDIfNewStapelIsCUngueltig(
   ergebnis: Ergebnis,
   isNewStapelCUngueltig: boolean
@@ -200,5 +219,9 @@ function removeWahlvorschlagIDIfNewStapelIsCUngueltig(
   if (isNewStapelCUngueltig) {
     ergebnis.wahlvorschlagID = null;
   }
+}
+
+function showConfirmDialog() {
+  isConfirmDialogVisible.value = true;
 }
 </script>

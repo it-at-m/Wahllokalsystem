1bf4a3b58181bc2538e4340bf37e058dc49b2742
ThoGro <thomas.grossbeck@muenchen.de>
2025-09-19 15:31:07 +0200

    Guard against undefined in navigation

--- wls-gui-wahllokalsystem/src/components/navigation/auszaehlung_wahlarten/TheOBWScoresListGroup.vue
+++ wls-gui-wahllokalsystem/src/components/navigation/auszaehlung_wahlarten/TheOBWScoresListGroup.vue
@@ -10,21 +10,27 @@
     dass das list-item für SRW und BAW mit der gleichen Route aufgerufen wird. Siehe
     https://github.com/vuetifyjs/vuetify/issues/20516 -->
     <v-list-item
+      :disabled="!obwWahlID"
       :title="titleStimmenZaehlen"
       :to="
-        routeWithNameAndParams(ROUTE_AUSZAEHLUNG_STIMMZETTEL, {
-          wahlId: String(obwWahlID),
-        })
+        obwWahlID
+          ? routeWithNameAndParams(ROUTE_AUSZAEHLUNG_STIMMZETTEL, {
+              wahlId: String(obwWahlID),
+            })
+          : routeWithName(EXAMPLE_ROUTES_NOTFOUND)
       "
     />
     <v-list-item title="Stapel c" />
     <v-list-item title="Stapel b" />
     <v-list-item
+      :disabled="!obwWahlID"
       title="Stapel a"
       :to="
-        routeWithNameAndParams(ROUTE_STAPEL_A, {
-          wahlId: String(obwWahlID),
-        })
+        obwWahlID
+          ? routeWithNameAndParams(ROUTE_STAPEL_A, {
+              wahlId: String(obwWahlID),
+            })
+          : routeWithName(EXAMPLE_ROUTES_NOTFOUND)
       "
     />
     <v-list-item title="Schnellmeldung" />
@@ -36,12 +42,16 @@
 import { computed } from "vue";
 
 import { useNavigationUtils } from "@/composables/navigation/navigationUtils.ts";
-import { ROUTE_AUSZAEHLUNG_STIMMZETTEL, ROUTE_STAPEL_A } from "@/constants.ts";
+import {
+  EXAMPLE_ROUTES_NOTFOUND,
+  ROUTE_AUSZAEHLUNG_STIMMZETTEL,
+  ROUTE_STAPEL_A,
+} from "@/constants.ts";
 import { useWahlenStore } from "@/stores/wahlenStore.ts";
 import { WahlWahlartEnum } from "@/types/wahl/WahlWahlartEnum.ts";
 
 const { wahlenActions } = useWahlenStore();
-const { routeWithNameAndParams } = useNavigationUtils();
+const { routeWithNameAndParams, routeWithName } = useNavigationUtils();
 
 defineProps<{
   titleStimmenZaehlen: string;

e2e54de3a238eb59dabb8daf75c29d6496e49aa2
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-09-23 09:23:24 +0200

    fix bug when ergebnisse is empty array in store

--- wls-gui-wahllokalsystem/stories/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.stories.ts
+++ wls-gui-wahllokalsystem/stories/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.stories.ts
@@ -121,3 +121,50 @@ export const Default: Story = {
     wahlbezirkID: wahlbezirkID,
   },
 };
+
+export const NoErgebnisseInStore: Story = {
+  loaders: [
+    async () => {
+      const ergebnismeldungsStore = useErgebnismeldungStore(pinia);
+
+      ergebnismeldungsStore.ergebnisse = [];
+
+      const wahlvorschlaegStore = useWahlvorschlaegeStore(pinia);
+      wahlvorschlaegStore.wahlvorschlaege = [
+        prepareWahlvorschlaege()
+          .wahlID(wahlID)
+          .wahlbezirkID(wahlbezirkID)
+          .wahlvorschlaege(
+            new Set([
+              prepareWahlvorschlag()
+                .identifikator("wahlvorschlag1")
+                .kurzname("Wahlvorschlag1")
+                .kandidaten(
+                  new Set([prepareKandidat().name("Kandidat 11").build()])
+                )
+                .build(),
+              prepareWahlvorschlag()
+                .identifikator("wahlvorschlag2")
+                .kurzname("Wahlvorschlag2")
+                .kandidaten(
+                  new Set([prepareKandidat().name("Kandidat 21").build()])
+                )
+                .build(),
+            ])
+          )
+          .build(),
+      ];
+
+      const userStore = useUserStore(pinia);
+      userStore.setUser(
+        prepareUser()
+          .wahlMetaData([{ wahlbezirkID, wahlID, wahlnummer: "0" }])
+          .build()
+      );
+    },
+  ],
+  args: {
+    wahlID: wahlID,
+    wahlbezirkID: wahlbezirkID,
+  },
+};

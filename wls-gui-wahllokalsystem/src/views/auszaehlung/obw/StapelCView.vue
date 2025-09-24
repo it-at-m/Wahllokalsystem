b6db7dec909daa8b19b5feca4423bbba05ec0040
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-09-24 17:55:46 +0200

    arrange erfassung and summary card next to each other when enough space

--- wls-gui-wahllokalsystem/src/views/auszaehlung/obw/StapelCView.vue
+++ wls-gui-wahllokalsystem/src/views/auszaehlung/obw/StapelCView.vue
@@ -1,20 +1,27 @@
 <template>
-  <div v-if="wahlbezirkID && wahlID">
-    <the-o-b-w-stapel-c-erfassung-card
-      :wahlbezirk-id="wahlbezirkID"
-      :wahl-id="wahlID"
-    />
-    <the-o-b-w-stapel-c-summary-card
-      v-if="totalSum > 0"
-      :wahlbezirk-id="wahlbezirkID"
-      :wahl-id="wahlID"
-    />
-  </div>
+  <v-row v-if="wahlbezirkID && wahlID">
+    <v-col :cols="colsErfassungsCard">
+      <the-o-b-w-stapel-c-erfassung-card
+        :wahlbezirk-id="wahlbezirkID"
+        :wahl-id="wahlID"
+      />
+    </v-col>
+    <v-col
+      v-if="hasResultsToShow"
+      :cols="colsSummaryCard"
+    >
+      <the-o-b-w-stapel-c-summary-card
+        :wahlbezirk-id="wahlbezirkID"
+        :wahl-id="wahlID"
+      />
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import { useRoute } from "vue-router";
+import { useDisplay } from "vuetify/framework";
 
 import TheOBWStapelCErfassungCard from "@/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCErfassungCard.vue";
 import TheOBWStapelCSummaryCard from "@/components/ergebnisermittlung/OBW/stapelC/TheOBWStapelCSummaryCard.vue";
@@ -23,6 +30,7 @@ import { useUserStore } from "@/stores/userStore.ts";
 
 const { params: routeParams } = useRoute();
 const { getWahlbezirkIdFromWahlMetaDataByWahlId } = useUserStore();
+const { sm, name } = useDisplay();
 
 const wahlID = computed(() => routeParams.wahlId as string);
 const wahlbezirkID = computed(
@@ -30,4 +38,10 @@ const wahlbezirkID = computed(
 );
 
 const { totalSum } = useOBWStapelCUtils(wahlID, wahlbezirkID);
+
+const colsErfassungsCard = computed(() =>
+  hasResultsToShow.value ? (sm.value ? 12 : 8) : 12
+);
+const colsSummaryCard = computed(() => (sm.value ? 12 : 4));
+const hasResultsToShow = computed(() => totalSum.value > 0);
 </script>

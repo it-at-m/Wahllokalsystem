7719ee65cbb570dea1409978ea9adac7aa545399
ThoGro <thomas.grossbeck@muenchen.de>
2025-09-30 17:30:55 +0200

    Nitpicks eingearbeitet

--- wls-gui-wahllokalsystem/tests/service-worker/wahl-worker.spec.ts
+++ wls-gui-wahllokalsystem/tests/service-worker/wahl-worker.spec.ts
@@ -13,6 +13,8 @@ vi.mock("workbox-routing", () => ({
 vi.mock("localforage");
 
 describe("wahl-worker.ts", () => {
+  const API_BASE_PATH_REGEX = new RegExp("/api/.+");
+
   afterEach(() => {
     vi.clearAllMocks();
   });
@@ -20,15 +22,13 @@ describe("wahl-worker.ts", () => {
   it("should_registerRequestHandler_when_serviceWorkerIsInitialised", () => {
     expect(mockDefinitions.registerRoute).toHaveBeenCalledTimes(2);
 
-    const regex = new RegExp("/api/.+");
-
     const getCall = mockDefinitions.registerRoute.mock.calls.find(
       (call) => call[2] === "GET"
     );
     expect(getCall).toBeDefined();
     if (getCall != undefined) {
-      expect(getCall[0]).toEqual(regex);
-      expect(getCall[1].toString()).contains("getRequestHandler");
+      expect(getCall[0]).toEqual(API_BASE_PATH_REGEX);
+      expect(getCall[1].name).equals("getRequestHandler");
     }
 
     const postCall = mockDefinitions.registerRoute.mock.calls.find(
@@ -36,14 +36,12 @@ describe("wahl-worker.ts", () => {
     );
     expect(postCall).toBeDefined();
     if (postCall != undefined) {
-      expect(postCall[0]).toEqual(regex);
-      expect(postCall[1].toString()).contains("postRequestHandler");
+      expect(postCall[0]).toEqual(API_BASE_PATH_REGEX);
+      expect(postCall[1].name).equals("postRequestHandler");
     }
   });
 
   it("should_coverBasePath_when_regexIsUsed", () => {
-    const basePathRegex = new RegExp("/api/.+");
-
     const testUrls = new Map<string, boolean>();
     testUrls.set("/api/test", true);
     testUrls.set("/api/test/123", true);
@@ -51,7 +49,7 @@ describe("wahl-worker.ts", () => {
     testUrls.set("/other/test", false);
 
     testUrls.forEach((expected, url) => {
-      const matches = basePathRegex.test(url);
+      const matches = API_BASE_PATH_REGEX.test(url);
       expect(matches).toBe(expected);
     });
   });

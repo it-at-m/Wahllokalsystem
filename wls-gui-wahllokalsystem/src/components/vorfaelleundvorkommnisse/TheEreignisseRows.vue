9ab920a1b06264def4d2dc035a2cbab57b84d11a
Robert Jasny <robert.jasny@muenchen.de>
2025-10-06 17:33:43 +0200

    context added to delete dialog

--- wls-gui-wahllokalsystem/src/components/vorfaelleundvorkommnisse/TheEreignisseRows.vue
+++ wls-gui-wahllokalsystem/src/components/vorfaelleundvorkommnisse/TheEreignisseRows.vue
@@ -5,12 +5,12 @@
       :key="index"
       :model-value="ereignis"
       :line-number="index + 1"
-      @delete="() => onDeleteIconClicked(index)"
+      @delete="() => onDeleteIconClicked(index, ereignis)"
     />
     <yes-no-dialog
       v-model="deleteDialog"
       dialogtitle="Ereignis löschen"
-      dialogtext="Möchten Sie dieses Ereignis wirklich löschen?"
+      :dialogtext="deleteDialogText"
       @no="onYesNoDialogNoClicked"
       @yes="onYesNoDialogYesClicked"
     />
@@ -18,29 +18,42 @@
 </template>
 
 <script setup lang="ts">
+import type { Ereignis } from "@/types/vorfaelleundvorkommnisse/Ereignis.ts";
+
 import { storeToRefs } from "pinia";
 import { ref } from "vue";
 
 import YesNoDialog from "@/components/common/YesNoDialog.vue";
 import BaseEreignisRow from "@/components/vorfaelleundvorkommnisse/BaseEreignisRow.vue";
+import { useDateTimeFormatter } from "@/composables/common/dateTimeFormatter.ts";
 import { useEreignisStore } from "@/stores/ereignisStore.ts";
 
 const ereignisStore = useEreignisStore();
 const { wahlbezirkEreignisse } = storeToRefs(ereignisStore);
 const deleteDialog = ref(false);
+const deleteDialogText = ref("");
 const deleteIndex = ref<number | null>(null);
 
 function closeYesNoDialog() {
   deleteDialog.value = false;
 }
 
-function showYesNoDialogForItem(index: number) {
+function showYesNoDialogForItem(index: number, ereignis: Ereignis) {
   deleteIndex.value = index;
   deleteDialog.value = true;
+  const { toHhMm, toGermanDate } = useDateTimeFormatter();
+  deleteDialogText.value =
+    "Möchten Sie das Ereignis  wirklich löschen?" +
+    " Datum: " +
+    toGermanDate(ereignis.uhrzeit) +
+    ", Uhrzeit: " +
+    toHhMm(ereignis.uhrzeit) +
+    ", Beschreibung: " +
+    ereignis.beschreibung;
 }
 
-function onDeleteIconClicked(index: number) {
-  showYesNoDialogForItem(index);
+function onDeleteIconClicked(index: number, ereignis: Ereignis) {
+  showYesNoDialogForItem(index, ereignis);
 }
 
 function onYesNoDialogNoClicked() {

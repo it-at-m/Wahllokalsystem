93e2678f1bc7f1ab775359b46475f5dbf86774c8
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-10-09 16:53:08 +0200

    #1922 update storybook

--- wls-gui-wahllokalsystem/stories/components/common/tables/BaseTableRowManager.stories.ts
+++ wls-gui-wahllokalsystem/stories/components/common/tables/BaseTableRowManager.stories.ts
@@ -30,12 +30,20 @@ const meta: Meta<typeof BaseTableRowManager> = {
         disable: true,
       },
     },
-    onChangeRowCountClicked: {
+    // @ts-expect-error/disabled-because-if-omitted-it-will-still-be-displayed-in-storybook
+    changeRowCountClicked: {
       table: {
         disable: true,
       },
     },
-    // todo: rules control anpassen
+    onChangeRowCountClicked: {
+      name: "changeRowCountClicked",
+      description:
+        "Wird ausgelöst, wenn auf den Button geklickt wird, und gibt den neuen Wert zurück",
+      table: {
+        category: "events",
+      },
+    },
   },
 };
 
@@ -59,6 +67,13 @@ export const DifferentLabels: Story = {
   },
 };
 
+/**
+ * ```
+ * const minNumber = (min: number) => (value: number) => value >= min || "Eingabe darf nicht kleiner als ${min} sein."
+ * const maxNumber = (max: number) => (value: number) => value <= max || "Eingabe darf nicht größer als ${max} sein."
+ * const rules = [minNumber(5), maxNumber(10)]
+ * ```
+ */
 export const AdditionalRulesMinAndMaxInput: Story = {
   args: {
     rules: [minNumber(5), maxNumber(10)],

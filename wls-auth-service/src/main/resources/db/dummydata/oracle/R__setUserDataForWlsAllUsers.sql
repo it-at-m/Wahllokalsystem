d610a71a8f397c878ede0bc84671d666ba38b9c1
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-10-09 23:24:13 +0200

    fix wrong wahlbezirkID for UWB at MBW

--- wls-auth-service/src/main/resources/db/dummydata/oracle/R__setUserDataForWlsAllUsers.sql
+++ wls-auth-service/src/main/resources/db/dummydata/oracle/R__setUserDataForWlsAllUsers.sql
@@ -17,5 +17,5 @@ SET wahlbezirkID     = '9899cae4-df9a-4ffc-a940-f20cf2280171',
     wahltag          = TO_TIMESTAMP('2026-01-01', 'yyyy-MM-dd'),
     wahlbezirkNummer = '9012',
     pin              = 'dummyPin',
-    wbid_wahlnummer  = '{"wbid_wahlnummer": [{"wahlbezirkID": "e5f6a7b8-c9d0-4e1f-2a3b-4c5d6e7f8a9b", "wahlnummer": "0", "wahlID": "b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e"}, {"wahlbezirkID": "9899cae4-df9a-4ffc-a940-f20cf2280171", "wahlnummer": "2", "wahlID": "28039463-3d3d-4ce5-acc1-0bef931a4409"}, {"wahlbezirkID": "9899cae4-df9a-4ffc-a940-f20cf2280172", "wahlnummer": "1", "wahlID": "d8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1"}]}'
+    wbid_wahlnummer  = '{"wbid_wahlnummer": [{"wahlbezirkID": "d4e5f6a7-b8c9-4d0e-1f2a-3b4c5d6e7f8a", "wahlnummer": "0", "wahlID": "b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e"}, {"wahlbezirkID": "9899cae4-df9a-4ffc-a940-f20cf2280171", "wahlnummer": "2", "wahlID": "28039463-3d3d-4ce5-acc1-0bef931a4409"}, {"wahlbezirkID": "9899cae4-df9a-4ffc-a940-f20cf2280172", "wahlnummer": "1", "wahlID": "d8d2dd22-cbf6-488e-b9bc-b8c2b0ab31a1"}]}'
 WHERE id in ('00000000-0000-0000-0000-000000000003');

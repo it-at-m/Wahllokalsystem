fcaa677f35cd964f09202fb6d10efb0e12c9e151
Robert Jasny <robert.jasny@muenchen.de>
2025-10-10 12:05:52 +0200

    Builder entfernt

--- wls-gui-wahllokalsystem/src/types/vorfaelleundvorkommnisse/EreignisPayload.ts
+++ wls-gui-wahllokalsystem/src/types/vorfaelleundvorkommnisse/EreignisPayload.ts
@@ -3,34 +3,3 @@ export interface EreignisPayload {
   timeStr?: string;
   beschreibung?: string;
 }
-
-export class EreignisBuilder implements EreignisPayload {
-  constructor(
-    public dateStr?: string,
-    public timeStr?: string,
-    public beschreibung?: string
-  ) {}
-
-  static createMinimal(): EreignisBuilder {
-    return new EreignisBuilder();
-  }
-
-  static createComplete(): EreignisBuilder {
-    return new EreignisBuilder("31.09.2025", "15:00", "Beschreibung");
-  }
-
-  withBeschreibung(beschreibung: string) {
-    this.beschreibung = beschreibung;
-    return this;
-  }
-
-  withDateOnly(dateOnly: string) {
-    this.dateStr = dateOnly;
-    return this;
-  }
-
-  withTimeOnly(timeOnly: string) {
-    this.timeStr = timeOnly;
-    return this;
-  }
-}

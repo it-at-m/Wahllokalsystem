2b96d56aa2996ab59e5e3d6af9f95f6019c69e23
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-10-14 23:01:25 +0200

    created tests for init of awerte

---
+++ wls-gui-wahllokalsystem/tests/utils/ergebnisermittlung/aWerteTestDataFactory.ts
@@ -0,0 +1,47 @@
+import type { AWerteDTO } from "@/api/wls-clients/generated-ergebnismeldung-api";
+import type { AWerte } from "@/types/ergebnisermittlung/AWerte.ts";
+
+import { proxyBuilder } from "@tests/utils/Builder.ts";
+import { useCommonTestDataFactory } from "@tests/utils/common/CommonTestDataFactory.ts";
+
+const { generateRandomString, generateRandomNumber } =
+  useCommonTestDataFactory();
+
+export function useAWerteTestDataFactory() {
+  function createAWerte(): AWerte {
+    return {
+      bezirkUndWahlId: {
+        wahlID: generateRandomString(10),
+        wahlbezirkID: generateRandomString(10),
+      },
+      a1: generateRandomNumber(4),
+      a2: generateRandomNumber(4),
+    };
+  }
+
+  function createAWerteDTO(): AWerteDTO {
+    return {
+      bezirkUndWahlID: {
+        wahlID: generateRandomString(10),
+        wahlbezirkID: generateRandomString(10),
+      },
+      a1: generateRandomNumber(4),
+      a2: generateRandomNumber(4),
+    };
+  }
+
+  function prepareAWerte() {
+    return proxyBuilder<AWerte>(createAWerte());
+  }
+
+  function prepareAWerteDTO() {
+    return proxyBuilder<AWerteDTO>(createAWerteDTO());
+  }
+
+  return {
+    createAWerte,
+    createAWerteDTO,
+    prepareAWerte,
+    prepareAWerteDTO,
+  };
+}

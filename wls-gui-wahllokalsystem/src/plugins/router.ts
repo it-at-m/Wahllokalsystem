5215f8cc1ac06e43d86acf1252297164dd432a7c
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-10-17 11:31:09 +0200

    re add navigation guard

--- wls-gui-wahllokalsystem/src/plugins/router.ts
+++ wls-gui-wahllokalsystem/src/plugins/router.ts
@@ -19,6 +19,7 @@ import {
   ROUTE_WAHLVORSTAND,
   ROUTES_HOME,
 } from "@/constants";
+import { useTaskManagerStore } from "@/stores/taskManagerStore.ts";
 import { useUserStore } from "@/stores/userStore.ts";
 import ErfassungStimmzettelView from "@/views/auszaehlung/ErfassungStimmzettelView.vue";
 import StapelCView from "@/views/auszaehlung/obw/StapelCView.vue";
@@ -148,6 +149,13 @@ const router = createRouter({
       left: 0,
     };
   },
+});
+
+router.beforeEach((to) => {
+  const { hasTasksToRun } = storeToRefs(useTaskManagerStore());
+  if (to.name != ROUTES_HOME && !hasTasksToRun.value) {
+    return { name: ROUTES_HOME };
+  }
 });
 
 export default router;

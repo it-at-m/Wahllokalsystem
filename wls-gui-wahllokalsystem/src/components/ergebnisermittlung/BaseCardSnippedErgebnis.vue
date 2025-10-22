48ed0fe0445c09a2e23412f7d91660430967442b
ThoGro <thomas.grossbeck@muenchen.de>
2025-10-22 16:43:46 +0200

    Use Keep-Alive and set ergebnis as modelValue

--- wls-gui-wahllokalsystem/src/components/ergebnisermittlung/BaseCardSnippedErgebnis.vue
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/BaseCardSnippedErgebnis.vue
@@ -4,15 +4,14 @@
       <v-card-title>{{ snippedTitle }}</v-card-title>
       <v-card-text>
         <base-number-input
-          :model-value="ergebnis"
+          v-model="modelValue.ergebnis"
           :rules="[required, minNumber(minValue), maxNumber(maxValue)]"
           min-width="20rem"
-          @update:model-value="onModelValueChanged(props.stapelArt, $event)"
         />
       </v-card-text>
       <v-card-actions>
         <base-button-save
-          :loading="isErgebnisseSaving"
+          :loading="isErgebnisSaving"
           :disabled="!inputForm"
           @click="onSaveClicked"
         />
@@ -24,34 +23,19 @@
 <script setup lang="ts">
 import type { Ergebnis } from "@/types/ergebnismeldung/Ergebnis.ts";
 
-import { storeToRefs } from "pinia";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
 import BaseNumberInput from "@/components/common/inputs/BaseNumberInput.vue";
 import { useRules } from "@/composables/common/rules.ts";
-import { useErgebnismeldungStore } from "@/stores/ergebnismeldungStore.ts";
-import { StapelArtEnum } from "@/types/ergebnismeldung/StapelArtEnum.ts";
 
 const { maxNumber, minNumber, required } = useRules();
-const {
-  getErgebnisseByWahlIdAndStapelartOrUndefined,
-  findAndUpdateErgebnisseByWahlIdAndStapelArt,
-  sendErgebnisseByStapelArt,
-} = useErgebnismeldungStore();
-const { isErgebnisseSaving } = storeToRefs(useErgebnismeldungStore());
 
-const props = defineProps({
+const modelValue = defineModel<Ergebnis>({ required: true });
+
+defineProps({
   snippedTitle: {
     type: String,
-    required: true,
-  },
-  wahlId: {
-    type: String,
-    required: true,
-  },
-  stapelArt: {
-    type: String as () => StapelArtEnum,
     required: true,
   },
   minValue: {
@@ -64,35 +48,20 @@ const props = defineProps({
     required: false,
     default: 9999,
   },
+  isErgebnisSaving: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
 });
 
-const ergebnis = computed(
-  () =>
-    getErgebnisseByWahlIdAndStapelartOrUndefined(
-      props.wahlId,
-      StapelArtEnum.MbwD
-    )?.ergebnisse[0]?.ergebnis
-);
+const emit = defineEmits<{
+  save: [];
+}>();
 
 const inputForm = ref<null | boolean>(null);
 
-function onModelValueChanged(
-  stapelArt: StapelArtEnum,
-  newValue?: number | null | undefined
-) {
-  const ergebnis: Ergebnis = {
-    wahlvorschlagID: null,
-    kandidatID: null,
-    wahlvorschlagsOrdnungszahl: null,
-    ergebnis: newValue ?? null,
-    numIndex: null,
-  };
-  findAndUpdateErgebnisseByWahlIdAndStapelArt(props.wahlId, stapelArt, [
-    ergebnis,
-  ]);
-}
-
 function onSaveClicked() {
-  sendErgebnisseByStapelArt(props.wahlId, props.stapelArt);
+  emit("save");
 }
 </script>

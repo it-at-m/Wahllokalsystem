03150fe5226edb68ae9d9ad0a709613419fc9c21
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-10-28 14:36:19 +0100

    move comment to have no trouble with single child rule of keep alive

--- wls-gui-wahllokalsystem/src/App.vue
+++ wls-gui-wahllokalsystem/src/App.vue
@@ -5,12 +5,14 @@
       <v-container fluid>
         <router-view v-slot="{ route, Component }">
           <v-fade-transition mode="out-in">
+            <!-- Keep alive is fundamental for our app, to work correctly - see doc for frontend architecture -->
             <keep-alive>
-              <!-- Keep alive is fundamental for our app, to work correctly - see doc for frontend architecture -->
               <component
                 :is="Component"
                 :key="route.fullPath"
-              />
+              >
+                <!-- :key attribute is fundamental for our app, to work correctly with keep alive - see doc for frontend architecture -->
+              </component>
             </keep-alive>
           </v-fade-transition>
         </router-view>

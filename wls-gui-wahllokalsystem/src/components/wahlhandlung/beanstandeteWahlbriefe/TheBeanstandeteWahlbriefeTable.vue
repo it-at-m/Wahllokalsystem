e7739e6865b36823c3899433ad174e49cb1674d9
Robert Jasny <robert.jasny@muenchen.de>
2025-10-28 11:10:03 +0100

    clearable disabled

--- wls-gui-wahllokalsystem/src/components/wahlhandlung/beanstandeteWahlbriefe/TheBeanstandeteWahlbriefeTable.vue
+++ wls-gui-wahllokalsystem/src/components/wahlhandlung/beanstandeteWahlbriefe/TheBeanstandeteWahlbriefeTable.vue
@@ -32,6 +32,7 @@
                 class="ml-5 fill-height"
                 :items="gruendeWahlscheine"
                 auto-select-first
+                :clearable="false"
                 :rules="[required]"
                 :data-test="`wahlscheingruende-input-${index - 1}`"
                 @update:model-value="
@@ -56,6 +57,7 @@
               label="Beschlussergebnis"
               :items="gruendeStimmzettel"
               auto-select-first
+              :clearable="false"
               :rules="[required]"
               :disabled="_isInputDisabled(index - 1)"
               :data-test="`stimmzettelgruende-input-${wahl.wahlID}-${index - 1}`"

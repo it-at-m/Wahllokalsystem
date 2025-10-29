dcb2676e9e0a5f8c1b3c4266bf08e92e33e575c4
ThoGro <thomas.grossbeck@muenchen.de>
2025-10-29 15:27:47 +0100

    Oracel Skripte für getrennte User je Wahl

--- wls-eai-service/src/main/resources/db/dummydata/oracle/V3_3_1__wahlbezirkTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/oracle/V3_3_1__wahlbezirkTestdaten.sql
@@ -1,9 +1,9 @@
 -- wahlbezirk for user wls_all_uwb
 INSERT INTO wahlbezirk (id, wahlbezirkArt, nummer, stimmzettelgebietID, a1, a2, a3)
-values ('9899cae4-df9a-4ffc-a940-f20cf2280171', 'UWB', 'Wahlbezirk 1', '00000000-0000-0000-0000-000000000005', 30, 5,
+values ('9899cae4-df9a-4ffc-a940-f20cf2280171', 'UWB', 'komm_uwb', '00000000-0000-0000-0000-000000000005', 30, 5,
         1);
 
 -- wahlbezirk for user wls_all_bwb
 INSERT INTO wahlbezirk (id, wahlbezirkArt, nummer, stimmzettelgebietID, a1, a2, a3)
-values ('9081145c-dc16-4a9b-8255-5c49ddb98c14', 'BWB', 'Wahlbezirk 4', '00000000-0000-0000-0000-000000000005', 42, 5,
+values ('9081145c-dc16-4a9b-8255-5c49ddb98c14', 'BWB', 'komm_bwb', '00000000-0000-0000-0000-000000000005', 42, 5,
         3);
--- wls-eai-service/src/main/resources/db/dummydata/oracle/V9_0__createMBWTestdaten.sql
+++ wls-eai-service/src/main/resources/db/dummydata/oracle/V9_0__createMBWTestdaten.sql
@@ -2,7 +2,7 @@
 -- Wahl erstellen (Migrationsbeiratswahl)
 INSERT INTO Wahl (id, name, wahlart, wahltagId)
 VALUES ('b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e', 'Migrationsbeiratswahl München 2026', 'MBW',
-        '827e9e6a-ebe9-483f-a9cd-162b26f348e8');
+        '5f10d96a-855e-44ec-afdb-7e95484c8e2f');
 
 -- Stimmzettelgebiet erstellen
 INSERT INTO Stimmzettelgebiet (id, nummer, name, stimmzettelgebietsart, wahlId)
@@ -11,12 +11,12 @@ VALUES ('c3d4e5f6-a7b8-4c9d-0e1f-2a3b4c5d6e7f', 'SZG-001', 'Stimmzettelgebiet M�
 
 -- Wahlbezirk 1 (UWB - Urnenwahl)
 INSERT INTO Wahlbezirk (id, wahlbezirkArt, nummer, a1, a2, a3, stimmzettelgebietId)
-VALUES ('d4e5f6a7-b8c9-4d0e-1f2a-3b4c5d6e7f8a', 'UWB', 'WB-UWB-001', 1500, 1200, 300,
+VALUES ('d4e5f6a7-b8c9-4d0e-1f2a-3b4c5d6e7f8a', 'UWB', 'mbw_uwb', 1500, 1200, 300,
         'c3d4e5f6-a7b8-4c9d-0e1f-2a3b4c5d6e7f');
 
 -- Wahlbezirk 2 (BWB - Briefwahl)
 INSERT INTO Wahlbezirk (id, wahlbezirkArt, nummer, a1, a2, a3, stimmzettelgebietId)
-VALUES ('e5f6a7b8-c9d0-4e1f-2a3b-4c5d6e7f8a9b', 'BWB', 'WB-BWB-001', 2000, 1800, 200,
+VALUES ('e5f6a7b8-c9d0-4e1f-2a3b-4c5d6e7f8a9b', 'BWB', 'mbw_bwb', 2000, 1800, 200,
         'c3d4e5f6-a7b8-4c9d-0e1f-2a3b4c5d6e7f');
 
 -- Wahlvorschlägeliste erstellen

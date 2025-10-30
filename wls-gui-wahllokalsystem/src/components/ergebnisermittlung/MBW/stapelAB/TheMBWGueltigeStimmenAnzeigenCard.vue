1293d6ed264cd89d6ee5174d22a468b7ef62ebe7
Robert Jasny <robert.jasny@muenchen.de>
2025-10-30 16:37:58 +0100

    Components und Story für Anzeige der gültigen Stimmen A+B

---
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/MBW/stapelAB/TheMBWGueltigeStimmenAnzeigenCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title> Gültige Stimmen </v-card-title>
+    <v-card-text>
+      <the-m-b-w-gueltige-stimmen-anzeigen-table
+        v-if="wahlbezirkID && wahlID"
+        v-model="modelValue"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+<script setup lang="ts">
+import type { MbwErgebnisseAndWahlvorschlag } from "@/types/ergebnisermittlung/MbwErgebnisseAndWahlvorschlag.ts";
+import type { PropType } from "vue";
+
+import TheMBWGueltigeStimmenAnzeigenTable from "@/components/ergebnisermittlung/MBW/stapelAB/TheMBWGueltigeStimmenAnzeigenTable.vue";
+
+const modelValue = defineModel({
+  type: Object as PropType<MbwErgebnisseAndWahlvorschlag[]>,
+  required: true,
+});
+
+defineProps({
+  wahlID: {
+    type: String,
+    required: true,
+  },
+  wahlbezirkID: {
+    type: String,
+    required: true,
+  },
+});
+</script>
---
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/MBW/stapelAB/TheMBWGueltigeStimmenAnzeigenTable.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <v-table striped="odd">
+      <thead>
+        <tr>
+          <th />
+          <th class="font-weight-bold text-left">Wahlvorschlag</th>
+          <th class="font-weight-bold text-right">Insgesamt</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="vorschlag in modelValue"
+          :key="vorschlag.wahlvorschlag.identifikator"
+        >
+          <td>D {{ vorschlag.wahlvorschlag.ordnungszahl }}</td>
+          <td>{{ vorschlag.wahlvorschlag.kurzname }}</td>
+          <td class="text-right">
+            {{
+              (vorschlag.ergebnisStapelA.ergebnis ?? 0) +
+              (vorschlag.ergebnisStapelB.ergebnis ?? 0)
+            }}
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>D</td>
+          <td class="font-weight-bold">Gültige Stimmen insgesamt</td>
+          <td class="font-weight-bold text-right">{{ totalSum }}</td>
+        </tr>
+      </tfoot>
+    </v-table>
+  </v-container>
+</template>
+<script setup lang="ts">
+import type { MbwErgebnisseAndWahlvorschlag } from "@/types/ergebnisermittlung/MbwErgebnisseAndWahlvorschlag.ts";
+import type { PropType } from "vue";
+
+import { computed } from "vue";
+
+const modelValue = defineModel({
+  type: Object as PropType<MbwErgebnisseAndWahlvorschlag[]>,
+  required: true,
+});
+
+const totalSum = computed(() => {
+  let total = 0;
+  for (const vorschlag of modelValue.value) {
+    total =
+      total +
+      (vorschlag.ergebnisStapelA.ergebnis ?? 0) +
+      (vorschlag.ergebnisStapelB.ergebnis ?? 0);
+  }
+  return total;
+});
+</script>

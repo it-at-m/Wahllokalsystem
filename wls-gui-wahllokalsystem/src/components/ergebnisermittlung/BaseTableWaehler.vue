bad899fdd14a73c51512d6750d27027c30926bfa
ThoGro <thomas.grossbeck@muenchen.de>
2025-10-31 17:53:49 +0100

    Komponente mit Tabelle zur Darstellung der Wähler

---
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/BaseTableWaehler.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-table>
+    <tbody>
+      <tr v-if="isUWB">
+        <td>B1</td>
+        <td>Wähler mit Stimmabgabevermerken im Wählerverzeichnis</td>
+        <td>{{ b1 }}</td>
+      </tr>
+      <tr v-if="isUWB">
+        <td>B2</td>
+        <td>Wähler mit Wahlschein</td>
+        <td>{{ b2 }}</td>
+      </tr>
+      <tr class="bg-grey-lighten-3">
+        <td
+          v-if="isUWB"
+          class="font-weight-bold"
+        >
+          B1+B2
+        </td>
+        <td
+          v-if="isBWB"
+          class="font-weight-bold"
+        >
+          B
+        </td>
+        <td class="font-weight-bold">Wähler insgesamt</td>
+        <td
+          v-if="isUWB"
+          class="font-weight-bold"
+        >
+          {{ b1 + b2 }}
+        </td>
+        <td
+          v-if="isBWB"
+          class="font-weight-bold"
+        >
+          {{ b }}
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { onMounted, ref } from "vue";
+
+import { useErgebnisermittlungService } from "@/composables/ergebnisermittlung/ergebnisermittlungService.ts";
+import { useStimmabgabevermerkeService } from "@/composables/stimmabgabevermerke/stimmabgabevermerkeService.ts";
+import { useUserStore } from "@/stores/userStore.ts";
+import { useWahlenStore } from "@/stores/wahlenStore.ts";
+
+const { isUWB, isBWB } = storeToRefs(useUserStore());
+const { waehlerverzeichnisActions, wahlenActions } = useWahlenStore();
+const { getStimmabgabevermerke } = useStimmabgabevermerkeService();
+const { getStimmzettelumschlaege } = useErgebnisermittlungService();
+
+const props = defineProps<{
+  wahlbezirkId: string;
+  wahlId: string;
+}>();
+
+const b1 = ref(0);
+const b2 = ref(0);
+const b = ref(0);
+
+onMounted(async () => {
+  if (isUWB.value) {
+    const waehlerverzeichnisNummer =
+      waehlerverzeichnisActions.getWaehlerverzeichnisNummerOrUndefinedById(
+        props.wahlId
+      );
+    if (waehlerverzeichnisNummer) {
+      const loadedStimmabgabevermerke = await getStimmabgabevermerke(
+        props.wahlbezirkId,
+        waehlerverzeichnisNummer
+      );
+      // @ts-expect-error: noUncheckedIndexedAccess for wahldaten[0] | siehe #2008
+      b1.value = loadedStimmabgabevermerke.wahldaten[0].vermerke.reduce(
+        (summe, vermerk) => {
+          return (
+            summe +
+            vermerk.stimmzettel.reduce((stimmSumme, stimmzettel) => {
+              return stimmSumme + (stimmzettel.anzahl || 0);
+            }, 0)
+          );
+        },
+        0
+      );
+      b2.value = Array.from(
+        // @ts-expect-error: noUncheckedIndexedAccess for wahldaten[0] | siehe #2008
+        loadedStimmabgabevermerke.wahldaten[0].eingenommeneWahlscheine.values()
+      ).reduce((sum, value) => sum + value, 0);
+    }
+  }
+  if (isBWB.value) {
+    const wahl = wahlenActions.getWahlOrUndefinedById(props.wahlId);
+    if (wahl) {
+      const loadedStimmzettelumschlaege = await getStimmzettelumschlaege(
+        wahl,
+        props.wahlbezirkId,
+        "",
+        false
+      );
+      b.value = loadedStimmzettelumschlaege?.anzahlWaehler || 0;
+    }
+  }
+});
+</script>
---
+++ wls-gui-wahllokalsystem/stories/components/ergebnisermittlung/BaseTableWaehler.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+import BaseTableWaehler from "@/components/ergebnisermittlung/BaseTableWaehler.vue";
+import pinia from "@/plugins/pinia.ts";
+import { useUserStore } from "@/stores/userStore.ts";
+import { createUserLocalDevelopment } from "@/types/User.ts";
+
+const wahlID = "wahlID";
+
+const meta = {
+  component: BaseTableWaehler,
+  args: {},
+} satisfies Meta<typeof BaseTableWaehler>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const WahlbezirksartIsUWB: Story = {
+  async beforeEach() {
+    const userStore = useUserStore(pinia);
+    userStore.setUser(createUserLocalDevelopment());
+  },
+  args: {
+    wahlId: wahlID,
+    wahlbezirkId: "wahlbezirkID",
+  },
+};
+
+export const WahlbezirksartIsBWB: Story = {
+  async beforeEach() {
+    const userStore = useUserStore(pinia);
+    const user = createUserLocalDevelopment();
+    user.wahlbezirksArt = "BWB";
+    userStore.setUser(user);
+  },
+  args: {
+    wahlId: wahlID,
+    wahlbezirkId: "wahlbezirkID",
+  },
+};

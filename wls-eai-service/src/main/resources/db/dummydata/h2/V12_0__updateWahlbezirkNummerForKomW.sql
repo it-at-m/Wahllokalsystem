f243182d498968315a63376d06a9ffd3686f84d6
Thomas Großbeck <thomas.grossbeck@muenchen.de>
2025-11-03 11:33:45 +0100

    Update wls-eai-service/src/main/resources/db/dummydata/h2/V12_0__updateWahlbezirkNummerForKomW.sql
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-eai-service/src/main/resources/db/dummydata/h2/V12_0__updateWahlbezirkNummerForKomW.sql
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V12_0__updateWahlbezirkNummerForKomW.sql
@@ -4,4 +4,4 @@ WHERE id in ('9899cae4-df9a-4ffc-a940-f20cf2280171', '9899cae4-df9a-4ffc-a940-f2
 
 UPDATE Wahlbezirk
 SET nummer = 'komw_bwb'
-WHERE id in ('9081145c-dc16-4a9b-8255-5c49ddb98c14') OR id in ('9081145c-dc16-4a9b-8255-5c49ddb98c15');
+WHERE id in ('9081145c-dc16-4a9b-8255-5c49ddb98c14', '9081145c-dc16-4a9b-8255-5c49ddb98c15');

f71f1f102719e3d02da68f1b7d844ca299b2dc8b
ThoGro <thomas.grossbeck@muenchen.de>
2025-11-04 15:53:11 +0100

    Add Task for erfasste Wahlbriefe

--- wls-gui-wahllokalsystem/src/composables/tasks/taskListService.ts
+++ wls-gui-wahllokalsystem/src/composables/tasks/taskListService.ts
@@ -14,6 +14,7 @@ import { useStimmzettelumschlaegeTaskFactory } from "@/composables/tasks/taskFac
 import { useUngueltigeWahlscheineTaskFactory } from "@/composables/tasks/taskFactories/ungueltigeWahlscheineTaskFactory.ts";
 import { useWaehlerTaskFactory } from "@/composables/tasks/taskFactories/waehlerTaskFactory.ts";
 import { useWaehlverzeichnisTaskFactory } from "@/composables/tasks/taskFactories/waehlverzeichnisTaskFactory.ts";
+import { useWahlbriefeTaskFactory } from "@/composables/tasks/taskFactories/wahlbriefeTaskFactory.ts";
 import { useWahlscheineTaskFactory } from "@/composables/tasks/taskFactories/wahlscheineTaskFactory.ts";
 import { useWahlvorschlaegeTaskFactory } from "@/composables/tasks/taskFactories/wahlvorschlaegeTaskFactory.ts";
 import { useWahlvorstandTaskFactory } from "@/composables/tasks/taskFactories/wahlvorstandTaskFactory.ts";
@@ -46,6 +47,7 @@ export function useTaskListService() {
   const { createTasks: createBegruendungTasks } = useBegruendungTaskFactory();
   const { createTasks: createAWerteTasks } = useAWerteTaskFactory();
   const { createTasks: createHandbuchTasks } = useHandbuchTaskFactory();
+  const { createTasks: createWahlbriefeTasks } = useWahlbriefeTaskFactory();
 
   function initTasklist() {
     const taskFactoryData = _createTaskFactoryData();
@@ -65,6 +67,7 @@ export function useTaskListService() {
       ...createBegruendungTasks(taskFactoryData),
       ...createAWerteTasks(taskFactoryData),
       ...createHandbuchTasks(taskFactoryData),
+      ...createWahlbriefeTasks(taskFactoryData),
     ];
   }
 

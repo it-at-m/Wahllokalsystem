4f615f5be89ec3a166c0b85259a4fa7682c95221
ThoGro <thomas.grossbeck@muenchen.de>
2025-11-06 18:27:39 +0100

    Add Test for StatusStore

--- wls-gui-wahllokalsystem/src/stores/statusStore.ts
+++ wls-gui-wahllokalsystem/src/stores/statusStore.ts
@@ -31,21 +31,14 @@ export const useStatusStore = defineStore(storeID, () => {
         wahlbezirkID,
         sendNotification
       );
-      console.debug("StatusForWahl: ", statusForWahl);
       if (statusForWahl) {
         status.value.push(statusForWahl);
       } else {
-        console.debug("Push: ", {
-          bezirkUndWahlID: { wahlID, wahlbezirkID },
-          schnellmeldung: DEFAULT_MELDUNG,
-          niederschrift: DEFAULT_MELDUNG,
-        });
         status.value.push({
           bezirkUndWahlID: { wahlID, wahlbezirkID },
           schnellmeldung: DEFAULT_MELDUNG,
           niederschrift: DEFAULT_MELDUNG,
         });
-        console.debug("danach: ", status.value);
       }
     } catch {
       throw Error(`Fehler beim Laden des Status für WahlID: ${wahlID}`);
@@ -53,6 +46,7 @@ export const useStatusStore = defineStore(storeID, () => {
   }
 
   return {
+    status,
     loadStatus,
   };
 });
--- wls-gui-wahllokalsystem/tests/stores/statusStore.spec.ts
+++ wls-gui-wahllokalsystem/tests/stores/statusStore.spec.ts
@@ -1,8 +1,10 @@
 import { createTestingPinia } from "@pinia/testing";
 import { useStatusTestDataFactory } from "@tests/utils/ergebnismeldung/statusTestDataFactory.ts";
+import { flushPromises } from "@vue/test-utils";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { useStatusStore } from "@/stores/statusStore.ts";
+import { MeldungValidierungsstatusEnum } from "@/types/ergebnismeldung/MeldungValidierungsstatusEnum.ts";
 
 const { createStatus } = useStatusTestDataFactory();
 
@@ -36,18 +38,81 @@ describe("statusStore.ts", () => {
   });
 
   describe("loadStatus", () => {
-    it("should_addStatus_when_loadStatusIsCalledWithValidWahlIdAndWahlbezirkId", () => {
+    it("should_addStatus_when_loadStatusIsCalledWithValidWahlIdAndWahlbezirkId", async () => {
       const status = createStatus();
       mockDefinitions.getStatus.mockReturnValue(status);
 
       unitUnderTest.loadStatus(wahlID, wahlbezirkID);
+
+      await flushPromises();
 
       expect(mockDefinitions.getStatus).toHaveBeenCalledWith(
         wahlID,
         wahlbezirkID,
         true
       );
-      //expect(unitUnderTest.status).toStrictEqual([status]);
+      expect(unitUnderTest.status).toStrictEqual([status]);
     });
+
+    it("should_addStatus_when_loadStatusIsCalledWithValidWahlIdAndWahlbezirkIdAndStatusArrayIsNotEmpty", async () => {
+      const status = createStatus();
+      mockDefinitions.getStatus.mockReturnValue(status);
+
+      const existingStatus = createStatus();
+      unitUnderTest.status = [existingStatus];
+
+      unitUnderTest.loadStatus(wahlID, wahlbezirkID);
+
+      await flushPromises();
+
+      expect(mockDefinitions.getStatus).toHaveBeenCalledWith(
+        wahlID,
+        wahlbezirkID,
+        true
+      );
+      expect(unitUnderTest.status).toStrictEqual([existingStatus, status]);
+    });
+
+    it("should_addDefaultStatus_when_loadStatusReturnsNoStatus", async () => {
+      mockDefinitions.getStatus.mockReturnValue(null);
+
+      const defaultStatus = {
+        bezirkUndWahlID: { wahlID, wahlbezirkID },
+        schnellmeldung: {
+          validierungsstatus: MeldungValidierungsstatusEnum.NichtValidiert,
+          gedruckt: false,
+        },
+        niederschrift: {
+          validierungsstatus: MeldungValidierungsstatusEnum.NichtValidiert,
+          gedruckt: false,
+        },
+      };
+
+      unitUnderTest.loadStatus(wahlID, wahlbezirkID);
+
+      await flushPromises();
+
+      expect(mockDefinitions.getStatus).toHaveBeenCalledWith(
+        wahlID,
+        wahlbezirkID,
+        true
+      );
+      expect(unitUnderTest.status).toStrictEqual([defaultStatus]);
+    });
+
+    it.each([{ sendNotification: true }, { sendNotification: false }])(
+      'should_callServiceWithSendNotification"$sendNotification"_when_notificationParameterIsUsed',
+      async (argument) => {
+        await unitUnderTest.loadStatus(
+          wahlID,
+          wahlbezirkID,
+          argument.sendNotification
+        );
+
+        expect(mockDefinitions.getStatus.mock.calls).toStrictEqual([
+          [wahlID, wahlbezirkID, argument.sendNotification],
+        ]);
+      }
+    );
   });
 });

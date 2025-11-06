bb9c8b00ba2313b41f79f2fa11715c16d3784861
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-11-06 17:37:28 +0100

    create table for votes for kandidaten of a wahlvorschlag

---
+++ wls-gui-wahllokalsystem/src/components/ergebnisermittlung/MBW/kandidatenStimmen/BaseWahlvorschlagKandidatenStimmenTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-table>
+    <colgroup>
+      <col style="width: 50px" />
+      <col />
+      <col style="width: 200px" />
+    </colgroup>
+    <thead>
+      <tr>
+        <td>Lfd. Nr.</td>
+        <td>Kandidatin/Kandidat</td>
+        <td>Schlusszahl</td>
+      </tr>
+    </thead>
+    <tbody>
+      <base-kandidat-row
+        v-for="(ergebnisAndKandidat, index) in ergebnisseAndKandidaten"
+        :key="index"
+        v-model="ergebnisAndKandidat.ergebnis"
+        :kandidat="ergebnisAndKandidat.kandidat"
+        :wahlvorschlagNummer="wahlvorschlagNummer"
+      />
+    </tbody>
+    <tfoot>
+      <tr>
+        <td>Gesamtstimmenzahl</td>
+        <td />
+        <td>0</td>
+      </tr>
+    </tfoot>
+  </v-table>
+</template>
+
+<script setup lang="ts">
+import type { ErgebnisAndKandidat } from "@/types/ergebnisermittlung/ErgebnisAndKandidat.ts";
+
+import BaseKandidatRow from "@/components/ergebnisermittlung/MBW/kandidatenStimmen/BaseKandidatRow.vue";
+
+const ergebnisseAndKandidaten = defineModel<ErgebnisAndKandidat[]>(
+  "modelValue",
+  {
+    required: true,
+  }
+);
+
+const props = defineProps({
+  wahlvorschlagNummer: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>

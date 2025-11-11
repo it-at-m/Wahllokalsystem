6b81dc2d66d5ba537609c51748cb80f215715b6b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-11-11 11:54:45 +0100

    create base button for folding

---
+++ wls-gui-wahllokalsystem/src/components/common/buttons/BaseButtonFolding.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn
+    :icon="icon"
+    variant="flat"
+    @click="onFoldingStateClicked()"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const isFoldedModel = defineModel("modelValue", {
+  type: Boolean,
+  required: false,
+});
+
+const icon = computed(() => (isFoldedModel.value ? "$expand" : "$collapse"));
+
+function onFoldingStateClicked() {
+  isFoldedModel.value = !isFoldedModel.value;
+}
+</script>

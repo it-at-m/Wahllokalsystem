4dc1114109971d751895667eb61a27789fd65cee
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-11-13 16:02:29 +0100

    revert set toJSON function from Set prototype

--- wls-gui-wahllokalsystem/.storybook/preview.ts
+++ wls-gui-wahllokalsystem/.storybook/preview.ts
@@ -12,10 +12,6 @@ setup((app) => {
 
 initialize();
 
-Set.prototype.toJSON = function toJSON() {
-  return [...Set.prototype.values.call(this)];
-};
-
 const preview: Preview = {
   tags: ["autodocs"],
   parameters: {

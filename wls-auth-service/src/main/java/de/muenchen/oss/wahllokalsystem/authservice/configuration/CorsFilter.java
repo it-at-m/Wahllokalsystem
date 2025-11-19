1563a86da4e3a72aa9add299df49ae1cd8df9ea6
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-11-19 18:07:42 +0100

    restrict cors origins

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/configuration/CorsFilter.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/configuration/CorsFilter.java
@@ -5,6 +5,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.val;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -12,10 +16,14 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@ConfigurationProperties(prefix = "service.config.cors")
 public class CorsFilter extends OncePerRequestFilter {
+
+    private List<String> allowedOrigins = new ArrayList<>();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        setHeaderAccessControlAllowOrigin(request, response);
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         //        response.setHeader("Access-Control-Allow-Origin", "https://localhost:8083/");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
@@ -25,5 +33,13 @@ public class CorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
         filterChain.doFilter(request, response);
+    }
+
+    private void setHeaderAccessControlAllowOrigin(HttpServletRequest request, final HttpServletResponse response) {
+        val requestOrigin = request.getHeader("Origin");
+        if (allowedOrigins.contains(requestOrigin)) {
+            response.setHeader("Access-Control-Allow-Origin", requestOrigin);
+        }
+
     }
 }

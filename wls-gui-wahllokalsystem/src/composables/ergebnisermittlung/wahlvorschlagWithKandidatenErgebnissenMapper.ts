eddbaf9e690b73226485750f2b3026a3833f6fcb
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2025-11-20 09:30:34 +0100

    improve Code
    Co-authored-by: J.R. <robert.jasny@muenchen.de>

--- wls-gui-wahllokalsystem/src/composables/ergebnisermittlung/wahlvorschlagWithKandidatenErgebnissenMapper.ts
+++ wls-gui-wahllokalsystem/src/composables/ergebnisermittlung/wahlvorschlagWithKandidatenErgebnissenMapper.ts
@@ -39,7 +39,7 @@ export function useWahlvorschlagWithKandidatenErgebnissenMapper() {
       bezirkUndWahlIDStapelart: {
         wahlID,
         wahlbezirkID,
-        stapelArt: stapelArt,
+        stapelArt,
       },
       ergebnisse,
     };

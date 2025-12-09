5a7be731656a0730cc5b9ff86c8aed3e12fedaed
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-12-09 11:00:58 +0100

    spottless:apply vorfälle und vorkommnisse service

--- wls-vorfaelleundvorkommnisse-service/src/main/java/de/muenchen/oss/wahllokalsystem/vorfaelleundvorkommnisseservice/rest/ereignis/WahlbezirkEreignisseDTO.java
+++ wls-vorfaelleundvorkommnisse-service/src/main/java/de/muenchen/oss/wahllokalsystem/vorfaelleundvorkommnisseservice/rest/ereignis/WahlbezirkEreignisseDTO.java
@@ -4,9 +4,4 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Builder;
 
-@Builder
-public record WahlbezirkEreignisseDTO(@NotNull String wahlbezirkID,
-                                      boolean keineVorfaelle,
-                                      boolean keineVorkommnisse,
-                                      List<EreignisDTO> ereigniseintraege) {
-}
+@Builder public record WahlbezirkEreignisseDTO(@NotNull String wahlbezirkID,boolean keineVorfaelle,boolean keineVorkommnisse,List<EreignisDTO>ereigniseintraege){}

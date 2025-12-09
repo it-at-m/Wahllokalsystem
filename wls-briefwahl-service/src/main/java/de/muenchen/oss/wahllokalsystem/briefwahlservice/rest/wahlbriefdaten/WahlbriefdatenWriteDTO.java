0180f0f12f43cd8e581c8b3717c2b930d59acf99
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2025-12-09 10:59:18 +0100

    spottless:apply briefwahl service

--- wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/rest/wahlbriefdaten/WahlbriefdatenWriteDTO.java
+++ wls-briefwahl-service/src/main/java/de/muenchen/oss/wahllokalsystem/briefwahlservice/rest/wahlbriefdaten/WahlbriefdatenWriteDTO.java
@@ -2,9 +2,4 @@ package de.muenchen.oss.wahllokalsystem.briefwahlservice.rest.wahlbriefdaten;
 
 import java.time.LocalDateTime;
 
-public record WahlbriefdatenWriteDTO(Long wahlbriefe,
-                                     Long verzeichnisseUngueltige,
-                                     Long nachtraege,
-                                     Long nachtraeglichUeberbrachte,
-                                     LocalDateTime zeitNachtraeglichUeberbrachte) {
-}
+public record WahlbriefdatenWriteDTO(Long wahlbriefe,Long verzeichnisseUngueltige,Long nachtraege,Long nachtraeglichUeberbrachte,LocalDateTime zeitNachtraeglichUeberbrachte){}

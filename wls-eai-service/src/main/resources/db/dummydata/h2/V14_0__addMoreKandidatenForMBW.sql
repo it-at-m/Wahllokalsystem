42a0871f1b6bf8988d2e8d9d3a9989d682779d69
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-01-13 18:39:35 +0100

    #1976 update dumydata

--- wls-eai-service/src/main/resources/db/dummydata/h2/V14_0__addMoreKandidatenForMBW.sql
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V14_0__addMoreKandidatenForMBW.sql
@@ -4,47 +4,66 @@
 
 -- Hinzufügen von 5 weiteren Kandidaten zu Wahlvorschlag 2
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('833d3874-63b7-4ad9-bf87-6ce62f5c8785', 'Sofia Kim', 6, false, 6, false, 'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
+VALUES ('833d3874-63b7-4ad9-bf87-6ce62f5c8785', 'Sofia Kim', 6, false, 1, false,
+        'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('162fc565-cde7-45fc-a4ee-158c1410fa81', 'Luca Meyer', 7, false, 7, false, 'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
+VALUES ('162fc565-cde7-45fc-a4ee-158c1410fa81', 'Luca Meyer', 7, false, 1, false,
+        'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('f2592609-2dd0-4d13-9900-05b4a18457d2', 'Mila Schmidt', 8, false, 8, false, 'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
+VALUES ('f2592609-2dd0-4d13-9900-05b4a18457d2', 'Mila Schmidt', 8, false, 1, false,
+        'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('6cdf6cc9-2a46-437b-87c3-0d5d2d10ffd0', 'Felix Braun', 9, false, 9, false, 'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
+VALUES ('6cdf6cc9-2a46-437b-87c3-0d5d2d10ffd0', 'Felix Braun', 9, false, 2, false,
+        'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('bd68f65a-d317-42b6-81d3-cd3b0271cba7', 'Leonie Fischer', 10, false, 10, false, 'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
+VALUES ('bd68f65a-d317-42b6-81d3-cd3b0271cba7', 'Leonie Fischer', 10, false, 2, false,
+        'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5f');
 
 -- Hinzufügen von 15 weiteren Kandidaten zu Wahlvorschlag 4
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('ac13dfc6-1167-47ba-a4ca-8f34a9c17cd7', 'Tomoko Suzuki', 7, false, 1, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('ac13dfc6-1167-47ba-a4ca-8f34a9c17cd7', 'Tomoko Suzuki', 7, false, 1, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('c109888f-b29d-49bf-9b78-929bdb517772', 'René Dupont', 8, false, 2, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('c109888f-b29d-49bf-9b78-929bdb517772', 'René Dupont', 8, false, 1, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('cb80bddf-b967-487d-8587-657b32561890', 'Ali Karam', 9, false, 3, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('cb80bddf-b967-487d-8587-657b32561890', 'Ali Karam', 9, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('e7f41b65-ec69-4e1a-a24d-820460ec09ca', 'Fiona O’Reilly', 10, false, 4, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('e7f41b65-ec69-4e1a-a24d-820460ec09ca', 'Fiona O’Reilly', 10, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('f0b10eeb-a34a-40a2-be7e-3e6ee6ef2e7e', 'Hugo Morales', 11, false, 5, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('f0b10eeb-a34a-40a2-be7e-3e6ee6ef2e7e', 'Hugo Morales', 11, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('b15e2f6d-7d80-4e0f-9b37-ef297bcf3659', 'Olivia Bennett', 12, false, 6, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('b15e2f6d-7d80-4e0f-9b37-ef297bcf3659', 'Olivia Bennett', 12, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('5827592d-ae6f-42c9-a860-c3c2ee962241', 'Nina Petrov', 13, false, 7, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('5827592d-ae6f-42c9-a860-c3c2ee962241', 'Nina Petrov', 13, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('bc1479fa-1bcc-42a7-b8c9-0f2821336033', 'Julian Wang', 14, false, 8, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('bc1479fa-1bcc-42a7-b8c9-0f2821336033', 'Julian Wang', 14, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('842fb1d9-3651-46b7-8feb-f991dac272ce', 'Sofia Alvi', 15, false, 9, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('842fb1d9-3651-46b7-8feb-f991dac272ce', 'Sofia Alvi', 15, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('6dcfaf88-10c5-46e0-bb7c-af10803baa31', 'Mateo Silva', 16, false, 10, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('6dcfaf88-10c5-46e0-bb7c-af10803baa31', 'Mateo Silva', 16, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('76b077b4-0cf4-4183-b232-41ca1a5ca238', 'Diana Morales', 17, false, 11, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('76b077b4-0cf4-4183-b232-41ca1a5ca238', 'Diana Morales', 17, false, 3, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('84d5d7a7-f9cc-4dc9-873f-ae23dc04db09', 'Ravi Singh', 18, false, 12, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('84d5d7a7-f9cc-4dc9-873f-ae23dc04db09', 'Ravi Singh', 18, false, 3, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('8fb96cdc-1d78-409e-b077-f5625800f280', 'Anya Novak', 19, false, 13, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('8fb96cdc-1d78-409e-b077-f5625800f280', 'Anya Novak', 19, false, 3, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('59b7e7aa-b394-4699-b992-4006889c996a', 'Tariq Anwar', 20, false, 14, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('59b7e7aa-b394-4699-b992-4006889c996a', 'Tariq Anwar', 20, false, 3, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('c2d7d85e-d9db-4bb2-967f-45448ce89fdc', 'Kimberly Price', 21, false, 15, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
+VALUES ('c2d7d85e-d9db-4bb2-967f-45448ce89fdc', 'Kimberly Price', 21, false, 3, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6f');
 
 -- ====================================
 -- WAHLBEZIRK 2 (BWB)
@@ -53,44 +72,62 @@ VALUES ('c2d7d85e-d9db-4bb2-967f-45448ce89fdc', 'Kimberly Price', 21, false, 15,
 
 -- Hinzufügen von 5 weiteren Kandidaten zu Wahlvorschlag 2
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('4a9450fe-61fb-4b43-a977-032354a5eb72', 'Sofia Kim', 6, false, 6, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
+VALUES ('4a9450fe-61fb-4b43-a977-032354a5eb72', 'Sofia Kim', 6, false, 1, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('3a6b56eb-64db-45e5-96f2-fefc2d49b848', 'Luca Meyer', 7, false, 7, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
+VALUES ('3a6b56eb-64db-45e5-96f2-fefc2d49b848', 'Luca Meyer', 7, false, 1, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('8a02a19c-bbf3-425c-9faa-539bc1164af2', 'Mila Schmidt', 8, false, 8, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
+VALUES ('8a02a19c-bbf3-425c-9faa-539bc1164af2', 'Mila Schmidt', 8, false, 1, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('98f8fe26-afbe-4d50-9787-6e00c0c363e0', 'Felix Braun', 9, false, 9, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
+VALUES ('98f8fe26-afbe-4d50-9787-6e00c0c363e0', 'Felix Braun', 9, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('1cce3efa-279e-4353-8c1c-92cb2a97aaaf', 'Leonie Fischer', 10, false, 10, false, 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
+VALUES ('1cce3efa-279e-4353-8c1c-92cb2a97aaaf', 'Leonie Fischer', 10, false, 2, false,
+        'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6a');
 
 -- Hinzufügen von 15 weiteren Kandidaten zu Wahlvorschlag 4
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
 VALUES ('c557bbad-d21e-4372-94c5-a3b14daedc50', 'Tomoko Suzuki', 7, false, 1, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('59993798-01aa-4628-af2f-a5e6d740826b', 'René Dupont', 8, false, 2, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('59993798-01aa-4628-af2f-a5e6d740826b', 'René Dupont', 8, false, 1, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('32c956d9-fd94-486e-8450-56f4d1ccc109', 'Ali Karam', 9, false, 3, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('32c956d9-fd94-486e-8450-56f4d1ccc109', 'Ali Karam', 9, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('8cc9d266-11a6-468e-a7ef-0ee51c32f19f', 'Fiona O’Reilly', 10, false, 4, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('8cc9d266-11a6-468e-a7ef-0ee51c32f19f', 'Fiona O’Reilly', 10, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('c2bc04ce-d363-4cdc-9cc0-fd19f124def5', 'Hugo Morales', 11, false, 5, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('c2bc04ce-d363-4cdc-9cc0-fd19f124def5', 'Hugo Morales', 11, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('0829bfbc-3aa9-4b7b-a2db-15ba678260fc', 'Olivia Bennett', 12, false, 6, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('0829bfbc-3aa9-4b7b-a2db-15ba678260fc', 'Olivia Bennett', 12, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('c6d22c58-8eab-4791-b15d-339fbe01c85f', 'Nina Petrov', 13, false, 7, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('c6d22c58-8eab-4791-b15d-339fbe01c85f', 'Nina Petrov', 13, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('44e4662f-8d48-4184-b9b5-223ba3936d67', 'Julian Wang', 14, false, 8, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('44e4662f-8d48-4184-b9b5-223ba3936d67', 'Julian Wang', 14, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('f05bafc3-3fdc-48a9-8b1b-f157b1bae025', 'Sofia Alvi', 15, false, 9, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('f05bafc3-3fdc-48a9-8b1b-f157b1bae025', 'Sofia Alvi', 15, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('117a9c29-9af2-4f40-bb28-912469435677', 'Mateo Silva', 16, false, 10, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('117a9c29-9af2-4f40-bb28-912469435677', 'Mateo Silva', 16, false, 2, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('1e2ca050-2250-4c08-956b-1b4eef0a4ac6', 'Diana Morales', 17, false, 11, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('1e2ca050-2250-4c08-956b-1b4eef0a4ac6', 'Diana Morales', 17, false, 3, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('b1b790cb-ff5f-4705-bfc3-fe875651e733', 'Ravi Singh', 18, false, 12, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('b1b790cb-ff5f-4705-bfc3-fe875651e733', 'Ravi Singh', 18, false, 3, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('7cff463b-0440-4bdc-bc0f-fef21fe3127d', 'Anya Novak', 19, false, 13, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('7cff463b-0440-4bdc-bc0f-fef21fe3127d', 'Anya Novak', 19, false, 3, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('a0451329-f163-4405-b56d-e4d5d3b64f42', 'Tariq Anwar', 20, false, 14, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('a0451329-f163-4405-b56d-e4d5d3b64f42', 'Tariq Anwar', 20, false, 3, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
 INSERT INTO Kandidat (id, name, listenposition, direktkandidat, tabellenSpalteInNiederschrift, einzelbewerber, wahlvorschlagId)
-VALUES ('fc72a596-f0ee-4641-a9fd-0e99a1e9745f', 'Kimberly Price', 21, false, 15, false, 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
+VALUES ('fc72a596-f0ee-4641-a9fd-0e99a1e9745f', 'Kimberly Price', 21, false, 3, false,
+        'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6e');
---
+++ wls-eai-service/src/main/resources/db/dummydata/h2/V15_0__fixMBWKandidatenTabellenSpaltenNummer.sql
@@ -0,0 +1,206 @@
+-- ====================================
+-- WAHLBEZIRK 1 (UWB)
+-- ====================================
+
+-- Wahlvorschlag 1 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a9b0c1d2-e3f4-4a5b-6c7d-8e9f0a1b2c3d';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b0c1d2e3-f4a5-4b6c-7d8e-9f0a1b2c3d4e';
+
+-- Wahlvorschlag 2 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e3f4a5b6-c7d8-4e9f-0a1b-2c3d4e5f6a7b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f4a5b6c7-d8e9-4f0a-1b2c-3d4e5f6a7b8c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a5b6c7d8-e9f0-4a1b-2c3d-4e5f6a7b8c9d';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b6c7d8e9-f0a1-4b2c-3d4e-5f6a7b8c9d0e';
+
+-- Wahlvorschlag 3 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e9f0a1b2-c3d4-4e5f-6a7b-8c9d0e1f2a3b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f0a1b2c3-d4e5-4f6a-7b8c-9d0e1f2a3b4c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5e';
+
+-- Wahlvorschlag 4 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd4e5f6a7-b8c9-4d0e-1f2a-3b4c5d6e7f8b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e5f6a7b8-c9d0-4e1f-2a3b-4c5d6e7f8a9c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f6a7b8c9-d0e1-4f2a-3b4c-5d6e7f8a9b0d';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a7b8c9d0-e1f2-4a3b-4c5d-6e7f8a9b0c1e';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b8c9d0e1-f2a3-4b4c-5d6e-7f8a9b0c1d2f';
+
+-- Wahlvorschlag 5 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e1f2a3b4-c5d6-4e7f-8a9b-0c1d2e3f4a5c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f2a3b4c5-d6e7-4f8a-9b0c-1d2e3f4a5b6d';
+
+-- Wahlvorschlag 6 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'c5d6e7f8-a9b0-4c1d-2e3f-4a5b6c7d8e9a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd6e7f8a9-b0c1-4d2e-3f4a-5b6c7d8e9f0b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e7f8a9b0-c1d2-4e3f-4a5b-6c7d8e9f0a1c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f8a9b0c1-d2e3-4f4a-5b6c-7d8e9f0a1b2d';
+
+-- Wahlvorschlag 7 (UWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'c1d2e3f4-a5b6-4c7d-8e9f-0a1b2c3d4e5a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd2e3f4a5-b6c7-4d8e-9f0a-1b2c3d4e5f6b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e3f4a5b6-c7d8-4e9f-0a1b-2c3d4e5f6a7c';
+
+-- ====================================
+-- WAHLBEZIRK 2 (BWB)
+-- (verwendet die gleichen Kandidaten wie UWB)
+-- ====================================
+
+-- Wahlvorschlag 1 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'c7d8e9f0-a1b2-4c3d-4e5f-6a7b8c9d0e1a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd8e9f0a1-b2c3-4d4e-5f6a-7b8c9d0e1f2b';
+
+-- Wahlvorschlag 2 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd4e5f6a7-b8c9-4d0e-1f2a-3b4c5d6e7f8c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e5f6a7b8-c9d0-4e1f-2a3b-4c5d6e7f8a9d';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f6a7b8c9-d0e1-4f2a-3b4c-5d6e7f8a9b0a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a7b8c9d0-e1f2-4a3b-4c5d-6e7f8a9b0c1a';
+
+-- Wahlvorschlag 3 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b8c9d0e1-f2a3-4b4c-5d6e-7f8a9b0c1d2a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'c9d0e1f2-a3b4-4c5d-6e7f-8a9b0c1d2e3b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd0e1f2a3-b4c5-4d6e-7f8a-9b0c1d2e3f4c';
+
+-- Wahlvorschlag 4 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b4c5d6e7-f8a9-4b0c-1d2e-3f4a5b6c7d8a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'c5d6e7f8-a9b0-4c1d-2e3f-4a5b6c7d8e9b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd6e7f8a9-b0c1-4d2e-3f4a-5b6c7d8e9f0c';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e7f8a9b0-c1d2-4e3f-4a5b-6c7d8e9f0a1e';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f8a9b0c1-d2e3-4f4a-5b6c-7d8e9f0a1b2a';
+
+-- Wahlvorschlag 5 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a9b0c1d2-e3f4-4a5b-6c7d-8e9f0a1b2c3f';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b0c1d2e3-f4a5-4b6c-7d8e-9f0a1b2c3d4a';
+
+-- Wahlvorschlag 6 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b6c7d8e9-f0a1-4b2c-3d4e-5f6a7b8c9d0a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'c7d8e9f0-a1b2-4c3d-4e5f-6a7b8c9d0e1b';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'd8e9f0a1-b2c3-4d4e-5f6a-7b8c9d0e1f2d';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'e9f0a1b2-c3d4-4e5f-6a7b-8c9d0e1f2a3e';
+
+-- Wahlvorschlag 7 (BWB)
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'f0a1b2c3-d4e5-4f6a-7b8c-9d0e1f2a3b4e';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5a';
+
+UPDATE Kandidat
+SET tabellenSpalteInNiederschrift = 1
+WHERE id = 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6b';

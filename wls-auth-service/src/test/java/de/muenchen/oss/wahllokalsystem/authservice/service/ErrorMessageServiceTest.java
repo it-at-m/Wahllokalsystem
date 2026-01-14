0a15fc8bfa71ea60aa67ba4cf701aab6340d1373
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-01-14 14:53:06 +0100

    created Tests

---
+++ wls-auth-service/src/test/java/de/muenchen/oss/wahllokalsystem/authservice/service/ErrorMessageServiceTest.java
@@ -0,0 +1,70 @@
+package de.muenchen.oss.wahllokalsystem.authservice.service;
+
+
+import de.muenchen.oss.wahllokalsystem.authservice.security.ErrorMessages;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
+
+class ErrorMessageServiceTest {
+
+    private final ErrorMessageService unitUnderTest = new ErrorMessageService();
+
+    @Nested
+    class getErrorMessage {
+
+        @Test
+        void should_returnTheMessageOfTheException_when_exceptionIsInstanceOfBadCredentialsException() {
+            val errorMessage = "bad credentials";
+            val result = unitUnderTest.getErrorMessage(new BadCredentialsException(errorMessage));
+
+            Assertions.assertThat(result).isEqualTo(errorMessage);
+        }
+
+        @Test
+        void should_returnTheMessageOfTheException_when_exceptionIsInstanceOfLockedException() {
+            val errorMessage = "bad credentials";
+            val result = unitUnderTest.getErrorMessage(new LockedException(errorMessage));
+
+            Assertions.assertThat(result).isEqualTo(errorMessage);
+        }
+
+        @Test
+        void should_returnTheMessageOfTheException_when_exceptionIsAccessDeniedException() {
+            val errorMessage = "bad credentials";
+            val result = unitUnderTest.getErrorMessage(new AccessDeniedException(errorMessage));
+
+            Assertions.assertThat(result).isEqualTo(errorMessage);
+        }
+
+        @Test
+        void should_returnTheMessageOfTheExceptionAfterFixedErrorMessage_when_exceptionIsInstanceOfSessionAuthenticationException() {
+            val errorMessage = "bad credentials";
+            val result = unitUnderTest.getErrorMessage(new SessionAuthenticationException(errorMessage));
+
+            Assertions.assertThat(result).isEqualTo(ErrorMessages.BENUTZER_BEREITS_ANGEMELDET + errorMessage);
+        }
+
+        @Test
+        void should_returnFixedErrorMessage_when_exceptionIsInstanceOfInternalAuthenticationServiceException() {
+            val errorMessage = "bad credentials";
+            val result = unitUnderTest.getErrorMessage(new InternalAuthenticationServiceException(errorMessage));
+
+            Assertions.assertThat(result).isEqualTo(ErrorMessages.LOGIN_BLOCKED_BY_LDAP_SERVER);
+        }
+
+        @Test
+        void should_returnFixedErrorMessage_when_exceptionIsNotHandledBefore() {
+            val errorMessage = "bad credentials";
+            val result = unitUnderTest.getErrorMessage(new Exception(errorMessage));
+
+            Assertions.assertThat(result).isEqualTo(ErrorMessages.INVALID_USERNAME_OR_PASSWORD);
+        }
+    }
+}

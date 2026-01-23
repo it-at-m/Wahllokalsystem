ea20f52796a055f7364660e2dbf22f7943c949a7
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-01-23 18:09:08 +0100

    remove todo comment

--- wls-gui-wahllokalsystem/src/types/ergebnismeldung/common/Status.ts
+++ wls-gui-wahllokalsystem/src/types/ergebnismeldung/common/Status.ts
@@ -2,9 +2,6 @@ import type { BezirkUndWahlID } from "@/types/ergebnismeldung/common/BezirkUndWa
 import type { Meldung } from "@/types/ergebnismeldung/common/Meldung.ts";
 
 export interface Status {
-  /*
-TODO: neuer Name? ElectionStatus weil es noch einen Status ohne eine bestimmte Wahl gibt?
-*/
   bezirkUndWahlID: BezirkUndWahlID;
   schnellmeldung: Meldung;
   niederschrift: Meldung;

84657dd87b304e83adde9bf83849ff3491028e18
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-01-23 18:06:04 +0100

    restore old empty line

--- wls-gui-wahllokalsystem/src/composables/wahlvorstand/wahlvorstandService.ts
+++ wls-gui-wahllokalsystem/src/composables/wahlvorstand/wahlvorstandService.ts
@@ -51,6 +51,7 @@ export function useWahlvorstandService() {
       wahlvorstand?.wahlvorstandsmitglieder.sort(
         compareWahlvorstandsMitglieder
       );
+
       return wahlvorstand;
     } catch (error) {
       if (sendNotification) {

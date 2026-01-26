0498779050c3d81185675c0fb4d7c7bc0da97eb7
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-01-26 08:35:28 +0100

    #1948 add helper functions for printing

--- wls-gui-wahllokalsystem/src/composables/ergebnismeldung/MBW/mbwUtils.ts
+++ wls-gui-wahllokalsystem/src/composables/ergebnismeldung/MBW/mbwUtils.ts
@@ -1,21 +1,28 @@
 import type { AWerte } from "@/types/ergebnismeldung/common/AWerte.ts";
 import type { BWerte } from "@/types/ergebnismeldung/common/BWerte.ts";
+import type { Status } from "@/types/ergebnismeldung/common/Status.ts";
+import type { ErgebnismeldungDruckInput } from "@/types/ergebnismeldung/MBW/ErgebnismeldungDruckInput.ts";
 import type { MbwErgebnisseAndWahlvorschlag } from "@/types/ergebnismeldung/MBW/MbwErgebnisseAndWahlvorschlag.ts";
+import type { Wahl } from "@/types/wahl/Wahl.ts";
 import type { Wahlvorschlag } from "@/types/wahlvorschlaege/Wahlvorschlag.ts";
 
 import { storeToRefs } from "pinia";
 import { ref } from "vue";
 
+import { useDateTimeFormatter } from "@/composables/common/dateTimeFormatter.ts";
 import { useLogging } from "@/composables/common/logging.ts";
+import { useNumberFormatter } from "@/composables/common/numberFormatter.ts";
 import { useAWerteService } from "@/composables/ergebnismeldung/common/aWerteService.ts";
 import { useErgebnisService } from "@/composables/ergebnismeldung/common/ergebnisService.ts";
 import { useMbwErgebnisAndWahlvorschlagMapper } from "@/composables/ergebnismeldung/MBW/mbwErgebnisAndWahlvorschlagMapper.ts";
 import { useStimmabgabevermerkeService } from "@/composables/stimmabgabevermerke/stimmabgabevermerkeService.ts";
 import { useWahlvorschlaegeService } from "@/composables/wahlvorschlaege/wahlvorschlaegeService.ts";
 import { useWahlvorschlagUtils } from "@/composables/wahlvorschlaege/wahlvorschlagUtils.ts";
+import { useStatusStore } from "@/stores/statusStore.ts";
 import { useUserStore } from "@/stores/userStore.ts";
 import { useWahlenStore } from "@/stores/wahlenStore.ts";
 import { StapelArtEnum } from "@/types/ergebnismeldung/common/StapelArtEnum.ts";
+import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
 
 const {
   postErgebnisse,
@@ -28,14 +35,23 @@ const { sortWahlvorschlaegeByOrdnungszahl } = useWahlvorschlagUtils();
 const { getAWerte } = useAWerteService();
 const { getStimmabgabevermerke } = useStimmabgabevermerkeService();
 const { logError } = useLogging("mbwUtils");
+const { convertToSixDigitArray } = useNumberFormatter();
+const { toGermanDate, toHhMm } = useDateTimeFormatter();
 
 export function useMbwUtils(wahlID: string, wahlbezirkID: string) {
   const { mapErgebnisseFromErgebnisseAndWahlvorschlagListToErgebnisse } =
     useMbwErgebnisAndWahlvorschlagMapper(wahlID, wahlbezirkID);
 
-  const { currentUserWahlbezirkID } = storeToRefs(useUserStore());
   const { wahlenActions, waehlerverzeichnisActions } = useWahlenStore();
-  const { isUWB, isBWB } = storeToRefs(useUserStore());
+  const {
+    isUWB,
+    isBWB,
+    currentUserWahlbezirkNummer,
+    currentUserWahlbezirkID,
+    currentUserWahlbezirksArt,
+  } = storeToRefs(useUserStore());
+  const { status } = storeToRefs(useStatusStore());
+  const { loadStatus } = useStatusStore();
 
   const isErgebnisseSaving = ref<boolean>(false);
   const isSendingSchnellmeldung = ref<boolean>(false);
@@ -202,6 +218,115 @@ export function useMbwUtils(wahlID: string, wahlbezirkID: string) {
     }
   }
 
+  async function prepareDataForErgebnismeldungDruck(
+    wahl: Wahl
+  ): Promise<ErgebnismeldungDruckInput> {
+    let aWerte: AWerte | undefined;
+    if (currentUserWahlbezirksArt.value == WahlbezirksArtEnum.UWB) {
+      aWerte = await getAWerteForWahlbezirkAndWahl();
+    } else {
+      aWerte = undefined;
+    }
+
+    const bWerte = await getBWerteForWahlbezirkAndWahl();
+
+    const ergebnisseAndWahlvorschlaege =
+      await loadAndCombineErgebnisseAndWahlvorschlaege();
+    let gueltigeStimmenGesamt = 0;
+    for (const vorschlag of ergebnisseAndWahlvorschlaege) {
+      gueltigeStimmenGesamt =
+        gueltigeStimmenGesamt +
+        (vorschlag.ergebnisStapelA.ergebnis ?? 0) +
+        (vorschlag.ergebnisStapelB.ergebnis ?? 0);
+    }
+
+    const ungueltige = await getErgebnisse(
+      wahlbezirkID,
+      wahlID,
+      StapelArtEnum.MbwDUngueltig,
+      false
+    );
+    const ungueltigeStimmen = ungueltige?.ergebnisse[0]?.ergebnis || 0;
+
+    const stimmenGesamt = gueltigeStimmenGesamt + ungueltigeStimmen;
+
+    // todo: status wird erst mit #2002 gepflegt
+    await loadStatus(wahlID, wahlbezirkID, false);
+
+    const statusForWahlAndWahlbezirk = status.value.find(
+      (status) =>
+        status.bezirkUndWahlID.wahlID == wahlID &&
+        status.bezirkUndWahlID.wahlbezirkID == wahlbezirkID
+    );
+    const footer = _createFooter(statusForWahlAndWahlbezirk);
+
+    const moreThan25WahlvorschlaegeListe = _getMoreThan25WahlvorschlaegeListe(
+      ergebnisseAndWahlvorschlaege
+    );
+
+    return {
+      wahlbezirksArt: currentUserWahlbezirksArt.value,
+      aktuelleWahl: wahl,
+      footer: footer,
+      alleStimmen: convertToSixDigitArray(stimmenGesamt),
+      gueltigeStimmenListe:
+        /*ergebnisseAndWahlvorschlaege*/ moreThan25WahlvorschlaegeListe,
+      gueltigeStimmenGesamt: convertToSixDigitArray(gueltigeStimmenGesamt),
+      ungueltigeStimmen: convertToSixDigitArray(ungueltigeStimmen),
+      bWerte: bWerte,
+      aWerte: aWerte,
+      wahlbezirkNummer: currentUserWahlbezirkNummer.value || "",
+      barcode: "",
+      sendOk: false,
+    };
+  }
+
+  function _getMoreThan25WahlvorschlaegeListe(
+    ergebnisse: MbwErgebnisseAndWahlvorschlag[]
+  ): MbwErgebnisseAndWahlvorschlag[] {
+    const neueListe: MbwErgebnisseAndWahlvorschlag[] = [];
+    let ordnungszahl = 1;
+
+    for (let i = 0; i < 5; i++) {
+      // 5x duplizieren
+      for (const originalErgebnis of ergebnisse) {
+        // zufällige zahlen auslassen um zu prüfen ob trotzdem nach jeweils 25 eine neue seite begonnen wird
+        if (
+          ordnungszahl == 3 ||
+          ordnungszahl == 4 ||
+          ordnungszahl == 5 ||
+          ordnungszahl == 9 ||
+          ordnungszahl == 10 ||
+          ordnungszahl == 12 ||
+          ordnungszahl == 15 ||
+          ordnungszahl == 19 ||
+          ordnungszahl == 22 ||
+          ordnungszahl == 23 ||
+          ordnungszahl == 26 ||
+          ordnungszahl == 27 ||
+          ordnungszahl == 33 ||
+          ordnungszahl == 34 ||
+          ordnungszahl == 37 ||
+          ordnungszahl == 38 ||
+          ordnungszahl == 39
+        ) {
+          ordnungszahl += 2;
+        }
+        const neuesErgebnis: MbwErgebnisseAndWahlvorschlag = {
+          ...originalErgebnis,
+          wahlvorschlag: {
+            ...originalErgebnis.wahlvorschlag,
+            ordnungszahl: ordnungszahl,
+          },
+        };
+        neueListe.push(neuesErgebnis);
+        ordnungszahl++;
+      }
+    }
+
+    return neueListe;
+  }
+
   async function _loadGueltigeErgebnisseByStapelArt(stapelArt: StapelArtEnum) {
     try {
       return await getErgebnisse(wahlbezirkID, wahlID, stapelArt, false);
@@ -232,6 +357,36 @@ export function useMbwUtils(wahlID: string, wahlbezirkID: string) {
     };
   }
 
+  function _createFooter(status: Status | undefined): string {
+    if (
+      status &&
+      status.schnellmeldung &&
+      status.schnellmeldung.validierungsstatus
+    ) {
+      const date = new Date();
+      const formattedDateWithTime = toGermanDate(date) + " " + toHhMm(date);
+
+      if (status.schnellmeldung.validierungsstatus === "VALIDE") {
+        return "" + _uuidv4() + ", " + formattedDateWithTime + " O";
+      } else {
+        return "" + _uuidv4() + ", " + formattedDateWithTime + " M";
+      }
+    } else {
+      return "test"; // todo was wird hier returned?
+    }
+  }
+
+  function _uuidv4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
+
   return {
     isErgebnisseSaving,
     isSendingSchnellmeldung,
@@ -240,5 +395,6 @@ export function useMbwUtils(wahlID: string, wahlbezirkID: string) {
     getAWerteForWahlbezirkAndWahl,
     getBWerteForWahlbezirkAndWahl,
     sendSchnellmeldung,
+    prepareDataForErgebnismeldungDruck,
   };
 }

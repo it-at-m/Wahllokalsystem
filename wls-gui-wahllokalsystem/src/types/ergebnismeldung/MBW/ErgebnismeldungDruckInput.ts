15cc3a527d03e4f29e5dd1145198bcbb1f7c2845
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-01-26 08:34:40 +0100

    #1948 add datatype

---
+++ wls-gui-wahllokalsystem/src/types/ergebnismeldung/MBW/ErgebnismeldungDruckInput.ts
@@ -0,0 +1,21 @@
+import type { AWerte } from "@/types/ergebnismeldung/common/AWerte.ts";
+import type { BWerte } from "@/types/ergebnismeldung/common/BWerte.ts";
+import type { MbwErgebnisseAndWahlvorschlag } from "@/types/ergebnismeldung/MBW/MbwErgebnisseAndWahlvorschlag.ts";
+import type { Wahl } from "@/types/wahl/Wahl.ts";
+import type { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
+
+export interface ErgebnismeldungDruckInput {
+  sendOk: boolean;
+  barcode: string;
+  wahlbezirkNummer: string;
+  aWerte?: AWerte;
+  bWerte: BWerte;
+  ungueltigeStimmen: string[];
+  gueltigeStimmenGesamt: string[];
+  gueltigeStimmenListe: MbwErgebnisseAndWahlvorschlag[];
+  alleStimmen: string[];
+  alleStimmenErgebnisGesamt?: string;
+  footer: string;
+  aktuelleWahl: Wahl;
+  wahlbezirksArt: WahlbezirksArtEnum;
+}

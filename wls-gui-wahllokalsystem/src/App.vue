336e76c654f274b5379a1650ef509ea6f2f98e6a
ThoGro <thomas.grossbeck@muenchen.de>
2026-02-10 13:55:10 +0100

    Add WahlschlussCheckDialog

--- wls-gui-wahllokalsystem/src/App.vue
+++ wls-gui-wahllokalsystem/src/App.vue
@@ -23,6 +23,9 @@
       v-if="isUWB && isTimeToCheckAnwesenheitInFuture"
       data-test="wahlvorstand-anwesenheits-check-popup-dialog"
     />
+    <the-wahlschluss-check-popup-dialog
+      v-if="isUWB && isTimeToCheckWahlschlussInFuture"
+    />
   </v-app>
 </template>
 
@@ -31,6 +34,7 @@ import { storeToRefs } from "pinia";
 import { computed, onMounted, onUnmounted } from "vue";
 
 import TheBroadcastReadConfirmationDialog from "@/components/broadcast/TheBroadcastReadConfirmationDialog.vue";
+import TheWahlschlussCheckPopupDialog from "@/components/wahlhandlung/TheWahlschlussCheckPopupDialog.vue";
 import TheWahlvorstandAnwesenheitsCheckPopupDialog from "@/components/wahlvorstand/TheWahlvorstandAnwesenheitsCheckPopupDialog.vue";
 import TheWlsAppBar from "@/components/wlsComponents/TheWlsAppBar.vue";
 import { useBroadcastCronjobService } from "@/composables/broadcast/broadcastCronjobService.ts";
@@ -42,7 +46,9 @@ import { useUserStore } from "@/stores/userStore.ts";
 import { useWahlenStore } from "@/stores/wahlenStore.ts";
 
 const { loadUser } = useUserStore();
-const { dateTimeToCheckAnwesenheit } = storeToRefs(useInfomanagementStore());
+const { dateTimeToCheckAnwesenheit, dateTimeToCheckWahlschluss } = storeToRefs(
+  useInfomanagementStore()
+);
 const { isUWB } = storeToRefs(useUserStore());
 const { initTasks } = useInitTaskManagerStore();
 const { wahlenActions } = useWahlenStore();
@@ -54,6 +60,12 @@ const { startBroadcastMessageInterval, stopBroadcastMessageInterval } =
 const isTimeToCheckAnwesenheitInFuture = computed(() =>
   dateTimeToCheckAnwesenheit.value
     ? isTodayOrFuture(dateTimeToCheckAnwesenheit.value)
+    : false
+);
+
+const isTimeToCheckWahlschlussInFuture = computed(() =>
+  dateTimeToCheckWahlschluss.value
+    ? isTodayOrFuture(dateTimeToCheckWahlschluss.value)
     : false
 );
 
---
+++ wls-gui-wahllokalsystem/src/components/wahlhandlung/TheWahlschlussCheckPopupDialog.vue
@@ -0,0 +1,74 @@
+<template>
+  <base-dialog
+    :visible="visible"
+    :dialogtitle="WAHLSCHLUSS_ERFASSEN"
+    :confirmtext="WAHLSCHLUSS_ERFASSEN"
+    canceltext="Bleiben"
+    icon="$information"
+    @confirm="onConfirmClicked"
+    @cancel="onCancelClicked"
+  >
+    <div>
+      Es ist {{ hourOfTimeToCheck }} Uhr und der Wahlschluss sollte erfasst
+      werden.
+    </div>
+    <div>
+      Stimmen Sie der sofortigen Weiterleitung zu, oder führen Sie diese Aufgabe
+      eigenverantwortlich später durch?
+    </div>
+  </base-dialog>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { computed, onMounted, onUnmounted, ref } from "vue";
+
+import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
+import { useDateOfActionTimeout } from "@/composables/scheduler/dateOfActionTimeout.ts";
+import { ROUTE_STIMMABGABE } from "@/constants.ts";
+import router from "@/plugins/router.ts";
+import { useInfomanagementStore } from "@/stores/infomanagementStore.ts";
+
+const { dateTimeToCheckWahlschluss } = storeToRefs(useInfomanagementStore());
+
+const WAHLSCHLUSS_ERFASSEN = "Wahlschluss erfassen";
+
+const { setupTimer, clearTimer } = useDateOfActionTimeout(
+  "Wahlschlusscheck Timeout",
+  dateTimeToCheckWahlschluss,
+  showDialog
+);
+
+const visible = ref(false);
+
+const hourOfTimeToCheck = computed(() => {
+  return dateTimeToCheckWahlschluss.value
+    ? new Date(dateTimeToCheckWahlschluss.value).getHours()
+    : 0;
+});
+
+onMounted(() => {
+  setupTimer();
+});
+
+onUnmounted(() => {
+  clearTimer();
+});
+
+function closeDialog() {
+  visible.value = false;
+}
+
+function onCancelClicked() {
+  closeDialog();
+}
+
+function onConfirmClicked() {
+  router.push({ name: ROUTE_STIMMABGABE });
+  closeDialog();
+}
+
+function showDialog() {
+  visible.value = true;
+}
+</script>

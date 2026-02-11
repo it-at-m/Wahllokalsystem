cdf9caab622eccc3137a60a22c9e482b0bb407b3
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-02-11 18:32:43 +0100

    #2037 add dialog component

---
+++ wls-gui-wahllokalsystem/src/components/wlsComponents/TheTestseiteDruckenDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <base-dialog
+    :visible="visible"
+    dialogtitle="Testdruck"
+    confirmtext="Testdruck starten"
+    canceltext="Schließen"
+    :cancel-disabled="cancelDisabled"
+    icon="$information"
+    @confirm="onConfirmClicked"
+    @cancel="onCancelClicked"
+  >
+    <div class="d-flex flex-column ga-5">
+      <div>
+        Bitte nehmen Sie sich ein paar Sekunden Zeit für einen Testdruck.
+      </div>
+      <div class="ml-10">
+        <ul>
+          <li>
+            Schalten Sie den Drucker ein und legen Sie ein Blatt Papier ein
+          </li>
+          <li>Im Anschluss starten Sie bitte den Testdruck</li>
+        </ul>
+      </div>
+      <div>
+        So können Sie sicherstellen, dass Ihr Drucker einwandfrei funktioniert.
+      </div>
+      <v-divider
+        :thickness="3"
+        color="black"
+      />
+      <div>
+        Sie können den Test auch später wiederholen. Öffnen Sie hierfür das
+        Hilfe-Menü (Fragezeichen-Symbol oben rechts). Dort finden Sie das Feld
+        "Testdruck starten".
+      </div>
+      <v-divider
+        :thickness="3"
+        color="black"
+      />
+      <div>
+        Sollten Sie Probleme beim Drucken haben, informieren Sie bitte das
+        Wahlamt.
+      </div>
+    </div>
+  </base-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
+import { useHelpIconCallbacks } from "@/composables/basisdaten/helpIconCallbacks.ts";
+
+const { printTestdruck } = useHelpIconCallbacks();
+
+const visible = ref(true);
+const cancelDisabled = ref(true);
+
+function closeDialog() {
+  visible.value = false;
+}
+
+function onCancelClicked() {
+  closeDialog();
+}
+
+function onConfirmClicked() {
+  printTestdruck();
+  cancelDisabled.value = false;
+}
+</script>

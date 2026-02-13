07e9fd89b9bd7a6865b8fa837948a25d51bae904
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-02-13 08:45:36 +0100

    created tests for serviceWorkerUtils

---
+++ wls-gui-wahllokalsystem/tests/composables/serviceWorker/servicWorkerUtils.spec.ts
@@ -0,0 +1,168 @@
+import type { ServiceWorkerMessage } from "@/types/serviceWorker/ServiceWorkerMessage.ts";
+
+import { useServiceWorkerTestDataFactory } from "@tests/utils/serviceWorker/serviceWorkerTestDataFactrory.ts";
+import {
+  afterAll,
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+
+import { useServiceWorkerUtils } from "@/composables/serviceWorker/serviceWorkerUtils.ts";
+
+const mockDefinitions = vi.hoisted(() => ({
+  postMessage: vi.fn(),
+}));
+
+Object.defineProperty(global.navigator, "serviceWorker", {
+  value: {
+    controller: {
+      postMessage: mockDefinitions.postMessage,
+    },
+  },
+  configurable: true,
+});
+
+const { createServiceWorkerMessage } = useServiceWorkerTestDataFactory();
+
+describe("serviceWorkerUtils.ts", () => {
+  let unitUnderTest: ReturnType<typeof useServiceWorkerUtils>;
+
+  beforeEach(() => {
+    unitUnderTest = useServiceWorkerUtils();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  afterAll(() => {
+    vi.resetAllMocks();
+  });
+
+  describe("awaitServiceWorkerActive", () => {
+    it("should_returnFalse_when_afterAllTriesAndTimeoutServiceWorkerControllerDoesNotExist", async () => {
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: null,
+        },
+        configurable: true,
+      });
+
+      const result = await unitUnderTest.awaitServiceWorkerActive();
+      expect(result).toStrictEqual(false);
+    });
+
+    it("should_returnTrue_when_serviceWorkerControllerAlreadyExists", async () => {
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: {
+            postMessage: mockDefinitions.postMessage,
+          },
+        },
+        configurable: true,
+      });
+
+      const result = await unitUnderTest.awaitServiceWorkerActive();
+      expect(result).toStrictEqual(true);
+    });
+
+    it("should_returnTrue_when_serviceWorkerWentActiveDuringWaits", async () => {
+      vi.useFakeTimers();
+
+      //start with mussing service worker controller
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: null,
+        },
+        configurable: true,
+      });
+
+      const numberOfTries = 4;
+      const delayForEachTryInMilliseconds = 200;
+      const waitForServiceWorkerPromise =
+        unitUnderTest.awaitServiceWorkerActive(
+          numberOfTries,
+          delayForEachTryInMilliseconds
+        );
+
+      vi.advanceTimersByTime(delayForEachTryInMilliseconds);
+      //service worker got online
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: {
+            postMessage: mockDefinitions.postMessage,
+          },
+        },
+        configurable: true,
+      });
+
+      const result = await waitForServiceWorkerPromise;
+      expect(result).toStrictEqual(true);
+      vi.useRealTimers();
+    });
+  });
+
+  describe("isServiceWorkerActive", () => {
+    it("should_returnTrue_when_serviceWorkerControllerExists", () => {
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: {
+            postMessage: mockDefinitions.postMessage,
+          },
+        },
+        configurable: true,
+      });
+
+      expect(unitUnderTest.isServiceWorkerActive()).toStrictEqual(true);
+    });
+
+    it("should_returnFalse_when_serviceWorkerControllerDoesNotExists", () => {
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: null,
+        },
+        configurable: true,
+      });
+
+      expect(unitUnderTest.isServiceWorkerActive()).toStrictEqual(false);
+    });
+  });
+
+  describe("sendMessage", () => {
+    it("should_sendMessage_when_serviceWorkerControllerExists", () => {
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: {
+            postMessage: mockDefinitions.postMessage,
+          },
+        },
+        configurable: true,
+      });
+
+      const messageToSend = createServiceWorkerMessage();
+      unitUnderTest.sendMessage(messageToSend);
+
+      expect(mockDefinitions.postMessage.mock.calls).toStrictEqual([
+        [messageToSend],
+      ]);
+    });
+
+    it("should_notSendMessage_when_serviceWorkerControllerDoesNotExists", () => {
+      Object.defineProperty(global.navigator, "serviceWorker", {
+        value: {
+          controller: null,
+        },
+        configurable: true,
+      });
+
+      const messageToSend = createServiceWorkerMessage();
+      unitUnderTest.sendMessage(messageToSend);
+
+      expect(mockDefinitions.postMessage.mock.calls.length).toStrictEqual(0);
+    });
+  });
+});

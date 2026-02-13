829499236aea8ce32e39ea11fc90d8dfc0e9223e
ThoGro <thomas.grossbeck@muenchen.de>
2026-02-13 11:18:17 +0100

    Card mit Actions in Base-Component ausgelagert

---
+++ wls-gui-wahllokalsystem/src/components/ergebnismeldung/common/BaseErgebnismeldungCardsContainer.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card>
+    <v-card-title class="font-weight-bold">{{ title }}</v-card-title>
+    <v-card-subtitle class="font-weight-bold mb-10">{{
+      subtitle
+    }}</v-card-subtitle>
+    <div class="d-flex flex-column ga-5 mx-4">
+      <slot />
+    </div>
+    <v-card-actions>
+      <base-button-save
+        :save-text="`${title} senden`"
+        prepend-icon="$cloudUpload"
+        :loading="isSending"
+        @click="$emit('save')"
+      />
+      <base-button-save
+        :save-text="`${title} korrigieren`"
+        prepend-icon="$edit"
+        :disabled="!isKorrigierenValid"
+        @click="$emit('edit')"
+      />
+      <base-button-save
+        :save-text="`${title} drucken`"
+        prepend-icon="$printer"
+        :disabled="!isDruckenValid"
+        :loading="isDruckenLoading"
+        @click="$emit('print')"
+      />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import BaseButtonSave from "@/components/common/buttons/BaseButtonSave.vue";
+
+defineProps<{
+  title: string;
+  subtitle: string;
+  isSending: boolean;
+  isKorrigierenValid: boolean | null | undefined;
+  isDruckenValid: boolean | null | undefined;
+  isDruckenLoading: boolean;
+}>();
+
+defineEmits<{
+  save: [];
+  edit: [];
+  print: [];
+}>();
+</script>
--- wls-gui-wahllokalsystem/src/components/ergebnismeldung/common/BaseErgebnismeldungViewContainer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="d-flex flex-column ga-5 mx-4">
-    <slot />
-  </div>
-</template>

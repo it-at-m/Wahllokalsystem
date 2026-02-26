0a82935f6ad172d6236a07594a15e26123e009ae
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-02-26 09:03:16 +0100

    #2036 änderungen von 2035

--- wls-gui-wahllokalsystem/src/components/navigation/TheScoresListGroupSelector.vue
+++ wls-gui-wahllokalsystem/src/components/navigation/TheScoresListGroupSelector.vue
@@ -22,6 +22,8 @@
       v-if="wahl.wahlart === WahlWahlartEnum.Mbw"
       :wahl-id="wahl.wahlID"
       :wahlbezirk-id="wahlbezirkIdForWahl"
+      :disabled="isMBWAuszaehlungDisabled"
+      :disabled-message="disabledMessagePreviousStepsRequired"
     />
   </div>
 </template>
@@ -30,6 +32,7 @@
 import type { Wahl } from "@/types/wahl/Wahl.ts";
 import type { PropType } from "vue";
 
+import { storeToRefs } from "pinia";
 import { computed } from "vue";
 
 import TheBAWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten/TheBAWScoresListGroup.vue";
@@ -38,6 +41,7 @@ import TheOBWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten
 import TheSRWScoresListGroup from "@/components/navigation/auszaehlung_wahlarten/TheSRWScoresListGroup.vue";
 import { useTextFormatter } from "@/composables/common/textFormatter.ts";
 import { useUserStore } from "@/stores/userStore.ts";
+import { useWorkflowStore } from "@/stores/workflowStore.ts";
 import { WahlWahlartEnum } from "@/types/wahl/WahlWahlartEnum.ts";
 
 const props = defineProps({
@@ -48,6 +52,12 @@ const props = defineProps({
 });
 
 const { getStimmzettelTermForWahl } = useTextFormatter();
+const {
+  isAnzahlWahlscheineErfasst,
+  isStimmabgabevermerkeErfasst,
+  isWahlvorstandErfasst,
+} = storeToRefs(useWorkflowStore());
+const { isBWB, isUWB } = storeToRefs(useUserStore());
 
 const titleStimmenZaehlen = computed(
   () => `Zählen der ${getStimmzettelTermForWahl(props.wahl)}`
@@ -56,4 +66,22 @@ const titleStimmenZaehlen = computed(
 const wahlbezirkIdForWahl = computed(() =>
   useUserStore().getWahlbezirkIdFromWahlMetaDataByWahlId(props.wahl.wahlID)
 );
+
+const isMBWAuszaehlungDisabled = computed(() => {
+  if (isUWB.value) {
+    return !isWahlvorstandErfasst.value || !isStimmabgabevermerkeErfasst.value;
+  } else {
+    return !isWahlvorstandErfasst.value || !isAnzahlWahlscheineErfasst.value;
+  }
+});
+
+const disabledMessagePreviousStepsRequired = computed(() => {
+  if (isBWB.value && isMBWAuszaehlungDisabled.value) {
+    return "Erst Wahlbriefe zählen.";
+  } else if (isUWB.value && isMBWAuszaehlungDisabled.value) {
+    return "Erst Stimmabgabevermerke erfassen.";
+  } else {
+    return "";
+  }
+});
 </script>

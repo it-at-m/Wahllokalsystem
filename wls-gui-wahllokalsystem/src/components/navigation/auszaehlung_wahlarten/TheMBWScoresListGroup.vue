133827e4428818ef0d763c7b0e6eb2aec9efcc38
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-02-26 08:56:05 +0100

    #2035 add disabled message for wahl

--- wls-gui-wahllokalsystem/src/components/navigation/auszaehlung_wahlarten/TheMBWScoresListGroup.vue
+++ wls-gui-wahllokalsystem/src/components/navigation/auszaehlung_wahlarten/TheMBWScoresListGroup.vue
@@ -3,26 +3,39 @@
     <template #activator="{ props }">
       <v-list-item
         v-bind="props"
-        title="🚧 Wahl des Migrationsbeirats"
-      />
+        title="Wahl des Migrationsbeirats"
+        :lines="groupActivatorListItemLines"
+      >
+        <template
+          v-if="disabled"
+          #subtitle
+        >
+          {{ disabledMessage }}
+        </template>
+      </v-list-item>
     </template>
     <v-list-item
       v-for="(route, index) in navigation"
       :key="index"
       :title="route.title"
       :to="route.targetRoute"
-      :disabled="route.disabled"
+      :disabled="disabled || route.disabled"
     />
   </v-list-group>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+
 import { useMbwNavigationService } from "@/composables/navigation/mbwNavigationService.ts";
 
-const { wahlbezirkId, wahlId } = defineProps<{
+const { wahlbezirkId, wahlId, disabled } = defineProps<{
   wahlId: string;
   wahlbezirkId: string;
+  disabled: boolean;
+  disabledMessage: string;
 }>();
 
 const { navigation } = useMbwNavigationService(wahlId, wahlbezirkId);
+const groupActivatorListItemLines = computed(() => (disabled ? false : "one"));
 </script>

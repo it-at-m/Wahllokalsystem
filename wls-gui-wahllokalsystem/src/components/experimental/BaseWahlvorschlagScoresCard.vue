949fec7772397ab7872e1e094e3c33b5cd76f8e2
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-03-05 20:59:56 +0100

    remove old ergebnis object

--- wls-gui-wahllokalsystem/src/components/experimental/BaseWahlvorschlagScoresCard.vue
+++ wls-gui-wahllokalsystem/src/components/experimental/BaseWahlvorschlagScoresCard.vue
@@ -8,23 +8,18 @@
       <base-kandidat-score
         v-for="(kandidat, index) in wahlvorschlag.kandidaten"
         :key="index"
-        :model-value="ergebnisOfKandidat(kandidat.identifikator)"
         :kandidat="kandidat"
         :listennummer="wahlvorschlag.ordnungszahl"
-        @update:model-value="
-          onUpdateOfErgebnisOfKandidat(kandidat.identifikator, $event)
-        "
       />
     </v-card-text>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import type { Ergebnis } from "@/types/ergebnismeldung/Ergebnis.ts";
 import type { StimmzettelWahlvorschlag } from "@/types/experimental/StimmzettelWahlvorschlag.ts";
 import type { PropType } from "vue";
 
-import { computed, ref } from "vue";
+import { computed } from "vue";
 
 import BaseKandidatScore from "@/components/experimental/BaseKandidatScore.vue";
 import { getStimmzettelManger } from "@/composables/experimental/stimmzettelManager.ts";
@@ -40,8 +35,6 @@ const stimmzettelManager = getStimmzettelManger({
   wahlbezirkId: "wahlbezirkId",
   wahlId: "wahlId",
 });
-
-const kandidatenErgebnisse = ref<Ergebnis[]>([]);
 
 const wahlvorschlagSelected = computed({
   set: (value: boolean) => {
@@ -60,25 +53,4 @@ const wahlvorschlagSelected = computed({
     );
   },
 });
-
-function ergebnisOfKandidat(kandidatId: string): Ergebnis {
-  let result = kandidatenErgebnisse.value.find(
-    (k) => k.kandidatID === kandidatId
-  );
-  if (result === undefined) {
-    result = {
-      kandidatID: kandidatId,
-      ergebnis: null,
-      wahlvorschlagID: props.wahlvorschlag?.identifikator,
-      numIndex: null,
-      wahlvorschlagsOrdnungszahl: props.wahlvorschlag?.ordnungszahl,
-    };
-    kandidatenErgebnisse.value.push(result);
-  }
-  return result;
-}
-
-function onUpdateOfErgebnisOfKandidat(kandidatId: string, ergebnis: Ergebnis) {
-  ergebnisOfKandidat(kandidatId).ergebnis = ergebnis.ergebnis;
-}
 </script>

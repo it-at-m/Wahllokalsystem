177777652f802cbda3b5d126797648ebac0aa980
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-03-12 08:16:55 +0100

    revert change of story dummy data

--- wls-gui-wahllokalsystem/stories/components/vorfaelleundvorkommnisse/BaseEreignisRow.stories.ts
+++ wls-gui-wahllokalsystem/stories/components/vorfaelleundvorkommnisse/BaseEreignisRow.stories.ts
@@ -35,7 +35,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    lineNumber: 1,
+    lineNumber: 42,
     modelValue: useVorfaelleundvorkommnisseTestDataFactory().createEreignis(),
   },
 };

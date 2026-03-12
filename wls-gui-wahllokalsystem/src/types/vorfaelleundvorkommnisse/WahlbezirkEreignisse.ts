9d61a48949cf1a5f9ade4dc207a38e25a0890177
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-03-12 17:05:47 +0100

    Revert "use undefined for empty default ereignis"
    This reverts commit e36918d9387a6bb15d16f190d925c35967cccfc7.

--- wls-gui-wahllokalsystem/src/types/vorfaelleundvorkommnisse/WahlbezirkEreignisse.ts
+++ wls-gui-wahllokalsystem/src/types/vorfaelleundvorkommnisse/WahlbezirkEreignisse.ts
@@ -16,6 +16,6 @@ export class WahlbezirkEreignisseBuilder implements WahlbezirkEreignisse {
   ) {}
 
   static createEmptyWahlbezirkEreignisse(): WahlbezirkEreignisseBuilder {
-    return new WahlbezirkEreignisseBuilder("", [], undefined, undefined);
+    return new WahlbezirkEreignisseBuilder("", [], true, true);
   }
 }

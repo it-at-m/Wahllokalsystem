f09a78630923918b31221c9ee2c25670dc962796
ThoGro <thomas.grossbeck@muenchen.de>
2026-04-29 16:00:14 +0200

    Fix lint

--- wls-gui-wahllokalsystem/src/views/ergebnismeldung/MBW/MBWSchnellmeldungView.vue
+++ wls-gui-wahllokalsystem/src/views/ergebnismeldung/MBW/MBWSchnellmeldungView.vue
@@ -1,46 +1,46 @@
 <template>
   <div>
-  <base-ergebnismeldung-cards-container
-    title="Schnellmeldung"
-    subtitle="Kontrolle, Übermittlung und Druck der Schnellmeldung"
-    :is-sending="isSendingSchnellmeldung"
-    :is-korrigieren-active="isKorrigierenValid"
-    :is-drucken-active="isDruckenValid"
-    :is-drucken-loading="isDruckenLoading"
-    :is-senden-active="isSendenActive"
-    @save="onSendenClicked"
-    @edit="onKorrigierenClicked"
-    @print="onDruckenClicked"
-  >
-    <the-m-b-w-wahlberechtigte-anzeigen-card
-      :wahlbezirk-id="wahlbezirkID"
-      :wahl-id="wahlID"
-    />
-    <the-m-b-w-waehler-anzeigen-card
-      :wahlbezirk-id="wahlbezirkID"
-      :wahl-id="wahlID"
-    />
-    <the-m-b-w-ungueltige-stimmen-anzeigen-card
-      :wahlbezirk-id="wahlbezirkID"
-      :wahl-id="wahlID"
-    />
-    <the-m-b-w-gueltige-stimmen-anzeigen-card
-      :is-schnellmeldung="true"
-      :wahlbezirk-id="wahlbezirkID"
-      :wahl-id="wahlID"
-    />
-  </base-ergebnismeldung-cards-container>
+    <base-ergebnismeldung-cards-container
+      title="Schnellmeldung"
+      subtitle="Kontrolle, Übermittlung und Druck der Schnellmeldung"
+      :is-sending="isSendingSchnellmeldung"
+      :is-korrigieren-active="isKorrigierenValid"
+      :is-drucken-active="isDruckenValid"
+      :is-drucken-loading="isDruckenLoading"
+      :is-senden-active="isSendenActive"
+      @save="onSendenClicked"
+      @edit="onKorrigierenClicked"
+      @print="onDruckenClicked"
+    >
+      <the-m-b-w-wahlberechtigte-anzeigen-card
+        :wahlbezirk-id="wahlbezirkID"
+        :wahl-id="wahlID"
+      />
+      <the-m-b-w-waehler-anzeigen-card
+        :wahlbezirk-id="wahlbezirkID"
+        :wahl-id="wahlID"
+      />
+      <the-m-b-w-ungueltige-stimmen-anzeigen-card
+        :wahlbezirk-id="wahlbezirkID"
+        :wahl-id="wahlID"
+      />
+      <the-m-b-w-gueltige-stimmen-anzeigen-card
+        :is-schnellmeldung="true"
+        :wahlbezirk-id="wahlbezirkID"
+        :wahl-id="wahlID"
+      />
+    </base-ergebnismeldung-cards-container>
     <offline-syncer-dialog
-        :is-dialog-visible="isOfflineSyncDialogVisible"
-        @sync-success="onSyncSuccess"
-        @sync-error="onSyncError"
+      :is-dialog-visible="isOfflineSyncDialogVisible"
+      @sync-success="onSyncSuccess"
+      @sync-error="onSyncError"
     />
     <base-dialog
-        :visible="isSyncErrorDialogVisible"
-        dialogtitle="Fehler bei der Synchronisation"
-        confirmtext="Hinweis schließen"
-        icon="$information"
-        @confirm="isSyncErrorDialogVisible = false"
+      :visible="isSyncErrorDialogVisible"
+      dialogtitle="Fehler bei der Synchronisation"
+      confirmtext="Hinweis schließen"
+      icon="$information"
+      @confirm="isSyncErrorDialogVisible = false"
     >
       <div class="mb-4">
         Bei der Synchronisation der Offline-Daten ist ein Fehler aufgetreten. Um
@@ -57,11 +57,13 @@ import type { SchnellmeldungDruckInput } from "@/types/ergebnismeldung/common/Sc
 import { computed, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
+import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
 import BaseErgebnismeldungCardsContainer from "@/components/ergebnismeldung/common/BaseErgebnismeldungCardsContainer.vue";
 import TheMBWGueltigeStimmenAnzeigenCard from "@/components/ergebnismeldung/MBW/stapelAB/TheMBWGueltigeStimmenAnzeigenCard.vue";
 import TheMBWWaehlerAnzeigenCard from "@/components/ergebnismeldung/MBW/stapelAB/TheMBWWaehlerAnzeigenCard.vue";
 import TheMBWWahlberechtigteAnzeigenCard from "@/components/ergebnismeldung/MBW/stapelAB/TheMBWWahlberechtigteAnzeigenCard.vue";
 import TheMBWUngueltigeStimmenAnzeigenCard from "@/components/ergebnismeldung/MBW/stapelC/TheMBWUngueltigeStimmenAnzeigenCard.vue";
+import OfflineSyncerDialog from "@/components/wlsComponents/OfflineSyncerDialog.vue";
 import { useStatusUtils } from "@/composables/ergebnismeldung/common/statusUtils.ts";
 import { useMbwUtils } from "@/composables/ergebnismeldung/MBW/mbwUtils.ts";
 import { useSchnellmeldungDruck } from "@/composables/ergebnismeldung/MBW/schnellmeldungDruck.ts";
@@ -73,8 +75,6 @@ import { useWorkflowStore } from "@/stores/workflowStore.ts";
 import { MeldungsArtEnum } from "@/types/ergebnismeldung/common/MeldungsartEnum.ts";
 import { MbwRoutesEnum } from "@/types/navigation/MbwRoutesEnum.ts";
 import { UserNotificationCategoryEnum } from "@/types/userNotification/UserNotificationCategoryEnum.ts";
-import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
-import OfflineSyncerDialog from "@/components/wlsComponents/OfflineSyncerDialog.vue";
 
 const route = useRoute();
 const router = useRouter();

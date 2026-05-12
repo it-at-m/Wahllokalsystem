2c81b7f46f7581d3f7f08021089436b710274349
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-05-12 18:10:37 +0200

    use only 2 registers and no separate column for edit beschluss

--- wls-gui-wahllokalsystem/src/components/experimental/TheBeschlussfassungEditCard.vue
+++ wls-gui-wahllokalsystem/src/components/experimental/TheBeschlussfassungEditCard.vue
@@ -18,90 +18,95 @@
       </div></v-card-title
     >
     <v-card-text>
-      <v-row>
-        <v-col cols="4">
-          <v-card>
-            <v-card-title>Beschluss dokumentieren</v-card-title>
-            <!-- In Übersicht -->
-            <v-card-text>
-              <ul class="ms-3">
-                <li v-if="beschlussStimmzettelFailureZuVieleStimmen">
-                  Mehr als 3 Stimmen bei einem Kandidaten
-                </li>
-                <li v-if="beschlussStimmzettelFailureListenkreuzen">
-                  Zu viele Listenkreuze
-                </li>
-              </ul>
-              <v-textarea
-                class="mt-2"
-                label="Beschluss verfassen"
-                auto-grow
-                rows="1"
+      <v-card>
+        <v-card-title>
+          <v-tabs
+            v-model="selectedTab"
+            slider-color="primary"
+            color="primary"
+            density="compact"
+          >
+            <v-tab value="overview">Verfassen</v-tab>
+            <v-tab value="input">Stimmzettel bearbeiten</v-tab>
+          </v-tabs>
+        </v-card-title>
+        <v-card-text>
+          <v-tabs-window v-model="selectedTab">
+            <v-tabs-window-item value="overview">
+              <v-row>
+                <v-col>
+                  <v-card class="ms-1 mb-1">
+                    <v-card-title>Beschluss dokumentieren</v-card-title>
+                    <!-- In Übersicht -->
+                    <v-card-text>
+                      <ul class="ms-3">
+                        <li v-if="beschlussStimmzettelFailureZuVieleStimmen">
+                          Mehr als 3 Stimmen bei einem Kandidaten
+                        </li>
+                        <li v-if="beschlussStimmzettelFailureListenkreuzen">
+                          Zu viele Listenkreuze
+                        </li>
+                      </ul>
+                      <v-textarea
+                        class="mt-2"
+                        label="Beschluss verfassen"
+                        auto-grow
+                        rows="1"
+                      />
+                      <v-radio-group label="Gültigkeit">
+                        <v-radio
+                          label="gültig"
+                          :disabled="!beschlussGueltigkeit1IsSelectable"
+                          value="1"
+                        />
+                        <v-radio
+                          label="teilweise gültig"
+                          :disabled="!beschlussGueltigkeit2IsSelectable"
+                          value="2"
+                        />
+                        <v-radio
+                          label="ungültig"
+                          :disabled="!beschlussGueltigkeit3IsSelectable"
+                          value="3"
+                        />
+                      </v-radio-group>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+                <v-col cols="4">
+                  <v-card class="me-1 mb-1">
+                    <v-card-title>Zusammenfassung Stimmzettel</v-card-title>
+                    <v-card-text>
+                      <v-row>
+                        <v-col>Kandidatenstimmen insgesamt</v-col>
+                        <v-col>45</v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col>Kandidatenstimmen ungültig</v-col>
+                        <v-col>12</v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col>Listenkreuze insgesamt</v-col>
+                        <v-col>3</v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-tabs-window-item>
+            <v-tabs-window-item value="input">
+              <the-stimmzettel-scores-card
+                v-if="wahlvorschlaege"
+                :wahlvorschlaege="wahlvorschlaege"
+                :stimmzettel-snapshots="stimmzettelSnapshots"
+                :is-saving-stimmzettel="isSavingStimmzettel"
+                :show-title="false"
+                @snapshot-created="onStimmzettelSnapshotCreated"
               />
-              <v-radio-group label="Gültigkeit">
-                <v-radio
-                  label="gültig"
-                  :disabled="!beschlussGueltigkeit1IsSelectable"
-                  value="1"
-                />
-                <v-radio
-                  label="teilweise gültig"
-                  :disabled="!beschlussGueltigkeit2IsSelectable"
-                  value="2"
-                />
-                <v-radio
-                  label="ungültig"
-                  :disabled="!beschlussGueltigkeit3IsSelectable"
-                  value="3"
-                />
-              </v-radio-group>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card>
-            <v-card-title>
-              <v-tabs
-                v-model="selectedTab"
-                slider-color="primary"
-                color="primary"
-                density="compact"
-              >
-                <v-tab value="overview">Übersicht</v-tab>
-                <v-tab value="input">Erfassung</v-tab>
-              </v-tabs>
-            </v-card-title>
-            <v-card-text>
-              <v-tabs-window v-model="selectedTab">
-                <v-tabs-window-item value="overview">
-                  <v-row>
-                    <v-col>Kandidatenstimmen insgesamt</v-col>
-                    <v-col>45</v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col>Kandidatenstimmen ungültig</v-col>
-                    <v-col>12</v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col>Listenkreuze insgesamt</v-col>
-                    <v-col>3</v-col>
-                  </v-row>
-                </v-tabs-window-item>
-                <v-tabs-window-item value="input">
-                  <the-stimmzettel-scores-card
-                    v-if="wahlvorschlaege"
-                    :wahlvorschlaege="wahlvorschlaege"
-                    :stimmzettel-snapshots="stimmzettelSnapshots"
-                    :is-saving-stimmzettel="isSavingStimmzettel"
-                    :show-title="false"
-                    @snapshot-created="onStimmzettelSnapshotCreated"
-                  />
-                </v-tabs-window-item>
-              </v-tabs-window>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+            </v-tabs-window-item>
+          </v-tabs-window>
+        </v-card-text>
+      </v-card>
     </v-card-text>
     <v-card-actions>
       <base-wls-button-save save-text="Speichern und fortfahren" />

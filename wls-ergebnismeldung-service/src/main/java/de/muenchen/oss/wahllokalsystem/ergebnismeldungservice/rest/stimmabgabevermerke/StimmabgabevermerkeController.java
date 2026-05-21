b1079fdca1da6145006a99305089569caed58785
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-05-21 14:56:11 +0200

    add open api documentation annotations

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/stimmabgabevermerke/StimmabgabevermerkeController.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/stimmabgabevermerke/StimmabgabevermerkeController.java
@@ -2,6 +2,11 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.stimmabgabev
 
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.AbstractController;
 import de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.stimmabgabevermerke.StimmabgabevermerkeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.HttpStatus;
@@ -22,6 +27,25 @@ public class StimmabgabevermerkeController extends AbstractController {
   private final StimmabgabevermerkeService stimmabgabevermerkeService;
   private final StimmabgabevermerkeDTOMapper stimmabgabevermerkeDTOMapper;
 
+  @Operation(
+      description =
+          "Lesen der Stimmabgabevermerke eines Wahlbezirkes für die Wahl eines Wählerverzeichnisses.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Es existieren Stimmabgabevermerke.",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = StimmabgabevermerkeDTO.class))
+            }),
+        @ApiResponse(
+            responseCode = "204",
+            description =
+                "Es existieren keine Stimmabgabevermerke entsprechend der Such-Kriterien.",
+            content = {@Content()})
+      })
   @GetMapping(
       "/businessActions/stimmabgabevermerke/{wahlbezirkID}/{wahlID}/{waehlerverzeichnisNummer}")
   public ResponseEntity<StimmabgabevermerkeDTO> getStimmabgabevermerke(
@@ -34,6 +58,15 @@ public class StimmabgabevermerkeController extends AbstractController {
             .map(stimmabgabevermerkeDTOMapper::toStimmabgabevermerkeDTO));
   }
 
+  @Operation(
+      description =
+          "Speichern der Stimmabgabevermerke eines Wahlbezirkes für die Wahl eines Wählerverzeichnisses.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Stimmabgabevermerke erfolgreich gespeichert")
+      })
   @PostMapping(
       "/businessActions/stimmabgabevermerke/{wahlbezirkID}/{wahlID}/{waehlerverzeichnisNummer}")
   @ResponseStatus(HttpStatus.OK)

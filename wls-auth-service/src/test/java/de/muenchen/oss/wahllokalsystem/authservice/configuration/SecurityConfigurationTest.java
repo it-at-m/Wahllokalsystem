2c528873814bcdb9c36cb91b64cacad2d669db8e
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-05-28 18:25:13 +0200

    adding tests at SecurityConfigurationTest

--- wls-auth-service/src/test/java/de/muenchen/oss/wahllokalsystem/authservice/configuration/SecurityConfigurationTest.java
+++ wls-auth-service/src/test/java/de/muenchen/oss/wahllokalsystem/authservice/configuration/SecurityConfigurationTest.java
@@ -1,16 +1,31 @@
 package de.muenchen.oss.wahllokalsystem.authservice.configuration;
 
 import static de.muenchen.oss.wahllokalsystem.authservice.TestConstants.SPRING_TEST_PROFILE;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.muenchen.oss.wahllokalsystem.authservice.MicroServiceApplication;
+import de.muenchen.oss.wahllokalsystem.authservice.rest.WahlbezirksartDTO;
+import de.muenchen.oss.wahllokalsystem.authservice.rest.WahllokalUserInfoDTO;
+import de.muenchen.oss.wahllokalsystem.authservice.service.SessionService;
+import de.muenchen.oss.wahllokalsystem.authservice.service.UserService;
+import java.time.LocalDate;
+import lombok.val;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(
@@ -22,6 +37,12 @@ import org.springframework.test.web.servlet.MockMvc;
 class SecurityConfigurationTest {
 
   @Autowired MockMvc api;
+
+  @Autowired ObjectMapper objectMapper;
+
+  @MockitoBean UserService userService;
+
+  @MockitoBean SessionService sessionService;
 
   @Test
   void should_returnStatusUnauthorized_when_accessingSecuredResourceRoot() throws Exception {
@@ -56,5 +77,143 @@ class SecurityConfigurationTest {
   @Test
   void should_returnStatusOk_when_accessingUnsecuredResourceSwaggerUi() throws Exception {
     api.perform(get("/swagger-ui/index.html")).andExpect(status().isOk());
+  }
+
+  @Nested
+  class User {
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingUnlockUserUnauthorized() throws Exception {
+      api.perform(post("/user/username/unlock").with(csrf())).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_ADMIN_ADMIN")
+    void should_permitAccess_when_accessingUnlockUserAuthorized() throws Exception {
+      api.perform(post("/user/username/unlock").with(csrf())).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingUserUnauthorized() throws Exception {
+      api.perform(get("/user")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void should_permitAccess_when_accessingUserAuthorized() throws Exception {
+      api.perform(get("/user")).andExpect(status().isOk());
+    }
+  }
+
+  @Nested
+  class Session {
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingInvalidateSessionUnauthorized() throws Exception {
+      api.perform(post("/oauthsessions/sessionID/invalidate").with(csrf()))
+          .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_ADMIN_ADMIN")
+    void should_permitAccess_when_accessingInvalidateSessionAuthorized() throws Exception {
+      api.perform(post("/oauthsessions/sessionID/invalidate").with(csrf()))
+          .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingOauthSessionsUnauthorized() throws Exception {
+      api.perform(get("/oauthsessions/")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_ADMIN_ADMIN")
+    void should_permitAccess_when_accessingOauthSessionsAuthorized() throws Exception {
+      api.perform(get("/oauthsessions/")).andExpect(status().isOk());
+    }
+  }
+
+  @Nested
+  class Wahllokalbenutzer {
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingGenerateAndExportUnauthorized() throws Exception {
+      val requestBodyAsString =
+          objectMapper.writeValueAsString(
+              new WahllokalUserInfoDTO[] {
+                new WahllokalUserInfoDTO(
+                    "wahlbezirknummer", LocalDate.now(), "wahlbezirkID", WahlbezirksartDTO.UWB, "")
+              });
+      api.perform(
+              post("/generateAndExportWahllokalbenutzer/wahltagID")
+                  .with(csrf())
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .content(requestBodyAsString))
+          .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void should_permitAccess_when_accessingGenerateAndExportAuthorized() throws Exception {
+      val requestBodyAsString =
+          objectMapper.writeValueAsString(
+              new WahllokalUserInfoDTO[] {
+                new WahllokalUserInfoDTO(
+                    "wahlbezirknummer", LocalDate.now(), "wahlbezirkID", WahlbezirksartDTO.UWB, "")
+              });
+      api.perform(
+              post("/generateAndExportWahllokalbenutzer/wahltagID")
+                  .with(csrf())
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .content(requestBodyAsString))
+          .andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingDeleteWahllokalbenutzerUnauthorized() throws Exception {
+      api.perform(delete("/deleteWahllokalbenutzer/wahltagID").with(csrf()))
+          .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void should_permitAccess_when_accessingDeleteWahllokalbenutzerAuthorized() throws Exception {
+      api.perform(delete("/deleteWahllokalbenutzer/wahltagID").with(csrf()))
+          .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingExportWahllokalbenutzerUnauthorized() throws Exception {
+      api.perform(get("/exportWahllokalbenutzer/wahltagID")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void should_permitAccess_when_accessingExportWahllokalbenutzerAuthorized() throws Exception {
+      api.perform(get("/exportWahllokalbenutzer/wahltagID")).andExpect(status().isCreated());
+    }
+  }
+
+  @Nested
+  class Authserver {
+
+    @Test
+    @WithAnonymousUser
+    void should_denyAccess_when_accessingLogouturlUnauthorized() throws Exception {
+      api.perform(get("/authserver/logouturl")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(authorities = "ROLE_ADMIN_ADMIN")
+    void should_permitAccess_when_accessingogouturlAuthorized() throws Exception {
+      api.perform(get("/authserver/logouturl")).andExpect(status().isOk());
+    }
   }
 }

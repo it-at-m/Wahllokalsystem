9a2816f6b960fd83f28ee6bb16336a2d7730faf9
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-05-28 13:38:39 +0200

    2656 finished zustand bleibt auch nach dem reload verfügbar - Stapel BC - Kandidatenstimmen (#2833)
    * redesign mbw ergebnisse init to calculate if stapel mbw bc is done
    
    * improve code - externalize function and rename factory
    
    * add mbw Taskfactory to taskList
    
    * update tests
    
    * 👌 - fix providing test case arguments

---
+++ wls-gui-wahllokalsystem/src/composables/tasks/taskFactories/mbwWahlvorschlaegeAndErgebnisseTaskFactory.ts
@@ -0,0 +1,88 @@
+import type { ExtendedWahlMetaData } from "@/composables/tasks/ExtendedWahlMetaData.ts";
+import type { TaskFactory } from "@/composables/tasks/TaskFactory.ts";
+import type { TaskFactoryContext } from "@/composables/tasks/TaskFactoryContext.ts";
+import type { Task } from "@/types/tasks/Task.ts";
+import type { Wahlvorschlag } from "@/types/wahlvorschlaege/Wahlvorschlag.ts";
+
+import { useErgebnismeldungStore } from "@/stores/ergebnismeldungStore.ts";
+import { useWahlvorschlaegeStore } from "@/stores/wahlvorschlaegeStore.ts";
+import { useWorkflowStore } from "@/stores/workflowStore.ts";
+import {
+  getStapelForWahlart,
+  StapelArtEnum,
+} from "@/types/ergebnismeldung/common/StapelArtEnum.ts";
+import { MbwRoutesEnum } from "@/types/navigation/MbwRoutesEnum.ts";
+import { WahlWahlartEnum } from "@/types/wahl/WahlWahlartEnum.ts";
+
+export function useMBWWahlvorschlaegeAndErgebnisseTaskFactory(): TaskFactory {
+  const wahlvorschlaegeStore = useWahlvorschlaegeStore();
+  const ergebnismeldungsStore = useErgebnismeldungStore();
+
+  function createTasks(taskFactoryContext: TaskFactoryContext): Task[] {
+    const tasks: Task[] = [];
+    taskFactoryContext.extendedWahlMetaData.forEach((extendedWahlMetaData) => {
+      if (extendedWahlMetaData.wahlArt === WahlWahlartEnum.Mbw) {
+        tasks.push(_createMBWTask(extendedWahlMetaData));
+      }
+    });
+    return tasks;
+  }
+
+  function _createMBWTask(taskFactoryMetaData: ExtendedWahlMetaData): Task {
+    return {
+      callback: async () => {
+        const allStapelForWahlart = getStapelForWahlart(
+          taskFactoryMetaData.wahlArt
+        );
+
+        const loadingPromises: Promise<void>[] = [];
+        allStapelForWahlart.forEach((stapelArt) => {
+          loadingPromises.push(
+            ergebnismeldungsStore.loadErgebnisseByStapelArt(
+              taskFactoryMetaData.wahlID,
+              stapelArt,
+              false
+            )
+          );
+        });
+
+        loadingPromises.push(
+          wahlvorschlaegeStore.loadWahlvorschlaege(
+            taskFactoryMetaData.wahlID,
+            taskFactoryMetaData.wahlbezirkID
+          )
+        );
+
+        await Promise.all(loadingPromises);
+
+        //Prüfen ob StapelMBW_BC vollständig ist, um den Navigationsworkflow korrekt setzen zu können
+        const ergebnisseForStapelBC =
+          ergebnismeldungsStore.getErgebnisseByWahlIdAndStapelartOrUndefined(
+            taskFactoryMetaData.wahlID,
+            StapelArtEnum.MbwBC
+          );
+        const wahlvorschlaege =
+          wahlvorschlaegeStore.getWahlvorschlaegeByWahlIDAndWahlbezirkID(
+            taskFactoryMetaData.wahlID,
+            taskFactoryMetaData.wahlbezirkID
+          )?.wahlvorschlaege || ([] as Wahlvorschlag[]);
+        const countKandidaten = wahlvorschlaege.flatMap(
+          (wahlvorschlag) => wahlvorschlag.kandidaten
+        ).length;
+
+        if (ergebnisseForStapelBC?.ergebnisse.length === countKandidaten) {
+          useWorkflowStore().setStepDone(
+            taskFactoryMetaData.wahlID,
+            taskFactoryMetaData.wahlbezirkID,
+            MbwRoutesEnum.MBW_STAPEL_BC
+          );
+        }
+      },
+      name: `Wahlvorschläge und Ergebnisse - ${taskFactoryMetaData.wahlName}`,
+    };
+  }
+
+  return {
+    createTasks,
+  };
+}

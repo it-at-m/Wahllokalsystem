7fcb232d3e6bcc723cef47b9e6f07ecdc8c1f76b
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-06-02 15:58:15 +0200

    #2754 fix layout for bwb views

--- wls-gui-wahllokalsystem/src/components/wahlhandlung/TheWahlumgebungBwbCard.vue
+++ wls-gui-wahllokalsystem/src/components/wahlhandlung/TheWahlumgebungBwbCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <div>
     <v-card>
       <v-card-title>Zahl der Wahlurnen</v-card-title>
       <v-card-text class="pb-0 pt-2">
@@ -32,7 +32,7 @@
         </v-form>
       </v-card-text>
     </v-card>
-  </v-container>
+  </div>
 </template>
 
 <script setup lang="ts">

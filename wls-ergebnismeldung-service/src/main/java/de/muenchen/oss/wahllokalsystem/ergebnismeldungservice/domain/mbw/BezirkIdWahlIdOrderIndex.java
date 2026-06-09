ef1532ca0b2ffa44344545142a82cac213b5d86c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-09 17:19:30 +0200

    add all and no args constructor

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/domain/mbw/BezirkIdWahlIdOrderIndex.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/domain/mbw/BezirkIdWahlIdOrderIndex.java
@@ -2,9 +2,13 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.mbw;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BezirkIdWahlIdOrderIndex {
 
   @NotNull @Size(max = 1000) private String wahlID;

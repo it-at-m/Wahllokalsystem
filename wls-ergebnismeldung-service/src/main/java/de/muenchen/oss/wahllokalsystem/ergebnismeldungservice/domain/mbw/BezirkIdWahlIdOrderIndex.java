8c41f9a536197e7d1dd93236f6349e3182f8b8c6
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-10 08:41:30 +0200

    add serializable

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/domain/mbw/BezirkIdWahlIdOrderIndex.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/domain/mbw/BezirkIdWahlIdOrderIndex.java
@@ -2,6 +2,7 @@ package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.mbw;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BezirkIdWahlIdOrderIndex {
+public class BezirkIdWahlIdOrderIndex implements Serializable {
 
   @NotNull @Size(max = 1000) private String wahlID;
 

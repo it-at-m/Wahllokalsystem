6b028adb3731f56a759e0b185faf3488d80ec11d
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-12 15:49:33 +0200

    show more data for ungueltige stimmzettel

--- wls-gui-wahllokalsystem/src/components/ergebnismeldung/MBW/stapelC/TheMBWUngueltigeStimmenAnzeigenCard.vue
+++ wls-gui-wahllokalsystem/src/components/ergebnismeldung/MBW/stapelC/TheMBWUngueltigeStimmenAnzeigenCard.vue
@@ -7,6 +7,8 @@
           <tr>
             <th class="index-column" />
             <th />
+            <th class="font-weight-bold text-right">Zweifelsfrei ungültig</th>
+            <th class="font-weight-bold text-right">Laut Beschluss ungültig</th>
             <th class="font-weight-bold text-right">Insgesamt</th>
           </tr>
         </thead>
@@ -15,6 +17,12 @@
             <td class="font-weight-bold index-column">C</td>
             <td class="font-weight-bold">Ungültige Stimmzettel</td>
             <td class="font-weight-bold text-right">{{ ungueltigeStimmen }}</td>
+            <td class="font-weight-bold text-right">
+              {{ ungueltigeStimmzettelNachBeschluss }}
+            </td>
+            <td class="font-weight-bold text-right">
+              {{ sumUngueltigeStimmzettel }}
+            </td>
           </tr>
         </tfoot>
       </v-table>
@@ -23,12 +31,15 @@
 </template>
 
 <script setup lang="ts">
-import { onActivated, ref } from "vue";
+import { computed, onActivated, ref } from "vue";
 
 import { useErgebnisService } from "@/composables/ergebnismeldung/common/ergebnisService.ts";
+import { useBedenklicheStimmzettelService } from "@/composables/ergebnismeldung/MBW/bedenklicheStimmzettelService.ts";
 import { StapelArtEnum } from "@/types/ergebnismeldung/common/StapelArtEnum.ts";
+import { ValidityEnum } from "@/types/ergebnismeldung/MBW/bedenklicheStimmzettel/ValidityEnum.ts";
 
 const { getErgebnisse } = useErgebnisService();
+const { getBedenklicheStimmzettel } = useBedenklicheStimmzettelService();
 
 const props = defineProps<{
   wahlbezirkId: string;
@@ -36,6 +47,10 @@ const props = defineProps<{
 }>();
 
 const ungueltigeStimmen = ref(0);
+const ungueltigeStimmzettelNachBeschluss = ref(0);
+const sumUngueltigeStimmzettel = computed(
+  () => ungueltigeStimmen.value + ungueltigeStimmzettelNachBeschluss.value
+);
 
 onActivated(async () => {
   const result = await getErgebnisse(
@@ -45,5 +60,16 @@ onActivated(async () => {
     false
   );
   ungueltigeStimmen.value = result?.ergebnisse[0]?.ergebnis || 0;
+
+  const bedenklicheStimmzettel = await getBedenklicheStimmzettel(
+    props.wahlId,
+    props.wahlbezirkId,
+    false
+  );
+  ungueltigeStimmzettelNachBeschluss.value = bedenklicheStimmzettel
+    ? bedenklicheStimmzettel.filter(
+        (stimmzettel) => stimmzettel.validity === ValidityEnum.VALID
+      ).length
+    : 0;
 });
 </script>

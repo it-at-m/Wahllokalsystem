400d63c37ed7ad563edbc947a8d0088f1732acb3
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-15 15:36:49 +0200

    apply coderabbit feedback

--- wls-gui-wahllokalsystem/src/composables/ergebnismeldung/MBW/mbwUtilsNiederschrift.ts
+++ wls-gui-wahllokalsystem/src/composables/ergebnismeldung/MBW/mbwUtilsNiederschrift.ts
@@ -290,7 +290,7 @@ export function useMbtUtilsNiederschrift(wahlID: string, wahlbezirkID: string) {
           "MBW_D_UNGUELTIG",
           false
         );
-        return loadedErgebnisse?.ergebnisse[0].ergebnis ?? 0;
+        return loadedErgebnisse?.ergebnisse[0]?.ergebnis ?? 0;
       } catch (error) {
         logError("Fehler beim Laden der Ergebnisse: ", error);
       }

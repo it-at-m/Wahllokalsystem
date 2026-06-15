60aba1eacff8314ba8d7ed5474bf13c839722144
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-15 15:02:12 +0200

    draft all designs

--- wls-gui-wahllokalsystem/src/components/ergebnismeldung/MBW/stapelD/BaseCardUngueltigeStimmzettelErfassen.vue
+++ wls-gui-wahllokalsystem/src/components/ergebnismeldung/MBW/stapelD/BaseCardUngueltigeStimmzettelErfassen.vue
@@ -3,37 +3,102 @@
     <v-form v-model="isFormValid">
       <v-card-title>Ungültige Stimmzettel</v-card-title>
       <v-card-text>
-        <base-number-input
-          v-model="modelValue.ergebnis"
-          :rules="[required]"
-          min-width="20rem"
-        />
-        <v-divider class="my-2" />
-        <v-row>
-          <v-col cols="4">
-            <div class="d-flex align-center ga-1">
-              <v-icon icon="$add" />Anzahl ungültiger Stimmzettel nach Beschluss
-            </div>
-          </v-col>
-          <v-col
-            cols="1"
-            class="d-flex align-center"
-            >{{ ungueltigeStimmzettelNachBeschluss }}</v-col
-          >
-        </v-row>
-        <v-row class="font-weight-bold">
-          <v-col cols="4">
-            <div class="d-flex align-center ga-1">
-              <v-icon icon="$equal" />
-              Summe ungültiger Stimmzettel
-            </div></v-col
-          >
-          <v-col
-            cols="1"
-            class="d-flex align-center"
-            >{{ summeUngueltigerStimmzettel }}</v-col
-          >
-        </v-row>
+        <v-table class="ungueltigeStimmzettelTable">
+          <tbody>
+            <tr>
+              <td class="descriptionCol">Anzahl ungültiger Stimmzettel</td>
+              <td class="valueCol">
+                <base-number-input
+                  v-model="modelValue.ergebnis"
+                  :rules="[required]"
+                  min-width="5rem"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>Anzahl ungültiger Stimmzettel nach Beschluss</td>
+              <td class="text-end">
+                {{ ungueltigeStimmzettelNachBeschluss }}
+              </td>
+            </tr>
+            <tr>
+              <td class="font-weight-bold">Summe ungültiger Stimmzettel</td>
+              <td class="font-weight-bold text-end">
+                {{ summeUngueltigerStimmzettel }}
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-card-text>
+
+      <v-card-text>
+        <div>
+          <base-number-input
+            v-model="modelValue.ergebnis"
+            :rules="[required]"
+            min-width="20rem"
+          />
+          <v-divider class="my-2" />
+          <v-row>
+            <v-col cols="4">
+              <div class="d-flex align-center ga-1">
+                <v-icon icon="$add" />Anzahl ungültiger Stimmzettel nach
+                Beschluss
+              </div>
+            </v-col>
+            <v-col
+              cols="1"
+              class="d-flex align-center"
+              >{{ ungueltigeStimmzettelNachBeschluss }}</v-col
+            >
+          </v-row>
+          <v-row class="font-weight-bold">
+            <v-col cols="4">
+              <div class="d-flex align-center ga-1">
+                <v-icon icon="$equal" />
+                Summe ungültiger Stimmzettel
+              </div></v-col
+            >
+            <v-col
+              cols="1"
+              class="d-flex align-center"
+              >{{ summeUngueltigerStimmzettel }}</v-col
+            >
+          </v-row>
+        </div>
+      </v-card-text>
+
+      <v-card-text>
+        <v-table>
+          <thead>
+            <tr>
+              <th />
+              <th class="font-weight-bold text-right">Zweifelsfrei ungültig</th>
+              <th class="font-weight-bold text-right">
+                Laut Beschluss ungültig
+              </th>
+              <th class="font-weight-bold text-right">Insgesamt</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <td class="font-weight-bold">Ungültige Stimmzettel</td>
+              <td class="font-weight-bold text-right">
+                <base-number-input
+                  v-model="modelValue.ergebnis"
+                  :rules="[required]"
+                  min-width="5rem"
+                />
+              </td>
+              <td class="font-weight-bold text-right">
+                {{ ungueltigeStimmzettelNachBeschluss }}
+              </td>
+              <td class="font-weight-bold text-right">
+                {{ summeUngueltigerStimmzettel }}
+              </td>
+            </tr>
+          </tfoot>
+        </v-table>
       </v-card-text>
       <v-card-actions>
         <base-wls-button-save
@@ -91,3 +156,13 @@ function onSaveClicked() {
   emit("save");
 }
 </script>
+
+<style scoped>
+.ungueltigeStimmzettelTable {
+  max-width: 600px;
+}
+
+.descriptionCol {
+  width: 66%;
+}
+</style>

ab9920471b8bd6bac47a90bea1447e23aa13cc89
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-06-22 14:31:24 +0200

    #2913 undo unwanted changes

--- wls-gui-wahllokalsystem/src/composables/ergebnismeldung/MBW/niederschriftDruckBWB.ts
+++ wls-gui-wahllokalsystem/src/composables/ergebnismeldung/MBW/niederschriftDruckBWB.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
 import type { NiederschriftDruckInputBWB } from "@/types/ergebnismeldung/MBW/niederschrift/NiederschriftDruckInputBWB.ts";

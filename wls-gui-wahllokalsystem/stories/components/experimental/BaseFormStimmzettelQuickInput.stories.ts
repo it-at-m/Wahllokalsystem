a0caa1ba9c51252abc11015de12a1c921bc95bf8
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-23 20:56:02 +0200

    fix storybook

--- wls-gui-wahllokalsystem/stories/components/experimental/BaseFormStimmzettelQuickInput.stories.ts
+++ wls-gui-wahllokalsystem/stories/components/experimental/BaseFormStimmzettelQuickInput.stories.ts
@@ -1,6 +1,6 @@
-import type { StoryObj } from "@storybook/vue3";
+import type { Meta, StoryObj } from "@storybook/vue3";
 
-import { fn } from "@storybook/test";
+import { fn } from "storybook/test";
 
 import BaseFormStimmzettelQuickInput from "@/components/experimental/BaseFormStimmzettelQuickInput.vue";
 

34e2fc8ebda7545ca251aec7d8be5269e2964ab3
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-06-26 09:00:32 +0200

    add explicit open api description

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/rest/roles/RolesController.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/rest/roles/RolesController.java
@@ -1,6 +1,11 @@
 package de.muenchen.oss.wahllokalsystem.authservice.rest.roles;
 
 import de.muenchen.oss.wahllokalsystem.authservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,18 @@ public class RolesController {
 
   private final UserService userService;
 
+  @Operation(description = "Lesen der Zuordnung technischer Rollenbezeichner zu fachlichen Rollen.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Eine Zuordnung ist vorhanden.",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = RoleMappingsDTO.class))
+            })
+      })
   @GetMapping("mappings")
   public RoleMappingsDTO getRoleMappings() {
     return new RoleMappingsDTO(

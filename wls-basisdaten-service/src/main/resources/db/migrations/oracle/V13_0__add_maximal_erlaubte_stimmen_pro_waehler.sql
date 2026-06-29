ecd57a9dbd7daa4e2c513907b6d5b0d4bb1a2b8a
C N <212502354+CNoehm@users.noreply.github.com>
2026-06-29 10:53:07 +0200

    Update wls-basisdaten-service/src/main/resources/db/migrations/oracle/V13_0__add_maximal_erlaubte_stimmen_pro_waehler.sql
    Co-authored-by: MrSebastian <13592751+MrSebastian@users.noreply.github.com>

--- wls-basisdaten-service/src/main/resources/db/migrations/oracle/V13_0__add_maximal_erlaubte_stimmen_pro_waehler.sql
+++ wls-basisdaten-service/src/main/resources/db/migrations/oracle/V13_0__add_maximal_erlaubte_stimmen_pro_waehler.sql
@@ -3,4 +3,6 @@ ALTER TABLE Kopfdaten
 
 UPDATE Kopfdaten
 SET maxstimmenprowaehler = 40
-WHERE WAHLID = 'b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e';
+WHERE WAHLID IN (
+    SELECT wahlid FROM WAHL WHERE wahlart = 'MBW'
+    );

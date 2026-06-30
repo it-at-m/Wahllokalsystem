69d141010933da526421daacf58fd019ebad4972
CNoehm <212502354+CNoehm@users.noreply.github.com>
2026-06-30 08:11:43 +0200

    2852 - DummyDaten auf Wert setzen aus Migration-Script entfernt.

--- wls-basisdaten-service/src/main/resources/db/migrations/h2/V15_0__addAnzahlNennungenToKandidat.sql
+++ wls-basisdaten-service/src/main/resources/db/migrations/h2/V15_0__addAnzahlNennungenToKandidat.sql
@@ -1,14 +1,6 @@
 ALTER TABLE kandidat
     ADD anzahlNennungen INT DEFAULT 1;
 
-UPDATE kandidat
-SET anzahlNennungen = 2
-WHERE name = 'Kimberly Price';
-
-UPDATE kandidat
-SET anzahlNennungen = 3
-WHERE name = 'Sofia Kim';
-
 ALTER TABLE kandidat
 ALTER COLUMN anzahlNennungen SET NOT NULL;
 
--- wls-basisdaten-service/src/main/resources/db/migrations/oracle/V15_0__addAnzahlNennungenToKandidat.sql
+++ wls-basisdaten-service/src/main/resources/db/migrations/oracle/V15_0__addAnzahlNennungenToKandidat.sql
@@ -1,14 +1,6 @@
 ALTER TABLE kandidat
     ADD anzahlNennungen NUMBER DEFAULT 1;
 
-UPDATE kandidat
-SET anzahlNennungen = 2
-where name = 'Kimberly Price';
-
-UPDATE kandidat
-SET anzahlNennungen = 3
-where name = 'Sofia Kim';
-
 ALTER TABLE kandidat
     MODIFY anzahlNennungen NOT NULL;
 

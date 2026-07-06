f9e80057f1789c0673ef6a360ca54473b02feb23
Robert Jasny <robert.jasny@muenchen.de>
2026-07-06 14:03:05 +0200

    #971 Mark active Wahltag in autocomplete

--- wls-gui-admintool/src/components/common/BaseAutocompleteWahltag.vue
+++ wls-gui-admintool/src/components/common/BaseAutocompleteWahltag.vue
@@ -8,7 +8,39 @@
     return-object
     data-test="autocompleteWahltage"
     no-data-text="Keine Wahltage gefunden"
-  />
+  >
+    <template #selection="{ item }">
+      <span>{{ itemTitle(item.raw) }}</span>
+      <v-chip
+        v-if="item.raw.isActive"
+        class="ml-2"
+        color="primary"
+        data-test="activeWahltag"
+        size="small"
+        variant="tonal"
+      >
+        Aktiv
+      </v-chip>
+    </template>
+    <template #item="{ props: itemProps, item }">
+      <v-list-item
+        v-bind="itemProps"
+        :title="itemTitle(item.raw)"
+      >
+        <template #append>
+          <v-chip
+            v-if="item.raw.isActive"
+            color="primary"
+            data-test="activeWahltag"
+            size="small"
+            variant="tonal"
+          >
+            Aktiv
+          </v-chip>
+        </template>
+      </v-list-item>
+    </template>
+  </v-autocomplete>
 </template>
 
 <script setup lang="ts">
@@ -16,7 +48,7 @@ import type { Wahltag } from "@/types/wahltag/Wahltag.ts";
 import type { PropType } from "vue";
 
 import { useDate } from "vuetify";
-import { VAutocomplete } from "vuetify/components";
+import { VAutocomplete, VChip, VListItem } from "vuetify/components";
 
 const date = useDate();
 

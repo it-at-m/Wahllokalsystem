adb16c8c5ad5a5d632364e5a4444945bb62706da
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-09 10:53:23 +0200

    bug fix: use value of computed property

--- wls-gui-wahllokalsystem/src/composables/navigation/mbwNavigationService.ts
+++ wls-gui-wahllokalsystem/src/composables/navigation/mbwNavigationService.ts
@@ -29,7 +29,7 @@ export function useMbwNavigationService(wahlID: string, wahlbezirkID: string) {
 
     const result: NavigationDefinition[] = [];
 
-    if (hasRoleSchriftfuehrung) {
+    if (hasRoleSchriftfuehrung.value) {
       result.push(..._createNavigationForSchriftfuehrung());
     }
 

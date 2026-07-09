651a8e1608ce14e4ae5eb1e6faf48e79ff8fda69
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-09 09:25:21 +0200

    add serializable

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/domain/stimmzettelerfassung/StimmzettelID.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/domain/stimmzettelerfassung/StimmzettelID.java
@@ -1,10 +1,11 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.domain.stimmzettelerfassung;
 
 import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class StimmzettelID {
+public class StimmzettelID implements Serializable {
   @NotNull private String wahlbezirkID;
 
   @NotNull private String wahlID;

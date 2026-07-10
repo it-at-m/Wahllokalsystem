ee2fa671925eef42b41318c64cffa49a58a23225
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-10 17:25:02 +0200

    use responseEntity on getStimmuettel; define create on Post; add explicit PathVars on getCount

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/stimmzettelerfassung/stimmzettel/StimmzettelController.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/stimmzettelerfassung/stimmzettel/StimmzettelController.java
@@ -12,11 +12,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -51,18 +54,21 @@ public class StimmzettelController {
             content = {@Content()})
       })
   @GetMapping("wahl/{wahlID}/wahlbezirk/{wahlbezirkID}/team/{teamID}/stimmzettel")
-  public List<StimmzettelOfTeamDTO> getStimmzettel(
+  public ResponseEntity<List<StimmzettelOfTeamDTO>> getStimmzettel(
       @PathVariable("wahlID") final String wahlID,
       @PathVariable("wahlbezirkID") final String wahlbezirkID,
       @PathVariable("teamID") final String teamID) {
-    return stimmzettelService
+    val stimmzettel =  stimmzettelService
         .getStimmzettel(new StimmzettelOwnerModel(wahlbezirkID, wahlID, teamID))
         .stream()
         .map(stimmzettelDTOMapper::toDTO)
         .toList();
+
+    return stimmzettel.isEmpty() ? ResponseEntity.status(HttpStatus.NO_CONTENT.value()).build() : ResponseEntity.ok(stimmzettel);
   }
 
   @PostMapping("wahl/{wahlID}/wahlbezirk/{wahlbezirkID}/team/{teamID}/stimmzettel")
+  @ResponseStatus(HttpStatus.CREATED)
   public void postStimmzettel(
       @PathVariable("wahlID") final String wahlID,
       @PathVariable("wahlbezirkID") final String wahlbezirkID,
@@ -74,7 +80,9 @@ public class StimmzettelController {
   }
 
   @GetMapping("wahl/{wahlID}/wahlbezirk/{wahlbezirkID}/anzahlStimmzettel")
-  public int getAnzahlStimmzettel(final BezirkUndWahlID bezirkUndWahlID) {
-    return stimmzettelService.getAnzahlStimmzettel(bezirkUndWahlID);
+  public int getAnzahlStimmzettel(
+          @PathVariable("wahlID") final String wahlID,
+          @PathVariable("wahlbezirkID") final String wahlbezirkID) {
+    return stimmzettelService.getAnzahlStimmzettel(new BezirkUndWahlID(wahlID, wahlbezirkID));
   }
 }

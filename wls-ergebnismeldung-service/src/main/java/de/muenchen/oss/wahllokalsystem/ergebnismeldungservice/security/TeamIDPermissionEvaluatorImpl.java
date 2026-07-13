cf97e2a330041590142bb508f4b3eb961731cde5
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-13 13:43:36 +0200

    implemented TeamIDPermissionEvaluator

---
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/security/TeamIDPermissionEvaluatorImpl.java
@@ -0,0 +1,51 @@
+package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.security;
+
+import de.muenchen.oss.wahllokalsystem.wls.common.security.Profiles;
+import de.muenchen.oss.wahllokalsystem.wls.common.security.authentication.AuthDetailRetriever;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component(TeamIDPermissionEvaluatorImpl.COMPONENT_NAME)
+@Profile("!" + Profiles.NO_BEZIRKS_ID_CHECK)
+@RequiredArgsConstructor
+@Slf4j
+public class TeamIDPermissionEvaluatorImpl implements TeamIDPermissionEvaluator {
+
+  private static final String AUTH_DETAILS_MAP_KEY_TEAM_ID = "teamID";
+
+  private final List<AuthDetailRetriever> authDetailRetrievers;
+
+  @Override
+  public boolean tokenUserteamIdMatches(
+      final String requiredTeamID, final Authentication authentication) {
+    if (authentication == null) {
+      log.warn("No authentication object for teamID = {}", requiredTeamID);
+      return false;
+    }
+
+    if (requiredTeamID == null) {
+      log.warn("Required teamID is null");
+      return false;
+    }
+
+    val teamIDFromToken = getTeamID(authentication);
+    return teamIDFromToken.map(s -> s.equals(requiredTeamID)).orElse(false);
+  }
+
+  private Optional<String> getTeamID(final Authentication auth) {
+    return getClaim(auth, AUTH_DETAILS_MAP_KEY_TEAM_ID);
+  }
+
+  private Optional<String> getClaim(final Authentication authentication, final String claimKey) {
+    val retriever =
+        authDetailRetrievers.stream().filter(r -> r.canHandle(authentication)).findFirst();
+    return retriever.flatMap(
+        authDetailRetriever -> authDetailRetriever.getDetail(claimKey, authentication));
+  }
+}
---
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/security/TeamIDPermissionEvaluatorImplTest.java
@@ -0,0 +1,98 @@
+package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.security;
+
+import de.muenchen.oss.wahllokalsystem.wls.common.security.authentication.AuthDetailRetriever;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import lombok.val;
+import org.assertj.core.api.Assertions;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.core.Authentication;
+
+class TeamIDPermissionEvaluatorImplTest {
+
+  AuthDetailRetriever authDetailRetriever = Mockito.mock(AuthDetailRetriever.class);
+
+  TeamIDPermissionEvaluatorImpl unitUnderTest =
+      new TeamIDPermissionEvaluatorImpl(List.of(authDetailRetriever));
+
+  @Nested
+  class TokenUserteamIdMatches {
+
+    @Test
+    void should_returnFalse_when_teamIDIsNull() {
+      Assertions.assertThat(
+              unitUnderTest.tokenUserteamIdMatches(null, Mockito.mock(Authentication.class)))
+          .isFalse();
+    }
+
+    @Test
+    void should_returnFalse_when_authenticationIsNUll() {
+      Assertions.assertThat(
+              unitUnderTest.tokenUserteamIdMatches(Instancio.create(String.class), null))
+          .isFalse();
+    }
+
+    @Test
+    void should_returnFalse_when_hasNoAuthDetailsHandlers() {
+      val unitUnderTest = new TeamIDPermissionEvaluatorImpl(Collections.emptyList());
+      Assertions.assertThat(
+              unitUnderTest.tokenUserteamIdMatches(
+                  Instancio.create(String.class), Mockito.mock(Authentication.class)))
+          .isFalse();
+    }
+
+    @Test
+    void should_returnFalse_when_noAuthDetailsHandlerCanHandleAuthentication() {
+      val requiredTeamID = Instancio.create(String.class);
+      val authentication = Mockito.mock(Authentication.class);
+
+      Mockito.when(authDetailRetriever.canHandle(authentication)).thenReturn(false);
+
+      Assertions.assertThat(unitUnderTest.tokenUserteamIdMatches(requiredTeamID, authentication))
+          .isFalse();
+    }
+
+    @Test
+    void should_returnFalse_when_authenticationHasNoTeamID() {
+      val requiredTeamID = Instancio.create(String.class);
+      val authentication = Mockito.mock(Authentication.class);
+
+      Mockito.when(authDetailRetriever.canHandle(authentication)).thenReturn(true);
+      Mockito.when(authDetailRetriever.getDetail(Mockito.any(), Mockito.eq(authentication)))
+          .thenReturn(Optional.empty());
+
+      val result = unitUnderTest.tokenUserteamIdMatches(requiredTeamID, authentication);
+      Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_returnFalse_when_requiredTeamIDDoesNotMatchWithAuthentication() {
+      val requiredTeamID = Instancio.create(String.class);
+      val authentication = Mockito.mock(Authentication.class);
+
+      Mockito.when(authDetailRetriever.canHandle(authentication)).thenReturn(true);
+      Mockito.when(authDetailRetriever.getDetail("teamID", authentication))
+          .thenReturn(Optional.of(requiredTeamID + "sth"));
+
+      val result = unitUnderTest.tokenUserteamIdMatches(requiredTeamID, authentication);
+      Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    void should_returnTrue_when_requiredTeamIDMatchesWithAuthentication() {
+      val requiredTeamID = Instancio.create(String.class);
+      val authentication = Mockito.mock(Authentication.class);
+
+      Mockito.when(authDetailRetriever.canHandle(authentication)).thenReturn(true);
+      Mockito.when(authDetailRetriever.getDetail("teamID", authentication))
+          .thenReturn(Optional.of(requiredTeamID));
+
+      val result = unitUnderTest.tokenUserteamIdMatches(requiredTeamID, authentication);
+      Assertions.assertThat(result).isTrue();
+    }
+  }
+}

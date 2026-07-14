b1fec99475fd243309ffb1c84842c093e5e4c4c3
CNoehm <CNoehm@users.noreply.github.com>
2026-07-14 10:19:28 +0000

    deploy: b13519706bfa5329abde6b334cdf76e10a6376be

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.USgAnqF7.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U.Djulheiw.js";import{_ as a}from"./theme.Ufqrh1VR.js";import"./chunk-5VM5RSS4.CcPQxF87.js";import"./chunk-XXDRQBXY.DbUDaQHt.js";import"./chunk-VR4S4FIN.DZu6TMCL.js";import"./chunk-32BRIVSS.kX3MzqEK.js";import"./framework.HWAOx3Ef.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};

eb5c0883e7ff7ab666a349321cc0d2794cbe9911
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-20 20:26:59 +0200

    add permissions and update Authorities

---
+++ wls-auth-service/src/main/resources/db/migrations/h2/V34_0__addPermissionsStimmzettelerfassungTeam.sql
@@ -0,0 +1,4 @@
+INSERT INTO Permission
+VALUES ('00000000-0000-0000-0005-000000000028', 'Ergebnismeldung_BUSINESSACTION_SaveStimmzettelerfassungTeamstatus');
+INSERT INTO Permission
+VALUES ('00000000-0000-0000-0005-000000000029', 'Ergebnismeldung_BUSINESSACTION_GetStimmzettelerfassungTeamstatus');
---
+++ wls-auth-service/src/main/resources/db/migrations/h2/V34_1__addStimmzettelerfassungTeamstatusPermissionToAuthorities.sql
@@ -0,0 +1,26 @@
+MERGE INTO Secauthorities_Secpermissions (authority_oid, permission_oid)
+    KEY(authority_oid, permission_oid)
+    VALUES (
+    (SELECT ID FROM Authority WHERE authority = 'WLS_WAHLVORSTAND'),
+    (SELECT ID FROM Permission WHERE permission = 'Ergebnismeldung_BUSINESSACTION_SaveStimmzettelerfassungTeamstatus')
+    );
+
+MERGE INTO Secauthorities_Secpermissions (authority_oid, permission_oid)
+    KEY(authority_oid, permission_oid)
+    VALUES (
+    (SELECT ID FROM Authority WHERE authority = 'WLS_WAHLVORSTAND'),
+    (SELECT ID FROM Permission WHERE permission = 'Ergebnismeldung_BUSINESSACTION_GetStimmzettelerfassungTeamstatus')
+    );
+MERGE INTO Secauthorities_Secpermissions (authority_oid, permission_oid)
+    KEY(authority_oid, permission_oid)
+    VALUES (
+    (SELECT ID FROM Authority WHERE authority = 'ERFASSUNGSTEAM'),
+    (SELECT ID FROM Permission WHERE permission = 'Ergebnismeldung_BUSINESSACTION_SaveStimmzettelerfassungTeamstatus')
+    );
+
+MERGE INTO Secauthorities_Secpermissions (authority_oid, permission_oid)
+    KEY(authority_oid, permission_oid)
+    VALUES (
+    (SELECT ID FROM Authority WHERE authority = 'ERFASSUNGSTEAM'),
+    (SELECT ID FROM Permission WHERE permission = 'Ergebnismeldung_BUSINESSACTION_GetStimmzettelerfassungTeamstatus')
+    );

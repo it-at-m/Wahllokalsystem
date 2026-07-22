be7fd66465b68d9b317a2afd070de42a3d009c3c
CNoehm <212502354+CNoehm@users.noreply.github.com>
2026-07-22 07:42:48 +0200

    2987 - v-card und actions für Dialoge.

--- wls-gui-wahllokalsystem/src/views/dse/StimmzettelerfassungView.vue
+++ wls-gui-wahllokalsystem/src/views/dse/StimmzettelerfassungView.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>🚧 TBD 🚧</h1>
+  <v-card>
+    <v-card-title>Stimmzettelerfassung</v-card-title>
+    <v-card-text>
+      <v-skeleton-loader boilerplate type="card-avatar" class="mt-3"/>
+    </v-card-text>
+    <v-card-actions>
+      <base-text-button :active="status!='IN_BEARBEITUNG'" @click="startErfassung">Starten</base-text-button>
+      <base-text-button :active="status=='IN_BEARBEITUNG'" @click="unterbrechen">Unterbrechen</base-text-button>
+      <base-text-button class="ms-auto" :active="status=='ABGESCHLOSSEN'" @click="beenden">Beenden</base-text-button>
+    </v-card-actions>
+  </v-card>
+  <base-dialog
+      dialogtext="🚧 TBD 🚧"
+      dialogtitle="Stimmzettel erfassen"
+      :model-value="erfassungDialog"
+      cancel-disabled
+      confirmtext="Ok"
+      @confirm="erfassungDialog = !erfassungDialog"
+  >
+  </base-dialog>
+  <base-dialog
+      dialogtext="🚧 TBD 🚧"
+      dialogtitle="Stimmzettelerfassung beenden"
+      :model-value="beendenDialog"
+      cancel-disabled
+      confirmtext="Ok"
+      @confirm="beendenDialog = !beendenDialog"
+  >
+  </base-dialog>
 </template>
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseTextButton from "@/components/common/buttons/BaseTextButton.vue";
+import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
+
+//TeamErfassungStatus: REGISTRIERT, IN_BEARBEITUNG,    UNTERBROCHEN,    ABGESCHLOSSEN
+const status = ref("REGISTRIERT");
+const erfassungDialog = ref(false);
+const beendenDialog = ref(false);
+
+async function startErfassung() {
+  status.value="IN_BEARBEITUNG";
+  erfassungDialog.value = true;
+}
+
+async function unterbrechen() {
+  status.value="UNTERBROCHEN";
+}
+
+async function beenden() {
+  status.value="ABGESCHLOSSEN";
+  beendenDialog.value=true;
+}
+</script>

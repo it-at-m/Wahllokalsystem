d6c3584fb8b84381a71fa0cb17e1fc9f8844e9b1
ThoGro <thomas.grossbeck@muenchen.de>
2026-07-23 15:41:54 +0200

    Add StimmzettelkennungDialog

---
+++ wls-gui-wahllokalsystem/src/components/stimmzettelerfassung/TheStimmzettelkennungDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-dialog
+      :visible="props.visible"
+    dialogtitle="Übertragung der Stimmzettelkennung"
+    icon="$information"
+      confirmtext="Bestätigen"
+      canceltext="Abbrechen"
+    @confirm="onConfirmClicked"
+    @cancel="onCancelClicked"
+  >
+    <div>
+      Bitte übertragen Sie die Stimmzettelkennung auf den Papier-Stimmzettel.
+    </div>
+    <div>
+      {{ stimmzettelkennung }}
+    </div>
+  </base-dialog>
+</template>
+
+<script setup lang="ts">
+import BaseDialog from "@/components/common/dialogs/BaseDialog.vue";
+import {computed} from "vue";
+import {useUserStore} from "@/stores/userStore.ts";
+
+const { currentUserTeamName } = useUserStore();
+
+const props = defineProps<{
+  visible: boolean;
+}>();
+
+const stimmzettelkennung = computed(() => {
+  return currentUserTeamName;
+});
+
+function onConfirmClicked() {
+
+}
+
+function onCancelClicked() {
+
+}
+</script>

b1bac51581d3082f336957e8c02d6528088e06d3
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-23 17:19:30 +0200

    fix test case name for negative tests

--- wls-gui-wahllokalsystem/tests/service-worker/wahlWorkerConstants.spec.ts
+++ wls-gui-wahllokalsystem/tests/service-worker/wahlWorkerConstants.spec.ts
@@ -9,7 +9,7 @@ describe("wahlWorkerConstants.ts", () => {
       "http://localhost:123/api/backend-service/resouce",
       "https://localhost/api/backend-service/resouce",
       "https://localhost:123/api/backend-service/resouce",
-    ])("should_match_when_urlIsForBackendService", (url) => {
+    ])("should_match_when_urlIsForBackendService'%s'", (url) => {
       expect(REGEX_MATCH_BACKEND_API_CALLS.test(url)).toBe(true);
     });
 
@@ -20,7 +20,7 @@ describe("wahlWorkerConstants.ts", () => {
       "https://localhost:123/backend-service/resouce",
       "http://localhost/src/wls-clients/api/backend-service/resouce",
       "https://localhost:123/src/wls-clients/api/backend-service/resouce",
-    ])("should_match_when_urlIsForBackendService", (url) => {
+    ])("should_notMatch_when_urlIsNotForBackendService'%s'", (url) => {
       expect(REGEX_MATCH_BACKEND_API_CALLS.test(url)).toBe(false);
     });
   });

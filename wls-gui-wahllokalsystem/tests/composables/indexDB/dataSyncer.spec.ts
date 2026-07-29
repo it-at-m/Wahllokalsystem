44896005ea19888bcf4694f965c8fa63a75578b2
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-29 17:24:34 +0200

    update dataSyncer Tests

--- wls-gui-wahllokalsystem/tests/composables/indexDB/dataSyncer.spec.ts
+++ wls-gui-wahllokalsystem/tests/composables/indexDB/dataSyncer.spec.ts
@@ -1,6 +1,7 @@
 import type { Task } from "@/types/tasks/Task.ts";
 
 import { useIndexDBValueTestDataFactory } from "@tests/utils/indexDB/IndexDBValueTestDataFactory.ts";
+import { useTasksTestDataFactory } from "@tests/utils/tasks/TasksTestDataFactory.ts";
 import { spyOn } from "storybook/test";
 import {
   afterAll,
@@ -39,6 +40,7 @@ vi.mock(import("@/composables/tasks/taskManager.ts"), () => ({
 }));
 
 const { prepareIndexDBValue } = useIndexDBValueTestDataFactory();
+const { createTask } = useTasksTestDataFactory();
 
 describe("dataSyncer.ts", () => {
   let unitUnderTest: ReturnType<typeof useDataSyncer>;
@@ -53,6 +55,83 @@ describe("dataSyncer.ts", () => {
 
   afterAll(() => {
     vi.resetAllMocks();
+  });
+
+  describe("dirtyTasksAfterSync ", () => {
+    it("should_setListWithTasks_when_dirtyTasksExistAfterRun", async () => {
+      const dirtyTasks = [
+        {
+          key: "key1",
+          item: prepareIndexDBValue().data("data1").timestamp(9).build(),
+        },
+        {
+          key: "key2",
+          item: prepareIndexDBValue().data("2").timestamp(10).build(),
+        },
+      ];
+      mockDefinitions.getDirtyItems.mockResolvedValue(dirtyTasks);
+
+      expect(unitUnderTest.dirtyTasksAfterSync.value).toStrictEqual(null);
+      await unitUnderTest.synchronizeOfflineData();
+
+      const expectedTasks = [
+        { name: "key1", callback: expect.anything() },
+        { name: "key2", callback: expect.anything() },
+      ];
+      expect(unitUnderTest.dirtyTasksAfterSync.value).toStrictEqual(
+        expectedTasks
+      );
+    });
+    it("should_setEmptyList_when_noDirtyTasksExistAfterRun", async () => {
+      mockDefinitions.getDirtyItems.mockResolvedValue([]);
+
+      unitUnderTest.dirtyTasksAfterSync.value = [createTask("task1")];
+
+      await unitUnderTest.synchronizeOfflineData();
+      expect(unitUnderTest.dirtyTasksAfterSync.value).toStrictEqual([]);
+    });
+  });
+
+  describe("hasDirtyTasksAfterSync", () => {
+    it("should_returnFalse_when_dirtyTasksAfterSyncIsNull", () => {
+      unitUnderTest.dirtyTasksAfterSync.value = null;
+      expect(unitUnderTest.hasDirtyTasksAfterSync.value).toStrictEqual(false);
+    });
+    it("should_returnFalse_when_dirtyTasksAfterSyncIsEmptyArray", () => {
+      unitUnderTest.dirtyTasksAfterSync.value = [];
+      expect(unitUnderTest.hasDirtyTasksAfterSync.value).toStrictEqual(false);
+    });
+    it("should_returnTrue_when_dirtyTasksAfterSyncIsArrayWithOneItem", () => {
+      unitUnderTest.dirtyTasksAfterSync.value = [createTask("1")];
+      expect(unitUnderTest.hasDirtyTasksAfterSync.value).toStrictEqual(true);
+    });
+    it("should_returnTrue_when_dirtyTasksAfterSyncIsArrayWithMultipleItems", () => {
+      unitUnderTest.dirtyTasksAfterSync.value = [
+        createTask("1"),
+        createTask("2"),
+        createTask("3"),
+        createTask("4"),
+        createTask("5"),
+        createTask("6"),
+      ];
+      expect(unitUnderTest.hasDirtyTasksAfterSync.value).toStrictEqual(true);
+    });
+  });
+
+  describe("numberOfDirtyTasksAfterSync", () => {
+    it("should_return0_when_dirtyTasksAfterSyncIsNull", () => {
+      unitUnderTest.dirtyTasksAfterSync.value = null;
+      expect(unitUnderTest.numberOfDirtyTasksAfterSync.value).toStrictEqual(0);
+    });
+    it("should_returnLength_when_dirtyTasksAfterSyncIsNotNull", () => {
+      unitUnderTest.dirtyTasksAfterSync.value = [
+        createTask("1"),
+        createTask("2"),
+        createTask("3"),
+      ];
+
+      expect(unitUnderTest.numberOfDirtyTasksAfterSync.value).toStrictEqual(3);
+    });
   });
 
   describe("getSyncTasks", () => {
@@ -156,6 +235,16 @@ describe("dataSyncer.ts", () => {
 
       expect(mockDefinitions.setTasks).toHaveBeenCalledOnce();
       expect(mockDefinitions.runAllTasks).toHaveBeenCalledOnce();
+    });
+
+    it("should_notDoAnything_when_syncIsInProgress", async () => {
+      unitUnderTest.isOfflineDataSyncing.value = true;
+      await unitUnderTest.synchronizeOfflineData();
+      unitUnderTest.isOfflineDataSyncing.value = false;
+
+      expect(mockDefinitions.getDirtyItems).not.toHaveBeenCalled();
+      expect(mockDefinitions.setTasks).not.toHaveBeenCalled();
+      expect(mockDefinitions.runAllTasks).not.toHaveBeenCalled();
     });
 
     it("should_setIsSyncingFalse_when_anErrorOccurred", async () => {

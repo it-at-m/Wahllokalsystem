aeee68a7924437374a7e46153d19d329c262b4ef
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-07-29 17:19:24 +0200

    revert complete opening and just add the new function

--- wls-gui-wahllokalsystem/src/stores/dataSyncStore.ts
+++ wls-gui-wahllokalsystem/src/stores/dataSyncStore.ts
@@ -5,7 +5,29 @@ import { useDataSyncer } from "@/composables/indexDB/dataSyncer.ts";
 const storeID = "dataSync";
 
 export const useDataSyncStore = defineStore(storeID, () => {
+  const {
+    synchronizeOfflineData,
+    getSyncTasks,
+    dirtyTasksAfterSync,
+    hasTasksToRun,
+    hasDirtyTasksAfterSync,
+    isOfflineDataSyncing,
+    numberOfDirtyTasksAfterSync,
+    numberOfTasksFinished,
+    numberOfTasksToRun,
+    lastSyncUpdateTime,
+  } = useDataSyncer();
+
   return {
-    ...useDataSyncer(),
+    dirtyTasksAfterSync,
+    hasDirtyTasksAfterSync,
+    hasTasksToRun,
+    numberOfDirtyTasksAfterSync,
+    numberOfTasksFinished,
+    numberOfTasksToRun,
+    isOfflineDataSyncing,
+    lastSyncUpdateTime,
+    synchronizeOfflineData,
+    getSyncTasks,
   };
 });

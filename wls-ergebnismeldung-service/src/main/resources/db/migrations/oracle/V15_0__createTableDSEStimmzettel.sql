d4ea577ab33c3325235a9f3ab04d633172337fad
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-08-04 14:55:50 +0200

    format file

--- wls-ergebnismeldung-service/src/main/resources/db/migrations/oracle/V15_0__createTableDSEStimmzettel.sql
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/oracle/V15_0__createTableDSEStimmzettel.sql
@@ -1,51 +1,57 @@
 DROP TABLE Stimmzettel_Kandidat;
 DROP TABLE Stimmzettel;
 
-CREATE TABLE Stimmzettel (
-    wahlbezirkID VARCHAR2(255) NOT NULL,
-    wahlID VARCHAR2(255) NOT NULL,
-    teamID VARCHAR2(255) NOT NULL,
+CREATE TABLE Stimmzettel
+(
+    wahlbezirkID       VARCHAR2(255) NOT NULL,
+    wahlID             VARCHAR2(255) NOT NULL,
+    teamID             VARCHAR2(255) NOT NULL,
     stimmzettelkennung NUMBER(10) NOT NULL,
-    invalideVotes NUMBER(10) NOT NULL,
-    gueltigkeit VARCHAR2(255) NOT NULL,
-    beschluss_pro NUMBER(10),
-    beschluss_contra NUMBER(10),
-    beschluss_text VARCHAR2(4000),
+    invalideVotes      NUMBER(10) NOT NULL,
+    gueltigkeit        VARCHAR2(255) NOT NULL,
+    beschluss_pro      NUMBER(10),
+    beschluss_contra   NUMBER(10),
+    beschluss_text     VARCHAR2(4000),
     CONSTRAINT pk_stimmzettel PRIMARY KEY (wahlbezirkID, wahlID, teamID, stimmzettelkennung)
 );
 
-CREATE TABLE Beschlussvormerkung (
-    id VARCHAR2(255) NOT NULL,
-    text VARCHAR2(4000) NOT NULL,
-    stimmzettel_wahlbezirkID VARCHAR2(255) NOT NULL,
-    stimmzettel_wahlID VARCHAR2(255) NOT NULL,
-    stimmzettel_teamID VARCHAR2(255) NOT NULL,
+CREATE TABLE Beschlussvormerkung
+(
+    id                             VARCHAR2(255) NOT NULL,
+    text                           VARCHAR2(4000) NOT NULL,
+    stimmzettel_wahlbezirkID       VARCHAR2(255) NOT NULL,
+    stimmzettel_wahlID             VARCHAR2(255) NOT NULL,
+    stimmzettel_teamID             VARCHAR2(255) NOT NULL,
     stimmzettel_stimmzettelkennung NUMBER(10) NOT NULL,
     CONSTRAINT pk_beschlussvormerkung PRIMARY KEY (id),
-    CONSTRAINT fk_beschlussvormerkung_stimmzettel FOREIGN KEY (stimmzettel_wahlbezirkID, stimmzettel_wahlID, stimmzettel_teamID, stimmzettel_stimmzettelkennung)
+    CONSTRAINT fk_beschlussvormerkung_stimmzettel FOREIGN KEY (stimmzettel_wahlbezirkID, stimmzettel_wahlID,
+                                                               stimmzettel_teamID, stimmzettel_stimmzettelkennung)
         REFERENCES Stimmzettel (wahlbezirkID, wahlID, teamID, stimmzettelkennung)
 );
 
-CREATE TABLE Wahlvorschlag (
-    id VARCHAR2(255) NOT NULL,
-    wahlvorschlagID VARCHAR2(255) NOT NULL,
-    selected NUMBER(1) NOT NULL,
-    stimmzettel_wahlbezirkID VARCHAR2(255) NOT NULL,
-    stimmzettel_wahlID VARCHAR2(255) NOT NULL,
-    stimmzettel_teamID VARCHAR2(255) NOT NULL,
+CREATE TABLE Wahlvorschlag
+(
+    id                             VARCHAR2(255) NOT NULL,
+    wahlvorschlagID                VARCHAR2(255) NOT NULL,
+    selected                       NUMBER(1) NOT NULL,
+    stimmzettel_wahlbezirkID       VARCHAR2(255) NOT NULL,
+    stimmzettel_wahlID             VARCHAR2(255) NOT NULL,
+    stimmzettel_teamID             VARCHAR2(255) NOT NULL,
     stimmzettel_stimmzettelkennung NUMBER(10) NOT NULL,
     CONSTRAINT pk_wahlvorschlag PRIMARY KEY (id),
-    CONSTRAINT fk_wahlvorschlag_stimmzettel FOREIGN KEY (stimmzettel_wahlbezirkID, stimmzettel_wahlID, stimmzettel_teamID, stimmzettel_stimmzettelkennung)
+    CONSTRAINT fk_wahlvorschlag_stimmzettel FOREIGN KEY (stimmzettel_wahlbezirkID, stimmzettel_wahlID,
+                                                         stimmzettel_teamID, stimmzettel_stimmzettelkennung)
         REFERENCES Stimmzettel (wahlbezirkID, wahlID, teamID, stimmzettelkennung)
 );
 
-CREATE TABLE Kandidat (
-    kandidatID VARCHAR2(255) NOT NULL,
-    nennungsNummer NUMBER(10) NOT NULL,
-    wahlvorschlag_id VARCHAR2(255) NOT NULL,
-    discarded NUMBER(1) NOT NULL,
-    votesByVoter NUMBER(10),
-    invalidVotes NUMBER(10),
+CREATE TABLE Kandidat
+(
+    kandidatID           VARCHAR2(255) NOT NULL,
+    nennungsNummer       NUMBER(10) NOT NULL,
+    wahlvorschlag_id     VARCHAR2(255) NOT NULL,
+    discarded            NUMBER(1) NOT NULL,
+    votesByVoter         NUMBER(10),
+    invalidVotes         NUMBER(10),
     votesByWahlvorschlag NUMBER(10),
     CONSTRAINT pk_kandidat PRIMARY KEY (kandidatID, nennungsNummer),
     CONSTRAINT fk_kandidat_wahlvorschlag FOREIGN KEY (wahlvorschlag_id)

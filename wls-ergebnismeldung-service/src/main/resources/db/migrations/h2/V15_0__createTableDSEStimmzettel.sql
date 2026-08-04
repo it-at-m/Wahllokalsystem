d49767472439fe2e20015426f6950db10a2eefbf
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-08-04 16:45:05 +0200

    improve foreign key column names

--- wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V15_0__createTableDSEStimmzettel.sql
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V15_0__createTableDSEStimmzettel.sql
@@ -17,30 +17,30 @@ CREATE TABLE Stimmzettel
 
 CREATE TABLE Beschlussgrund
 (
-    id                             VARCHAR(255) NOT NULL,
-    text                           VARCHAR(100) NOT NULL,
-    stimmzettel_wahlbezirkID       VARCHAR(255) NOT NULL,
-    stimmzettel_wahlID             VARCHAR(255) NOT NULL,
-    stimmzettel_teamID             VARCHAR(255) NOT NULL,
-    stimmzettel_stimmzettelkennung INT          NOT NULL,
+    id                    VARCHAR(255) NOT NULL,
+    text                  VARCHAR(100) NOT NULL,
+    fk_wahlbezirkID       VARCHAR(255) NOT NULL,
+    fk_wahlID             VARCHAR(255) NOT NULL,
+    fk_teamID             VARCHAR(255) NOT NULL,
+    fk_stimmzettelkennung INT          NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_beschlussgrund_stimmzettel FOREIGN KEY (stimmzettel_wahlbezirkID, stimmzettel_wahlID,
-                                                          stimmzettel_teamID, stimmzettel_stimmzettelkennung)
+    CONSTRAINT fk_beschlussgrund_stimmzettel FOREIGN KEY (fk_wahlbezirkID, fk_wahlID,
+                                                          fk_teamID, fk_stimmzettelkennung)
         REFERENCES Stimmzettel (wahlbezirkID, wahlID, teamID, stimmzettelkennung)
 );
 
 CREATE TABLE Wahlvorschlag
 (
-    id                             VARCHAR(255) NOT NULL,
-    wahlvorschlagID                VARCHAR(255) NOT NULL,
-    selected                       BOOLEAN      NOT NULL,
-    stimmzettel_wahlbezirkID       VARCHAR(255) NOT NULL,
-    stimmzettel_wahlID             VARCHAR(255) NOT NULL,
-    stimmzettel_teamID             VARCHAR(255) NOT NULL,
-    stimmzettel_stimmzettelkennung INT          NOT NULL,
+    id                    VARCHAR(255) NOT NULL,
+    wahlvorschlagID       VARCHAR(255) NOT NULL,
+    selected              BOOLEAN      NOT NULL,
+    fk_wahlbezirkID       VARCHAR(255) NOT NULL,
+    fk_wahlID             VARCHAR(255) NOT NULL,
+    fk_teamID             VARCHAR(255) NOT NULL,
+    fk_stimmzettelkennung INT          NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_wahlvorschlag_stimmzettel FOREIGN KEY (stimmzettel_wahlbezirkID, stimmzettel_wahlID,
-                                                         stimmzettel_teamID, stimmzettel_stimmzettelkennung)
+    CONSTRAINT fk_wahlvorschlag_stimmzettel FOREIGN KEY (fk_wahlbezirkID, fk_wahlID,
+                                                         fk_teamID, fk_stimmzettelkennung)
         REFERENCES Stimmzettel (wahlbezirkID, wahlID, teamID, stimmzettelkennung)
 );
 
@@ -48,13 +48,13 @@ CREATE TABLE Kandidat
 (
     kandidatID           VARCHAR(255) NOT NULL,
     nennungsNummer       INT          NOT NULL,
-    wahlvorschlag_id     VARCHAR(255) NOT NULL,
+    fk_wahlvorschlagID   VARCHAR(255) NOT NULL,
     discarded            BOOLEAN      NOT NULL,
     votesByVoter         INT,
     invalidVotes         INT,
     votesByWahlvorschlag INT,
     PRIMARY KEY (kandidatID, nennungsNummer),
-    CONSTRAINT fk_kandidat_wahlvorschlag FOREIGN KEY (wahlvorschlag_id)
+    CONSTRAINT fk_kandidat_wahlvorschlag FOREIGN KEY (fk_wahlvorschlagID)
         REFERENCES Wahlvorschlag (id),
-    CONSTRAINT uq_kandidat_per_wahlvorschlag UNIQUE (wahlvorschlag_id, kandidatID, nennungsNummer)
+    CONSTRAINT uq_kandidat_per_wahlvorschlag UNIQUE (fk_wahlvorschlagID, kandidatID, nennungsNummer)
 );

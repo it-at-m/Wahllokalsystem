ebf13d6e96876cb12dd26c8ce15eb816a8f429a8
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-08-09 17:51:47 +0200

    fix sql syntax

--- wls-auth-service/src/main/resources/db/migrations/oracle/V36_0__createOAuth2AuthorizationTable.sql
+++ wls-auth-service/src/main/resources/db/migrations/oracle/V36_0__createOAuth2AuthorizationTable.sql
@@ -1,39 +1,47 @@
 CREATE TABLE oauth2_authorization
 (
-    id                            varchar2(100)  NOT NULL,
-    registered_client_id          varchar2(100)  NOT NULL,
-    principal_name                varchar2(200)  NOT NULL,
-    authorization_grant_type      varchar2(100)  NOT NULL,
-    authorized_scopes             varchar2(1000) DEFAULT NULL,
-    attributes                    varchar2(4000) DEFAULT NULL,
-    state                         varchar2(500)  DEFAULT NULL,
-    authorization_code_value      blob      DEFAULT NULL,
-    authorization_code_issued_at  timestamp DEFAULT NULL,
-    authorization_code_expires_at timestamp DEFAULT NULL,
-    authorization_code_metadata   varchar2(2000) DEFAULT NULL,
-    access_token_value            blob      DEFAULT NULL,
-    access_token_issued_at        timestamp DEFAULT NULL,
-    access_token_expires_at       timestamp DEFAULT NULL,
-    access_token_metadata         varchar2(2000) DEFAULT NULL,
-    access_token_type             varchar2(100)  DEFAULT NULL,
-    access_token_scopes           varchar2(1000) DEFAULT NULL,
-    oidc_id_token_value           blob      DEFAULT NULL,
-    oidc_id_token_issued_at       timestamp DEFAULT NULL,
-    oidc_id_token_expires_at      timestamp DEFAULT NULL,
-    oidc_id_token_metadata        varchar2(2000) DEFAULT NULL,
-    refresh_token_value           blob      DEFAULT NULL,
-    refresh_token_issued_at       timestamp DEFAULT NULL,
-    refresh_token_expires_at      timestamp DEFAULT NULL,
-    refresh_token_metadata        varchar2(2000) DEFAULT NULL,
-    user_code_value               blob      DEFAULT NULL,
-    user_code_issued_at           timestamp DEFAULT NULL,
-    user_code_expires_at          timestamp DEFAULT NULL,
-    user_code_metadata            varchar2(2000) DEFAULT NULL,
-    device_code_value             blob      DEFAULT NULL,
-    device_code_issued_at         timestamp DEFAULT NULL,
-    device_code_expires_at        timestamp DEFAULT NULL,
-    device_code_metadata          varchar2(2000) DEFAULT NULL,
-    PRIMARY KEY (id)
+    id                            VARCHAR2(100)    NOT NULL,
+    registered_client_id          VARCHAR2(100)    NOT NULL,
+    principal_name                VARCHAR2(200)    NOT NULL,
+    authorization_grant_type      VARCHAR2(100)    NOT NULL,
+    authorized_scopes             VARCHAR2(4000),
+
+    attributes                    VARCHAR2(4000),
+    state                         VARCHAR2(500),
+
+    authorization_code_value      VARCHAR2(4000),
+    authorization_code_issued_at  TIMESTAMP,
+    authorization_code_expires_at TIMESTAMP,
+    authorization_code_metadata   VARCHAR2(4000),
+
+    access_token_value            VARCHAR2(4000),
+    access_token_issued_at        TIMESTAMP,
+    access_token_expires_at       TIMESTAMP,
+    access_token_metadata         VARCHAR2(4000),
+    access_token_type             VARCHAR2(100),
+    access_token_scopes           VARCHAR2(4000),
+
+    oidc_id_token_value           VARCHAR2(4000),
+    oidc_id_token_issued_at       TIMESTAMP,
+    oidc_id_token_expires_at      TIMESTAMP,
+    oidc_id_token_metadata        VARCHAR2(4000),
+
+    refresh_token_value           VARCHAR2(4000),
+    refresh_token_issued_at       TIMESTAMP,
+    refresh_token_expires_at      TIMESTAMP,
+    refresh_token_metadata        VARCHAR2(4000),
+
+    user_code_value               VARCHAR2(4000),
+    user_code_issued_at           TIMESTAMP,
+    user_code_expires_at          TIMESTAMP,
+    user_code_metadata            VARCHAR2(4000),
+
+    device_code_value             VARCHAR2(4000),
+    device_code_issued_at         TIMESTAMP,
+    device_code_expires_at        TIMESTAMP,
+    device_code_metadata          VARCHAR2(4000),
+
+    CONSTRAINT pk_oauth2_authorization PRIMARY KEY (id)
 );
 
 CREATE INDEX idx_oauth2_authorization_registered_client_id

e4404a9f1dbaf37c4feaf038e9fb09eda6dd8569
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-08-09 17:15:36 +0200

    use JdbcOAuth2AuthorizationService

---
+++ wls-auth-service/src/main/resources/db/migrations/oracle/V36_0__createOAuth2AuthorizationTable.sql
@@ -0,0 +1,43 @@
+CREATE TABLE oauth2_authorization
+(
+    id                            varchar2(100)  NOT NULL,
+    registered_client_id          varchar2(100)  NOT NULL,
+    principal_name                varchar2(200)  NOT NULL,
+    authorization_grant_type      varchar2(100)  NOT NULL,
+    authorized_scopes             varchar2(1000) DEFAULT NULL,
+    attributes                    varchar2(4000) DEFAULT NULL,
+    state                         varchar2(500)  DEFAULT NULL,
+    authorization_code_value      blob      DEFAULT NULL,
+    authorization_code_issued_at  timestamp DEFAULT NULL,
+    authorization_code_expires_at timestamp DEFAULT NULL,
+    authorization_code_metadata   varchar2(2000) DEFAULT NULL,
+    access_token_value            blob      DEFAULT NULL,
+    access_token_issued_at        timestamp DEFAULT NULL,
+    access_token_expires_at       timestamp DEFAULT NULL,
+    access_token_metadata         varchar2(2000) DEFAULT NULL,
+    access_token_type             varchar2(100)  DEFAULT NULL,
+    access_token_scopes           varchar2(1000) DEFAULT NULL,
+    oidc_id_token_value           blob      DEFAULT NULL,
+    oidc_id_token_issued_at       timestamp DEFAULT NULL,
+    oidc_id_token_expires_at      timestamp DEFAULT NULL,
+    oidc_id_token_metadata        varchar2(2000) DEFAULT NULL,
+    refresh_token_value           blob      DEFAULT NULL,
+    refresh_token_issued_at       timestamp DEFAULT NULL,
+    refresh_token_expires_at      timestamp DEFAULT NULL,
+    refresh_token_metadata        varchar2(2000) DEFAULT NULL,
+    user_code_value               blob      DEFAULT NULL,
+    user_code_issued_at           timestamp DEFAULT NULL,
+    user_code_expires_at          timestamp DEFAULT NULL,
+    user_code_metadata            varchar2(2000) DEFAULT NULL,
+    device_code_value             blob      DEFAULT NULL,
+    device_code_issued_at         timestamp DEFAULT NULL,
+    device_code_expires_at        timestamp DEFAULT NULL,
+    device_code_metadata          varchar2(2000) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_oauth2_authorization_registered_client_id
+    ON oauth2_authorization (registered_client_id);
+
+CREATE INDEX idx_oauth2_authorization_principal_name
+    ON oauth2_authorization (principal_name);

558e15e505dfcb9b87050379871414385e63a03d
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-08-10 10:08:08 +0200

    persist oauth2_authorization

--- wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/service/WahlbezirksartModel.java
+++ wls-auth-service/src/main/java/de/muenchen/oss/wahllokalsystem/authservice/service/WahlbezirksartModel.java
@@ -1,5 +1,8 @@
 package de.muenchen.oss.wahllokalsystem.authservice.service;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public enum WahlbezirksartModel {
   UWB,
   BWB

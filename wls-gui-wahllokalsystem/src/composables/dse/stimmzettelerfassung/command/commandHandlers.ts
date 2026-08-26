7499e15758d5ceb2bc4440ccdecfb53cc103d73a
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-08-26 12:09:03 +0200

    #3370 move command inside stimmzettelerfassung

--- wls-gui-wahllokalsystem/src/composables/dse/stimmzettelerfassung/command/commandHandlers.ts
+++ wls-gui-wahllokalsystem/src/composables/dse/stimmzettelerfassung/command/commandHandlers.ts
@@ -1,4 +1,4 @@
-import type { CommandHandler } from "@/types/dse/command/CommandHandler.ts";
+import type { CommandHandler } from "@/types/dse/stimmzettelerfassung/command/CommandHandler.ts";
 
 import { useAddVotesToSingleKandidatHandler } from "@/composables/dse/stimmzettelerfassung/command/addVotesToSingleKandidatHandler.ts";
 

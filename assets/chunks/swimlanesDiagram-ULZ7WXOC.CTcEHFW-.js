765d26daf4b58e413ebc2a6add2db58974a47958
ThoGro <ThoGro@users.noreply.github.com>
2026-08-27 13:46:23 +0000

    deploy: 5c5fbb4c80d378dfeb00093f2c9e819e0af09669

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.CTcEHFW-.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-UKHOOZJN.CTr3vDpL.js";import{_ as a}from"./theme.C-ejv_no.js";import"./chunk-5VM5RSS4.BMeFctPT.js";import"./chunk-XXDRQBXY.FqB0Dees.js";import"./chunk-KBJHAD2P.CnUXEL8l.js";import"./chunk-2GRJ4B5K.CsDESozg.js";import"./framework.HWAOx3Ef.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};

d42362b3419c18abe9ecf7b563c58f873c2a0f64
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-07 16:58:53 +0200

    #3422 re add ergebnismeldung release

--- wls-gui-wahllokalsystem/src/api/wls-clients/generated-ergebnismeldung-api/api.ts
+++ wls-gui-wahllokalsystem/src/api/wls-clients/generated-ergebnismeldung-api/api.ts
@@ -804,17 +804,10 @@ export interface SystemBeschlussgrundDTO {
 }
 
 export const SystemBeschlussgrundDTOReasonEnum = {
-    WaehlerwilleIstZweifelsfreiErkennbar: 'WAEHLERWILLE_IST_ZWEIFELSFREI_ERKENNBAR',
     ZuVieleEinzelstimmenAberImGesamtstimmenlimit: 'ZU_VIELE_EINZELSTIMMEN_ABER_IM_GESAMTSTIMMENLIMIT',
     KeineReststimmenvergabeMoeglich: 'KEINE_RESTSTIMMENVERGABE_MOEGLICH',
     EinzelneStimmenUngueltig: 'EINZELNE_STIMMEN_UNGUELTIG',
-    WaehlerwilleNichtZweifelsfreiErkennbar: 'WAEHLERWILLE_NICHT_ZWEIFELSFREI_ERKENNBAR',
-    ZuVieleEinzelstimmenOderListenkreuze: 'ZU_VIELE_EINZELSTIMMEN_ODER_LISTENKREUZE',
-    StimmzettelMitBesonderemZusatz: 'STIMMZETTEL_MIT_BESONDEREM_ZUSATZ',
-    NichtAmtlicherStimmzettel: 'NICHT_AMTLICHER_STIMMZETTEL',
-    BriefwahlMehrereStimmzettelInUmschlagIdentischGekennzeichnet: 'BRIEFWAHL_MEHRERE_STIMMZETTEL_IN_UMSCHLAG_IDENTISCH_GEKENNZEICHNET',
-    BriefwahlMehrereStimmzettelInUmschlagUnterschiedlichGekennzeichnet: 'BRIEFWAHL_MEHRERE_STIMMZETTEL_IN_UMSCHLAG_UNTERSCHIEDLICH_GEKENNZEICHNET',
-    BriefwahlMehrereStimmzettelInUmschlagLeerUndGekennzeichnet: 'BRIEFWAHL_MEHRERE_STIMMZETTEL_IN_UMSCHLAG_LEER_UND_GEKENNZEICHNET'
+    ZuVieleEinzelstimmenOderListenkreuze: 'ZU_VIELE_EINZELSTIMMEN_ODER_LISTENKREUZE'
 } as const;
 
 export type SystemBeschlussgrundDTOReasonEnum = typeof SystemBeschlussgrundDTOReasonEnum[keyof typeof SystemBeschlussgrundDTOReasonEnum];

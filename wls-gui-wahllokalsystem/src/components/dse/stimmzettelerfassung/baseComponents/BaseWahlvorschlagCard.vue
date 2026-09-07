28f1d5108c0566eed2f9d6d9eedb59971febcb74
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-07 15:26:50 +0200

    fix styling of focused kandidat

--- wls-gui-wahllokalsystem/src/components/dse/stimmzettelerfassung/baseComponents/BaseWahlvorschlagCard.vue
+++ wls-gui-wahllokalsystem/src/components/dse/stimmzettelerfassung/baseComponents/BaseWahlvorschlagCard.vue
@@ -45,26 +45,28 @@
       style="min-height: 0"
     >
       <v-list style="flex: 1 1 auto; min-height: 0; overflow-y: auto">
-        <v-list-item
+        <template
           v-for="(kandidat, index) in kandidatenListe"
-          :id="`kandidat-${index}`"
           :key="index"
-          ref="listItems"
-          :class="{
-            activeKandidat:
-              kandidat.kandidatId === activeKandidat?.kandidatId &&
-              kandidat.nennung === activeKandidat?.nennung,
-          }"
-          tabindex="-1"
         >
+          <v-list-item
+            :id="`kandidat-${index}`"
+            ref="listItems"
+            :class="{
+              activeKandidat:
+                kandidat.kandidatId === activeKandidat?.kandidatId &&
+                kandidat.nennung === activeKandidat?.nennung,
+            }"
+            tabindex="-1"
+          >
+            <base-kandidat-list-item-content :kandidat="kandidat" />
+          </v-list-item>
           <v-divider
-            v-if="index !== 0"
             :variant="
-              isDividerZwischenGleichemKandidat(index) ? 'dashed' : 'solid'
-            "
+                isDividerZwischenGleichemKandidat(index) ? 'dashed' : 'solid'
+              "
           />
-          <base-kandidat-list-item-content :kandidat="kandidat" />
-        </v-list-item>
+        </template>
       </v-list>
     </v-card-text>
   </v-card>

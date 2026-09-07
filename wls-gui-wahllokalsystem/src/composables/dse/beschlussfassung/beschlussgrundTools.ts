ce03afa6ba2918f54155d1a3cfb57001e934920b
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-07 11:22:19 +0200

    use enum for wahlvorstand beschluss vorschlaege

--- wls-gui-wahllokalsystem/src/composables/dse/beschlussfassung/beschlussgrundTools.ts
+++ wls-gui-wahllokalsystem/src/composables/dse/beschlussfassung/beschlussgrundTools.ts
@@ -1,16 +1,17 @@
 import type { WahlvorstandBeschlussgrund } from "@/types/dse/beschlussfassung/WahlvorstandBeschlussgrund.ts";
 
+import { WahlvorstandBeschlussvorschlaegeEnum } from "@/types/dse/beschlussfassung/WahlvorstandBeschlussvorschlaegeEnum.ts";
+
 export function useBeschlussgrundTools() {
   const commonWahlvorstandBeschlussvorschlaege = [
-    "einzelne Stimmen ungültig",
-    "Wählerwille ist nicht zweifelsfrei erkennbar",
-    "Stimmzettel ist mit einem besonderen Merkmal, Zusatz oder Vorbehalt versehen",
-    "Stimmzettel ist nicht amtlich hergestellt (zum Beispiel von einer anderen Gemeinde)",
+    WahlvorstandBeschlussvorschlaegeEnum.WaehlerwilleNichtZweifelsfreiErkennbar,
+    WahlvorstandBeschlussvorschlaegeEnum.StimmzettelMitBesonderemZusatz,
+    WahlvorstandBeschlussvorschlaegeEnum.NichtAmtlicherStimmzettel,
   ];
   const bwbWahlvorstandBeschlussvorschlaege = [
-    "Mehrere gleich gekennzeichnete Stimmzettel im Umschlag",
-    "Mehrere Stimmzettel im Umschlag, einer gekennzeichnet, die anderen leer",
-    "Mehrere unterschiedlich gekennzeichnete Stimmzettel im Umschlag",
+    WahlvorstandBeschlussvorschlaegeEnum.BriefwahlMehrereStimmzettelInUmschlagIdentischGekennzeichnet,
+    WahlvorstandBeschlussvorschlaegeEnum.BriefwahlMehrereStimmzettelInUmschlagLeerUndGekennzeichnet,
+    WahlvorstandBeschlussvorschlaegeEnum.BriefwahlMehrereStimmzettelInUmschlagUnterschiedlichGekennzeichnet,
   ];
 
   function getWahlvorstandBeschlussvorschlaege(isBWB: boolean) {
---
+++ wls-gui-wahllokalsystem/src/types/dse/beschlussfassung/WahlvorstandBeschlussvorschlaegeEnum.ts
@@ -0,0 +1,18 @@
+export const WahlvorstandBeschlussvorschlaegeEnum = {
+  WaehlerwilleNichtZweifelsfreiErkennbar:
+    "Wählerwille ist nicht zweifelsfrei erkennbar",
+  ZuVieleEinzelstimmenOderListenkreuze:
+    "ZU_VIELE_EINZELSTIMMEN_ODER_LISTENKREUZE",
+  StimmzettelMitBesonderemZusatz:
+    "Stimmzettel ist mit einem besonderen Merkmal, Zusatz oder Vorbehalt versehen",
+  NichtAmtlicherStimmzettel:
+    "Stimmzettel ist nicht amtlich hergestellt (zum Beispiel von einer anderen Gemeinde)",
+  BriefwahlMehrereStimmzettelInUmschlagIdentischGekennzeichnet:
+    "Mehrere gleich gekennzeichnete Stimmzettel im Umschlag",
+  BriefwahlMehrereStimmzettelInUmschlagUnterschiedlichGekennzeichnet:
+    "Mehrere unterschiedlich gekennzeichnete Stimmzettel im Umschlag",
+  BriefwahlMehrereStimmzettelInUmschlagLeerUndGekennzeichnet:
+    "Mehrere Stimmzettel im Umschlag, einer gekennzeichnet, die anderen leer",
+} as const;
+export type WahlvorstandBeschlussvorschlaegeEnum =
+  (typeof WahlvorstandBeschlussvorschlaegeEnum)[keyof typeof WahlvorstandBeschlussvorschlaegeEnum];

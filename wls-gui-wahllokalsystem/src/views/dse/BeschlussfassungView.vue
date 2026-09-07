717811d76edaae183f901dbc6e58e9768ed02a1a
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-07 09:03:46 +0200

    #2988 remove unused div

--- wls-gui-wahllokalsystem/src/views/dse/BeschlussfassungView.vue
+++ wls-gui-wahllokalsystem/src/views/dse/BeschlussfassungView.vue
@@ -1,32 +1,30 @@
 <template>
-  <div>
-    <v-card>
-      <v-card-title>Beschlussfassung</v-card-title>
-      <v-card-text>
-        <base-progress-linear
-          class="text-center"
-          titel="bereits gefasste Beschlüsse"
-          :is-loading="false"
-          :current="completedStimmzettelForBeschlussfassung.length"
-          :total="stimmzettelForBeschlussfassung.length"
-          color="success" />
-        <base-beschlussfassung-uebersicht-table
-          :stimmzettel-liste="stimmzettelForBeschlussfassung"
-          :stimmzettel-loading="isStimmzettelForBeschlussLoading"
-          @edit-beschluss-stimmzettel="onBeschlussBearbeitenClicked($event)"
-      /></v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <base-text-button
-          active
-          :disabled="isBeschlussfassungBeendenButtonDisabled"
-          @click="onBeschlussfassungBeendenClicked"
-        >
-          Beschlussfassung Beenden
-        </base-text-button>
-      </v-card-actions>
-    </v-card>
-  </div>
+  <v-card>
+    <v-card-title>Beschlussfassung</v-card-title>
+    <v-card-text>
+      <base-progress-linear
+        class="text-center"
+        titel="bereits gefasste Beschlüsse"
+        :is-loading="false"
+        :current="completedStimmzettelForBeschlussfassung.length"
+        :total="stimmzettelForBeschlussfassung.length"
+        color="success" />
+      <base-beschlussfassung-uebersicht-table
+        :stimmzettel-liste="stimmzettelForBeschlussfassung"
+        :stimmzettel-loading="isStimmzettelForBeschlussLoading"
+        @edit-beschluss-stimmzettel="onBeschlussBearbeitenClicked($event)"
+    /></v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <base-text-button
+        active
+        :disabled="isBeschlussfassungBeendenButtonDisabled"
+        @click="onBeschlussfassungBeendenClicked"
+      >
+        Beschlussfassung Beenden
+      </base-text-button>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script setup lang="ts">

a260f2b974085e02248ca161164c884407620866
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-09 14:30:35 +0200

    #3384 remove unused prop

--- wls-gui-wahllokalsystem/src/composables/dse/stimmzettelerfassung/managedStimmzettel.ts
+++ wls-gui-wahllokalsystem/src/composables/dse/stimmzettelerfassung/managedStimmzettel.ts
@@ -36,8 +36,6 @@ function _useManagedStimmzettel(stimmzettel: Ref<Stimmzettel>, wahlID: string) {
   const { addInvalidVotesToKandidat, removeInvalidVotesFromKandidat } =
     useManagedStimmzettelUngueltigeStimmeUtils();
 
-  const hasAnyValuesSet = computed(() => kandidatenWithValues.value.length > 0);
-
   const kandidatenWithValues = computed(() =>
     kandidatenOfStimmzettel.value.filter(_hasKandidatAnyStimmeOrStreichung)
   );
@@ -362,7 +360,6 @@ function _useManagedStimmzettel(stimmzettel: Ref<Stimmzettel>, wahlID: string) {
 
   return {
     changeHistory,
-    hasAnyValuesSet,
     resetStimmzettel,
     kandidatAddEinzelstimmenOrThrow,
     kandidatRemoveEinzelstimmenOrThrow,

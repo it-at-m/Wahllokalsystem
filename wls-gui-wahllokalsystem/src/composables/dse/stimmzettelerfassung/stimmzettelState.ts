6f2b44459e4ba612d9977fc142b4689d7c2d936b
Thomas Großbeck <thomas.grossbeck@muenchen.de>
2026-09-11 12:22:21 +0200

    3162 ergebniszusammenfassung der stimmzettelerfassung je team (#3454)

---
+++ wls-gui-wahllokalsystem/src/composables/dse/stimmzettelerfassung/stimmzettelState.ts
@@ -0,0 +1,57 @@
+import type { Stimmzettel } from "@/types/dse/persistedStimmzettel/Stimmzettel.ts";
+import type { Ref } from "vue";
+
+import { computed, onActivated, readonly, ref } from "vue";
+
+import { useStimmzettelService } from "@/composables/dse/stimmzettelerfassung/stimmzettelService.ts";
+
+const { getStimmzettel, saveStimmzettel } = useStimmzettelService();
+
+export function useStimmzettelState(
+  wahlID: string,
+  wahlbezirkID: string,
+  teamID: string
+) {
+  const isStimmzettelLoading = ref(false);
+  const savedStimmzettel: Ref<Stimmzettel[]> = ref([]);
+
+  const hasStimmzettel = computed(() => savedStimmzettel.value.length > 0);
+
+  onActivated(async () => {
+    await _loadStimmzettel();
+  });
+
+  async function saveNewStimmzettel(stimmzettel: Stimmzettel) {
+    const newStimmzettelCollectionToSave = [
+      ...savedStimmzettel.value,
+      stimmzettel,
+    ];
+    await saveStimmzettel(
+      wahlID,
+      wahlbezirkID,
+      teamID,
+      newStimmzettelCollectionToSave
+    );
+    savedStimmzettel.value = newStimmzettelCollectionToSave;
+  }
+
+  async function _loadStimmzettel() {
+    isStimmzettelLoading.value = true;
+    try {
+      savedStimmzettel.value = await getStimmzettel(
+        wahlID,
+        wahlbezirkID,
+        teamID
+      );
+    } finally {
+      isStimmzettelLoading.value = false;
+    }
+  }
+
+  return {
+    isStimmzettelLoading: readonly(isStimmzettelLoading),
+    savedStimmzettel: computed(() => savedStimmzettel.value),
+    hasStimmzettel,
+    saveNewStimmzettel,
+  };
+}

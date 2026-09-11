8855d771d5a18099a19eaf5fce2d2e33311fc3a0
MrSebastian <MrSebastian@users.noreply.github.com>
2026-09-11 11:05:25 +0000

    deploy: 54e9b8a13d24ff59892526551c0b575b3d7d4bdc

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.C1w66ERk.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-UKHOOZJN.BLj9lcR-.js";import{_ as a}from"./theme.BoDkf5VC.js";import"./chunk-5VM5RSS4.ChPjUwjW.js";import"./chunk-XXDRQBXY.BAfWThqh.js";import"./chunk-KBJHAD2P.BChtcWLe.js";import"./chunk-2GRJ4B5K.C6395OfC.js";import"./framework.HWAOx3Ef.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};

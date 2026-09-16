0af5adf282b23194370857117ded366bd1efb15c
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-16 09:44:48 +0200

    #3455 use correct testdata

--- wls-gui-wahllokalsystem/tests/composables/dse/beschlussfassung/beschlussgrundTools.spec.ts
+++ wls-gui-wahllokalsystem/tests/composables/dse/beschlussfassung/beschlussgrundTools.spec.ts
@@ -5,8 +5,10 @@ import { useBeschlussgrundTools } from "@/composables/dse/beschlussfassung/besch
 import { SystemBeschlussgrundReasonEnum } from "@/types/dse/beschlussfassung/SystemBeschlussgrundReasonEnum.ts";
 import { WahlvorstandBeschlussvorschlaegeEnum } from "@/types/dse/beschlussfassung/WahlvorstandBeschlussvorschlaegeEnum.ts";
 
-const { createStimmzettelSystemBeschlussgrund } =
-  useStimmzettelTestDataFactory();
+const {
+  createStimmzettelSystemBeschlussgrund,
+  createStimmzettelWahlvorstandBeschlussgrund,
+} = useStimmzettelTestDataFactory();
 
 describe("useBeschlussgrundTools.ts", () => {
   let unitUnderTest: ReturnType<typeof useBeschlussgrundTools>;
@@ -17,15 +19,15 @@ describe("useBeschlussgrundTools.ts", () => {
 
   describe("sortWahlvorstandBeschlussgruende", () => {
     const wvGrund1 = {
-      ...createStimmzettelSystemBeschlussgrund(),
+      ...createStimmzettelWahlvorstandBeschlussgrund(),
       text: "kaffee ausgeschüttet",
     };
     const wvGrund2 = {
-      ...createStimmzettelSystemBeschlussgrund(),
+      ...createStimmzettelWahlvorstandBeschlussgrund(),
       text: WahlvorstandBeschlussvorschlaegeEnum.NichtAmtlicherStimmzettel,
     };
     const wvGrund3 = {
-      ...createStimmzettelSystemBeschlussgrund(),
+      ...createStimmzettelWahlvorstandBeschlussgrund(),
       text: WahlvorstandBeschlussvorschlaegeEnum.WaehlerwilleNichtZweifelsfreiErkennbar,
     };
 
@@ -39,7 +41,7 @@ describe("useBeschlussgrundTools.ts", () => {
         wvBeschlussgruende: [wvGrund1, wvGrund2, wvGrund3],
       },
     ])(
-      `should_returnSortedSystemBeschlussgruende_when_givenListOfSystemBeschlussgruendeThatIs'$text'`,
+      `should_returnSortedWahlvorstandBeschlussgruende_when_givenListOfWahlvorstandBeschlussgruendeThatIs'$text'`,
       ({ wvBeschlussgruende }) => {
         const expectedResult = [wvGrund1, wvGrund2, wvGrund3];
 

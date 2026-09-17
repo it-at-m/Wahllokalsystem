ca3eeba4bf7f289e493ba5b928c6b2f8a2f39c4f
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-17 16:01:05 +0200

    remove TODOs

--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/utils/InstancioModels.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/utils/InstancioModels.java
@@ -50,7 +50,6 @@ public class InstancioModels {
         .toModel();
   }
 
-  //TODO stimmzettelkennung als supplier
   public static Model<Stimmzettel> createEmptyValidStimmzettelModel(
       String wahlID, String wahlbezirkID, String teamID, int stimmzettelkennung) {
     return Instancio.ofBlank(Stimmzettel.class)
@@ -62,7 +61,6 @@ public class InstancioModels {
         .toModel();
   }
 
-  //TODO stimmzettelkennung als supplier
   public static Model<Stimmzettel> createEmptyInvalidStimmzettelModel(
       String wahlID, String wahlbezirkID, String teamID, int stimmzettelkennung) {
     return Instancio.ofBlank(Stimmzettel.class)

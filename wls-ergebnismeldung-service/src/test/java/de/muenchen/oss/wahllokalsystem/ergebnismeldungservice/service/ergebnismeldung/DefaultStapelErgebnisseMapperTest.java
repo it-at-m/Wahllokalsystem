cf5d226ab77b814013f7e4c36201c5b4123f8040
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-18 07:12:30 +0200

    fix test

--- wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnismeldung/DefaultStapelErgebnisseMapperTest.java
+++ wls-ergebnismeldung-service/src/test/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnismeldung/DefaultStapelErgebnisseMapperTest.java
@@ -125,7 +125,7 @@ class DefaultStapelErgebnisseMapperTest {
     void should_returnTrue_when_anyWahlartIsGiven(final WahlartModel wahlart) {
       val result = unitUnderTest.canHandleWahlart(wahlart);
 
-      Assertions.assertThat(result).isEqualTo(WahlartModel.MBW.equals(wahlart));
+      Assertions.assertThat(result).isEqualTo(!WahlartModel.MBW.equals(wahlart));
       Mockito.verifyNoInteractions(ergebnisseService, wahlartPredicateHolder);
     }
   }

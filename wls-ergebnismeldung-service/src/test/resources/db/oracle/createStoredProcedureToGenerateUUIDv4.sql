72ee7de5208c0217bc5237802a6f0b62c268976c
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-20 14:01:39 +0200

    wip: duplicate stimmzettel

---
+++ wls-ergebnismeldung-service/src/test/resources/db/oracle/createStoredProcedureToGenerateUUIDv4.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION uuid_v4 RETURN VARCHAR2 IS
+    g RAW(16) := SYS_GUID();
+    v VARCHAR2(36);
+BEGIN
+    SELECT LOWER(
+                   SUBSTR(hex, 1, 8)  || '-' || SUBSTR(hex, 9, 4)   || '-' ||
+                   SUBSTR(hex, 13, 4) || '-' || SUBSTR(hex, 17, 4)  || '-' ||
+                   SUBSTR(hex, 21, 12)
+           ) AS display_uuid
+    INTO v
+    FROM (SELECT RAWTOHEX(SYS_GUID()) AS hex FROM DUAL);
+
+    RETURN v;
+END;
+/

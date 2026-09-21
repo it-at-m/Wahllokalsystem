c30c9dcbc0d3d87144cce4e0595182b807aea115
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-21 15:31:38 +0200

    add h2 indizes

---
+++ wls-ergebnismeldung-service/src/main/resources/db/migrations/h2/V19_0__addStimmzettelIndizes.sql
@@ -0,0 +1,36 @@
+CREATE INDEX idx_stimmzettel_wahl_wahlbezirk_gueltigkeit_invalidevotes
+    ON stimmzettel (wahlid, wahlbezirkid, gueltigkeit, invalidevotes);
+
+CREATE INDEX idx_stimmzettel_wahl_wahlbezirk_gueltigkeit
+    ON stimmzettel (wahlid, wahlbezirkid, gueltigkeit);
+
+CREATE INDEX idx_wahlvorschlag_stimmzettel_selected
+    ON wahlvorschlag (
+                      stimmzettel_wahlid,
+                      stimmzettel_wahlbezirkid,
+                      stimmzettel_teamid,
+                      stimmzettel_stimmzettelkennung,
+                      selected
+        );
+
+--- Foreign key: wahlvorschlag -> stimmzettel
+CREATE INDEX idx_wahlvorschlag_stimmzettel
+    ON wahlvorschlag (
+                      stimmzettel_wahlid,
+                      stimmzettel_wahlbezirkid,
+                      stimmzettel_teamid,
+                      stimmzettel_stimmzettelkennung
+        );
+
+--- Foreign key: kandidat -> wahlvorschlag
+CREATE INDEX idx_kandidat_wahlvorschlag
+    ON kandidat (
+                 wahlvorschlag_id
+        );
+
+CREATE INDEX idx_kandidat_discarded_votesbyvoter_invalidevotes
+    ON kandidat (
+                 discarded,
+                 votesByVoter,
+                 invalidvotes
+        );

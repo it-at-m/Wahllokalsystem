c80ba87cb1a8e15d5e0edaf00c061921020286be
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-23 16:51:18 +0200

    #3270 undo not needed change

--- wls-gui-wahllokalsystem/src/types/dse/beschlussfassung/WahlvorstandBeschlussgrund.ts
+++ wls-gui-wahllokalsystem/src/types/dse/beschlussfassung/WahlvorstandBeschlussgrund.ts
@@ -1,5 +1,3 @@
-import type { WahlvorstandBeschlussvorschlaegeEnum } from "@/types/dse/beschlussfassung/WahlvorstandBeschlussvorschlaegeEnum.ts";
-
 export interface WahlvorstandBeschlussgrund {
-  text: string | WahlvorstandBeschlussvorschlaegeEnum;
+  text: string;
 }

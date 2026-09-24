3d70fbeafa310b7f1393bc1e545aa48a8828afed
ThoGro <thomas.grossbeck@muenchen.de>
2026-09-24 10:09:26 +0200

    Update Dokumentart Beschlussentscheidungen and add test

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/common/DokumentartDTO.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/rest/common/DokumentartDTO.java
@@ -1,8 +1,7 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.rest.common;
 
-/** API-Dokumentart nur fuer Ausdruck-Endpunkte: V1, V3, V4. */
 public enum DokumentartDTO {
   V1,
   V3,
-  V4
+  BESCHLUSSENTSCHEIDUNGEN
 }
--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ausdruck/DokumentartModel.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ausdruck/DokumentartModel.java
@@ -1,8 +1,7 @@
 package de.muenchen.oss.wahllokalsystem.ergebnismeldungservice.service.ausdruck;
 
-/** Service-Layer Abbild der Dokumentart; erweitert V1/V3 um V4 fuer Ausdrucke. */
 public enum DokumentartModel {
   V1,
   V3,
-  V4
+  BESCHLUSSENTSCHEIDUNGEN
 }

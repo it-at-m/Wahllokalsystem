02a63f95b603a42867c3c326c7882003aae62bf4
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-24 17:06:57 +0200

    3258 - Fix exception während Validierung des aktuellen Zustands im Rahmen der DSE (#3529)
    * reverse validation order to early abort by exception
    
    * convert exception into false

--- wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnismeldung/validation/electionType/MbwValidationImpl.java
+++ wls-ergebnismeldung-service/src/main/java/de/muenchen/oss/wahllokalsystem/ergebnismeldungservice/service/ergebnismeldung/validation/electionType/MbwValidationImpl.java
@@ -14,11 +14,13 @@ import de.muenchen.oss.wahllokalsystem.wls.common.security.domain.BezirkUndWahlI
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MbwValidationImpl implements ElectionTypeValidation {
 
   private final DefaultElectionTypeValidator validator;
@@ -38,8 +40,13 @@ public class MbwValidationImpl implements ElectionTypeValidation {
       final Long waehlerverzeichnisNummer,
       final MeldungsartModel meldungsart)
       throws WlsException {
-    val necessaryStacks = buildNecessaryStack();
+    val bezirkUndWahlId = new BezirkUndWahlID(wahlID, wahlbezirkID);
 
+    if (isValidDSE(meldungsart, bezirkUndWahlId)) {
+      return true;
+    }
+
+    val necessaryStacks = buildNecessaryStack();
     boolean stacksValid =
         validator.checkValidation(
             WahlbezirkArtModel.UWB,
@@ -47,17 +54,9 @@ public class MbwValidationImpl implements ElectionTypeValidation {
             wahlID,
             waehlerverzeichnisNummer,
             necessaryStacks);
-    val bezirkUndWahlId = new BezirkUndWahlID(wahlID, wahlbezirkID);
-
     boolean hasBedenklich = hasBedenklicheStimmzettel(bezirkUndWahlId);
-
     // Stapelerfassung valid
-    if (stacksValid && hasBedenklich) {
-      return true;
-    }
-
-    // invalid in general or valid for DSE
-    return isValidDSE(meldungsart, bezirkUndWahlId);
+    return stacksValid && hasBedenklich;
   }
 
   @Override
@@ -67,8 +66,15 @@ public class MbwValidationImpl implements ElectionTypeValidation {
       final Long waehlerverzeichnisNummer,
       final MeldungsartModel meldungsart)
       throws WlsException {
-    val necessaryStacks = buildNecessaryStack();
 
+    val bezirkUndWahlId = new BezirkUndWahlID(wahlID, wahlbezirkID);
+
+    // invalid in general or valid for DSE
+    if (isValidDSE(meldungsart, bezirkUndWahlId)) {
+      return true;
+    }
+
+    val necessaryStacks = buildNecessaryStack();
     boolean stacksValid =
         validator.checkValidation(
             WahlbezirkArtModel.BWB,
@@ -76,16 +82,10 @@ public class MbwValidationImpl implements ElectionTypeValidation {
             wahlID,
             waehlerverzeichnisNummer,
             necessaryStacks);
-    val bezirkUndWahlId = new BezirkUndWahlID(wahlID, wahlbezirkID);
     boolean hasBedenklich = hasBedenklicheStimmzettel(bezirkUndWahlId);
 
     // Stapelerfassung valid
-    if (stacksValid && hasBedenklich) {
-      return true;
-    }
-
-    // invalid in general or valid for DSE
-    return isValidDSE(meldungsart, bezirkUndWahlId);
+    return stacksValid && hasBedenklich;
   }
 
   private List<Stapelart> buildNecessaryStack() {
@@ -102,14 +102,19 @@ public class MbwValidationImpl implements ElectionTypeValidation {
 
   private boolean isValidDSE(
       final MeldungsartModel meldungsart, final BezirkUndWahlID bezirkUndWahlID) {
-    val status = stimmzettelerfassungService.getStimmzettelerfassungStatus(bezirkUndWahlID);
-    // Schnellmeldung
-    if (MeldungsartModel.V3.equals(meldungsart)) {
-      return status.map(ErfassungStatusModel::isStimmzettelerfassungAbgeschlossen).orElse(false);
-    } else if (MeldungsartModel.V1.equals(meldungsart)) {
-      // Niederschrift
-      return status.map(ErfassungStatusModel::isBeschlussfassungAbgeschlossen).orElse(false);
+    try {
+      val status = stimmzettelerfassungService.getStimmzettelerfassungStatus(bezirkUndWahlID);
+      // Schnellmeldung
+      if (MeldungsartModel.V3.equals(meldungsart)) {
+        return status.map(ErfassungStatusModel::isStimmzettelerfassungAbgeschlossen).orElse(false);
+      } else if (MeldungsartModel.V1.equals(meldungsart)) {
+        // Niederschrift
+        return status.map(ErfassungStatusModel::isBeschlussfassungAbgeschlossen).orElse(false);
+      }
+      return false;
+    } catch (final RuntimeException ex) {
+      log.warn("exception occurred during check if dse ist valid", ex);
+      return false;
     }
-    return false;
   }
 }

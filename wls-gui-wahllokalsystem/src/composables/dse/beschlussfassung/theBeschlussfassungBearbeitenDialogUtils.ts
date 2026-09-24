19533f933e446813caf46aad39698ee0aedb22a4
vjohnslhm <145964798+vjohnslhm@users.noreply.github.com>
2026-09-24 16:40:55 +0200

    #3271 more two way binding for beschluss infos

--- wls-gui-wahllokalsystem/src/composables/dse/beschlussfassung/theBeschlussfassungBearbeitenDialogUtils.ts
+++ wls-gui-wahllokalsystem/src/composables/dse/beschlussfassung/theBeschlussfassungBearbeitenDialogUtils.ts
@@ -1,11 +1,15 @@
 import type { BeschlussAbstimmungsergebnis } from "@/types/dse/beschlussfassung/BeschlussAbstimmungsergebnis.ts";
+import type { BeschlussfassungDialogDetails } from "@/types/dse/beschlussfassung/BeschlussfassungDialogDetails.ts";
 import type { PersistedStimmzettel } from "@/types/dse/stimmzettelerfassung/PersistedStimmzettel.ts";
 import type { Ref } from "vue";
 
 import { storeToRefs } from "pinia";
 import { ref, watch, watchEffect } from "vue";
 
+import { useBeschlussgrundTools } from "@/composables/dse/beschlussfassung/beschlussgrundTools.ts";
+import { useTheBeschlussFassenTabUtils } from "@/composables/dse/beschlussfassung/theBeschlussFassenTabUtils.ts";
 import { useWahlvorstandStore } from "@/stores/wahlvorstandStore.ts";
+import { StimmzettelGueltigkeitEnum } from "@/types/dse/stimmzettelerfassung/StimmzettelGueltigkeitEnum.ts";
 
 export function useTheBeschlussfassungBearbeitenDialogUtils(
   stimmzettel: Ref<PersistedStimmzettel | undefined>
@@ -13,6 +17,11 @@ export function useTheBeschlussfassungBearbeitenDialogUtils(
   const { anwesendeWahlvorstandsmitgliederAnzahl } = storeToRefs(
     useWahlvorstandStore()
   );
+  const {
+    createAndSetSelectedBeschlussgrundOptionsBasedOnStimmzettelAndGueltigkeit,
+    isStimmzettelGueltigBasedOnVormerkungsgruenden,
+  } = useTheBeschlussFassenTabUtils();
+  const { getBeschlussgrundEnumValueAsString } = useBeschlussgrundTools();
 
   const abstimmungsergebnis = ref<BeschlussAbstimmungsergebnis>({
     stimmenDafuer: null,
@@ -22,10 +31,20 @@ export function useTheBeschlussfassungBearbeitenDialogUtils(
     abstimmungIsUngueltig: false,
   });
 
+  const beschlussDetails = ref<BeschlussfassungDialogDetails>({
+    isGueltig: null,
+    beschlussgruende: [],
+    andererGrund: "",
+    andererGrundChecked: false,
+    beschlussText: "",
+  });
+
   watch(
     stimmzettel,
     () => {
-      const beschlussfassung = stimmzettel.value?.beschlussfassung;
+      if (!stimmzettel.value) return;
+
+      const beschlussfassung = stimmzettel.value.beschlussfassung;
       if (beschlussfassung) {
         const unentschieden = beschlussfassung.pro === beschlussfassung.contra;
 
@@ -36,6 +55,14 @@ export function useTheBeschlussfassungBearbeitenDialogUtils(
           abstimmungIsUnentschieden: unentschieden,
           abstimmungIsUngueltig: false,
         };
+        beschlussDetails.value = {
+          isGueltig:
+            stimmzettel.value.gueltigkeit == StimmzettelGueltigkeitEnum.Valid,
+          beschlussgruende: [],
+          andererGrund: "",
+          andererGrundChecked: false,
+          beschlussText: "",
+        };
       } else {
         abstimmungsergebnis.value = {
           stimmenDafuer: null,
@@ -44,9 +71,24 @@ export function useTheBeschlussfassungBearbeitenDialogUtils(
           abstimmungIsUnentschieden: false,
           abstimmungIsUngueltig: false,
         };
+        beschlussDetails.value = {
+          isGueltig: isStimmzettelGueltigBasedOnVormerkungsgruenden(
+            stimmzettel.value
+          ),
+          beschlussgruende: [],
+          andererGrund: "",
+          andererGrundChecked: false,
+          beschlussText: "",
+        };
+        _rebuildBeschlussgruende();
       }
     },
     { immediate: true }
+  );
+
+  watch(
+    () => beschlussDetails.value.isGueltig,
+    () => _rebuildBeschlussgruende()
   );
 
   watchEffect(() => {
@@ -54,20 +96,46 @@ export function useTheBeschlussfassungBearbeitenDialogUtils(
     const dagegen = abstimmungsergebnis.value.stimmenDagegen;
 
     const total = (dafuer ?? 0) + (dagegen ?? 0);
+    const stimmenNotNull = dafuer != null && dagegen != null;
 
     const ungueltig =
-      dafuer == null || dagegen == null
-        ? false
-        : total < 3 ||
-          total > anwesendeWahlvorstandsmitgliederAnzahl.value ||
-          dafuer < dagegen;
+      stimmenNotNull &&
+      (total < 3 ||
+        total > anwesendeWahlvorstandsmitgliederAnzahl.value ||
+        dafuer < dagegen);
 
-    const unentschieden =
-      dafuer != null && dagegen != null && !ungueltig && dafuer === dagegen;
+    const unentschieden = stimmenNotNull && !ungueltig && dafuer === dagegen;
 
     abstimmungsergebnis.value.abstimmungIsUngueltig = ungueltig;
     abstimmungsergebnis.value.abstimmungIsUnentschieden = unentschieden;
+
+    beschlussDetails.value.andererGrundChecked =
+      !!beschlussDetails.value.andererGrund;
+
+    beschlussDetails.value.beschlussText = _mergeAndReturnBeschlussText();
   });
 
-  return { abstimmungsergebnis };
+  function _rebuildBeschlussgruende() {
+    const gruende =
+      createAndSetSelectedBeschlussgrundOptionsBasedOnStimmzettelAndGueltigkeit(
+        beschlussDetails.value.isGueltig,
+        stimmzettel.value
+      );
+    beschlussDetails.value.andererGrund = gruende.andererGrund;
+    beschlussDetails.value.beschlussgruende = gruende.beschlussgruende;
+  }
+
+  function _mergeAndReturnBeschlussText() {
+    const selectedGruende = beschlussDetails.value.beschlussgruende
+      .filter((option) => option.selected)
+      .map((option) => getBeschlussgrundEnumValueAsString(option.grund))
+      .join(", ");
+    const andereGruende = beschlussDetails.value.andererGrundChecked
+      ? beschlussDetails.value.andererGrund
+      : "";
+
+    return [selectedGruende, andereGruende].filter(Boolean).join(", ");
+  }
+
+  return { abstimmungsergebnis, beschlussDetails };
 }

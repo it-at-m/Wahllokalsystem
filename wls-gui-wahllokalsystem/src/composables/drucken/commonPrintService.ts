70cf68cbe7da00d1db9900d008066cb40aa84775
MrSebastian <13592751+MrSebastian@users.noreply.github.com>
2026-09-24 12:30:09 +0200

    create service with common print functions

---
+++ wls-gui-wahllokalsystem/src/composables/drucken/commonPrintService.ts
@@ -0,0 +1,88 @@
+import type { MeldungsartEnum } from "@/types/ergebnismeldung/common/MeldungsartEnum.ts";
+import type { Status } from "@/types/ergebnismeldung/common/Status.ts";
+import type { Wahl } from "@/types/wahl/Wahl.ts";
+
+import JsBarcode from "jsbarcode";
+
+import { useDateTimeFormatter } from "@/composables/common/dateTimeFormatter.ts";
+import { useUserNotificationService } from "@/composables/userNotification/userNotificationService.ts";
+import { MeldungsArtEnum } from "@/types/ergebnismeldung/common/MeldungsartEnum.ts";
+import { UserNotificationCategoryEnum } from "@/types/userNotification/UserNotificationCategoryEnum.ts";
+import { WahlbezirksArtEnum } from "@/types/wahlbezirksArtEnum.ts";
+
+export function useCommonPrintService() {
+  const { toGermanDate, toHhMm } = useDateTimeFormatter();
+  const { addNotification } = useUserNotificationService();
+
+  function createBarcode(
+    wahl: Wahl,
+    meldungsart: MeldungsartEnum,
+    wahlbezirkArt: WahlbezirksArtEnum,
+    wahlbezirkNummer: string
+  ) {
+    const canvas = document.createElement("canvas");
+    const barcodeContent = _createBarcodeString(
+      wahl,
+      meldungsart,
+      wahlbezirkArt,
+      wahlbezirkNummer
+    );
+    JsBarcode(canvas, barcodeContent, { displayValue: false });
+    return canvas.toDataURL("image/jpeg");
+  }
+
+  function createFooter(
+    status: Status | undefined,
+    meldungsArt: MeldungsartEnum
+  ) {
+    if (meldungsArt == MeldungsArtEnum.Schnellmeldung) {
+      if (
+        status &&
+        status.schnellmeldung &&
+        status.schnellmeldung.validierungsstatus
+      ) {
+        const date = new Date();
+        const formattedDateWithTime = toGermanDate(date) + " " + toHhMm(date);
+
+        if (status.schnellmeldung.validierungsstatus === "VALIDE") {
+          return crypto.randomUUID() + ", " + formattedDateWithTime + " O";
+        } else {
+          return crypto.randomUUID() + ", " + formattedDateWithTime + " M";
+        }
+      }
+    } else {
+      // to be implemented - #1978
+      return "";
+    }
+  }
+
+  function _createBarcodeString(
+    wahl: Wahl,
+    meldungsart: MeldungsartEnum,
+    wahlbezirkArt: WahlbezirksArtEnum,
+    wahlbezirkNummer: string
+  ) {
+    const wahlbezirkKurzbezeichnung =
+      wahlbezirkArt == WahlbezirksArtEnum.UWB
+        ? "SBZ" // Stimmbezirk (Urnenwahl)
+        : "BWBZ"; // Briefwahlbezirk (Briefwahl)
+    const meldungsartKurzbezeichnung =
+      meldungsart == MeldungsArtEnum.Schnellmeldung ? "S" : "N";
+    const wahlbezirkNummerAsInt = parseInt(wahlbezirkNummer, 10);
+    const wahlDatum = toGermanDate(wahl.wahltag);
+    if (wahl.kennzeichen && wahlbezirkNummerAsInt && wahlDatum) {
+      return `${wahl.kennzeichen}${wahlDatum}-${meldungsartKurzbezeichnung}-${wahlbezirkKurzbezeichnung}-${wahlbezirkNummer}`;
+    } else {
+      addNotification(
+        "Fehler beim Erstellen des Barcodes",
+        UserNotificationCategoryEnum.WARNING
+      );
+      return "";
+    }
+  }
+
+  return {
+    createBarcode,
+    createFooter,
+  };
+}
